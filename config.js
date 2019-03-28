cfc1a538c92e6c3671e9c97481fad5a51031fe02
junguo <zisuting@hotmail.com>
2019-03-28 16:32:45 +0800

    first submit

---
+++ config.js
@@ -0,0 +1,12 @@
+key = 'f3d985bc-887d-45bb-8db4-ceff2sedd87fe';
+secret = '1319B1CBE5ddddwwsEF56DAA7';
+passphrase = 'xewwdd';
+urlHost =  'https://www.okex.com';
+websocekHost='wss://real.okex.com:10442/ws/v3';
+
+
+module.exports.key = key;
+module.exports.secret = secret;
+module.exports.passphrase = passphrase;
+module.exports.urlHost = urlHost;
+module.exports.websocekHost = websocekHost;
