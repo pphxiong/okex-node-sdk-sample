312a5e53f49db19ee696a316e57f6acaab5647b3
dehua.xiong <dehua.xiong@percent.cn>
2020-08-20 00:37:45 +0800

    '添加client'

--- server/app/index.js
+++ server/app/index.js
@@ -46,8 +46,7 @@ app.get('/account/getWallet', function(req, response) {
 app.get('/futures/getOrders', function(req, response) {
     const { query = {} } = req;
     const { instrument_id } = query; // "BTC-USD-200828"
-    console.log(req,instrument_id)
-    authClient.futures().getOrders("BTC-USD-200828", { state: 2, limit: 20 }).then(res => {
+    authClient.futures().getOrders(instrument_id, { state: 2, limit: 20 }).then(res => {
         send(response, { errcode: 0, errmsg: 'ok', data: res })
     });
 });
