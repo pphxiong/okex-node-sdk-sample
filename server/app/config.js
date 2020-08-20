a99bc61f5fefe95891697b51f27f3bd929935569
dehua.xiong <dehua.xiong@percent.cn>
2020-08-20 19:49:35 +0800

    'babel'

--- server/app/config.js
+++ server/app/config.js
@@ -1,8 +1,8 @@
-key = 'b060841b-f9f0-4b23-a3e5-0520c5587087';
-secret = '0BD4704A951F18384D520B27882B5872';
-passphrase = 'Xiong092479';
-urlHost =  'https://www.okex.com';
-websocekHost='wss://real.okex.com:8443/ws/v3';
+const key = 'b060841b-f9f0-4b23-a3e5-0520c5587087';
+const secret = '0BD4704A951F18384D520B27882B5872';
+const passphrase = 'Xiong092479';
+const urlHost =  'https://www.okex.com';
+const websocekHost='wss://real.okex.com:8443/ws/v3';
 
 
 module.exports.httpkey = key;
