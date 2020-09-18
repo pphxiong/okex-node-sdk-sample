e1b5b51c948f3933858d7298db31d8dcef59032d
dehua.xiong <dehua.xiong@percent.cn>
2020-09-18 10:19:12 +0800

    'bug'

--- server/app/index.js
+++ server/app/index.js
@@ -630,7 +630,7 @@ const autoOperateByHoldingTime = async (holding,ratio,condition) => {
     const continuousObj = continuousMap[instrument_id];
     const lastObj = lastOrderMap[instrument_id];
     const winOrderObj = winOrderMap[instrument_id];
-    console.log(instrument_id, continuousObj)
+    console.log(instrument_id, continuousObj.continuousWinNum, continuousObj.continuousLossNum, continuousObj.continuousBatchNum, continuousObj.continuousProfitNum)
     // 盈利，半仓，止盈
     if(ratio > condition * 1 * frequency && !continuousMap.continuousProfitNum) {
         if(winOrderObj.order_id){
