163d44ce14ef654829ef0c8ebe99007c67f662c8
dehua.xiong <dehua.xiong@percent.cn>
2020-09-27 13:45:51 +0800

    'bug'

--- server/app/swapSimulationApi.js
+++ server/app/swapSimulationApi.js
@@ -193,6 +193,7 @@ app.get('/swap/getLeverage', function(req, response) {
 
 app.get('/swap/postOrder', function(req, response) {
     const {query = {}} = req;
+    query['order_type'] = 4;
     cAuthClient
         .swap
         .postOrder(query)
