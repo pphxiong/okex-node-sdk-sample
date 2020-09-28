2651cfc52180f8c7fab6d371c0ac3fc499af9525
dehua.xiong <dehua.xiong@percent.cn>
2020-09-28 11:07:45 +0800

    'bug'

--- server/app/customSimulationAuthClient.js
+++ server/app/customSimulationAuthClient.js
@@ -38,11 +38,10 @@ function customAuthClient(key, secret, passphrase, apiUri = 'https://www.okex.co
         const bodyJson = JSON.stringify(body);
         const signObj = getSignature('post', url, { body: bodyJson });
         signObj['content-type'] = 'application/json; charset=utf-8';
-        console.log('signObj',signObj)
         return request(apiUri + url,{
             method: 'post',
             headers: signObj,
-            data: body
+            data: bodyJson
         })
     }
 
