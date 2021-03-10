441348b33348deeaa8345039a56c3844455687c1
pphxiong <1017551718@qq.com>
2021-03-10 13:56:37 +0800

    'bug'

--- server/app/swapApi.js
+++ server/app/swapApi.js
@@ -19,8 +19,8 @@ let mode = 4; //下单模式
 let frequency = 1;
 const winRatio = 2;
 const lossRatio = 9;
-let initPosition = 4;
-let LEVERAGE = 10
+let initPosition = 5;
+let LEVERAGE = 20
 
 const continuousMap = {
     [BTC_INSTRUMENT_ID]: {
