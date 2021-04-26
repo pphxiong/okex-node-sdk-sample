714e21a4ae1beb3e4c350f67d051c66362ab6195
pphxiong <1017551718@qq.com>
2021-04-26 14:35:42 +0800

    'bug'

--- server/app/swapSimulationApi.js
+++ server/app/swapSimulationApi.js
@@ -899,7 +899,7 @@ const startInterval = async () => {
     const { mark_price } = await cAuthClient.swap.getMarkPrice(XRP_INSTRUMENT_ID);
 
     const payload = {
-        granularity: 60 * 3, // 单位为秒
+        granularity: 60 * 5, // 单位为秒
         limit: 100,
         // start,
         // end
@@ -1076,7 +1076,7 @@ const startInterval = async () => {
          */
     }
 
-    await waitTime(1000 * 4 * 1)
+    await waitTime(1000 * 5 * 1)
     await startInterval()
 
     // let btcHolding = globalBtcHolding
