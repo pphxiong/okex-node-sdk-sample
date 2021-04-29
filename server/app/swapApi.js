8746135168e984f11c83bc26c35df8743160fd7c
pphxiong <1017551718@qq.com>
2021-04-29 14:21:42 +0800

    'bug'

--- server/app/swapApi.js
+++ server/app/swapApi.js
@@ -947,8 +947,8 @@ let openMarketPrice = 0
 let globalColumnsObjList;
 const startInterval = async () => {
     const payload = {
-        granularity: 60 * 1, // 单位为秒
-        limit: 100,
+        granularity: 60 * 3, // 单位为秒
+        // limit: 300,
         // start,
         // end
     }
