90c8edb96cc40949e230bf2f75ca607dc25f0fa7
pphxiong <1017551718@qq.com>
2021-05-05 22:00:51 +0800

    'bug'

--- server/app/swapApi.js
+++ server/app/swapApi.js
@@ -18,7 +18,7 @@ let frequency = 1;
 const winRatio = 2;
 const lossRatio = 9;
 let LEVERAGE = 10
-let initPosition = LEVERAGE * 1 * 2;
+let initPosition = LEVERAGE * 1.2;
 // let initPosition = LEVERAGE * 10 / 2;
 
 const continuousMap = {
