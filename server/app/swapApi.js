bc34e8c64f06c7a8804a9d91cc612066d451d72b
pphxiong <1017551718@qq.com>
2021-06-03 15:08:57 +0800

    'bug'

--- server/app/swapApi.js
+++ server/app/swapApi.js
@@ -1166,8 +1166,8 @@ const startInterval = async () => {
 
         let columnsObjList = []
 
-        // const allList = globalColumnsObjList.concat([Number(mark_price)])
-        const allList = globalColumnsObjList
+        const allList = globalColumnsObjList.concat([Number(mark_price)])
+        // const allList = globalColumnsObjList
         // allList.map((item,index)=>{
         //         const result = getRSI(item,allList.slice(0,index+1))
         //     columnsObjList.push(result)
