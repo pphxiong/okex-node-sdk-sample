7aef2b75022af7203ab62f957ef3385b4567f303
pphxiong <1017551718@qq.com>
2021-06-03 10:08:54 +0800

    'bug'

--- server/app/swapApi.js
+++ server/app/swapApi.js
@@ -1188,7 +1188,7 @@ const startInterval = async () => {
         // }
 
         allList.pop()
-        const result = getRSI(allList[allList.length-1],allList.slice(-15))
+        const result = getRSI(allList[allList.length-1],allList.slice(-50))
         columnsObjList.push(result)
 
         columnsObjList = columnsObjList.reverse()
