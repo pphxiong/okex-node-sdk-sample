4e65fc481380b5e5df52c1da6dc61bc403adca9e
pphxiong <1017551718@qq.com>
2021-06-08 10:05:33 +0800

    '重启'

--- server/check.js
+++ server/check.js
@@ -6,25 +6,27 @@ function check() {
     last = exec('lsof -i:8091');
     last.on('exit', function (code) {
         console.log(code)
-        if (code != "0") {
+        if (code == "0") {
             console.log('restarting');
-            console.log('主服务已经关闭，正在重启');
+            // console.log('主服务已经关闭，正在重启');
             run();
         }else{
             console.log('running')
-            console.log('主服务正在运行中...');
+            // console.log('主服务正在运行中...');
         }
     })
     setTimeout(check,5000)
 }
 
 function run() {
-    last = exec('npm run restart');
+    last = exec('npm run start');
     last.on('exit',function(code){
         if (code == "0") {
-            console.log('主服务已重启成功');
+            console.log('restarting success')
+            // console.log('主服务已重启成功');
         }else{
-            console.log('主服务重启失败');
+            console.log('restarting failed')
+            // console.log('主服务重启失败');
         }
     })
 }
