d52aa2b7c3b1a42c1b59bb1999ccd32b06a8ec91
pphxiong <1017551718@qq.com>
2021-06-15 14:26:36 +0800

    bug

--- server/app/swapSimulationApi.js
+++ server/app/swapSimulationApi.js
@@ -18,7 +18,7 @@ let frequency = 1;
 const winRatio = 2;
 const lossRatio = 9;
 let LEVERAGE = 10
-let initPosition = 30;
+let initPosition = 28;
 // let initPosition = LEVERAGE * 10 / 2;
 
 const continuousMap = {
