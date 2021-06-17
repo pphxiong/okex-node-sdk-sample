7d9f1bbe57be60939d12a74b18dac23dd861915e
pphxiong <1017551718@qq.com>
2021-06-17 13:41:46 +0800

    'bug'

--- server/app/swapApi.js
+++ server/app/swapApi.js
@@ -283,7 +283,7 @@ const autoOpenOtherOrderSingle = async (params = {}) => {
         const payload = {
             size,
             type,
-            order_type: 0, //1：只做Maker, 2：全部成交或立即取消 4：市价委托
+            order_type: 1, //1：只做Maker, 2：全部成交或立即取消 4：市价委托
             instrument_id,
             price,
             match_price: 0
@@ -306,7 +306,7 @@ const autoOpenOtherOrderSingle = async (params = {}) => {
         }
         const { mark_price } = await cAuthClient.swap.getMarkPrice(ETH_INSTRUMENT_ID);
         await postOrder(nextQty,mark_price)
-    },1000 * 8)
+    },1000 * 6)
 }
 // 平仓
 const closeHalfPosition = async (holding, oldPosition = initPosition) => {
@@ -354,7 +354,7 @@ const closeHalfPosition = async (holding, oldPosition = initPosition) => {
         }
         const { mark_price } = await cAuthClient.swap.getMarkPrice(ETH_INSTRUMENT_ID);
         await postOrder(nextQty,mark_price)
-    },1000 * 6)
+    },1000 * 4)
 }
 
 // 开仓，availRatio开仓比例
