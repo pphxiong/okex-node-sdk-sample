597f599800d1ce7fd785ce653ee01bccba3a1a56
pphxiong <1017551718@qq.com>
2021-06-21 15:58:13 +0800

    bug

--- server/app/swapApi.js
+++ server/app/swapApi.js
@@ -1375,7 +1375,8 @@ const startInterval = async () => {
             // ||
             (
                 deadOverlappingNum >= 1
-                // && latestRSI.RSI1 <= latestRSI.RSI2 - 5
+                ||
+                (latestRSI.RSI1 <= 25 && latestRSI.RSI1 <= latestRSI.RSI2 && latestRSI.RSI2 <= latestRSI.RSI3)
             )
             // ||
             // (latestRSI.RSI1 <= latestRSI.RSI2 && latestRSI.RSI2 <= latestRSI.RSI3)
@@ -1435,7 +1436,8 @@ const startInterval = async () => {
             // ||
             (
                 goldOverlappingNum >= 1
-                // && latestRSI.RSI1 >= latestRSI.RSI2 + 5
+                ||
+                (latestRSI.RSI1 >= 75 && latestRSI.RSI1 >= latestRSI.RSI2 && latestRSI.RSI2 >= latestRSI.RSI3)
             )
             // ||
             // (latestRSI.RSI1 >= latestRSI.RSI2 && latestRSI.RSI2 >= latestRSI.RSI3)
