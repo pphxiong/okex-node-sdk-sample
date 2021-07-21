9f7ac92207e24e2e311c6ee973e1b363522556d8
pphxiong <1017551718@qq.com>
2021-07-21 22:46:23 +0800

    策略

--- server/app/swapSimulationApi.js
+++ server/app/swapSimulationApi.js
@@ -763,24 +763,21 @@ const checkDeal = async data => {
             shortRatio = - shortRatio
         }
 
-        const openLongCondition = Number(macdList[macdList.length-1].column) > Number(macdList[macdList.length-2].column)
-            && rsiList[rsiList.length-1].RSI1 > rsiList[rsiList.length-2].RSI1
-            && rsiList[rsiList.length-1].RSI2 > rsiList[rsiList.length-2].RSI2
-            && rsiList[rsiList.length-1].RSI3 > rsiList[rsiList.length-2].RSI3
-            && rsiList[rsiList.length-1].RSI1 > rsiList[rsiList.length-1].RSI3
-            && rsiList[rsiList.length-1].RSI3 > 50
-
-        const openShortCondition =  Number(macdList[macdList.length-1].column) < Number(macdList[macdList.length-2].column)
+        const openLongCondition = Number(macdList[macdList.length-1].column) < Number(macdList[macdList.length-2].column)
             && rsiList[rsiList.length-1].RSI1 < rsiList[rsiList.length-2].RSI1
             && rsiList[rsiList.length-1].RSI2 < rsiList[rsiList.length-2].RSI2
             && rsiList[rsiList.length-1].RSI3 < rsiList[rsiList.length-2].RSI3
             && rsiList[rsiList.length-1].RSI1 < rsiList[rsiList.length-1].RSI3
-            && rsiList[rsiList.length-1].RSI3 < 50
+            && rsiList[rsiList.length-1].RSI3 < 40
 
-        const closeLongCondition = Number(macdList[macdList.length-1].column) < Number(macdList[macdList.length-2].column)
-            && rsiList[rsiList.length-1].RSI1 < rsiList[rsiList.length-2].RSI1
-            && rsiList[rsiList.length-1].RSI2 < rsiList[rsiList.length-2].RSI2
-            && rsiList[rsiList.length-1].RSI3 < rsiList[rsiList.length-2].RSI3
+        const openShortCondition = Number(macdList[macdList.length-1].column) > Number(macdList[macdList.length-2].column)
+            && rsiList[rsiList.length-1].RSI1 > rsiList[rsiList.length-2].RSI1
+            && rsiList[rsiList.length-1].RSI2 > rsiList[rsiList.length-2].RSI2
+            && rsiList[rsiList.length-1].RSI3 > rsiList[rsiList.length-2].RSI3
+            && rsiList[rsiList.length-1].RSI1 > rsiList[rsiList.length-1].RSI3
+            && rsiList[rsiList.length-1].RSI3 > 60
+
+        const closeLongCondition = openShortCondition
             || isForceDeal
 
             // ||
@@ -791,10 +788,7 @@ const checkDeal = async data => {
             //     && rsiList[rsiList.length-1].RSI3 < 50
             // )
 
-        const closeShortCondition = Number(macdList[macdList.length-1].column) > Number(macdList[macdList.length-2].column)
-            && rsiList[rsiList.length-1].RSI1 > rsiList[rsiList.length-2].RSI1
-            && rsiList[rsiList.length-1].RSI2 > rsiList[rsiList.length-2].RSI2
-            && rsiList[rsiList.length-1].RSI3 > rsiList[rsiList.length-2].RSI3
+        const closeShortCondition = openLongCondition
             || isForceDeal
 
             // ||
