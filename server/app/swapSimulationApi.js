be2ae81c00bc129ac917122e6333171b3163d2f7
pphxiong <1017551718@qq.com>
2021-07-21 17:22:43 +0800

    bug

--- server/app/swapSimulationApi.js
+++ server/app/swapSimulationApi.js
@@ -766,12 +766,12 @@ const checkDeal = async data => {
         const openLongCondition = Number(macdList[macdList.length-1].column) > 0
             && rsiList[rsiList.length-1].RSI1 > rsiList[rsiList.length-1].RSI2
             && rsiList[rsiList.length-1].RSI2 > rsiList[rsiList.length-1].RSI3
-            && rsiList[rsiList.length-1].RSI3 < 50
+            && rsiList[rsiList.length-1].RSI1 < 50
 
         const openShortCondition = Number(macdList[macdList.length-1].column) < 0
             && rsiList[rsiList.length-1].RSI1 < rsiList[rsiList.length-1].RSI2
             && rsiList[rsiList.length-1].RSI2 < rsiList[rsiList.length-1].RSI3
-            && rsiList[rsiList.length-1].RSI3 > 50
+            && rsiList[rsiList.length-1].RSI1 > 50
 
         const closeLongCondition = longRatio <= -0.95
             || openShortCondition
