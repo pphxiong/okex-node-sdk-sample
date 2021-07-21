6b49ab52e5cb71213c2c8f051da527c33bec9b13
pphxiong <1017551718@qq.com>
2021-07-21 15:46:13 +0800

    bug

--- client/src/services/login.js
+++ client/src/services/login.js
@@ -1,10 +1,11 @@
 import request from '@/utils/request';
 
 export async function fakeAccountLogin(params) {
-  return request('/api/login/account', {
-    method: 'POST',
-    data: params,
-  });
+  // return request('/api/login/account', {
+  //   method: 'POST',
+  //   data: params,
+  // });
+  return Promise.resolve({"status":"ok","type":"account","currentAuthority":"admin"})
 }
 export async function getFakeCaptcha(mobile) {
   return request(`/api/login/captcha?mobile=${mobile}`);
