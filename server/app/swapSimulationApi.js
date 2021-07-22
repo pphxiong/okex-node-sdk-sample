22f23aed19dd370323164d8aaf3f7fc763553425
pphxiong <1017551718@qq.com>
2021-07-22 10:35:24 +0800

    bug

--- server/app/swapSimulationApi.js
+++ server/app/swapSimulationApi.js
@@ -763,15 +763,27 @@ const checkDeal = async data => {
             shortRatio = - shortRatio
         }
 
-        const openLongCondition = Number(macdList[macdList.length-1].column) > Number(macdList[macdList.length-2].column)
+        const openLongCondition = (Number(macdList[macdList.length-1].column) > 0
             && rsiList[rsiList.length-1].RSI1 < rsiList[rsiList.length-1].RSI3
-            // && rsiList[rsiList.length-1].RSI2 > rsiList[rsiList.length-1].RSI3
-            && rsiList[rsiList.length-1].RSI3 > 50
+            && rsiList[rsiList.length-1].RSI3 > 50)
+            ||
+            (Number(macdList[macdList.length-1].column) > Number(macdList[macdList.length-2].column)
+                && rsiList[rsiList.length-1].RSI3 < 35
+                && rsiList[rsiList.length-1].RSI1 > rsiList[rsiList.length-2].RSI1
+                && rsiList[rsiList.length-1].RSI2 > rsiList[rsiList.length-2].RSI2
+                && rsiList[rsiList.length-1].RSI3 > rsiList[rsiList.length-2].RSI3
+            )
 
-        const openShortCondition = Number(macdList[macdList.length-1].column) < Number(macdList[macdList.length-2].column)
+        const openShortCondition = (Number(macdList[macdList.length-1].column) < 0
             && rsiList[rsiList.length-1].RSI1 > rsiList[rsiList.length-1].RSI3
-            // && rsiList[rsiList.length-1].RSI2 < rsiList[rsiList.length-1].RSI3
-            && rsiList[rsiList.length-1].RSI3 < 50
+            && rsiList[rsiList.length-1].RSI3 < 50)
+            ||
+            (Number(macdList[macdList.length-1].column) < Number(macdList[macdList.length-2].column)
+            && rsiList[rsiList.length-1].RSI3 > 65
+            && rsiList[rsiList.length-1].RSI1 < rsiList[rsiList.length-2].RSI1
+            && rsiList[rsiList.length-1].RSI2 < rsiList[rsiList.length-2].RSI2
+            && rsiList[rsiList.length-1].RSI3 < rsiList[rsiList.length-2].RSI3
+            )
 
         const closeLongCondition = openShortCondition
             || isForceDeal
