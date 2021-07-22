f1800efbd97d2421469700db100718f6635a3c8e
pphxiong <1017551718@qq.com>
2021-07-22 16:54:29 +0800

    bug

--- server/app/swapSimulationApi.js
+++ server/app/swapSimulationApi.js
@@ -768,25 +768,25 @@ const checkDeal = async data => {
 
         const openLongCondition = (Number(macdList[macdList.length-1].column) > 0
             && rsiList[rsiList.length-1].RSI1 < rsiList[rsiList.length-1].RSI3
-            // && rsiList[rsiList.length-1].RSI2 < rsiList[rsiList.length-1].RSI3
+            && rsiList[rsiList.length-1].RSI2 < rsiList[rsiList.length-1].RSI3
             && rsiList[rsiList.length-1].RSI3 > 50
-            // && rsiList[rsiList.length-1].RSI3 < 52
+            && rsiList[rsiList.length-1].RSI3 < 52
         )
 
         const openShortCondition = (Number(macdList[macdList.length-1].column) < 0
             && rsiList[rsiList.length-1].RSI1 > rsiList[rsiList.length-1].RSI3
-            // && rsiList[rsiList.length-1].RSI2 > rsiList[rsiList.length-1].RSI3
+            && rsiList[rsiList.length-1].RSI2 > rsiList[rsiList.length-1].RSI3
             && rsiList[rsiList.length-1].RSI3 < 50
-            // && rsiList[rsiList.length-1].RSI3 > 48
+            && rsiList[rsiList.length-1].RSI3 > 48
         )
 
         const closeLongCondition = openShortCondition
             || isForceDeal
-            || longRatio < - 0.382
+            || longRatio < - 0.168
 
         const closeShortCondition = openLongCondition
             || isForceDeal
-            || shortRatio < - 0.382
+            || shortRatio < - 0.168
 
         // console.log('************************************', moment().format('YYYY-MM-DD HH:mm:ss'))
         // console.log('------------------')
