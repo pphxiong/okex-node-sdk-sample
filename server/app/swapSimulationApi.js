7a790bc636726e03d04b985843acaf74105d9919
pphxiong <1017551718@qq.com>
2021-07-22 11:25:09 +0800

    bug

--- server/app/swapSimulationApi.js
+++ server/app/swapSimulationApi.js
@@ -765,18 +765,18 @@ const checkDeal = async data => {
 
         const openLongCondition = (Number(macdList[macdList.length-1].column) > 0
             && rsiList[rsiList.length-1].RSI1 < rsiList[rsiList.length-1].RSI3
-            && rsiList[rsiList.length-1].RSI3 > 50)
+            && rsiList[rsiList.length-1].RSI3 > 50 && rsiList[rsiList.length-1].RSI3 < 75)
 
         const openShortCondition = (Number(macdList[macdList.length-1].column) < 0
             && rsiList[rsiList.length-1].RSI1 > rsiList[rsiList.length-1].RSI3
-            && rsiList[rsiList.length-1].RSI3 < 50)
+            && rsiList[rsiList.length-1].RSI3 < 50 && rsiList[rsiList.length-1].RSI3 > 25)
 
         const closeLongCondition = openShortCondition
             || (rsiList[rsiList.length-1].RSI3 > 75
-                && Number(macdList[macdList.length-1].column) < Number(macdList[macdList.length-2].column)
-                && rsiList[rsiList.length-1].RSI1 < rsiList[rsiList.length-2].RSI1
-                && rsiList[rsiList.length-1].RSI2 < rsiList[rsiList.length-2].RSI2
-                && rsiList[rsiList.length-1].RSI3 < rsiList[rsiList.length-2].RSI3
+                // && Number(macdList[macdList.length-1].column) < Number(macdList[macdList.length-2].column)
+                // && rsiList[rsiList.length-1].RSI1 < rsiList[rsiList.length-2].RSI1
+                // && rsiList[rsiList.length-1].RSI2 < rsiList[rsiList.length-2].RSI2
+                // && rsiList[rsiList.length-1].RSI3 < rsiList[rsiList.length-2].RSI3
             )
             || isForceDeal
             || longRatio < - 0.95
@@ -791,10 +791,10 @@ const checkDeal = async data => {
 
         const closeShortCondition = openLongCondition
             || (rsiList[rsiList.length-1].RSI3 < 25
-                && Number(macdList[macdList.length-1].column) > Number(macdList[macdList.length-2].column)
-                && rsiList[rsiList.length-1].RSI1 > rsiList[rsiList.length-2].RSI1
-                && rsiList[rsiList.length-1].RSI2 > rsiList[rsiList.length-2].RSI2
-                && rsiList[rsiList.length-1].RSI3 > rsiList[rsiList.length-2].RSI3
+                // && Number(macdList[macdList.length-1].column) > Number(macdList[macdList.length-2].column)
+                // && rsiList[rsiList.length-1].RSI1 > rsiList[rsiList.length-2].RSI1
+                // && rsiList[rsiList.length-1].RSI2 > rsiList[rsiList.length-2].RSI2
+                // && rsiList[rsiList.length-1].RSI3 > rsiList[rsiList.length-2].RSI3
             )
             || isForceDeal
             || shortRatio < - 0.95
