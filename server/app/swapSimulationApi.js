2cc762035b1a43a878f5efa380fbca7d8dc7b84f
pphxiong <1017551718@qq.com>
2021-07-22 21:16:58 +0800

    bug

--- server/app/swapSimulationApi.js
+++ server/app/swapSimulationApi.js
@@ -766,17 +766,19 @@ const checkDeal = async data => {
             shortRatio = - shortRatio
         }
 
-        const openLongCondition = (Number(macdList[macdList.length-1].column) > 1
+        const openLongCondition = (Number(macdList[macdList.length-1].column) > 0
             && rsiList[rsiList.length-1].RSI1 < rsiList[rsiList.length-1].RSI3
             && rsiList[rsiList.length-1].RSI3 > 50
-            && rsiList[rsiList.length-1].RSI1 > 50
+            && Number(macdList[macdList.length-1].diff) > Number(macdList[macdList.length-1].dea) + 1
+            // && rsiList[rsiList.length-1].RSI1 > 50
             // && rsiList[rsiList.length-1].RSI2 < 60
         )
 
-        const openShortCondition = (Number(macdList[macdList.length-1].column) < -1
+        const openShortCondition = (Number(macdList[macdList.length-1].column) < 0
             && rsiList[rsiList.length-1].RSI1 > rsiList[rsiList.length-1].RSI3
             && rsiList[rsiList.length-1].RSI3 < 50
-            && rsiList[rsiList.length-1].RSI1 < 50
+            && Number(macdList[macdList.length-1].diff) < Number(macdList[macdList.length-1].dea) - 1
+            // && rsiList[rsiList.length-1].RSI1 < 50
             // && rsiList[rsiList.length-1].RSI2 > 40
         )
 
