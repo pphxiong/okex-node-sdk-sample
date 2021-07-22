c3837d77f0c62d829a0f7287c74f77cb6ebf011f
pphxiong <1017551718@qq.com>
2021-07-22 09:49:52 +0800

    bug

--- server/app/swapSimulationApi.js
+++ server/app/swapSimulationApi.js
@@ -765,15 +765,13 @@ const checkDeal = async data => {
 
         const openLongCondition = Number(macdList[macdList.length-1].column) > 0
             && rsiList[rsiList.length-1].RSI1 < rsiList[rsiList.length-1].RSI3
-            && rsiList[rsiList.length-1].RSI3 > 50
+            // && rsiList[rsiList.length-1].RSI3 > 50
 
         const openShortCondition = Number(macdList[macdList.length-1].column) < 0
             && rsiList[rsiList.length-1].RSI1 > rsiList[rsiList.length-1].RSI3
-            && rsiList[rsiList.length-1].RSI3 < 50
+            // && rsiList[rsiList.length-1].RSI3 < 50
 
-        const closeLongCondition = (Number(macdList[macdList.length-1].column) < 0
-            && rsiList[rsiList.length-1].RSI1 < rsiList[rsiList.length-1].RSI3
-            && rsiList[rsiList.length-1].RSI3 < 50)
+        const closeLongCondition = openShortCondition
             || isForceDeal
             || longRatio < - 0.95
 
@@ -785,9 +783,7 @@ const checkDeal = async data => {
             //     && rsiList[rsiList.length-1].RSI3 < 50
             // )
 
-        const closeShortCondition = (Number(macdList[macdList.length-1].column) > 0
-            && rsiList[rsiList.length-1].RSI1 > rsiList[rsiList.length-1].RSI3
-            && rsiList[rsiList.length-1].RSI3 > 50)
+        const closeShortCondition = openLongCondition
             || isForceDeal
             || shortRatio < - 0.95
             // ||
