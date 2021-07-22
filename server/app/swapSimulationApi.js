54ea70a40c14f8ce2623ea214a33ae209127eb9e
pphxiong <1017551718@qq.com>
2021-07-22 15:58:24 +0800

    bug

--- server/app/swapSimulationApi.js
+++ server/app/swapSimulationApi.js
@@ -768,33 +768,25 @@ const checkDeal = async data => {
 
         const openLongCondition = (Number(macdList[macdList.length-1].column) > 0 && Number(macdList[macdList.length-1].column) < 5
             && rsiList[rsiList.length-1].RSI1 < rsiList[rsiList.length-1].RSI3
-            && rsiList[rsiList.length-1].RSI3 > 50)
+            && rsiList[rsiList.length-1].RSI3 > 45
+        )
 
         const openShortCondition = (Number(macdList[macdList.length-1].column) < 0 && Number(macdList[macdList.length-1].column) > -5
             && rsiList[rsiList.length-1].RSI1 > rsiList[rsiList.length-1].RSI3
-            && rsiList[rsiList.length-1].RSI3 < 50)
+            && rsiList[rsiList.length-1].RSI3 < 55
+        )
 
         const closeLongCondition = openShortCondition
             ||
             Number(macdList[macdList.length-1].column) > 5
             || isForceDeal
             || longRatio < - 0.95
-            || (Number(macdList[macdList.length-1].column) < 0
-                && rsiList[rsiList.length-1].RSI1 < rsiList[rsiList.length-1].RSI2
-                && rsiList[rsiList.length-1].RSI2 < rsiList[rsiList.length-1].RSI3
-                && rsiList[rsiList.length-1].RSI3 < 50
-            )
 
         const closeShortCondition = openLongCondition
             ||
             Number(macdList[macdList.length-1].column) < -5
             || isForceDeal
             || shortRatio < - 0.95
-            || (Number(macdList[macdList.length-1].column) > 0
-                && rsiList[rsiList.length-1].RSI1 > rsiList[rsiList.length-1].RSI2
-                && rsiList[rsiList.length-1].RSI2 > rsiList[rsiList.length-1].RSI3
-                && rsiList[rsiList.length-1].RSI3 > 50
-            )
 
         // console.log('************************************', moment().format('YYYY-MM-DD HH:mm:ss'))
         // console.log('------------------')
