01eb0958ccd1740d306d37af2d1d5cf691d8cbd0
pphxiong <1017551718@qq.com>
2021-07-23 14:35:38 +0800

    bug

--- server/app/swapSimulationApi.js
+++ server/app/swapSimulationApi.js
@@ -768,23 +768,19 @@ const checkDeal = async data => {
 
         const MAIN_OPEN_LONG_CONDITION = Number(macdList[macdList.length-1].column) > 0
             // && rsiList[rsiList.length-1].RSI1 < rsiList[rsiList.length-1].RSI3
-            // && rsiList[rsiList.length-1].RSI3 > 50
-            && Number(macdList[macdList.length-1].diff) > 0
-            && Number(macdList[macdList.length-1].dea) > 0
+            && rsiList[rsiList.length-1].RSI1 >= 50 + 15
 
         const MAIN_OPEN_SHORT_CONDITION = Number(macdList[macdList.length-1].column) < 0
             // && rsiList[rsiList.length-1].RSI1 > rsiList[rsiList.length-1].RSI3
-            // && rsiList[rsiList.length-1].RSI3 < 50
-            && Number(macdList[macdList.length-1].diff) < 0
-            && Number(macdList[macdList.length-1].dea) < 0
+            && rsiList[rsiList.length-1].RSI1 <= 50 - 15
 
-        const openLongCondition = MAIN_OPEN_LONG_CONDITION
-            || rsiList[rsiList.length-1].RSI3 < 20
+        const openLongCondition = Number(macdList[macdList.length-1].column) < 0
+            || rsiList[rsiList.length-1].RSI1 <= 50 - 15
             // || rsiList[rsiList.length-1].RSI1 < 10
             // && rsiList[rsiList.length-1].RSI1 < rsiList[rsiList.length-1].RSI3 - 2
 
-        const openShortCondition = MAIN_OPEN_SHORT_CONDITION
-            || rsiList[rsiList.length-1].RSI3 > 80
+        const openShortCondition =  Number(macdList[macdList.length-1].column) > 0
+            || rsiList[rsiList.length-1].RSI1 >= 50 + 15
             // || rsiList[rsiList.length-1].RSI1 > 90
             // && rsiList[rsiList.length-1].RSI1 > rsiList[rsiList.length-1].RSI3 + 2
 
