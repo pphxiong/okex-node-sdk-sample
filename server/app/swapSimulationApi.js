0be6979d0e3c78898a15ac9f7d317edd14ef0be4
pphxiong <1017551718@qq.com>
2021-07-23 15:10:35 +0800

    bug

--- server/app/swapSimulationApi.js
+++ server/app/swapSimulationApi.js
@@ -767,39 +767,24 @@ const checkDeal = async data => {
         }
 
         const MAIN_OPEN_LONG_CONDITION = (Number(macdList[macdList.length-1].column) > 0
-            && rsiList[rsiList.length-1].RSI1 < rsiList[rsiList.length-1].RSI3
+            && rsiList[rsiList.length-1].RSI1 < rsiList[rsiList.length-1].RSI3 && rsiList[rsiList.length-1].RSI1 > 45
             && rsiList[rsiList.length-1].RSI3 > 48)
             || rsiList[rsiList.length-1].RSI3 < 20
 
         const MAIN_OPEN_SHORT_CONDITION = (Number(macdList[macdList.length-1].column) < 0
-            && rsiList[rsiList.length-1].RSI1 > rsiList[rsiList.length-1].RSI3
+            && rsiList[rsiList.length-1].RSI1 > rsiList[rsiList.length-1].RSI3 && rsiList[rsiList.length-1].RSI1 < 55
             && rsiList[rsiList.length-1].RSI3 < 48)
             || rsiList[rsiList.length-1].RSI3 > 80
 
         const openLongCondition = MAIN_OPEN_LONG_CONDITION
-            // || rsiList[rsiList.length-1].RSI1 < 10
-            // && rsiList[rsiList.length-1].RSI1 < rsiList[rsiList.length-1].RSI3 - 2
 
         const openShortCondition = MAIN_OPEN_SHORT_CONDITION
-            // || rsiList[rsiList.length-1].RSI1 > 90
-            // && rsiList[rsiList.length-1].RSI1 > rsiList[rsiList.length-1].RSI3 + 2
-
 
         const closeLongCondition = MAIN_OPEN_SHORT_CONDITION
             || isForceDeal
-            // || rsiList[rsiList.length-1].RSI1 > 80
-            // || (rsiList[rsiList.length-1].RSI3 < 45
-            // // //     // && Number(macdList[macdList.length-1].column) < 0
-            // // //     && longRatio < -0.12
-            // )
 
         const closeShortCondition = MAIN_OPEN_LONG_CONDITION
             || isForceDeal
-            // || rsiList[rsiList.length-1].RSI1 < 20
-            // || (rsiList[rsiList.length-1].RSI3 > 45
-            // //     // && Number(macdList[macdList.length-1].column) > 0
-            // //     && shortRatio < -0.12
-            // )
 
         // console.log('************************************', moment().format('YYYY-MM-DD HH:mm:ss'))
         // console.log('------------------')
