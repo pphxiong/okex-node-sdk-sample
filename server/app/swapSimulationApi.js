e72b2d976713183774e6c08018f82e74a98fa11b
pphxiong <1017551718@qq.com>
2021-07-23 08:35:25 +0800

    bug

--- server/app/swapSimulationApi.js
+++ server/app/swapSimulationApi.js
@@ -784,16 +784,18 @@ const checkDeal = async data => {
         const closeLongCondition = MAIN_OPEN_SHORT_CONDITION
             || isForceDeal
             || rsiList[rsiList.length-1].RSI3 > 80
-            || (rsiList[rsiList.length-1].RSI3 < 50
+            || (rsiList[rsiList.length-1].RSI3 < 45
                 // && Number(macdList[macdList.length-1].column) < 0
-                && longRatio < -0.12)
+                // && longRatio < -0.12
+            )
 
         const closeShortCondition = MAIN_OPEN_LONG_CONDITION
             || isForceDeal
             || rsiList[rsiList.length-1].RSI3 < 20
-            || (rsiList[rsiList.length-1].RSI3 > 50
+            || (rsiList[rsiList.length-1].RSI3 > 55
                 // && Number(macdList[macdList.length-1].column) > 0
-                && shortRatio < -0.12)
+                // && shortRatio < -0.12
+            )
 
         // console.log('************************************', moment().format('YYYY-MM-DD HH:mm:ss'))
         // console.log('------------------')
