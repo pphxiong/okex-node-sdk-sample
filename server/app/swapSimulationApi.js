01f49d165530c29da57319323db98609df8c1e0a
pphxiong <1017551718@qq.com>
2021-07-23 17:35:50 +0800

    bug

--- server/app/swapSimulationApi.js
+++ server/app/swapSimulationApi.js
@@ -772,18 +772,18 @@ const checkDeal = async data => {
         }
 
         const MAIN_OPEN_LONG_CONDITION = (Number(macdList[macdList.length-1].column) > 0
-            // && Number(macdList[macdList.length-1].diff) > 0
+            && Number(macdList[macdList.length-1].diff) > 0
             && rsiList[rsiList.length-1].RSI1 < rsiList[rsiList.length-1].RSI3
             && rsiList[rsiList.length-1].RSI3 > 50
-            && rsiList[rsiList.length-1].RSI3 < 70
+            // && rsiList[rsiList.length-1].RSI3 < 70
             )
             || rsiList[rsiList.length-1].RSI3 < 20
 
         const MAIN_OPEN_SHORT_CONDITION = (Number(macdList[macdList.length-1].column) < 0
-            // && Number(macdList[macdList.length-1].diff) < 0
+            && Number(macdList[macdList.length-1].diff) < 0
             && rsiList[rsiList.length-1].RSI1 > rsiList[rsiList.length-1].RSI3
             && rsiList[rsiList.length-1].RSI3 < 50
-            && rsiList[rsiList.length-1].RSI3 > 35
+            // && rsiList[rsiList.length-1].RSI3 > 35
             )
             || rsiList[rsiList.length-1].RSI3 > 80
 
