0ce28bbc28b9be41ff3a92389e4e59374d81d36f
pphxiong <1017551718@qq.com>
2021-07-24 08:23:12 +0800

    bug

--- server/app/swapSimulationApi.js
+++ server/app/swapSimulationApi.js
@@ -773,21 +773,21 @@ const checkDeal = async data => {
 
         const MAIN_OPEN_LONG_CONDITION = (Number(macdList[macdList.length-1].column) > 0
             // && Number(macdList[macdList.length-1].diff) > 0
-            && rsiList[rsiList.length-1].RSI1 < rsiList[rsiList.length-1].RSI3
+            // && rsiList[rsiList.length-1].RSI1 < rsiList[rsiList.length-1].RSI3
             // && rsiList[rsiList.length-1].RSI1 > 45 && rsiList[rsiList.length-1].RSI1 < 50
             // && rsiList[rsiList.length-1].RSI2 > 50
-            && rsiList[rsiList.length-1].RSI3 > 50
-            // && rsiList[rsiList.length-1].RSI3 < 70
+            && rsiList[rsiList.length-1].RSI3 > 40
+            && rsiList[rsiList.length-1].RSI3 < 70
             )
             || rsiList[rsiList.length-1].RSI3 < 20
             || rsiList[rsiList.length-1].RSI1 < 10
 
         const MAIN_OPEN_SHORT_CONDITION = (Number(macdList[macdList.length-1].column) < 0
             // && Number(macdList[macdList.length-1].diff) < 0
-            && rsiList[rsiList.length-1].RSI1 > rsiList[rsiList.length-1].RSI3
+            // && rsiList[rsiList.length-1].RSI1 > rsiList[rsiList.length-1].RSI3
             // && rsiList[rsiList.length-1].RSI1 < 55 && rsiList[rsiList.length-1].RSI1 > 50
-            // && rsiList[rsiList.length-1].RSI2 < 50
-            && rsiList[rsiList.length-1].RSI3 < 50
+            && rsiList[rsiList.length-1].RSI3 > 40
+            && rsiList[rsiList.length-1].RSI3 < 70
             // && rsiList[rsiList.length-1].RSI3 > 35
             )
             || rsiList[rsiList.length-1].RSI3 > 80
@@ -800,13 +800,13 @@ const checkDeal = async data => {
         const closeLongCondition = MAIN_OPEN_SHORT_CONDITION
             || isForceDeal
             || longRatio < - 0.95
-            || (Number(macdList[macdList.length-1].column) < 0 && rsiList[rsiList.length-1].RSI3 < 50 && longRatio < 0)
+            // || (Number(macdList[macdList.length-1].column) < 0 && rsiList[rsiList.length-1].RSI3 < 50 && longRatio < 0)
             // || ((maxWinRatio < 0.06 && longRatio < -0.10) || (maxWinRatio > 0.06 && longRatio < 0.02))
 
         const closeShortCondition = MAIN_OPEN_LONG_CONDITION
             || isForceDeal
             || shortRatio < - 0.95
-            || (Number(macdList[macdList.length-1].column) > 0 && rsiList[rsiList.length-1].RSI3 > 50 && shortRatio < 0)
+            // || (Number(macdList[macdList.length-1].column) > 0 && rsiList[rsiList.length-1].RSI3 > 50 && shortRatio < 0)
             // || ((maxWinRatio < 0.06 && shortRatio < -0.10) || (maxWinRatio > 0.06 && shortRatio < 0.02))
 
         // console.log('************************************', moment().format('YYYY-MM-DD HH:mm:ss'))
