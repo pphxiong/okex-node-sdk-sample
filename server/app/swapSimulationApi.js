0155365d7fb6b781f06a54eeca15c55a4f105288
pphxiong <1017551718@qq.com>
2021-07-26 15:54:17 +0800

    bug

--- server/app/swapSimulationApi.js
+++ server/app/swapSimulationApi.js
@@ -751,14 +751,8 @@ const checkDeal = async data => {
             // && !ifMacdLongContinuity
             // && ifMacdLongGreaterContinuity
             )
-            // ||
-            // (
-            //     Number(macdList[macdList.length-1].column) < 0
-            //     && rsiList[rsiList.length-1].RSI1 > rsiList[rsiList.length-1].RSI3
-            //     && rsiList[rsiList.length-1].RSI3 < 40
-            // )
-            // || rsiList[rsiList.length-1].RSI3 < 20
-            // || rsiList[rsiList.length-1].RSI1 < 10
+            || rsiList[rsiList.length-1].RSI3 < 20
+            || rsiList[rsiList.length-1].RSI1 < 10
 
         const MAIN_OPEN_SHORT_CONDITION = (Number(macdList[macdList.length-1].column) < 0
                 && rsiList[rsiList.length-1].RSI1 > rsiList[rsiList.length-1].RSI3
@@ -767,8 +761,8 @@ const checkDeal = async data => {
                 // && !ifMacdShortContinuity
                 // && ifMacdShortLessContinuity
             )
-            // || rsiList[rsiList.length-1].RSI3 > 80
-            // || rsiList[rsiList.length-1].RSI1 > 90
+            || rsiList[rsiList.length-1].RSI3 > 80
+            || rsiList[rsiList.length-1].RSI1 > 90
 
         const openLongCondition = MAIN_OPEN_LONG_CONDITION
 
