caf3df4a803cdddc135922b5d2916327fcaea08b
pphxiong <1017551718@qq.com>
2021-07-26 16:37:57 +0800

    bug

--- server/app/swapSimulationApi.js
+++ server/app/swapSimulationApi.js
@@ -744,20 +744,20 @@ const checkDeal = async data => {
             return cur;
         })
 
-        const MAIN_OPEN_LONG_CONDITION = (Number(macdList[macdList.length-1].column) > 0
-            && rsiList[rsiList.length-1].RSI1 < rsiList[rsiList.length-1].RSI3
+        const MAIN_OPEN_LONG_CONDITION = (Number(macdList[macdList.length-1].column) < 0
+            && rsiList[rsiList.length-1].RSI1 > rsiList[rsiList.length-1].RSI2
+            && rsiList[rsiList.length-1].RSI2 > rsiList[rsiList.length-1].RSI3
             && rsiList[rsiList.length-1].RSI3 > 50
-            && rsiList[rsiList.length-1].RSI3 < 65
             // && !ifMacdLongContinuity
             // && ifMacdLongGreaterContinuity
             )
             // || rsiList[rsiList.length-1].RSI3 < 20
             // || rsiList[rsiList.length-1].RSI1 < 10
 
-        const MAIN_OPEN_SHORT_CONDITION = (Number(macdList[macdList.length-1].column) < 0
-                && rsiList[rsiList.length-1].RSI1 > rsiList[rsiList.length-1].RSI3
-                && rsiList[rsiList.length-1].RSI3 < 50
-                && rsiList[rsiList.length-1].RSI3 > 35
+        const MAIN_OPEN_SHORT_CONDITION = (Number(macdList[macdList.length-1].column) > 0
+            && rsiList[rsiList.length-1].RSI1 < rsiList[rsiList.length-1].RSI2
+            && rsiList[rsiList.length-1].RSI2 < rsiList[rsiList.length-1].RSI3
+            && rsiList[rsiList.length-1].RSI3 < 50
                 // && !ifMacdShortContinuity
                 // && ifMacdShortLessContinuity
             )
@@ -778,8 +778,7 @@ const checkDeal = async data => {
             //     && (longRatio > 0.02 || longRatio < - 0.1 || Number(macdList[macdList.length-1].column) < 0)
             // )
             // ||
-            (Number(macdList[macdList.length-1].column) < 0 && rsiList[rsiList.length-1].RSI1 > rsiList[rsiList.length-1].RSI3)
-            || openShortCondition
+            openShortCondition
             || isForceDeal
             || longRatio < - 0.95
 
@@ -794,8 +793,7 @@ const checkDeal = async data => {
             //     && (shortRatio > 0.02 || shortRatio < - 0.1 || Number(macdList[macdList.length-1].column) > 0)
             // )
             // ||
-            (Number(macdList[macdList.length-1].column) > 0 && rsiList[rsiList.length-1].RSI1 < rsiList[rsiList.length-1].RSI3)
-            || openLongCondition
+            openLongCondition
             || isForceDeal
             || shortRatio < - 0.95
 
