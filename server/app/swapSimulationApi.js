3829e3535afb968fac66915296ca3bd74418264c
pphxiong <1017551718@qq.com>
2021-07-26 11:04:59 +0800

    bug

--- server/app/swapSimulationApi.js
+++ server/app/swapSimulationApi.js
@@ -753,8 +753,8 @@ const checkDeal = async data => {
         const openShortCondition = MAIN_OPEN_SHORT_CONDITION
 
         const closeLongCondition = (
-                Number(macdList[macdList.length-1].column) < 0
-                &&
+                // Number(macdList[macdList.length-1].column) < 0
+                // &&
                 rsiList[rsiList.length-1].RSI1 < rsiList[rsiList.length-1].RSI2
                 && rsiList[rsiList.length-1].RSI2 < rsiList[rsiList.length-1].RSI3
                 && rsiList[rsiList.length-1].RSI3 < 50
@@ -764,11 +764,12 @@ const checkDeal = async data => {
             || longRatio < - 0.95
 
         const closeShortCondition = (
-                Number(
-                macdList[macdList.length-1].column) > 0
-                &&
+                // Number(
+                // macdList[macdList.length-1].column) > 0
+                // &&
                 rsiList[rsiList.length-1].RSI1 > rsiList[rsiList.length-1].RSI2
                 && rsiList[rsiList.length-1].RSI2 > rsiList[rsiList.length-1].RSI3
+                && rsiList[rsiList.length-1].RSI3 > 50
             )
             || openLongCondition
             || isForceDeal
