cb68986035c663d3d4be4f40df651e37b4d49dbb
pphxiong <1017551718@qq.com>
2021-07-27 17:19:43 +0800

    bug

--- server/app/swapApi.js
+++ server/app/swapApi.js
@@ -544,14 +544,14 @@ const checkDeal = async data => {
 
         const MAIN_OPEN_LONG_CONDITION = (Number(macdList[macdList.length-1].column) > 0
                 && rsiList[rsiList.length-1].RSI1 < rsiList[rsiList.length-1].RSI3
-                && rsiList[rsiList.length-1].RSI3 > 50
+                && rsiList[rsiList.length-1].RSI3 > 51
             )
             || rsiList[rsiList.length-1].RSI3 < 20
             || rsiList[rsiList.length-1].RSI1 < 10
 
         const MAIN_OPEN_SHORT_CONDITION = (Number(macdList[macdList.length-1].column) < 0
                 && rsiList[rsiList.length-1].RSI1 > rsiList[rsiList.length-1].RSI3
-                && rsiList[rsiList.length-1].RSI3 < 50
+                && rsiList[rsiList.length-1].RSI3 < 49
             )
             || rsiList[rsiList.length-1].RSI3 > 80
             || rsiList[rsiList.length-1].RSI1 > 90
