8bc6ae0801c9e0b5fed361fe6bbef00b5d3a567c
pphxiong <1017551718@qq.com>
2021-07-27 09:31:15 +0800

    bug

--- server/app/swapSimulationApi.js
+++ server/app/swapSimulationApi.js
@@ -792,7 +792,7 @@ const checkDeal = async (data,isAutoReset = true) => {
             // ||
             isForceDeal
             || longRatio < - 0.95
-            // || longRatio < LOSS_MAX
+            || longRatio < LOSS_MAX
             || (longRatio < WIN_MAX && maxWinRatio > WIN_MAX)
 
         const closeShortCondition =
@@ -803,7 +803,7 @@ const checkDeal = async (data,isAutoReset = true) => {
             // ||
             isForceDeal
             || shortRatio < - 0.95
-            // || shortRatio < LOSS_MAX
+            || shortRatio < LOSS_MAX
             || (shortRatio < WIN_MAX && maxWinRatio > WIN_MAX)
 
         // console.log('************************************', moment().format('YYYY-MM-DD HH:mm:ss'))
