bdb19be8ea8e457c9e52d727ef2f8e7870ff898b
pphxiong <1017551718@qq.com>
2021-07-27 09:57:34 +0800

    bug

--- server/app/swapSimulationApi.js
+++ server/app/swapSimulationApi.js
@@ -791,7 +791,7 @@ const checkDeal = async (data,isAutoReset = true) => {
             || isForceDeal
             || longRatio < - 0.95
             // || longRatio < LOSS_MAX
-            || (longRatio < WIN_MAX && maxWinRatio > WIN_MAX)
+            // || (longRatio < WIN_MAX && maxWinRatio > WIN_MAX)
 
         const closeShortCondition =
             openLongCondition
@@ -801,7 +801,7 @@ const checkDeal = async (data,isAutoReset = true) => {
             || isForceDeal
             || shortRatio < - 0.95
             // || shortRatio < LOSS_MAX
-            || (shortRatio < WIN_MAX && maxWinRatio > WIN_MAX)
+            // || (shortRatio < WIN_MAX && maxWinRatio > WIN_MAX)
 
         // console.log('************************************', moment().format('YYYY-MM-DD HH:mm:ss'))
         // console.log('------------------')
@@ -813,7 +813,7 @@ const checkDeal = async (data,isAutoReset = true) => {
         const closeLong = async () => {
             if(longHolding && Number(longHolding.positionAmt)){
                 totalProfit += longRatio;
-                totalProfit += - 0.04 * 0.01 * LEVERAGE
+                totalProfit += - 0.038 * 0.01 * LEVERAGE
                 longHolding = {}
                 // currentPosition = {}
                 longPosition = {}
@@ -842,7 +842,7 @@ const checkDeal = async (data,isAutoReset = true) => {
         const closeShort = async () => {
             if(shortHolding && Number(shortHolding.positionAmt)){
                 totalProfit += shortRatio
-                totalProfit += - 0.04 * 0.01 * LEVERAGE
+                totalProfit += - 0.038 * 0.01 * LEVERAGE
                 shortHolding = {}
                 // currentPosition = {}
                 shortPosition = {}
@@ -917,7 +917,7 @@ const checkDeal = async (data,isAutoReset = true) => {
                         macdList,
                         rsiList,
                     }
-                    totalProfit += - 0.04 * 0.01 * LEVERAGE
+                    totalProfit += - 0.038 * 0.01 * LEVERAGE
                     dealDetailList.push(dealDetail)
                 }
             }catch (e){
@@ -953,7 +953,7 @@ const checkDeal = async (data,isAutoReset = true) => {
                         macdList,
                         rsiList,
                     }
-                    totalProfit += - 0.04 * 0.01 * LEVERAGE
+                    totalProfit += - 0.038 * 0.01 * LEVERAGE
                     dealDetailList.push(dealDetail)
                 }
             }catch (e){
