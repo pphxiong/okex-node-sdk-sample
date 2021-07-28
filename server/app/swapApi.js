a6df038e074856c35787acc94f8ba306b8344558
pphxiong <1017551718@qq.com>
2021-07-28 09:18:53 +0800

    bug

--- server/app/swapApi.js
+++ server/app/swapApi.js
@@ -548,7 +548,6 @@ const checkDeal = async data => {
                 && rsiList[rsiList.length-1].RSI1 < rsiList[rsiList.length-1].RSI3
                 && rsiList[rsiList.length-2].RSI1 > rsiList[rsiList.length-2].RSI3
                 && rsiList[rsiList.length-1].RSI3 > 51
-                && rsiList[rsiList.length-1].RSI3 < 70
             )
             || rsiList[rsiList.length-1].RSI3 < 20
             || rsiList[rsiList.length-1].RSI1 < 10
@@ -557,7 +556,6 @@ const checkDeal = async data => {
                 && rsiList[rsiList.length-1].RSI1 > rsiList[rsiList.length-1].RSI3
                 && rsiList[rsiList.length-2].RSI1 < rsiList[rsiList.length-2].RSI3
                 && rsiList[rsiList.length-1].RSI3 < 49
-                && rsiList[rsiList.length-1].RSI3 > 30
             )
             || rsiList[rsiList.length-1].RSI3 > 80
             || rsiList[rsiList.length-1].RSI1 > 90
