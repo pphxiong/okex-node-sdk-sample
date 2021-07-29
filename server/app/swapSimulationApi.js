b55832d03864a60a2f9dbc0d1e4c3bce88b10ebe
pphxiong <1017551718@qq.com>
2021-07-29 14:55:05 +0800

    bug

--- server/app/swapSimulationApi.js
+++ server/app/swapSimulationApi.js
@@ -562,23 +562,23 @@ const checkDeal = async (data,isAutoReset = true) => {
                 && rsiList[rsiList.length-1].RSI1 < rsiList[rsiList.length-1].RSI3
                 && rsiList[rsiList.length-2].RSI1 > rsiList[rsiList.length-2].RSI3
                 && rsiList[rsiList.length-1].RSI3 > longCondition
-            )
+            ) && !ifMacdWeakenContinuity && !ifMacdEnhanceContinuity
 
         const MAIN_OPEN_SHORT_CONDITION = (Number(macdList[macdList.length-1].column) < 0
                 && rsiList[rsiList.length-1].RSI1 > rsiList[rsiList.length-1].RSI3
                 && rsiList[rsiList.length-2].RSI1 < rsiList[rsiList.length-2].RSI3
                 && rsiList[rsiList.length-1].RSI3 < shortCondition
                 // && !ifLatestBottom
-            )
+            ) && !ifMacdWeakenContinuity && !ifMacdEnhanceContinuity
 
         const IS_TOP = rsiList[rsiList.length-1].RSI1 > 90 || rsiList[rsiList.length-1].RSI3 > 70
         const IS_BOTTOM = rsiList[rsiList.length-1].RSI1 < 10 || rsiList[rsiList.length-1].RSI3 < 30
 
-        const openLongCondition = (MAIN_OPEN_LONG_CONDITION && !ifMacdWeakenContinuity) || (MAIN_OPEN_SHORT_CONDITION && ifMacdEnhanceContinuity)
-        const openShortCondition = (MAIN_OPEN_SHORT_CONDITION && !ifMacdEnhanceContinuity) || (MAIN_OPEN_LONG_CONDITION && ifMacdWeakenContinuity)
+        const openLongCondition = MAIN_OPEN_LONG_CONDITION
+        const openShortCondition = MAIN_OPEN_SHORT_CONDITION
 
         const closeLongCondition =
-            openShortCondition
+            MAIN_OPEN_SHORT_CONDITION
             || isForceDeal
             // || (rsiList[rsiList.length-2].RSI1 > rsiList[rsiList.length-1].RSI1 - 20 && longRatio > 0)
             // || IS_TOP
@@ -588,7 +588,7 @@ const checkDeal = async (data,isAutoReset = true) => {
             // || (longRatio < WIN_MAX && maxWinRatio > WIN_MAX)
 
         const closeShortCondition =
-            openLongCondition
+            MAIN_OPEN_LONG_CONDITION
             || isForceDeal
             // || (rsiList[rsiList.length-2].RSI1 < rsiList[rsiList.length-1].RSI1 + 20 && shortRatio > 0)
             // || IS_BOTTOM
