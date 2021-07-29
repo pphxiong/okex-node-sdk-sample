ae1a4e05877f490032acc8e79f66a18faaa8c2e7
pphxiong <1017551718@qq.com>
2021-07-29 08:26:27 +0800

    bug

--- server/app/swapSimulationApi.js
+++ server/app/swapSimulationApi.js
@@ -574,13 +574,13 @@ const checkDeal = async (data,isAutoReset = true) => {
         const closeLongCondition =
             MAIN_OPEN_SHORT_CONDITION
             || isForceDeal
-            // || rsiList[rsiList.length-1].RSI3 > 80
-            // || rsiList[rsiList.length-1].RSI1 > 90
+            || (rsiList[rsiList.length-1].RSI1 < rsiList[rsiList.length-1].RSI3 - 10 && rsiList[rsiList.length-1].RSI3 < 50)
             // || longRatio < LOSS_MAX
             // || (longRatio < WIN_MAX && maxWinRatio > WIN_MAX)
 
         const closeShortCondition =
             MAIN_OPEN_LONG_CONDITION
+            || (rsiList[rsiList.length-1].RSI1 > rsiList[rsiList.length-1].RSI3 + 10 && rsiList[rsiList.length-1].RSI3 > 50)
             || isForceDeal
             // || rsiList[rsiList.length-1].RSI3 < 20
             // || rsiList[rsiList.length-1].RSI1 < 10
