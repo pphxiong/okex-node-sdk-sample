7b6733cffbb8c24da820975dd0cb8c7f3490c93f
pphxiong <1017551718@qq.com>
2021-07-29 17:38:44 +0800

    bug

--- server/app/swapSimulationApi.js
+++ server/app/swapSimulationApi.js
@@ -393,7 +393,7 @@ app.get('/swap/getHistory', async (req, response) => {
 
 app.get('/swap/startHearBeat', async (req, response) => {
     const {query = {}} = req;
-    const { time, date, interval = '5m', limit = 1500, isAutoReset = true } = query;
+    const { time, date, interval = '5m', limit = 1500, isAutoReset = false } = query;
     try{
         dealDetailList = [];
         mostLoss = INIT_MOST_LOSS;
