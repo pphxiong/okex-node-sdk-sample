d9c871fc570c9b447cb1afdafe6e9e2fe53b772b
pphxiong <1017551718@qq.com>
2021-07-29 17:05:08 +0800

    bug

--- server/app/swapSimulationApi.js
+++ server/app/swapSimulationApi.js
@@ -573,7 +573,7 @@ const checkDeal = async (data,isAutoReset = true) => {
                 && rsiList[rsiList.length-1].RSI1 > rsiList[rsiList.length-1].RSI3
                 && rsiList[rsiList.length-2].RSI1 < rsiList[rsiList.length-2].RSI3
                 && rsiList[rsiList.length-1].RSI3 < shortCondition
-                && !ifMacdEnhanceContinuity
+                // && !ifMacdEnhanceContinuity
             )
             || IS_TOP
 
