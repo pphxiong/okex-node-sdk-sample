a991a11e4c2134454b1d0c2bb737078cce7825bf
pphxiong <1017551718@qq.com>
2021-07-29 10:17:55 +0800

    bug

--- server/app/swapSimulationApi.js
+++ server/app/swapSimulationApi.js
@@ -478,11 +478,11 @@ app.get('/swap/getLatestProfit', async (req, response) => {
 });
 
 const checkDeal = async (data,isAutoReset = true) => {
-    for(let i = 0; i < data.macdList.length - 9; i++){
+    for(let i = 0; i < data.macdList.length - 29; i++){
         checkByStep({
-            macdList: data.macdList.slice(i,i+10),
-            rsiList: data.rsiList.slice(i,i+10),
-        },isAutoReset && i == data.macdList.length - 10)
+            macdList: data.macdList.slice(i,i+30),
+            rsiList: data.rsiList.slice(i,i+30),
+        },isAutoReset && i == data.macdList.length - 30)
     }
 
     function checkByStep(data,isForceDeal){
@@ -573,8 +573,8 @@ const checkDeal = async (data,isAutoReset = true) => {
         const IS_TOP = rsiList[rsiList.length-1].RSI1 > 90 || rsiList[rsiList.length-1].RSI3 > 70
         const IS_BOTTOM = rsiList[rsiList.length-1].RSI1 < 10 || rsiList[rsiList.length-1].RSI3 < 30
 
-        const openLongCondition = (MAIN_OPEN_LONG_CONDITION && !ifLatestTop)
-        const openShortCondition = (MAIN_OPEN_SHORT_CONDITION && !ifLatestBottom)
+        const openLongCondition = (MAIN_OPEN_LONG_CONDITION && !ifLatestTop) || IS_BOTTOM
+        const openShortCondition = (MAIN_OPEN_SHORT_CONDITION && !ifLatestBottom) || IS_TOP
 
         const closeLongCondition =
             MAIN_OPEN_SHORT_CONDITION
