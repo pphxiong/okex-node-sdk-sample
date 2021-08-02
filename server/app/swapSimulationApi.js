199018cd3e53c3c9fa521f22a33d4e9e28cdacc4
pphxiong <1017551718@qq.com>
2021-08-02 14:37:44 +0800

    bug

--- server/app/swapSimulationApi.js
+++ server/app/swapSimulationApi.js
@@ -564,8 +564,8 @@ const checkDeal = async (data,isAutoReset = true) => {
         const REVERSE_LONG_CONDITION = ifMacdEnhanceContinuity
             && (rsiList[rsiList.length-1].RSI1 > 75 || rsiList[rsiList.length-1].RSI3 > 55)
 
-        const REVERSE_SHORT_CONDITION = ifMacdWeakenContinuity && Number(macdList[0].column) < 0
-            && rsiList[rsiList.length-1].RSI1 < 40 && rsiList[rsiList.length-1].RSI3 > 55
+        const REVERSE_SHORT_CONDITION = ifMacdShortLessContinuity
+            && rsiList[rsiList.length-1].RSI1 < 35 && rsiList[rsiList.length-1].RSI3 > 55
 
         const MAIN_OPEN_LONG_CONDITION = (Number(macdList[macdList.length-1].column) > 0
                 && rsiList[rsiList.length-1].RSI1 < rsiList[rsiList.length-1].RSI3
@@ -580,7 +580,7 @@ const checkDeal = async (data,isAutoReset = true) => {
                 && rsiList[rsiList.length-2].RSI1 < rsiList[rsiList.length-2].RSI3
                 && rsiList[rsiList.length-1].RSI3 < shortCondition
             )
-            || REVERSE_SHORT_CONDITION
+            // || REVERSE_SHORT_CONDITION
 
         const openLongCondition = MAIN_OPEN_LONG_CONDITION
         const openShortCondition = MAIN_OPEN_SHORT_CONDITION
@@ -677,7 +677,6 @@ const checkDeal = async (data,isAutoReset = true) => {
                         }
                     }
                     longHolding = {}
-                    // currentPosition = {}
                     longPosition = {}
                     maxWinRatio = 0;
                     longPatchNum = 0;
@@ -712,7 +711,6 @@ const checkDeal = async (data,isAutoReset = true) => {
                         }
                     }
                     shortHolding = {}
-                    // currentPosition = {}
                     shortPosition = {}
                     maxWinRatio = 0;
                     shortPatchNum = 0;
@@ -752,8 +750,8 @@ const checkDeal = async (data,isAutoReset = true) => {
                     // && (!shortHolding || !Number(shortHolding.positionAmt))
                 ){
                     // closeShort()
-                    // const openPositionAmt = (shortRatio * Number(shortHolding.positionAmt)) < LOSS_MAX ? INIT_POSITION * 2 : INIT_POSITION
-                    const openPositionAmt = INIT_POSITION
+                    const openPositionAmt = shortRatio < LOSS_MAX ? INIT_POSITION * 2 : INIT_POSITION
+                    // const openPositionAmt = INIT_POSITION
                     longPosition = {
                         positionSide: 'LONG',
                         leverage: LEVERAGE,
@@ -790,8 +788,8 @@ const checkDeal = async (data,isAutoReset = true) => {
                     (!shortHolding || !Number(shortHolding.positionAmt))
                 ){
                     // closeLong()
-                    // const openPositionAmt = (longRatio * Number(longHolding.positionAmt)) < LOSS_MAX ? INIT_POSITION * 2 : INIT_POSITION
-                    const openPositionAmt = INIT_POSITION
+                    const openPositionAmt = longRatio < LOSS_MAX ? INIT_POSITION * 2 : INIT_POSITION
+                    // const openPositionAmt = INIT_POSITION
                     shortPosition = {
                         positionSide: 'SHORT',
                         leverage: LEVERAGE,
