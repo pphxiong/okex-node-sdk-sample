1b170b80d2c2c901936d71f49a23a9461df2c79b
pphxiong <1017551718@qq.com>
2021-08-03 23:19:49 +0800

    bug

--- server/app/swapSimulationApi.js
+++ server/app/swapSimulationApi.js
@@ -580,15 +580,16 @@ const checkDeal = async (data,isAutoReset = true) => {
                 && rsiList[rsiList.length-3].RSI3 > longCondition
             )
 
-        const REVERSE_SHORT_CONDITION = Number(macdList[macdList.length-1].column) < 0
-            && rsiList[rsiList.length-1].RSI3 < shortCondition
-            && rsiList[rsiList.length-2].RSI3 < shortCondition
-            && rsiList[rsiList.length-3].RSI3 < shortCondition
+        const REVERSE_SHORT_CONDITION = Number(macdList[macdList.length-1].column) > 0
+            && rsiList[rsiList.length-1].RSI1 < rsiList[rsiList.length-1].RSI3
+            && rsiList[rsiList.length-2].RSI1 > rsiList[rsiList.length-2].RSI3
+            && rsiList[rsiList.length-1].RSI3 > 55
 
         const MAIN_OPEN_LONG_CONDITION = (Number(macdList[macdList.length-1].column) > 0
                 && rsiList[rsiList.length-1].RSI1 < rsiList[rsiList.length-1].RSI3
                 && rsiList[rsiList.length-2].RSI1 > rsiList[rsiList.length-2].RSI3
                 && rsiList[rsiList.length-1].RSI3 > longCondition
+                && rsiList[rsiList.length-1].RSI3 < 55
                 && !ifMacdWeakenContinuity
             )
             || REVERSE_LONG_CONDITION
@@ -598,7 +599,7 @@ const checkDeal = async (data,isAutoReset = true) => {
                 && rsiList[rsiList.length-2].RSI1 < rsiList[rsiList.length-2].RSI3
                 && rsiList[rsiList.length-1].RSI3 < shortCondition
             )
-            // || REVERSE_SHORT_CONDITION
+            || REVERSE_SHORT_CONDITION
 
         const openLongCondition = MAIN_OPEN_LONG_CONDITION
         const openShortCondition = MAIN_OPEN_SHORT_CONDITION
