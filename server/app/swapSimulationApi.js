16734eb0c7a3707b181997e3870e23486747df97
pphxiong <1017551718@qq.com>
2021-08-03 15:28:35 +0800

    bug

--- server/app/swapSimulationApi.js
+++ server/app/swapSimulationApi.js
@@ -584,7 +584,8 @@ const checkDeal = async (data,isAutoReset = true) => {
                 && rsiList[rsiList.length-2].RSI1 < rsiList[rsiList.length-2].RSI3
                 && rsiList[rsiList.length-1].RSI3 < shortCondition
             )
-            || (rsiList[rsiList.length-1].RSI1 < rsiList[rsiList.length-1].RSI3
+            || (Number(macdList[macdList.length-1].column) < 0
+                && rsiList[rsiList.length-1].RSI1 < rsiList[rsiList.length-1].RSI3
                 && rsiList[rsiList.length-1].RSI3 < shortCondition
                 && rsiList[rsiList.length-2].RSI3 > longCondition
             )
