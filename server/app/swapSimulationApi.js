4f341d5ab861f3a874d8977eb6c6759bac8256bd
pphxiong <1017551718@qq.com>
2021-08-03 15:09:15 +0800

    bug

--- server/app/swapSimulationApi.js
+++ server/app/swapSimulationApi.js
@@ -579,14 +579,12 @@ const checkDeal = async (data,isAutoReset = true) => {
             )
             || REVERSE_LONG_CONDITION
 
-        const MAIN_OPEN_SHORT_CONDITION = (Number(macdList[macdList.length-1].column) < 0
-                && rsiList[rsiList.length-1].RSI1 > rsiList[rsiList.length-1].RSI3
+        const MAIN_OPEN_SHORT_CONDITION = (
+            // Number(macdList[macdList.length-1].column) < 0
+            //     &&
+            rsiList[rsiList.length-1].RSI1 > rsiList[rsiList.length-1].RSI3
                 && rsiList[rsiList.length-2].RSI1 < rsiList[rsiList.length-2].RSI3
                 && rsiList[rsiList.length-1].RSI3 < shortCondition
-            )
-            || (Number(macdList[macdList.length-1].column) > 0
-                && Number(macdList[macdList.length-1].column) < 0
-                && rsiList[rsiList.length-1].RSI3 > 60
             )
 
         const openLongCondition = MAIN_OPEN_LONG_CONDITION
