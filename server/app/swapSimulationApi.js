1eaef40227efb56263d41306b766d51771a817fc
pphxiong <1017551718@qq.com>
2021-08-03 16:02:28 +0800

    bug

--- server/app/swapSimulationApi.js
+++ server/app/swapSimulationApi.js
@@ -580,7 +580,7 @@ const checkDeal = async (data,isAutoReset = true) => {
             || REVERSE_LONG_CONDITION
             || (Number(macdList[macdList.length-1].column) > 0
                 && Number(macdList[macdList.length-2].column) > 0
-                && rsiList[rsiList.length-2].RSI3 < shortCondition
+                && rsiList[rsiList.length-2].RSI3 > longCondition
                 && rsiList[rsiList.length-1].RSI3 > longCondition
             )
 
@@ -591,7 +591,7 @@ const checkDeal = async (data,isAutoReset = true) => {
             )
             || (Number(macdList[macdList.length-1].column) < 0
                 && Number(macdList[macdList.length-2].column) < 0
-                && rsiList[rsiList.length-2].RSI3 > longCondition
+                && rsiList[rsiList.length-2].RSI3 < shortCondition
                 && rsiList[rsiList.length-1].RSI3 < shortCondition
             )
 
