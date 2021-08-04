9927a4505b162a88fb606d2e45d29c2c67fd04b3
pphxiong <1017551718@qq.com>
2021-08-04 14:19:39 +0800

    bug

--- server/app/swapSimulationApi.js
+++ server/app/swapSimulationApi.js
@@ -574,18 +574,18 @@ const checkDeal = async (data,isAutoReset = true) => {
         const REVERSE_LONG_CONDITION =
             (ifMacdEnhanceContinuity
             && (rsiList[rsiList.length-1].RSI1 > 75 || rsiList[rsiList.length-1].RSI3 > 55))
-            // ||
-            // (
-            //     Number(macdList[macdList.length-1].column) < 0
-            //     && rsiList[rsiList.length-1].RSI1 > rsiList[rsiList.length-1].RSI3
-            //     && rsiList[rsiList.length-2].RSI1 < rsiList[rsiList.length-2].RSI3
-            //     && (rsiList[rsiList.length-1].RSI3 > longCondition || rsiList[rsiList.length-1].RSI3 < 35)
-            // )
+            ||
+            (
+                Number(macdList[macdList.length-1].column) > 0
+                && rsiList[rsiList.length-1].RSI1 > rsiList[rsiList.length-1].RSI3
+                && rsiList[rsiList.length-2].RSI1 < rsiList[rsiList.length-2].RSI3
+                && rsiList[rsiList.length-1].RSI3 > longCondition
+            )
 
-        const REVERSE_SHORT_CONDITION = Number(macdList[macdList.length-1].column) > 0
+        const REVERSE_SHORT_CONDITION = Number(macdList[macdList.length-1].column) < 0
             && rsiList[rsiList.length-1].RSI1 < rsiList[rsiList.length-1].RSI3
             && rsiList[rsiList.length-2].RSI1 > rsiList[rsiList.length-2].RSI3
-            && (rsiList[rsiList.length-1].RSI3 < shortCondition || rsiList[rsiList.length-1].RSI3 > 55)
+            && rsiList[rsiList.length-1].RSI3 > 55
 
         const MAIN_OPEN_LONG_CONDITION = (Number(macdList[macdList.length-1].column) > 0
                 && rsiList[rsiList.length-1].RSI1 < rsiList[rsiList.length-1].RSI3
@@ -600,7 +600,7 @@ const checkDeal = async (data,isAutoReset = true) => {
                 && rsiList[rsiList.length-2].RSI1 < rsiList[rsiList.length-2].RSI3
                 && rsiList[rsiList.length-1].RSI3 < shortCondition
             )
-            // || REVERSE_SHORT_CONDITION
+            || REVERSE_SHORT_CONDITION
 
         const openLongCondition = MAIN_OPEN_LONG_CONDITION
         const openShortCondition = MAIN_OPEN_SHORT_CONDITION
