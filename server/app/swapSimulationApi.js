dcd3584abaccea44749e7090c6a73826d8c31c70
pphxiong <1017551718@qq.com>
2021-08-07 20:12:37 +0800

    bug

--- server/app/swapSimulationApi.js
+++ server/app/swapSimulationApi.js
@@ -651,14 +651,14 @@ const checkDeal = async (data,isAutoReset = true) => {
             (Number(macdList[macdList.length-1].column) < 0
                 && rsiList[rsiList.length-1].RSI1 > rsiList[rsiList.length-1].RSI3
                 && rsiList[rsiList.length-2].RSI1 < rsiList[rsiList.length-2].RSI3
-                && rsiList[rsiList.length-1].RSI3 < longCondition
+                && rsiList[rsiList.length-1].RSI3 < longCondition + 2
             )
 
         const MAIN_CLOSE_SHORT_CONDITION =
             (Number(macdList[macdList.length-1].column) > 0
                 && rsiList[rsiList.length-1].RSI1 < rsiList[rsiList.length-1].RSI3
                 && rsiList[rsiList.length-2].RSI1 > rsiList[rsiList.length-2].RSI3
-                && rsiList[rsiList.length-1].RSI3 > shortCondition
+                && rsiList[rsiList.length-1].RSI3 > shortCondition - 2
             )
 
         if(modeChange) lastMode = lastMode ? 0 : 1
