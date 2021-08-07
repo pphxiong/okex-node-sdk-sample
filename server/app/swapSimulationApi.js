e64978798ed0d85f811e87dbab51b08583c48312
pphxiong <1017551718@qq.com>
2021-08-07 09:20:04 +0800

    bug

--- server/app/swapSimulationApi.js
+++ server/app/swapSimulationApi.js
@@ -636,9 +636,9 @@ const checkDeal = async (data,isAutoReset = true) => {
                 && rsiList[rsiList.length-1].RSI1 < rsiList[rsiList.length-1].RSI3
                 && rsiList[rsiList.length-2].RSI1 > rsiList[rsiList.length-2].RSI3
                 && rsiList[rsiList.length-1].RSI3 > longCondition
-                && !ifMacdWeakenContinuity
+                // && !ifMacdWeakenContinuity
             )
-            || REVERSE_LONG_CONDITION
+            // || REVERSE_LONG_CONDITION
 
         const MAIN_OPEN_SHORT_CONDITION =
             (Number(macdList[macdList.length-1].column) < 0
@@ -649,15 +649,15 @@ const checkDeal = async (data,isAutoReset = true) => {
 
         const MAIN_CLOSE_LONG_CONDITION =
             (Number(macdList[macdList.length-1].column) < 0
-                // && rsiList[rsiList.length-1].RSI1 > rsiList[rsiList.length-1].RSI3
-                // && rsiList[rsiList.length-2].RSI1 < rsiList[rsiList.length-2].RSI3
+                && rsiList[rsiList.length-1].RSI1 > rsiList[rsiList.length-1].RSI3
+                && rsiList[rsiList.length-2].RSI1 < rsiList[rsiList.length-2].RSI3
                 && rsiList[rsiList.length-1].RSI3 < longCondition
             )
 
         const MAIN_CLOSE_SHORT_CONDITION =
             (Number(macdList[macdList.length-1].column) > 0
-                // && rsiList[rsiList.length-1].RSI1 < rsiList[rsiList.length-1].RSI3
-                // && rsiList[rsiList.length-2].RSI1 > rsiList[rsiList.length-2].RSI3
+                && rsiList[rsiList.length-1].RSI1 < rsiList[rsiList.length-1].RSI3
+                && rsiList[rsiList.length-2].RSI1 > rsiList[rsiList.length-2].RSI3
                 && rsiList[rsiList.length-1].RSI3 > shortCondition
             )
 
