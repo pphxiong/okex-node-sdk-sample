8fe9cb9c0035e4b4c7cf3fa3c9d9493b0f908c18
pphxiong <1017551718@qq.com>
2021-08-07 17:44:44 +0800

    bug

--- server/app/swapSimulationApi.js
+++ server/app/swapSimulationApi.js
@@ -649,17 +649,17 @@ const checkDeal = async (data,isAutoReset = true) => {
 
         const MAIN_CLOSE_LONG_CONDITION =
             MAIN_OPEN_SHORT_CONDITION
-            ||
-            (Number(macdList[macdList.length-1].column) < 0
-            && rsiList[rsiList.length-1].RSI3 < longCondition
-            && longRatio < LOSS_MAX)
+            // ||
+            // (Number(macdList[macdList.length-1].column) < 0
+            // && rsiList[rsiList.length-1].RSI3 < longCondition
+            // && longRatio < LOSS_MAX)
 
         const MAIN_CLOSE_SHORT_CONDITION =
             MAIN_OPEN_LONG_CONDITION
-            ||
-            (Number(macdList[macdList.length-1].column) > 0
-            && rsiList[rsiList.length-1].RSI3 > shortCondition
-            && shortRatio < LOSS_MAX)
+            // ||
+            // (Number(macdList[macdList.length-1].column) > 0
+            // && rsiList[rsiList.length-1].RSI3 > shortCondition
+            // && shortRatio < LOSS_MAX)
 
         if(modeChange) lastMode = lastMode ? 0 : 1
 
