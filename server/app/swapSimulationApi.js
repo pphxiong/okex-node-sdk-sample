280f4af436f678bccbbb9faceb14505c6878f60c
pphxiong <1017551718@qq.com>
2021-08-09 16:24:14 +0800

    bug

--- server/app/swapSimulationApi.js
+++ server/app/swapSimulationApi.js
@@ -662,22 +662,10 @@ const checkDeal = async (data,isAutoReset = true) => {
             ifMacdNegativeContinuity
 
         const MAIN_CLOSE_LONG_CONDITION =
-            (Number(macdList[macdList.length-1].column) < 0
-                && rsiList[rsiList.length-1].RSI1 > rsiList[rsiList.length-1].RSI3
-                && rsiList[rsiList.length-2].RSI1 < rsiList[rsiList.length-2].RSI3
-                && (rsiList[rsiList.length-1].RSI3 < shortCondition
-                    // || rsiList[rsiList.length-1].RSI3 > longCondition + 10
-                )
-            )
+            MAIN_OPEN_SHORT_CONDITION
 
         const MAIN_CLOSE_SHORT_CONDITION =
-            (Number(macdList[macdList.length-1].column) > 0
-                && rsiList[rsiList.length-1].RSI1 < rsiList[rsiList.length-1].RSI3
-                && rsiList[rsiList.length-2].RSI1 > rsiList[rsiList.length-2].RSI3
-                && (rsiList[rsiList.length-1].RSI3 > longCondition
-                    // || rsiList[rsiList.length-1].RSI3 < shortCondition - 10
-                )
-            )
+            MAIN_OPEN_LONG_CONDITION
 
         if(modeChange) lastMode = lastMode ? 0 : 1
 
