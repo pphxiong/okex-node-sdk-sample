f5d6c927c65cfe22f178faa0d262c4dd6148ef42
pphxiong <1017551718@qq.com>
2021-08-09 10:15:35 +0800

    bug

--- server/app/swapSimulationApi.js
+++ server/app/swapSimulationApi.js
@@ -593,10 +593,10 @@ const checkDeal = async (data,isAutoReset = true) => {
         });
 
         let ifMacdPositiveContinuity = macdList.slice(-3).every((item,index,arr)=>{
-            return item.column > 0 && rsiList.slice(-3)[index].RSI3 < longCondition
+            return item.column > 0 && rsiList.slice(-3)[index].RSI3 > longCondition
         });
         let ifMacdNegativeContinuity = macdList.slice(-3).every((item,index,arr)=>{
-            return item.column < 0 && rsiList.slice(-3)[index].RSI3 > shortCondition
+            return item.column < 0 && rsiList.slice(-3)[index].RSI3 < shortCondition
         });
 
         const ifLatestTop = rsiList.some(item=>item.RSI1 > 90 || item.RSI3 > 70)
@@ -635,7 +635,7 @@ const checkDeal = async (data,isAutoReset = true) => {
                 && rsiList[rsiList.length-2].RSI1 > rsiList[rsiList.length-2].RSI3
                 && rsiList[rsiList.length-1].RSI3 > longCondition
                 // && !ifMacdWeakenContinuity
-            ) || ifMacdPositiveContinuity
+            ) || (ifMacdPositiveContinuity && shortRatio < 0)
             // || REVERSE_LONG_CONDITION
 
         const MAIN_OPEN_SHORT_CONDITION =
@@ -643,7 +643,7 @@ const checkDeal = async (data,isAutoReset = true) => {
                 && rsiList[rsiList.length-1].RSI1 > rsiList[rsiList.length-1].RSI3
                 && rsiList[rsiList.length-2].RSI1 < rsiList[rsiList.length-2].RSI3
                 && rsiList[rsiList.length-1].RSI3 < shortCondition
-            ) || ifMacdNegativeContinuity
+            ) || (ifMacdNegativeContinuity && longRatio < 0)
 
         const MAIN_CLOSE_LONG_CONDITION =
             MAIN_OPEN_SHORT_CONDITION
