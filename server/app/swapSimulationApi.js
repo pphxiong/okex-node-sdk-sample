832bfb0f1293afe8bb37e5059611fec49a0aa27d
pphxiong <1017551718@qq.com>
2021-08-09 08:52:03 +0800

    bug

--- server/app/swapSimulationApi.js
+++ server/app/swapSimulationApi.js
@@ -633,8 +633,8 @@ const checkDeal = async (data,isAutoReset = true) => {
 
         const MAIN_OPEN_LONG_CONDITION =
             (Number(macdList[macdList.length-1].column) > 0
-                // && rsiList[rsiList.length-1].RSI1 < rsiList[rsiList.length-1].RSI3
-                // && rsiList[rsiList.length-2].RSI1 > rsiList[rsiList.length-2].RSI3
+                && rsiList[rsiList.length-1].RSI1 < rsiList[rsiList.length-1].RSI3
+                && rsiList[rsiList.length-2].RSI1 > rsiList[rsiList.length-2].RSI3
                 && rsiList[rsiList.length-1].RSI3 > longCondition
                 // && !ifMacdWeakenContinuity
             )
@@ -642,24 +642,24 @@ const checkDeal = async (data,isAutoReset = true) => {
 
         const MAIN_OPEN_SHORT_CONDITION =
             (Number(macdList[macdList.length-1].column) < 0
-                // && rsiList[rsiList.length-1].RSI1 > rsiList[rsiList.length-1].RSI3
-                // && rsiList[rsiList.length-2].RSI1 < rsiList[rsiList.length-2].RSI3
+                && rsiList[rsiList.length-1].RSI1 > rsiList[rsiList.length-1].RSI3
+                && rsiList[rsiList.length-2].RSI1 < rsiList[rsiList.length-2].RSI3
                 && rsiList[rsiList.length-1].RSI3 < shortCondition
             )
 
         const MAIN_CLOSE_LONG_CONDITION =
             // MAIN_OPEN_SHORT_CONDITION
             (Number(macdList[macdList.length-1].column) < 0
-                // && rsiList[rsiList.length-1].RSI1 > rsiList[rsiList.length-1].RSI3
-                // && rsiList[rsiList.length-2].RSI1 < rsiList[rsiList.length-2].RSI3
+                && rsiList[rsiList.length-1].RSI1 > rsiList[rsiList.length-1].RSI3
+                && rsiList[rsiList.length-2].RSI1 < rsiList[rsiList.length-2].RSI3
                 && rsiList[rsiList.length-1].RSI3 < longCondition
             )
 
         const MAIN_CLOSE_SHORT_CONDITION =
             // MAIN_OPEN_LONG_CONDITION
             (Number(macdList[macdList.length-1].column) > 0
-                // && rsiList[rsiList.length-1].RSI1 < rsiList[rsiList.length-1].RSI3
-                // && rsiList[rsiList.length-2].RSI1 > rsiList[rsiList.length-2].RSI3
+                && rsiList[rsiList.length-1].RSI1 < rsiList[rsiList.length-1].RSI3
+                && rsiList[rsiList.length-2].RSI1 > rsiList[rsiList.length-2].RSI3
                 && rsiList[rsiList.length-1].RSI3 > shortCondition
             )
 
