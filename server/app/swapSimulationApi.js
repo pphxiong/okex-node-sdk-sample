94a8ce477de4cb5a0b2833de97c6084f93fc92d3
pphxiong <1017551718@qq.com>
2021-08-09 15:51:29 +0800

    bug

--- server/app/swapSimulationApi.js
+++ server/app/swapSimulationApi.js
@@ -658,27 +658,19 @@ const checkDeal = async (data,isAutoReset = true) => {
                 && rsiList[rsiList.length-1].RSI1 > rsiList[rsiList.length-1].RSI3
                 && rsiList[rsiList.length-2].RSI1 < rsiList[rsiList.length-2].RSI3
                 && rsiList[rsiList.length-1].RSI3 < shortCondition
-            )
+            ) && false
             // ||
             // ifMacdNegativeContinuity
 
         const MAIN_CLOSE_LONG_CONDITION =
-            MAIN_OPEN_SHORT_CONDITION
-            ||
             (Number(macdList[macdList.length-1].column) < 0
                 && rsiList[rsiList.length-1].RSI1 > rsiList[rsiList.length-1].RSI3
                 && rsiList[rsiList.length-2].RSI1 < rsiList[rsiList.length-2].RSI3
-                && rsiList[rsiList.length-1].RSI3 > longCondition + 5
+                && rsiList[rsiList.length-1].RSI3 < shortCondition
             )
 
         const MAIN_CLOSE_SHORT_CONDITION =
             MAIN_OPEN_LONG_CONDITION
-            ||
-            (Number(macdList[macdList.length-1].column) > 0
-                && rsiList[rsiList.length-1].RSI1 < rsiList[rsiList.length-1].RSI3
-                && rsiList[rsiList.length-2].RSI1 > rsiList[rsiList.length-2].RSI3
-                && rsiList[rsiList.length-1].RSI3 < shortCondition - 5
-            )
 
         if(modeChange) lastMode = lastMode ? 0 : 1
 
