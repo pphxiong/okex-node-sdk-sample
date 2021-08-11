2dc7afb1a6180467b91f4583b86b735228a68fbe
pphxiong <1017551718@qq.com>
2021-08-11 08:51:22 +0800

    bug

--- server/app/swapSimulationApi.js
+++ server/app/swapSimulationApi.js
@@ -609,10 +609,10 @@ const checkDeal = async (data,isAutoReset = true) => {
         const latestMacdList = macdList.slice(-4)
         const latestRsiList = rsiList.slice(-4)
         let ifMacdPositiveContinuity = latestMacdList.every((item,index,arr)=>{
-            return latestRsiList[index].RSI1 > latestRsiList[index].RSI3 && latestRsiList[index].RSI3 > shortCondition
+            return latestRsiList[index].RSI1 > latestRsiList[index].RSI3 && latestRsiList[index].RSI3 > shortCondition - 5
         });
         let ifMacdNegativeContinuity = latestMacdList.every((item,index,arr)=>{
-            return latestRsiList[index].RSI1 < latestRsiList[index].RSI3 && latestRsiList[index].RSI3 < longCondition
+            return latestRsiList[index].RSI1 < latestRsiList[index].RSI3 && latestRsiList[index].RSI3 < longCondition + 5
         });
 
         const ifLatestTop = rsiList.some(item=>item.RSI1 > 90 || item.RSI3 > 70)
