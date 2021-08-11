cd56c21e3b3233d7d115ecff24f0e764647c0223
pphxiong <1017551718@qq.com>
2021-08-11 08:46:32 +0800

    bug

--- server/app/swapSimulationApi.js
+++ server/app/swapSimulationApi.js
@@ -647,9 +647,8 @@ const checkDeal = async (data,isAutoReset = true) => {
 
         const MAIN_OPEN_LONG_CONDITION =
             (
-                // Number(macdList[macdList.length-1].column) > 0
-                // &&
-                rsiList[rsiList.length-1].RSI1 < rsiList[rsiList.length-1].RSI3
+                Number(macdList[macdList.length-1].column) > 0
+                && rsiList[rsiList.length-1].RSI1 < rsiList[rsiList.length-1].RSI3
                 && rsiList[rsiList.length-2].RSI1 > rsiList[rsiList.length-2].RSI3
                 && rsiList[rsiList.length-1].RSI3 > longCondition
             )
@@ -660,9 +659,8 @@ const checkDeal = async (data,isAutoReset = true) => {
 
         const MAIN_OPEN_SHORT_CONDITION =
             (
-                // Number(macdList[macdList.length-1].column) < 0
-                // &&
-                rsiList[rsiList.length-1].RSI1 > rsiList[rsiList.length-1].RSI3
+                Number(macdList[macdList.length-1].column) < 0
+                && rsiList[rsiList.length-1].RSI1 > rsiList[rsiList.length-1].RSI3
                 && rsiList[rsiList.length-2].RSI1 < rsiList[rsiList.length-2].RSI3
                 && rsiList[rsiList.length-1].RSI3 < shortCondition
             )
