f7ed6947d353ceaec6a2f893bb363a9b5935f311
pphxiong <1017551718@qq.com>
2021-08-11 15:16:17 +0800

    bug

--- server/app/swapSimulationApi.js
+++ server/app/swapSimulationApi.js
@@ -514,7 +514,6 @@ function fibonacci(n) {
     };
     return fibonacci(n - 2) + fibonacci(n - 1);
 }
-const fiList = [1,1.1,2,3,5,8,13,21,34]
 
 const checkDeal = async (data,isAutoReset = true) => {
     for(let i = 0; i < data.macdList.length - 9; i++){
@@ -786,6 +785,7 @@ const checkDeal = async (data,isAutoReset = true) => {
             }
         }
 
+        const fiList = [1,1.1,2,3,5,8,13,21,34]
         //开多仓条件
         if(
             openLongCondition
