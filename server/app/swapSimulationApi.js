09959c18b5e670f0629a8f0d10f81c207d0c41ec
pphxiong <1017551718@qq.com>
2021-08-11 17:28:04 +0800

    bug

--- server/app/swapSimulationApi.js
+++ server/app/swapSimulationApi.js
@@ -804,7 +804,7 @@ const checkDeal = async (data,isAutoReset = true) => {
                     const increasePosition = FI_LIST[fiIndex+1] * INIT_POSITION
                     const decreasePosition = INIT_POSITION / 2
                     let openPositionAmt = INIT_POSITION
-                    const ratio = longHolding ? shortRatio * Number(longHolding.positionAmt) : shortRatio
+                    const ratio = shortHolding ? shortRatio * Number(shortHolding.positionAmt) : shortRatio
                     if(ratio < 0 && ratio > LOSS_MAX){
                         openPositionAmt = increasePosition
                     }else if(ratio < LOSS_MAX){
