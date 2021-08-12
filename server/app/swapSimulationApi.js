236fa7cd44b78eaf843977ddc71804ea604f9c6b
pphxiong <1017551718@qq.com>
2021-08-12 15:32:39 +0800

    bug

--- server/app/swapSimulationApi.js
+++ server/app/swapSimulationApi.js
@@ -816,7 +816,7 @@ const checkDeal = async (data,isAutoReset = true) => {
                     let fiIndex = INCREASE_FI_LIST.findIndex(item=>shortHolding && item==Number(shortHolding.positionAmt))
                     const ratio = shortRatio
 
-                    fiIndex = fiIndex == -1 ? fiIndex.length-2 : fiIndex
+                    fiIndex = fiIndex == -1 ? INCREASE_FI_LIST.length-2 : fiIndex
                     fiIndex = fiIndex == 0 ? -1 : fiIndex
                     const increasePosition = INCREASE_FI_LIST[fiIndex+1] * INIT_POSITION
                     const decreasePosition = INCREASE_FI_LIST[0]
@@ -875,7 +875,7 @@ const checkDeal = async (data,isAutoReset = true) => {
                     let fiIndex = INCREASE_FI_LIST.findIndex(item=>longHolding && item==Number(longHolding.positionAmt))
                     const ratio = longRatio
 
-                    fiIndex = fiIndex == -1 ? fiIndex.length-2 : fiIndex
+                    fiIndex = fiIndex == -1 ? INCREASE_FI_LIST.length-2 : fiIndex
                     fiIndex = fiIndex == 0 ? -1 : fiIndex
                     const increasePosition = INCREASE_FI_LIST[fiIndex+1] * INIT_POSITION
                     const decreasePosition = INCREASE_FI_LIST[0]
