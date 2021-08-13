2da73d35ea9bf8ea3ef8e3666b570d45a020c376
pphxiong <1017551718@qq.com>
2021-08-13 11:03:57 +0800

    bug

--- server/app/swapSimulationApi.js
+++ server/app/swapSimulationApi.js
@@ -596,10 +596,10 @@ const checkDeal = async (data,isAutoReset = true) => {
         const latestMacdList = macdList.slice(-4)
         const latestRsiList = rsiList.slice(-4)
         let ifMacdPositiveContinuity = latestMacdList.every((item,index,arr)=>{
-            return latestRsiList[index].RSI1 > latestRsiList[index].RSI3 /*&& latestRsiList[index].RSI3 > longCondition*/
+            return latestRsiList[index].RSI1 > latestRsiList[index].RSI3 && latestRsiList[index].RSI3 > longCondition
         });
         let ifMacdNegativeContinuity = latestMacdList.every((item,index,arr)=>{
-            return latestRsiList[index].RSI1 < latestRsiList[index].RSI3 /*&& latestRsiList[index].RSI3 < shortCondition*/
+            return latestRsiList[index].RSI1 < latestRsiList[index].RSI3 && latestRsiList[index].RSI3 < shortCondition
         });
 
         const REVERSE_LONG_CONDITION =
