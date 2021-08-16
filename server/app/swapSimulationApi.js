e7459ee95c10b8e66e2dc288842dcf17e986783e
pphxiong <1017551718@qq.com>
2021-08-16 09:01:03 +0800

    bug

--- server/app/swapSimulationApi.js
+++ server/app/swapSimulationApi.js
@@ -18,7 +18,7 @@ const LEVERAGE = 10;
 const INTERVAL = '5m';
 const LOSS_MAX = - 0.1 * LEVERAGE / 10;
 const WIN_MAX = 0.1 * LEVERAGE / 10;
-const INCREASE_FI_LIST = [0.5,0.6,1.4,2,3.5]
+const INCREASE_FI_LIST = [0.4,0.8,1.2,2,3.2]
 const INIT_MOST_LOSS = {
     profit: 0,
     time: null,
