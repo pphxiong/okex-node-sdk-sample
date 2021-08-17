59b42e618bb05d0a504dbf4348c09f569d3fa3da
pphxiong <1017551718@qq.com>
2021-08-17 17:27:44 +0800

    bug

--- server/app/swapApi.js
+++ server/app/swapApi.js
@@ -11,7 +11,7 @@ const LEVERAGE = 20;
 const LOSS_MAX = - 0.1 * LEVERAGE / 10;
 const WIN_MAX = 0.1 * LEVERAGE / 10;
 const BAO_RATIO = - 0.809;
-const INCREASE_FI_LIST = [1,1.5,2.5,4,5].map(item=>item * 3)
+const INCREASE_FI_LIST = [1,1.5,2.5,3.5,4.5,5].map(item=>item * 3)
 
 const INIT_MOST_LOSS = {
     profit: 0,
