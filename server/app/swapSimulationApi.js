8167792280a4b6477e51756b290c90c16a8592f8
pphxiong <1017551718@qq.com>
2021-08-22 08:38:35 +0800

    bug

--- server/app/swapSimulationApi.js
+++ server/app/swapSimulationApi.js
@@ -20,7 +20,7 @@ const WIN_MAX = 0.1 * LEVERAGE / 10;
 const BAO_RATIO = - 0.809;
 // const BAO_RATIO = LOSS_MAX * 2;
 const CAPITAL_RATIO = 3;
-const INCREASE_FI_LIST = [1,2,3,5].map(item=>item * CAPITAL_RATIO)
+const INCREASE_FI_LIST = [1,2,3,4.5,5].map(item=>item * CAPITAL_RATIO)
 const INIT_POSITION = INCREASE_FI_LIST[0];
 
 
@@ -819,7 +819,7 @@ const checkDeal = async (data,isAutoReset = true) => {
                     // closeShort()
                     // const openPositionAmt = shortRatio < LOSS_MAX ? INIT_POSITION * 2 : INIT_POSITION
                     let fiIndex = INCREASE_FI_LIST.findIndex(item=>shortHolding && item==Number(shortHolding.positionAmt))
-                    fiIndex = fiIndex == INCREASE_FI_LIST.length-1 ? -1 : fiIndex
+                    fiIndex = fiIndex == INCREASE_FI_LIST.length-1 ? INCREASE_FI_LIST.length-2 : fiIndex
 
                     let openPositionAmt = INIT_POSITION
                     const ratio = shortRatio
@@ -875,7 +875,7 @@ const checkDeal = async (data,isAutoReset = true) => {
                     // closeLong()
                     // const openPositionAmt = longRatio < LOSS_MAX ? INIT_POSITION * 2 : INIT_POSITION
                     let fiIndex = INCREASE_FI_LIST.findIndex(item=>longHolding && item==Number(longHolding.positionAmt))
-                    fiIndex = fiIndex == INCREASE_FI_LIST.length-1 ? -1 : fiIndex
+                    fiIndex = fiIndex == INCREASE_FI_LIST.length-1 ? INCREASE_FI_LIST.length-2 : fiIndex
 
                     let openPositionAmt = INIT_POSITION
                     const ratio = longRatio
