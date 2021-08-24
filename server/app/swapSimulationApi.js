221f85db256684be3d9b2a2dc8fd8433576f460c
pphxiong <1017551718@qq.com>
2021-08-24 16:31:32 +0800

    bug

--- server/app/swapSimulationApi.js
+++ server/app/swapSimulationApi.js
@@ -20,7 +20,7 @@ const WIN_MAX = (0.1 * LEVERAGE) / 10;
 const BAO_RATIO = -0.809;
 // const BAO_RATIO = LOSS_MAX * 2;
 const CAPITAL_RATIO = 3;
-const INCREASE_FI_LIST = [1,2,6,4,3,5].map((item) => item * CAPITAL_RATIO);
+const INCREASE_FI_LIST = [1,2,5,4.5,4,3,6].map((item) => item * CAPITAL_RATIO);
 const INIT_POSITION = INCREASE_FI_LIST[0];
 let MODE = 1;
 
