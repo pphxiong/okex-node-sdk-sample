5d6d0710a89b2baf1a6dcb382c3e022045de3f11
pphxiong <1017551718@qq.com>
2021-08-24 16:35:12 +0800

    bug

--- server/app/swapApiBN.js
+++ server/app/swapApiBN.js
@@ -11,7 +11,7 @@ const LOSS_MAX = - 0.1 * LEVERAGE / 10;
 const WIN_MAX = 0.1 * LEVERAGE / 10;
 const BAO_RATIO = - 0.809;
 const CAPITAL_RATIO = 1;
-const INCREASE_FI_LIST = [1,2,3,5,6].map(item=>item * CAPITAL_RATIO)
+const INCREASE_FI_LIST = [1,2,5,4,3,6].map(item=>item * CAPITAL_RATIO)
 const INIT_POSITION = CAPITAL_RATIO;
 
 const INIT_MOST_LOSS = {
