4c4c5d213c61f648ea567eb9f0eed8ba422fe80c
pphxiong <1017551718@qq.com>
2021-08-25 11:23:34 +0800

    bug

--- server/app/swapSimulationApi.js
+++ server/app/swapSimulationApi.js
@@ -953,11 +953,11 @@ const checkDeal = async (data, isAutoReset = true) => {
             openPositionAmt = decreasePosition;
           }
           if (MODE == 2) {
-            openPositionAmt = INIT_POSITION * 3;
+            openPositionAmt = INIT_POSITION;
             if((closeLongCondition && longRatio < LOSS_MAX * 2) ||
                 (closeShortCondition && shortRatio < LOSS_MAX * 2)){
                 MODE = 1;
-                openPositionAmt = INCREASE_FI_LIST[INCREASE_FI_LIST.length-1]
+                openPositionAmt = INCREASE_FI_LIST[INCREASE_FI_LIST.length-2]
             }
           }
           totalCapital += -0.038 * 0.01 * LEVERAGE;
@@ -1024,11 +1024,11 @@ const checkDeal = async (data, isAutoReset = true) => {
             openPositionAmt = decreasePosition;
           }
           if (MODE == 2) {
-            openPositionAmt = INIT_POSITION * 3;
+            openPositionAmt = INIT_POSITION;
             if((closeLongCondition && longRatio < LOSS_MAX * 1) ||
                 (closeShortCondition && shortRatio < LOSS_MAX * 1)){
               MODE = 1;
-              // openPositionAmt = INCREASE_FI_LIST[INCREASE_FI_LIST.length-1]
+              openPositionAmt = INCREASE_FI_LIST[INCREASE_FI_LIST.length-2]
             }
           }
           totalCapital += -0.038 * 0.01 * LEVERAGE;
