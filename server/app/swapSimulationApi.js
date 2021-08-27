d04883d0e76ce841a78288ed4350d5a0a29623c2
pphxiong <1017551718@qq.com>
2021-08-27 16:26:57 +0800

    bug

--- server/app/swapSimulationApi.js
+++ server/app/swapSimulationApi.js
@@ -689,6 +689,15 @@ const checkDeal = async (data, isAutoReset = true) => {
       );
     });
 
+    let fiIndex = INCREASE_FI_LIST.findIndex(
+        (item) => holding && item == Number(holding.positionAmt)
+    );
+    fiIndex =
+        fiIndex == INCREASE_FI_LIST.length - 1
+            ? INCREASE_FI_LIST.length - 2
+            : fiIndex;
+    // if (ifMacdPositiveContinuity || ifMacdNegativeContinuity) fiIndex = -1;
+
     const MAIN_OPEN_LONG_CONDITION =
       Number(macdList[macdList.length - 1].column) > 0 &&
       rsiList[rsiList.length - 1].RSI1 < rsiList[rsiList.length - 1].RSI3 &&
@@ -705,11 +714,11 @@ const checkDeal = async (data, isAutoReset = true) => {
 
     const MAIN_OPEN_LONG_CONDITION1 =
       MAIN_OPEN_LONG_CONDITION ||
-      (ifMacdPositiveContinuity && shortRatio > WIN_MAX * 2) ||
+      (ifMacdPositiveContinuity /*&& shortRatio > WIN_MAX * 2*/) ||
       shortRatio < BAO_RATIO;
     const MAIN_OPEN_SHORT_CONDITION1 =
       MAIN_OPEN_SHORT_CONDITION ||
-      (ifMacdNegativeContinuity && longRatio > WIN_MAX * 2) ||
+      (ifMacdNegativeContinuity /*&& longRatio > WIN_MAX * 2*/) ||
       longRatio < BAO_RATIO;
     const MAIN_CLOSE_LONG_CONDITION1 = MAIN_OPEN_SHORT_CONDITION1;
     const MAIN_CLOSE_SHORT_CONDITION1 = MAIN_OPEN_LONG_CONDITION1;
@@ -740,15 +749,6 @@ const checkDeal = async (data, isAutoReset = true) => {
     const closeShortCondition =
       (MODE == 1 ? MAIN_CLOSE_SHORT_CONDITION1 : MAIN_CLOSE_SHORT_CONDITION2) ||
       isForceDeal;
-
-    let fiIndex = INCREASE_FI_LIST.findIndex(
-      (item) => holding && item == Number(holding.positionAmt)
-    );
-    fiIndex =
-      fiIndex == INCREASE_FI_LIST.length - 1
-        ? INCREASE_FI_LIST.length - 2
-        : fiIndex;
-    // if (ifMacdPositiveContinuity || ifMacdNegativeContinuity) fiIndex = -1;
 
     if (ifIgnore) {
       // if(longRatio < LOSS_MAX || shortRatio < LOSS_MAX){
