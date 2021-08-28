a04875b01749e46975cfdc32ba8f15b5f0334177
pphxiong <1017551718@qq.com>
2021-08-28 19:39:26 +0800

    bug

--- server/app/swapSimulationApi.js
+++ server/app/swapSimulationApi.js
@@ -25,7 +25,7 @@ const MODE_RATIO = {
   2: [0.5, 1, 1.5, 2, 3, 4.5, 6, 8].map((item) => item / 1),
 };
 let MODE = 1;
-const INCREASE_FI_LIST = MODE_RATIO[1].map((item) => item * CAPITAL_RATIO);
+const INCREASE_FI_LIST = MODE_RATIO[2].map((item) => item * CAPITAL_RATIO);
 const INIT_POSITION = INCREASE_FI_LIST[0];
 
 let modeChange = false;
@@ -726,11 +726,13 @@ const checkDeal = async (data, isAutoReset = true) => {
 
     const MAIN_OPEN_LONG_CONDITION1 =
       MAIN_OPEN_LONG_CONDITION ||
-      (ifMacdPositiveContinuity && shortRatio > WIN_MAX * 2) ||
+      ifMacdPositiveContinuity ||
+      // && shortRatio > WIN_MAX * 2
       shortRatio < BAO_RATIO;
     const MAIN_OPEN_SHORT_CONDITION1 =
       MAIN_OPEN_SHORT_CONDITION ||
-      (ifMacdNegativeContinuity && longRatio > WIN_MAX * 2) ||
+      ifMacdNegativeContinuity ||
+      // && longRatio > WIN_MAX * 2
       longRatio < BAO_RATIO;
     const MAIN_CLOSE_LONG_CONDITION1 = MAIN_OPEN_SHORT_CONDITION1;
     const MAIN_CLOSE_SHORT_CONDITION1 = MAIN_OPEN_LONG_CONDITION1;
