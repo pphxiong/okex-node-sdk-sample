1a597aeb9603e6685ccd39c6c8cd380c92562df7
pphxiong <1017551718@qq.com>
2021-08-31 09:16:20 +0800

    bug

--- server/app/swapSimulationApi.js
+++ server/app/swapSimulationApi.js
@@ -26,7 +26,7 @@ const MODE_RATIO = {
 };
 let MODE = 1;
 let MODE2_NUM = 0;
-const INCREASE_FI_LIST = MODE_RATIO[MODE].map((item) => item * CAPITAL_RATIO);
+const INCREASE_FI_LIST = MODE_RATIO[2].map((item) => item * CAPITAL_RATIO);
 const INIT_POSITION = INCREASE_FI_LIST[0];
 
 let modeChange = false;
