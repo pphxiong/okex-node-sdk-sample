edfeb3263593b16d0b60e1e764e2a66dd28325ba
pphxiong <1017551718@qq.com>
2021-09-13 14:01:47 +0800

    bug

--- server/app/swapSimulationApi.js
+++ server/app/swapSimulationApi.js
@@ -505,8 +505,10 @@ app.get('/swap/startHearBeat', async (req, response) => {
 
     const payload = {
       interval,
-      limit: limit * 2,
-      startTime: moment(Number(time)).subtract(1, 'days').valueOf(),
+      // limit: limit * 2,
+      // startTime: moment(Number(time)).subtract(1, 'days').valueOf(),
+      limit,
+      endTime: time,
     };
     const data = await cAuthClientBN.common.getHistory(BN_SYMBOL, payload);
     const list = data;
