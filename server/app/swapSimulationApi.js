d253140dc56bf6dda812f96a612162d1edace496
pphxiong <1017551718@qq.com>
2021-09-13 17:20:05 +0800

    bug

--- server/app/swapSimulationApi.js
+++ server/app/swapSimulationApi.js
@@ -19,8 +19,8 @@ const LOSS_MAX = (-0.1 * LEVERAGE) / 10;
 const WIN_MAX = (0.1 * LEVERAGE) / 10;
 const BAO_RATIO = -0.95;
 // const BAO_RATIO = LOSS_MAX * 2;
-const CAPITAL_RATIO = 1;
-const DEFAULT_POSITION_RATIO_LIST = [1, 2, 3];
+const CAPITAL_RATIO = 3;
+const DEFAULT_POSITION_RATIO_LIST = [1, 2, 3, 5];
 const MODE_RATIO = {
   1: DEFAULT_POSITION_RATIO_LIST,
   2: [1, 1.5, 5, 4.5, 3, 6],
