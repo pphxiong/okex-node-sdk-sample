609dffb3d8f7d41c5b0389b083d37ecea9aed416
pphxiong <1017551718@qq.com>
2021-09-13 15:49:20 +0800

    bug

--- server/app/swapSimulationApi.js
+++ server/app/swapSimulationApi.js
@@ -786,10 +786,7 @@ const checkDeal = async (data, isAutoReset = true) => {
       MODE = MODE == 1 ? 2 : 1;
     }
 
-    fiIndex =
-      fiIndex == INCREASE_FI_LIST.length - 1
-        ? INCREASE_FI_LIST.length - 2
-        : fiIndex;
+    fiIndex = fiIndex == INCREASE_FI_LIST.length - 1 ? -1 : fiIndex;
 
     if (
       (MODE == 1 && closeLongCondition && longRatio > WIN_MAX * 2) ||
