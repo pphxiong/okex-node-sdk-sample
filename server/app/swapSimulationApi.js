f076ba7fbefcbf9febf5aa54797cc0549e576563
pphxiong <1017551718@qq.com>
2021-09-13 15:47:05 +0800

    bug

--- server/app/swapSimulationApi.js
+++ server/app/swapSimulationApi.js
@@ -20,7 +20,7 @@ const WIN_MAX = (0.1 * LEVERAGE) / 10;
 const BAO_RATIO = -0.95;
 // const BAO_RATIO = LOSS_MAX * 2;
 const CAPITAL_RATIO = 3;
-const DEFAULT_POSITION_RATIO_LIST = [1];
+const DEFAULT_POSITION_RATIO_LIST = [1, 2, 5, 4, 3, 4.5];
 const MODE_RATIO = {
   1: DEFAULT_POSITION_RATIO_LIST,
   2: [1, 1.5, 5, 4.5, 3, 6],
@@ -783,7 +783,7 @@ const checkDeal = async (data, isAutoReset = true) => {
       (closeLongCondition || closeShortCondition) &&
       fiIndex == INCREASE_FI_LIST.length - 1
     ) {
-      // MODE = MODE == 1 ? 2 : 1;
+      MODE = MODE == 1 ? 2 : 1;
     }
 
     fiIndex =
