83b307897e8af0406e0b8074590ebaa1ca6b83f9
pphxiong <1017551718@qq.com>
2021-09-14 08:31:33 +0800

    bug

--- server/app/swapSimulationApi.js
+++ server/app/swapSimulationApi.js
@@ -723,7 +723,7 @@ const checkDeal = async (data, isAutoReset = true) => {
     // (shortRatio < LOSS_MAX || shortRatio >= 0);
     // && !ifIgnore;
 
-    constMAIN_OPEN_LONG_CONDITION =
+    const MAIN_OPEN_LONG_CONDITION =
       Number(macdList[macdList.length - 1].column) > 0 &&
       rsiList[rsiList.length - 1].RSI1 < rsiList[rsiList.length - 1].RSI3 &&
       rsiList[rsiList.length - 2].RSI1 > rsiList[rsiList.length - 2].RSI3 &&
