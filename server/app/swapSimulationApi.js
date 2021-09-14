5ce1033ec766bfb080d7bb06b59ec4618da8b215
pphxiong <1017551718@qq.com>
2021-09-14 10:38:21 +0800

    bug

--- server/app/swapSimulationApi.js
+++ server/app/swapSimulationApi.js
@@ -481,7 +481,7 @@ app.get('/swap/startHearBeat', async (req, response) => {
     date,
     interval = INTERVAL,
     limit = 1500,
-    isAutoReset = false,
+    isAutoReset = true,
     isInit = false,
   } = query;
   try {
