b6563e69b8224429044a4523680c731b4beb011e
pphxiong <1017551718@qq.com>
2021-09-17 00:34:14 +0800

    bug

--- server/app/swapSimulationApi.js
+++ server/app/swapSimulationApi.js
@@ -720,9 +720,38 @@ const checkDeal = async (data, isAutoReset = true) => {
       );
     });
 
-    const MAIN_OPEN_LONG_CONDITION = ifMacdNegativeContinuity;
+    const isHasLongCondition = latestMacdList.some((item, index) => {
+      if (index == 0) return false;
+      return (
+        Number(latestMacdList[index].column) > 0 &&
+        latestRsiList[index].RSI1 < latestRsiList[index].RSI3 &&
+        latestRsiList[index - 1].RSI1 > latestRsiList[index - 1].RSI3 &&
+        latestRsiList[index].RSI3 > longCondition
+      );
+    });
 
-    const MAIN_OPEN_SHORT_CONDITION = ifMacdPositiveContinuity;
+    const isHasShortCondition = latestMacdList.some((item, index) => {
+      if (index == 0) return false;
+      return (
+        Number(latestMacdList[index].column) < 0 &&
+        latestRsiList[index].RSI1 > latestRsiList[index].RSI3 &&
+        latestRsiList[index - 1].RSI1 < latestRsiList[index - 1].RSI3 &&
+        latestRsiList[index].RSI3 < shortCondition
+      );
+    });
+
+    const MAIN_OPEN_LONG_CONDITION =
+      isHasLongCondition &&
+      latestRsiList[latestRsiList.length - 1].RSI1 >
+        latestRsiList[latestRsiList.length - 1].RSI3 &&
+      latestRsiList[latestRsiList.length - 2].RSI1 <
+        latestRsiList[latestRsiList.length - 2].RSI3;
+    const MAIN_OPEN_SHORT_CONDITION =
+      isHasShortCondition &&
+      latestRsiList[latestRsiList.length - 1].RSI1 <
+        latestRsiList[latestRsiList.length - 1].RSI3 &&
+      latestRsiList[latestRsiList.length - 2].RSI1 >
+        latestRsiList[latestRsiList.length - 2].RSI3;
 
     const MAIN_OPEN_LONG_CONDITION1 = MAIN_OPEN_LONG_CONDITION;
     // ||
@@ -733,20 +762,12 @@ const checkDeal = async (data, isAutoReset = true) => {
     // (ifRSINegativeContinuity && longRatio > WIN_MAX * 2);
     // || longRatio < BAO_RATIO;
     const MAIN_CLOSE_LONG_CONDITION1 =
-      Number(macdList[macdList.length - 1].column) <
-        Number(macdList[macdList.length - 2].column) &&
-      Number(macdList[macdList.length - 2].column) <
-        Number(macdList[macdList.length - 3].column);
-    // Number(macdList[macdList.length - 1].column) < 0 &&
-    // rsiList[rsiList.length - 1].RSI3 < shortCondition;
+      Number(macdList[macdList.length - 1].column) < 0 ||
+      rsiList[rsiList.length - 1].RSI3 < shortCondition;
     // rsiList[rsiList.length - 1].RSI3 > LONG_CONDITION; /*|| (longRatio > WIN_MAX * 5 && Number(macdList[macdList.length - 1].column) < 0)*/
     const MAIN_CLOSE_SHORT_CONDITION1 =
-      Number(macdList[macdList.length - 1].column) >
-        Number(macdList[macdList.length - 2].column) &&
-      Number(macdList[macdList.length - 2].column) >
-        Number(macdList[macdList.length - 3].column);
-    // Number(macdList[macdList.length - 1].column) > 0 &&
-    // rsiList[rsiList.length - 1].RSI3 > longCondition;
+      Number(macdList[macdList.length - 1].column) > 0 ||
+      rsiList[rsiList.length - 1].RSI3 > longCondition;
     /*|| (shortRatio > WIN_MAX * 5 && Number(macdList[macdList.length - 1].column) > 0)*/
 
     if (modeChange) lastMode = lastMode ? 0 : 1;
