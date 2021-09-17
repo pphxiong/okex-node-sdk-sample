e8c16dac0ca1980f578204b61bb9648526884ac8
pphxiong <1017551718@qq.com>
2021-09-17 10:48:54 +0800

    bug

--- server/app/swapSimulationApi.js
+++ server/app/swapSimulationApi.js
@@ -16,7 +16,7 @@ const BN_SYMBOL = 'ETHUSDT';
 const LEVERAGE = 10;
 const INTERVAL = '5m';
 const LOSS_MAX = (-0.1 * LEVERAGE) / 10;
-const WIN_MAX = (0.05 * LEVERAGE) / 10;
+const WIN_MAX = (0.08 * LEVERAGE) / 10;
 const BAO_RATIO = -0.95;
 // const BAO_RATIO = LOSS_MAX * 2;
 const CAPITAL_RATIO = 3;
@@ -751,15 +751,13 @@ const checkDeal = async (data, isAutoReset = true) => {
     // ||
     // (ifRSINegativeContinuity && longRatio > WIN_MAX * 2);
     // || longRatio < BAO_RATIO;
-    const MAIN_CLOSE_LONG_CONDITION1 =
-      MAIN_OPEN_SHORT_CONDITION1 ||
-      (ifMacdNegativeContinuity && longRatio > WIN_MAX);
+    const MAIN_CLOSE_LONG_CONDITION1 = MAIN_OPEN_SHORT_CONDITION1;
+    // ||(ifMacdNegativeContinuity && longRatio > WIN_MAX);
     // ||
     // rsiList[rsiList.length - 1].RSI3 < shortCondition;
     // rsiList[rsiList.length - 1].RSI3 > LONG_CONDITION; /*|| (longRatio > WIN_MAX * 5 && Number(macdList[macdList.length - 1].column) < 0)*/
-    const MAIN_CLOSE_SHORT_CONDITION1 =
-      MAIN_OPEN_LONG_CONDITION1 ||
-      (ifMacdPositiveContinuity && shortRatio > WIN_MAX);
+    const MAIN_CLOSE_SHORT_CONDITION1 = MAIN_OPEN_LONG_CONDITION1;
+    // ||(ifMacdPositiveContinuity && shortRatio > WIN_MAX);
     // ||
     // rsiList[rsiList.length - 1].RSI3 > longCondition;
     /*|| (shortRatio > WIN_MAX * 5 && Number(macdList[macdList.length - 1].column) > 0)*/
