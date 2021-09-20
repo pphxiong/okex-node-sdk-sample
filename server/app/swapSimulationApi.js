5c53e39d54148b109bff7cc60b77040bf9b61d58
pphxiong <1017551718@qq.com>
2021-09-20 09:22:28 +0800

    bug

--- server/app/swapSimulationApi.js
+++ server/app/swapSimulationApi.js
@@ -777,10 +777,12 @@ const checkDeal = async (data, isAutoReset = true) => {
 
     if (modeChange) lastMode = lastMode ? 0 : 1;
 
-    const MAIN_OPEN_LONG_CONDITION2 = MAIN_OPEN_SHORT_CONDITION;
+    const MAIN_OPEN_LONG_CONDITION2 =
+      MAIN_OPEN_SHORT_CONDITION || shortRatio < LOSS_MAX;
     // || (ifRSIPositiveContinuity && shortRatio > WIN_MAX * 2);
 
-    const MAIN_OPEN_SHORT_CONDITION2 = MAIN_OPEN_LONG_CONDITION;
+    const MAIN_OPEN_SHORT_CONDITION2 =
+      MAIN_OPEN_LONG_CONDITION || longRatio < LOSS_MAX;
     // ||(ifRSINegativeContinuity && longRatio > WIN_MAX * 2);
 
     const MAIN_CLOSE_LONG_CONDITION2 = MAIN_OPEN_SHORT_CONDITION2;
