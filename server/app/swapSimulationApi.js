3f8c1fbddcd43b755ce7b05ce353aee10e9628c1
pphxiong <1017551718@qq.com>
2021-09-20 23:54:07 +0800

    bug

--- server/app/swapSimulationApi.js
+++ server/app/swapSimulationApi.js
@@ -15,7 +15,7 @@ const generatePositionList = (init, num) => {
   const arr = [init];
   let i = 0;
   while (i < num) {
-    init = Number((init + init * 0.14).toFixed(1));
+    init = Number((init + init * 0.12).toFixed(1));
     arr.push(init);
     i++;
   }
@@ -30,7 +30,7 @@ const LOSS_MAX = (-0.15 * LEVERAGE) / 10;
 const WIN_MAX = (0.08 * LEVERAGE) / 10;
 const BAO_RATIO = -0.95;
 // const BAO_RATIO = LOSS_MAX * 2;
-const CAPITAL_RATIO = 2.5;
+const CAPITAL_RATIO = 3;
 const DEFAULT_POSITION_RATIO_LIST = generatePositionList(1, 10);
 // const DEFAULT_POSITION_RATIO_LIST = [2];
 const MODE_RATIO = {
@@ -51,7 +51,7 @@ const INIT_MOST_LOSS = {
   profit: 0,
   time: null,
 };
-let totalCapital = 8 * CAPITAL_RATIO;
+let totalCapital = 20;
 let totalPosition = 0;
 let minTotalCapital = totalCapital;
 let maxOpenPosition = 0;
