9d2c86c61f6839f3aacdc47852e750ee90f23b5a
pphxiong <1017551718@qq.com>
2021-09-20 23:45:02 +0800

    bug

--- server/app/swapSimulationApi.js
+++ server/app/swapSimulationApi.js
@@ -15,7 +15,7 @@ const generatePositionList = (init, num) => {
   const arr = [init];
   let i = 0;
   while (i < num) {
-    init = Number((init + init * 0.12).toFixed(1));
+    init = Number((init + init * 0.14).toFixed(1));
     arr.push(init);
     i++;
   }
