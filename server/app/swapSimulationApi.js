99c3e678c76c6f78e00213a6998a45f86d821a4f
pphxiong <1017551718@qq.com>
2021-09-22 21:06:58 +0800

    bug

--- server/app/swapSimulationApi.js
+++ server/app/swapSimulationApi.js
@@ -758,38 +758,28 @@ const checkDeal = async (data, isAutoReset = true) => {
       rsiList[rsiList.length - 2].RSI1 < rsiList[rsiList.length - 2].RSI3 &&
       rsiList[rsiList.length - 1].RSI3 < shortCondition;
 
-    const MAIN_OPEN_LONG_CONDITION1 =
-      MAIN_OPEN_LONG_CONDITION ||
-      (Number(macdList[macdList.length - 1].column) > 0 &&
-        rsiList[rsiList.length - 1].RSI1 > rsiList[rsiList.length - 1].RSI2 &&
-        rsiList[rsiList.length - 1].RSI2 > rsiList[rsiList.length - 1].RSI3 &&
-        rsiList[rsiList.length - 1].RSI3 < shortCondition - 10);
+    const MAIN_OPEN_LONG_CONDITION1 = MAIN_OPEN_LONG_CONDITION;
+    // ||
+    // (Number(macdList[macdList.length - 1].column) > 0 &&
+    //   rsiList[rsiList.length - 1].RSI1 > rsiList[rsiList.length - 1].RSI2 &&
+    //   rsiList[rsiList.length - 1].RSI2 > rsiList[rsiList.length - 1].RSI3 &&
+    //   rsiList[rsiList.length - 1].RSI3 < shortCondition - 10);
     //  ||(ifRSIPositiveContinuity && shortRatio > WIN_MAX * 2);
     // || shortRatio < BAO_RATIO;
-    const MAIN_OPEN_SHORT_CONDITION1 =
-      MAIN_OPEN_SHORT_CONDITION ||
-      (Number(macdList[macdList.length - 1].column) < 0 &&
-        rsiList[rsiList.length - 1].RSI1 < rsiList[rsiList.length - 1].RSI2 &&
-        rsiList[rsiList.length - 1].RSI2 < rsiList[rsiList.length - 1].RSI3 &&
-        rsiList[rsiList.length - 1].RSI3 > longCondition + 10);
+    const MAIN_OPEN_SHORT_CONDITION1 = MAIN_OPEN_SHORT_CONDITION;
+    // ||
+    // (Number(macdList[macdList.length - 1].column) < 0 &&
+    //   rsiList[rsiList.length - 1].RSI1 < rsiList[rsiList.length - 1].RSI2 &&
+    //   rsiList[rsiList.length - 1].RSI2 < rsiList[rsiList.length - 1].RSI3 &&
+    //   rsiList[rsiList.length - 1].RSI3 > longCondition + 10);
     //  ||(ifRSINegativeContinuity && longRatio > WIN_MAX * 2);
     // || longRatio < BAO_RATIO;
-    const MAIN_CLOSE_LONG_CONDITION1 =
-      MAIN_OPEN_SHORT_CONDITION1 ||
-      (Number(macdList[macdList.length - 1].column) < 0 &&
-        rsiList[rsiList.length - 1].RSI1 < rsiList[rsiList.length - 1].RSI2 &&
-        rsiList[rsiList.length - 1].RSI2 < rsiList[rsiList.length - 1].RSI3 &&
-        rsiList[rsiList.length - 1].RSI3 > longCondition + 10);
+    const MAIN_CLOSE_LONG_CONDITION1 = MAIN_OPEN_SHORT_CONDITION1;
     // ||(ifMacdNegativeContinuity && longRatio > WIN_MAX);
     // ||
     // rsiList[rsiList.length - 1].RSI3 < shortCondition;
     // rsiList[rsiList.length - 1].RSI3 > LONG_CONDITION; /*|| (longRatio > WIN_MAX * 5 && Number(macdList[macdList.length - 1].column) < 0)*/
-    const MAIN_CLOSE_SHORT_CONDITION1 =
-      MAIN_OPEN_LONG_CONDITION1 ||
-      (Number(macdList[macdList.length - 1].column) > 0 &&
-        rsiList[rsiList.length - 1].RSI1 > rsiList[rsiList.length - 1].RSI2 &&
-        rsiList[rsiList.length - 1].RSI2 > rsiList[rsiList.length - 1].RSI3 &&
-        rsiList[rsiList.length - 1].RSI3 < shortCondition - 10);
+    const MAIN_CLOSE_SHORT_CONDITION1 = MAIN_OPEN_LONG_CONDITION1;
     // ||(ifMacdPositiveContinuity && shortRatio > WIN_MAX);
     // ||
     // rsiList[rsiList.length - 1].RSI3 > longCondition;
