9ad083a2d3c5483c030283dffcee41bd5b88350b
pphxiong <1017551718@qq.com>
2021-09-22 11:21:23 +0800

    bug

--- server/app/swapSimulationApi.js
+++ server/app/swapSimulationApi.js
@@ -751,12 +751,12 @@ const checkDeal = async (data, isAutoReset = true) => {
       Number(macdList[macdList.length - 1].column) > 0 &&
       rsiList[rsiList.length - 1].RSI1 < rsiList[rsiList.length - 1].RSI2 &&
       rsiList[rsiList.length - 2].RSI1 > rsiList[rsiList.length - 2].RSI2 &&
-      rsiList[rsiList.length - 1].RSI3 > longCondition;
+      rsiList[rsiList.length - 1].RSI2 > longCondition;
     const MAIN_OPEN_SHORT_CONDITION =
       Number(macdList[macdList.length - 1].column) < 0 &&
       rsiList[rsiList.length - 1].RSI1 > rsiList[rsiList.length - 1].RSI2 &&
       rsiList[rsiList.length - 2].RSI1 < rsiList[rsiList.length - 2].RSI2 &&
-      rsiList[rsiList.length - 1].RSI3 < shortCondition;
+      rsiList[rsiList.length - 1].RSI2 < shortCondition;
 
     const MAIN_OPEN_LONG_CONDITION1 = MAIN_OPEN_LONG_CONDITION;
     //  ||(ifRSIPositiveContinuity && shortRatio > WIN_MAX * 2);
