8db26b7060904952f89805bb9932337430a83478
pphxiong <1017551718@qq.com>
2021-09-22 10:03:36 +0800

    bug

--- server/app/swapSimulationApi.js
+++ server/app/swapSimulationApi.js
@@ -748,27 +748,17 @@ const checkDeal = async (data, isAutoReset = true) => {
     });
 
     const MAIN_OPEN_LONG_CONDITION =
-      (Number(macdList[macdList.length - 2].column) > 0 &&
-        rsiList[rsiList.length - 2].RSI1 < rsiList[rsiList.length - 2].RSI3 &&
-        rsiList[rsiList.length - 3].RSI1 > rsiList[rsiList.length - 3].RSI3 &&
-        rsiList[rsiList.length - 2].RSI3 > longCondition &&
-        Number(macdList[macdList.length - 1].column) > 0) ||
-      (Number(macdList[macdList.length - 2].column) < 0 &&
+      (Number(macdList[macdList.length - 1].column) > 0 &&
+        rsiList[rsiList.length - 1].RSI1 < rsiList[rsiList.length - 1].RSI3 &&
         rsiList[rsiList.length - 2].RSI1 > rsiList[rsiList.length - 2].RSI3 &&
-        rsiList[rsiList.length - 3].RSI1 < rsiList[rsiList.length - 3].RSI3 &&
-        rsiList[rsiList.length - 2].RSI3 < shortCondition &&
-        Number(macdList[macdList.length - 1].column) > 0);
+        rsiList[rsiList.length - 1].RSI3 > longCondition) ||
+      (shortRatio < 0 && Number(macdList[macdList.length - 1].column) > 0);
     const MAIN_OPEN_SHORT_CONDITION =
-      (Number(macdList[macdList.length - 2].column) < 0 &&
-        rsiList[rsiList.length - 2].RSI1 > rsiList[rsiList.length - 2].RSI3 &&
-        rsiList[rsiList.length - 3].RSI1 < rsiList[rsiList.length - 3].RSI3 &&
-        rsiList[rsiList.length - 2].RSI3 < shortCondition &&
-        Number(macdList[macdList.length - 1].column) < 0) ||
-      (Number(macdList[macdList.length - 2].column) > 0 &&
+      (Number(macdList[macdList.length - 1].column) < 0 &&
+        rsiList[rsiList.length - 1].RSI1 > rsiList[rsiList.length - 1].RSI3 &&
         rsiList[rsiList.length - 2].RSI1 < rsiList[rsiList.length - 2].RSI3 &&
-        rsiList[rsiList.length - 3].RSI1 > rsiList[rsiList.length - 3].RSI3 &&
-        rsiList[rsiList.length - 2].RSI3 > longCondition &&
-        Number(macdList[macdList.length - 1].column) < 0);
+        rsiList[rsiList.length - 1].RSI3 < shortCondition) ||
+      (longRatio < 0 && Number(macdList[macdList.length - 1].column) < 0);
 
     const MAIN_OPEN_LONG_CONDITION1 = MAIN_OPEN_LONG_CONDITION;
     //  ||(ifRSIPositiveContinuity && shortRatio > WIN_MAX * 2);
