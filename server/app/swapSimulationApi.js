cb1b6f58cae3561ecce7ce1789f00382e851e463
dehuaxiong <1017551718@qq.com>
2021-09-26 10:24:41 +0800

    bug

--- server/app/swapSimulationApi.js
+++ server/app/swapSimulationApi.js
@@ -759,7 +759,8 @@ const checkDeal = async (data, isAutoReset = true) => {
       rsiList[rsiList.length - 2].RSI1 < rsiList[rsiList.length - 2].RSI3 &&
       rsiList[rsiList.length - 1].RSI3 < shortCondition;
 
-    const MAIN_OPEN_LONG_CONDITION1 = MAIN_OPEN_LONG_CONDITION;
+    const MAIN_OPEN_LONG_CONDITION1 =
+      MAIN_OPEN_LONG_CONDITION || ifRSIPositiveContinuity;
     // ||
     // (Number(macdList[macdList.length - 1].column) > 0 &&
     //   rsiList[rsiList.length - 1].RSI1 > rsiList[rsiList.length - 1].RSI2 &&
@@ -767,7 +768,8 @@ const checkDeal = async (data, isAutoReset = true) => {
     //   rsiList[rsiList.length - 1].RSI3 < shortCondition - 10);
     //  ||(ifRSIPositiveContinuity && shortRatio > WIN_MAX * 2);
     // || shortRatio < BAO_RATIO;
-    const MAIN_OPEN_SHORT_CONDITION1 = MAIN_OPEN_SHORT_CONDITION;
+    const MAIN_OPEN_SHORT_CONDITION1 =
+      MAIN_OPEN_SHORT_CONDITION || ifRSINegativeContinuity;
     // ||
     // (Number(macdList[macdList.length - 1].column) < 0 &&
     //   rsiList[rsiList.length - 1].RSI1 < rsiList[rsiList.length - 1].RSI2 &&
