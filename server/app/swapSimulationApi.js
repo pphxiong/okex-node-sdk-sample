de0a6fc329ec1c9b04058503374beb5a48ab7759
pphxiong <1017551718@qq.com>
2021-09-26 15:04:47 +0800

    bug

--- server/app/swapSimulationApi.js
+++ server/app/swapSimulationApi.js
@@ -24,7 +24,7 @@ const generatePositionList = (init, num) => {
 
 // const OK_INSTRUMENT_ID = "ETH-USDT-SWAP";
 const BN_SYMBOL = "ETHUSDT";
-const LEVERAGE = 20;
+const LEVERAGE = 10;
 const INTERVAL = "5m";
 const LOSS_MAX = (-0.08 * LEVERAGE) / 10;
 const WIN_MAX = (0.08 * LEVERAGE) / 10;
