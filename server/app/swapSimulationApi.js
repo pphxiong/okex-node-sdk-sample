62348ba9d4e499673488251111cf78a9adaadac6
pphxiong <1017551718@qq.com>
2021-09-29 22:44:48 +0800

    bug

--- server/app/swapSimulationApi.js
+++ server/app/swapSimulationApi.js
@@ -805,11 +805,14 @@ const checkDeal = async (data, isAutoReset = true) => {
     const MAIN_CLOSE_LONG_CONDITION2 =
       MAIN_OPEN_SHORT_CONDITION2 ||
       (rsiList[rsiList.length - 1].RSI1 < rsiList[rsiList.length - 1].RSI3 &&
-        rsiList[rsiList.length - 2].RSI1 > rsiList[rsiList.length - 2].RSI3);
+        rsiList[rsiList.length - 2].RSI1 > rsiList[rsiList.length - 2].RSI3 &&
+        longRatio < 0);
+
     const MAIN_CLOSE_SHORT_CONDITION2 =
       MAIN_OPEN_LONG_CONDITION2 ||
       (rsiList[rsiList.length - 1].RSI1 > rsiList[rsiList.length - 1].RSI3 &&
-        rsiList[rsiList.length - 2].RSI1 < rsiList[rsiList.length - 2].RSI3);
+        rsiList[rsiList.length - 2].RSI1 < rsiList[rsiList.length - 2].RSI3 &&
+        shortRatio < 0);
 
     modeChange = false;
     let openLongCondition =
