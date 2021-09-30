335a63754a7beac46d48e8f94e750388527bc2a0
pphxiong <1017551718@qq.com>
2021-09-30 14:48:30 +0800

    bug

--- server/app/swapSimulationApi.js
+++ server/app/swapSimulationApi.js
@@ -938,10 +938,10 @@ const checkDeal = async (data, isAutoReset = true) => {
         await patchPosition(shortHolding, "SHORT");
         shortPatchNum += 1;
       } else if (longHolding && Number(longHolding.positionAmt)) {
-        if (longRatio < LOSS_MAX * 2 && !isForceDeal) {
+        if (longRatio < LOSS_MAX * 2 && !isForceDeal && false) {
           await patchPosition(longHolding, "LONG");
           longPatchNum += 1;
-        } else if (longRatio > 0) {
+        } else if (longRatio > LOSS_MAX || longRatio < BAO_RATIO) {
           if (longRatio < 0) modeChange = true;
           const currentProfit =
             (longRatio * longHolding.positionAmt) / LEVERAGE -
@@ -989,10 +989,10 @@ const checkDeal = async (data, isAutoReset = true) => {
         await patchPosition(longHolding, "LONG");
         longPatchNum += 1;
       } else if (shortHolding && Number(shortHolding.positionAmt)) {
-        if (shortRatio < LOSS_MAX * 2 && !isForceDeal) {
+        if (shortRatio < LOSS_MAX * 2 && !isForceDeal && false) {
           await patchPosition(shortHolding, "SHORT");
           shortPatchNum += 1;
-        } else if (shortRatio > 0) {
+        } else if (shortRatio > LOSS_MAX || shortRatio < BAO_RATIO) {
           if (shortRatio < 0) modeChange = true;
           const currentProfit =
             (shortRatio * shortHolding.positionAmt) / LEVERAGE -
