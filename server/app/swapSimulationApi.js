a903343a4bf694925469e82fc6f74d7f5ac13df7
pphxiong <1017551718@qq.com>
2021-10-08 17:08:03 +0800

    bug

--- server/app/swapSimulationApi.js
+++ server/app/swapSimulationApi.js
@@ -32,7 +32,7 @@ const LEVERAGE = 20;
 const INTERVAL = "5m";
 const BAO_RATIO = -0.95;
 const LOSS_MAX = (-0.2 * 1 * LEVERAGE) / 10;
-const WIN_MAX = (0.1 * 1 * LEVERAGE) / 10;
+const WIN_MAX = (0.08 * 1 * LEVERAGE) / 10;
 // const BAO_RATIO = LOSS_MAX * 2;
 const CAPITAL_RATIO = 1;
 const DEFAULT_POSITION_RATIO_LIST = generatePositionList(1, 5);
@@ -1086,7 +1086,7 @@ const checkDeal = async (data, isAutoReset = true) => {
           const ratio = shortRatio;
           const increasePosition = INCREASE_FI_LIST[fiIndex + 1];
           const decreasePosition = INCREASE_FI_LIST[0];
-          if (continuous_loss > 3) {
+          if (continuous_loss > 5) {
             openPositionAmt = increasePosition;
           }
           // else if (ratio < LOSS_MAX) {
@@ -1145,7 +1145,7 @@ const checkDeal = async (data, isAutoReset = true) => {
           const ratio = longRatio;
           const increasePosition = INCREASE_FI_LIST[fiIndex + 1];
           const decreasePosition = INCREASE_FI_LIST[0];
-          if (continuous_loss > 3) {
+          if (continuous_loss > 5) {
             openPositionAmt = increasePosition;
           }
           // else if (ratio < LOSS_MAX) {
