aea72aaf5a7adbb46fa5fc0d6c04f01c7f86d64b
pphxiong <1017551718@qq.com>
2021-10-08 09:20:16 +0800

    bug

--- server/app/swapSimulationApi.js
+++ server/app/swapSimulationApi.js
@@ -800,7 +800,7 @@ const checkDeal = async (data, isAutoReset = true) => {
       closeShortCondition ||
       (longRatio == 0 && shortRatio == 0)
     ) {
-      const random = getRandomNumberByRange(0, 1);
+      const random = getRandomNumberByRange(0, 2);
       if (random == 0) {
         openLongCondition = true;
       } else {
