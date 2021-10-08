4de7fca375e0f93136970186eda94b39885716aa
pphxiong <1017551718@qq.com>
2021-10-08 11:11:51 +0800

    bug

--- server/app/swapSimulationApi.js
+++ server/app/swapSimulationApi.js
@@ -30,9 +30,9 @@ function getRandomNumberByRange(start, end) {
 const BN_SYMBOL = "ETHUSDT";
 const LEVERAGE = 20;
 const INTERVAL = "5m";
-const BAO_RATIO = -0.336;
+const BAO_RATIO = -0.168;
 const LOSS_MAX = BAO_RATIO;
-const WIN_MAX = (0.168 * LEVERAGE) / 10;
+const WIN_MAX = (0.336 * LEVERAGE) / 10;
 // const BAO_RATIO = LOSS_MAX * 2;
 const CAPITAL_RATIO = 1;
 const DEFAULT_POSITION_RATIO_LIST = generatePositionList(1, 0);
