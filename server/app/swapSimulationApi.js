5bd4244b609ad4aeb46f564eae171fafb164091f
pphxiong <1017551718@qq.com>
2021-10-09 17:28:06 +0800

    bug

--- server/app/swapSimulationApi.js
+++ server/app/swapSimulationApi.js
@@ -31,7 +31,7 @@ const BN_SYMBOL = "ETHUSDT";
 const LEVERAGE = 20;
 const INTERVAL = "5m";
 const BAO_RATIO = -0.95;
-const LOSS_MAX = (-0.3 * 1 * LEVERAGE) / 10;
+const LOSS_MAX = (-0.2 * 1 * LEVERAGE) / 10;
 const WIN_MAX = (0.2 * 1 * LEVERAGE) / 10;
 // const BAO_RATIO = LOSS_MAX * 2;
 const CAPITAL_RATIO = 1;
@@ -813,8 +813,8 @@ const checkDeal = async (data, isAutoReset = true) => {
     openShortCondition = false;
 
     if (
-      (closeLongCondition && longRatio > WIN_MAX) ||
-      (closeShortCondition && shortRatio > WIN_MAX) ||
+      (closeLongCondition && longRatio < LOSS_MAX) ||
+      (closeShortCondition && shortRatio < LOSS_MAX) ||
       (longRatio == 0 && shortRatio == 0)
     ) {
       // if (longRatio > 0) {
@@ -1032,7 +1032,7 @@ const checkDeal = async (data, isAutoReset = true) => {
       ) {
         await patchPosition(shortHolding, "SHORT");
       } else if (longHolding && Number(longHolding.positionAmt)) {
-        if (longRatio < LOSS_MAX && longPatchNum < 3) {
+        if (longRatio > WIN_MAX && longPatchNum < 3) {
           await patchPosition(longHolding, "LONG");
         } else if (longRatio < LOSS_MAX || longRatio > WIN_MAX) {
           if (longRatio < 0) modeChange = true;
@@ -1082,7 +1082,7 @@ const checkDeal = async (data, isAutoReset = true) => {
       ) {
         await patchPosition(longHolding, "LONG");
       } else if (shortHolding && Number(shortHolding.positionAmt)) {
-        if (shortRatio < LOSS_MAX && shortPatchNum < 3) {
+        if (shortRatio > WIN_MAX && shortPatchNum < 3) {
           await patchPosition(shortHolding, "SHORT");
         } else if (shortRatio < LOSS_MAX || shortRatio > WIN_MAX) {
           if (shortRatio < LOSS_MAX) baoNumTotal++;
