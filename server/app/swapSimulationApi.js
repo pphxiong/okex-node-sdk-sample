9721ae9f7be8a520d9b5972ab4a0f75bef50777f
pphxiong <1017551718@qq.com>
2021-10-11 08:50:33 +0800

    bug

--- server/app/swapSimulationApi.js
+++ server/app/swapSimulationApi.js
@@ -1030,7 +1030,7 @@ const checkDeal = async (data, isAutoReset = true) => {
       ) {
         await patchPosition(shortHolding, "SHORT");
       } else if (longHolding && Number(longHolding.positionAmt)) {
-        if (longPatchNum < 3) {
+        if (longPatchNum < 3 && false) {
           await patchPosition(longHolding, "LONG");
         } else if (longRatio < LOSS_MAX || longRatio > WIN_MAX || true) {
           if (longRatio < 0) modeChange = true;
@@ -1084,7 +1084,7 @@ const checkDeal = async (data, isAutoReset = true) => {
       ) {
         await patchPosition(longHolding, "LONG");
       } else if (shortHolding && Number(shortHolding.positionAmt)) {
-        if (shortPatchNum < 3) {
+        if (shortPatchNum < 3 && false) {
           await patchPosition(shortHolding, "SHORT");
         } else if (shortRatio < LOSS_MAX || shortRatio > WIN_MAX || true) {
           if (shortRatio < LOSS_MAX) {
