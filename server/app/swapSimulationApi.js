12d417176062fd0e33bdfef4e21bfa626f928664
pphxiong <1017551718@qq.com>
2021-10-11 17:21:03 +0800

    bug

--- server/app/swapSimulationApi.js
+++ server/app/swapSimulationApi.js
@@ -31,7 +31,7 @@ const BN_SYMBOL = "ETHUSDT";
 const LEVERAGE = 20;
 const INTERVAL = "5m";
 const BAO_RATIO = -0.95;
-const LOSS_MAX = (-0.1 * 1 * LEVERAGE) / 10;
+const LOSS_MAX = ((-0.1 / 2) * LEVERAGE) / 10;
 const WIN_MAX = (0.1 * 1 * LEVERAGE) / 10;
 // const BAO_RATIO = LOSS_MAX * 2;
 const CAPITAL_RATIO = 1;
@@ -786,12 +786,14 @@ const checkDeal = async (data, isAutoReset = true) => {
 
     const MAIN_CLOSE_LONG_CONDITION1 =
       MAIN_OPEN_SHORT_CONDITION ||
-      (Number(macdList[macdList.length - 1].column) < 0 && longRatio < 0);
+      (Number(macdList[macdList.length - 1].column) < 0 &&
+        longRatio < LOSS_MAX);
     // || longRatio < LOSS_MAX;
 
     const MAIN_CLOSE_SHORT_CONDITION1 =
       MAIN_OPEN_LONG_CONDITION ||
-      (Number(macdList[macdList.length - 1].column) > 0 && shortRatio < 0);
+      (Number(macdList[macdList.length - 1].column) > 0 &&
+        shortRatio < LOSS_MAX);
     // || shortRatio < LOSS_MAX;
 
     if (modeChange) lastMode = lastMode ? 0 : 1;
