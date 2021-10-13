279842adbe85f723dd2c0215c284c7a5933a9e62
pphxiong <1017551718@qq.com>
2021-10-13 08:55:02 +0800

    bug

--- server/app/swapApiBN.js
+++ server/app/swapApiBN.js
@@ -483,8 +483,13 @@ const checkDeal = async (data) => {
     console.log("************************************", currentTime);
     console.log("------------------");
     console.log("mark_price", mark_price);
-    console.log("macdList", macdList.slice(-1));
-    console.log("rsiList", rsiList.slice(-1));
+    console.log("closeShortCondition", closeShortCondition);
+    console.log(
+      "shortHolding",
+      shortHolding && Math.abs(Number(shortHolding.positionAmt))
+    );
+    // console.log("macdList", macdList.slice(-1));
+    // console.log("rsiList", rsiList.slice(-1));
     console.log("------------------");
 
     const patchPosition = async (holding, direction) => {
