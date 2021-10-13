376858ed788605b4bb2c9689d028641271166db6
pphxiong <1017551718@qq.com>
2021-10-13 14:52:31 +0800

    bug

--- server/app/swapSimulationApi.js
+++ server/app/swapSimulationApi.js
@@ -788,18 +788,14 @@ const checkDeal = async (data, isAutoReset = true) => {
     const MAIN_OPEN_SHORT_CONDITION1 = MAIN_OPEN_SHORT_CONDITION;
 
     const MAIN_CLOSE_LONG_CONDITION1 =
-      (Number(macdList[macdList.length - 1].column) <
-        Number(macdList[macdList.length - 2].column) &&
-        rsiList[rsiList.length - 2].RSI3 > longCondition) ||
+      MAIN_OPEN_SHORT_CONDITION ||
       (Number(macdList[macdList.length - 1].column) < 0 &&
         // rsiList[rsiList.length - 2].RSI3 < shortCondition &&
         longRatio < LOSS_MAX);
     // || longRatio < LOSS_MAX;
 
     const MAIN_CLOSE_SHORT_CONDITION1 =
-      (Number(macdList[macdList.length - 1].column) >
-        Number(macdList[macdList.length - 2].column) &&
-        rsiList[rsiList.length - 2].RSI3 < shortCondition) ||
+      MAIN_OPEN_LONG_CONDITION ||
       (Number(macdList[macdList.length - 1].column) > 0 &&
         // rsiList[rsiList.length - 2].RSI3 > longCondition &&
         shortRatio < LOSS_MAX);
