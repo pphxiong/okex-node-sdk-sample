dfe18991a44ed6b106dc7569e56a2d8d1bc76d6e
pphxiong <1017551718@qq.com>
2021-10-14 10:40:54 +0800

    bug

--- server/app/swapSimulationApi.js
+++ server/app/swapSimulationApi.js
@@ -32,7 +32,7 @@ const LEVERAGE = 20;
 const INTERVAL = "5m";
 const BAO_RATIO = -0.95;
 const LOSS_MAX = ((-0.1 / 2) * LEVERAGE) / 10;
-const WIN_MAX = ((0.1 / 4) * LEVERAGE) / 10;
+const WIN_MAX = ((0.1 / 2) * LEVERAGE) / 10;
 // const BAO_RATIO = LOSS_MAX * 2;
 const CAPITAL_RATIO = 1 * 10;
 const DEFAULT_POSITION_RATIO_LIST = generatePositionList(1, 0);
@@ -786,11 +786,11 @@ const checkDeal = async (data, isAutoReset = true) => {
 
     const MAIN_OPEN_SHORT_CONDITION1 = MAIN_OPEN_SHORT_CONDITION;
 
-    const MAIN_CLOSE_LONG_CONDITION1 = MAIN_OPEN_SHORT_CONDITION1;
-    // || longRatio < LOSS_MAX;
+    const MAIN_CLOSE_LONG_CONDITION1 =
+      MAIN_OPEN_SHORT_CONDITION1 || longRatio > WIN_MAX;
 
-    const MAIN_CLOSE_SHORT_CONDITION1 = MAIN_OPEN_LONG_CONDITION1;
-    // || shortRatio < LOSS_MAX;
+    const MAIN_CLOSE_SHORT_CONDITION1 =
+      MAIN_OPEN_LONG_CONDITION1 || shortRatio > WIN_MAX;
 
     if (modeChange) lastMode = lastMode ? 0 : 1;
 
