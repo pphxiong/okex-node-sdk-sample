a948d179fd914fbd5d272e93f9a8ca4840414c11
pphxiong <1017551718@qq.com>
2021-10-14 08:53:52 +0800

    bug

--- server/app/swapSimulationApi.js
+++ server/app/swapSimulationApi.js
@@ -84,8 +84,8 @@ let rsi2 = 12;
 let rsi3 = 24;
 
 const DEFAULT_CONDITION = 48;
-let longCondition = 60;
-let shortCondition = 40;
+let longCondition = 80;
+let shortCondition = 20;
 
 let lastMode = 0;
 
@@ -771,12 +771,14 @@ const checkDeal = async (data, isAutoReset = true) => {
     });
 
     const MAIN_OPEN_LONG_CONDITION =
-      rsiList[rsiList.length - 1].RSI1 > longCondition;
+      rsiList[rsiList.length - 1].RSI1 > longCondition &&
+      rsiList[rsiList.length - 1].RSI3 < DEFAULT_CONDITION;
     // Number(macdList[macdList.length - 1].column) > 0 &&
     // rsiList[rsiList.length - 2].RSI3 < shortCondition;
 
     const MAIN_OPEN_SHORT_CONDITION =
-      rsiList[rsiList.length - 1].RSI1 < shortCondition;
+      rsiList[rsiList.length - 1].RSI1 < shortCondition &&
+      rsiList[rsiList.length - 1].RSI3 > DEFAULT_CONDITION;
     // Number(macdList[macdList.length - 1].column) < 0 &&
     // rsiList[rsiList.length - 2].RSI3 > longCondition;
     // ||
@@ -786,16 +788,10 @@ const checkDeal = async (data, isAutoReset = true) => {
 
     const MAIN_OPEN_SHORT_CONDITION1 = MAIN_OPEN_SHORT_CONDITION;
 
-    const MAIN_CLOSE_LONG_CONDITION1 =
-      MAIN_OPEN_SHORT_CONDITION ||
-      Number(macdList[macdList.length - 1].column) <
-        Number(macdList[macdList.length - 2].column);
+    const MAIN_CLOSE_LONG_CONDITION1 = MAIN_OPEN_SHORT_CONDITION;
     // || longRatio < LOSS_MAX;
 
-    const MAIN_CLOSE_SHORT_CONDITION1 =
-      MAIN_OPEN_LONG_CONDITION ||
-      Number(macdList[macdList.length - 1].column) >
-        Number(macdList[macdList.length - 2].column);
+    const MAIN_CLOSE_SHORT_CONDITION1 = MAIN_OPEN_LONG_CONDITION;
     // || shortRatio < LOSS_MAX;
 
     if (modeChange) lastMode = lastMode ? 0 : 1;
