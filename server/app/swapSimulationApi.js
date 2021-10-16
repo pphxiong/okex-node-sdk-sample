4ef78430723f6b4393f6d7098856b370d009080e
pphxiong <1017551718@qq.com>
2021-10-16 17:09:04 +0800

    新

--- server/app/swapSimulationApi.js
+++ server/app/swapSimulationApi.js
@@ -773,14 +773,15 @@ const checkDeal = async (data, isAutoReset = true) => {
     const MAIN_OPEN_LONG_CONDITION =
       // rsiList[rsiList.length - 1].RSI1 > rsiList[rsiList.length - 2].RSI1 &&
       // rsiList[rsiList.length - 1].RSI1 - rsiList[rsiList.length - 2].RSI1 > 40;
-      Number(macdList[macdList.length - 1].column) > 0 &&
-      rsiList[rsiList.length - 1].RSI2 > longCondition;
+      // Number(macdList[macdList.length - 1].column) > 0 &&
+      rsiList[rsiList.length - 1].RSI1 > rsiList[rsiList.length - 2].RSI1 &&
+      rsiList[rsiList.length - 1].RSI3 > longCondition;
 
     const MAIN_OPEN_SHORT_CONDITION =
       // rsiList[rsiList.length - 1].RSI1 < rsiList[rsiList.length - 2].RSI1 &&
       // rsiList[rsiList.length - 1].RSI1 - rsiList[rsiList.length - 2].RSI1 < -40;
-      Number(macdList[macdList.length - 1].column) < 0 &&
-      rsiList[rsiList.length - 1].RSI2 < shortCondition;
+      rsiList[rsiList.length - 1].RSI1 < rsiList[rsiList.length - 2].RSI1 &&
+      rsiList[rsiList.length - 1].RSI3 < shortCondition;
 
     const MAIN_OPEN_LONG_CONDITION1 = MAIN_OPEN_LONG_CONDITION;
 
