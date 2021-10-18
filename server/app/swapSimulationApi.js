141ec911e9c5871f36c196fd79df074dab003149
pphxiong <1017551718@qq.com>
2021-10-18 17:19:35 +0800

    bug

--- server/app/swapSimulationApi.js
+++ server/app/swapSimulationApi.js
@@ -63,7 +63,7 @@ const INIT_MOST_LOSS = {
   profit: 0,
   time: null,
 };
-let totalCapital = (INIT_POSITION / LEVERAGE) * 5;
+let totalCapital = (INIT_POSITION / LEVERAGE) * 8;
 let totalPosition = 0;
 let minTotalCapital = totalCapital;
 let maxOpenPosition = 0;
