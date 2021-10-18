cf821b158e538b484ec71dfb92e650cca456b4b2
pphxiong <1017551718@qq.com>
2021-10-18 21:20:32 +0800

    新

--- server/app/swapSimulationApi.js
+++ server/app/swapSimulationApi.js
@@ -772,15 +772,15 @@ const checkDeal = async (data, isAutoReset = true) => {
 
     const MAIN_OPEN_LONG_CONDITION =
       Number(macdList[macdList.length - 1].column) > 0 &&
-      rsiList[rsiList.length - 1].RSI1 > rsiList[rsiList.length - 1].RSI3 &&
-      // rsiList[rsiList.length - 2].RSI1 > rsiList[rsiList.length - 2].RSI3 &&
+      rsiList[rsiList.length - 1].RSI1 < rsiList[rsiList.length - 1].RSI3 &&
+      rsiList[rsiList.length - 2].RSI1 > rsiList[rsiList.length - 2].RSI3 &&
       rsiList[rsiList.length - 1].RSI3 > longCondition;
     // ||shortRatio < BAO_RATIO;
 
     const MAIN_OPEN_SHORT_CONDITION =
       Number(macdList[macdList.length - 1].column) < 0 &&
-      rsiList[rsiList.length - 1].RSI1 < rsiList[rsiList.length - 1].RSI3 &&
-      // rsiList[rsiList.length - 2].RSI1 < rsiList[rsiList.length - 2].RSI3 &&
+      rsiList[rsiList.length - 1].RSI1 > rsiList[rsiList.length - 1].RSI3 &&
+      rsiList[rsiList.length - 2].RSI1 < rsiList[rsiList.length - 2].RSI3 &&
       rsiList[rsiList.length - 1].RSI3 < shortCondition;
     // ||longRatio < BAO_RATIO;
 
@@ -1195,7 +1195,7 @@ const checkDeal = async (data, isAutoReset = true) => {
       }
     }
 
-    // INIT_POSITION = (totalCapital * LEVERAGE) / 10;
+    INIT_POSITION = (totalCapital * LEVERAGE) / 10;
 
     //开多仓条件
     if (openLongCondition) {
