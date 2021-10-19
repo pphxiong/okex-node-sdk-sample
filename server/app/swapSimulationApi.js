675994250823d5b7c1d47f83dc580d1a03d15bd1
pphxiong <1017551718@qq.com>
2021-10-19 11:24:51 +0800

    crossenv

--- server/app/swapSimulationApi.js
+++ server/app/swapSimulationApi.js
@@ -1205,13 +1205,13 @@ const checkDeal = async (data, isAutoReset = true) => {
     //   totalCapital = totalCapital - receiveCapital;
     // }
 
-    if (longRatio < LOSS_MAX || shortRatio < LOSS_MAX) {
+    // if (longRatio < LOSS_MAX || shortRatio < LOSS_MAX) {
     //   INIT_POSITION = (totalCapital * LEVERAGE) / 10;
     // } else {
-      INIT_POSITION = Math.min(
-        (totalCapital * LEVERAGE) / 10,
-        ORIGIN_INIT_POSITION * 4
-      );
+    INIT_POSITION = Math.min(
+      (totalCapital * LEVERAGE) / 10,
+      ORIGIN_INIT_POSITION * 4
+    );
     // }
 
     //开多仓条件
