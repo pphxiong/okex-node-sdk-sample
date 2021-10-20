e0af5cb58cb5fed3f41fa0b79e48643b8642e521
pphxiong <1017551718@qq.com>
2021-10-20 13:56:48 +0800

    btc

--- server/app/swapSimulationApi.js
+++ server/app/swapSimulationApi.js
@@ -1212,7 +1212,7 @@ const checkDeal = async (data, isAutoReset = true) => {
 
     INIT_POSITION = Math.min(
       (totalCapital * LEVERAGE) / POSITION_RATIO,
-      ORIGIN_INIT_POSITION * 3
+      ORIGIN_INIT_POSITION * 2
     );
 
     // INIT_POSITION = (totalCapital * LEVERAGE) / POSITION_RATIO;
