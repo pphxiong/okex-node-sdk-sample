d2c32212d98d102267374751f8630d7f605de2c3
pphxiong <1017551718@qq.com>
2021-10-21 22:23:29 +0800

    新

--- server/app/swapSimulationApi.js
+++ server/app/swapSimulationApi.js
@@ -734,8 +734,8 @@ const checkDeal = async (data, isAutoReset = true) => {
         return arr[index].column > arr[index - 1].column;
       });
 
-    const latestMacdList = macdList.slice(-30);
-    const latestRsiList = rsiList.slice(-30);
+    const latestMacdList = macdList.slice(-40);
+    const latestRsiList = rsiList.slice(-40);
     let ifRSIPositiveContinuity = latestMacdList.every((item, index, arr) => {
       return latestRsiList[index].RSI3 > longCondition;
     });
