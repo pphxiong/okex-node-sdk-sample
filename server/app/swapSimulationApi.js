398909fa673c5bdf032719bc76c00b899e1ce75c
pphxiong <1017551718@qq.com>
2021-10-22 10:00:41 +0800

    bug

--- server/app/swapSimulationApi.js
+++ server/app/swapSimulationApi.js
@@ -35,7 +35,7 @@ const LOSS_MAX = ((-0.1 / 2) * LEVERAGE) / 10;
 const WIN_MAX = (0.1 * 4 * LEVERAGE) / 10;
 // const BAO_RATIO = LOSS_MAX * 2;
 const CAPITAL_RATIO = 1;
-const ORIGIN_INIT_POSITION = 10;
+const ORIGIN_INIT_POSITION = 20;
 const DEFAULT_POSITION_RATIO_LIST = generatePositionList(
   ORIGIN_INIT_POSITION,
   0
@@ -67,7 +67,7 @@ const INIT_MOST_LOSS = {
   profit: 0,
   time: null,
 };
-const POSITION_RATIO = 10;
+const POSITION_RATIO = 5;
 const ORIGIN_TOTAL_CAPITAL = (INIT_POSITION / LEVERAGE) * POSITION_RATIO;
 let totalCapital = ORIGIN_TOTAL_CAPITAL;
 let totalPosition = 0;
@@ -1153,7 +1153,7 @@ const checkDeal = async (data, isAutoReset = true) => {
       longRatio > WIN_MAX || shortRatio > WIN_MAX
         ? Math.min(
             (totalCapital * LEVERAGE) / POSITION_RATIO,
-            ORIGIN_INIT_POSITION
+            ORIGIN_INIT_POSITION * 2
           )
         : Math.min(
             (totalCapital * LEVERAGE) / POSITION_RATIO,
