c4bbff1cc7c9feb841dbd65cf788c3ae81aab013
pphxiong <1017551718@qq.com>
2021-10-22 09:51:44 +0800

    bug

--- server/app/swapSimulationApi.js
+++ server/app/swapSimulationApi.js
@@ -1149,18 +1149,12 @@ const checkDeal = async (data, isAutoReset = true) => {
     //   totalCapital = totalCapital - receiveCapital;
     // }
 
-    // INIT_POSITION =
-    //   longRatio > WIN_MAX || shortRatio > WIN_MAX
-    //     ? Math.min(
-    //         (totalCapital * LEVERAGE) / POSITION_RATIO,
-    //         ORIGIN_INIT_POSITION * 2
-    //       )
-    //     : Math.min(
-    //         (totalCapital * LEVERAGE) / POSITION_RATIO,
-    //         ORIGIN_INIT_POSITION * 2
-    //       );
+    INIT_POSITION = Math.min(
+      (totalCapital * LEVERAGE) / POSITION_RATIO,
+      ORIGIN_INIT_POSITION * 3
+    );
 
-    INIT_POSITION = (totalCapital * LEVERAGE) / POSITION_RATIO;
+    // INIT_POSITION = (totalCapital * LEVERAGE) / POSITION_RATIO;
 
     //开多仓条件
     if (openLongCondition) {
