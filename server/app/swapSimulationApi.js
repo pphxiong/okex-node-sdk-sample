5fe355e464df029353d91380fec8dd87d482e359
pphxiong <1017551718@qq.com>
2021-10-26 08:29:42 +0800

    bug

--- server/app/swapSimulationApi.js
+++ server/app/swapSimulationApi.js
@@ -841,8 +841,8 @@ const checkDeal = async (data, isAutoReset = true) => {
       }
     } else if (
       MODE == 2 &&
-      ((ifRSIPositiveContinuity && longRatio < 0) ||
-        (ifRSINegativeContinuity && shortRatio < 0))
+      ((ifRSIPositiveContinuity && longRatio < LOSS_MAX) ||
+        (ifRSINegativeContinuity && shortRatio < LOSS_MAX))
     ) {
       MODE = 1;
       // if (longRatio < 0) {
