ea03b9fe0d6d3c02bfb56b5df81d144e379e5eb3
pphxiong <1017551718@qq.com>
2021-11-17 16:37:10 +0800

    bug

--- server/app/swapApiBN.js
+++ server/app/swapApiBN.js
@@ -153,7 +153,7 @@ function getUUID() {
     // eslint-disable-next-line no-bitwise
     return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
   }
-  return `${S4() + S4()}-${S4()}-${S4()}-${S4()}-${S4()}${S4()}${S4()}`;
+  return `${S4() + S4()}${S4()}${S4()}${S4()}${S4()}${S4()}${S4()}`;
 }
 
 let openOrigClientOrderId = "";
