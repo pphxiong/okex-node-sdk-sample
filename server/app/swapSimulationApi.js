7c15e4274d7a8394d5c1f7a0509862b701a93acc
pphxiong <1017551718@qq.com>
2021-11-18 10:40:24 +0800

    bug

--- server/app/swapSimulationApi.js
+++ server/app/swapSimulationApi.js
@@ -45,7 +45,7 @@ const MODE_RATIO = {
   1: DEFAULT_POSITION_RATIO_LIST,
   2: DEFAULT_POSITION_RATIO_LIST,
 };
-const DEFAULT_MODE = 2;
+const DEFAULT_MODE = 1;
 let MODE = DEFAULT_MODE;
 let MODE2_NUM = 0;
 const INCREASE_FI_LIST = MODE_RATIO[MODE].map((item) =>
@@ -845,9 +845,7 @@ const checkDeal = async (data, isAutoReset = true) => {
     if (MODE == 1) {
       if (
         (closeLongCondition && longRatio > WIN_MAX) ||
-        (closeShortCondition && shortRatio > WIN_MAX) ||
-        (closeLongCondition && longRatio < 0) ||
-        (closeShortCondition && shortRatio < 0)
+        (closeShortCondition && shortRatio > WIN_MAX)
       ) {
         MODE = 2;
         openLongCondition = !openLongCondition;
