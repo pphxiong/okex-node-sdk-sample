88eed93d71d64c6241503fa1a2e53924fd673930
pphxiong <1017551718@qq.com>
2021-11-19 13:32:25 +0800

    bug

--- server/app/swapApiBN.js
+++ server/app/swapApiBN.js
@@ -639,7 +639,7 @@ const checkDeal = async (data) => {
     };
 
     //平多仓条件
-    if (closeLongCondition) {
+    if (closeLongCondition || true) {
       try {
         await closeLongPosition();
       } catch (e) {
