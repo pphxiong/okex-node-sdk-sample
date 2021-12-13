45525865cf6eb4d3c906ab6f888234caa1984ff6
pphxiong <1017551718@qq.com>
2021-12-13 12:18:58 +0800

    bug

--- server/app/swapApiBN.js
+++ server/app/swapApiBN.js
@@ -28,7 +28,7 @@ const INCREASE_FI_LIST = generatePositionList(ORIGIN_INIT_POSITION, 0).map(
   (item) => Number((item * CAPITAL_RATIO).toFixed(1))
 );
 let INIT_POSITION = INCREASE_FI_LIST[0];
-const POSITION_RATIO = 10;
+const POSITION_RATIO = 5;
 let RESTART_TIME = 0;
 
 let MODE = 1;
