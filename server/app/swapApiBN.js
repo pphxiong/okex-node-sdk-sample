9b88458a3d11861998238233bbb62ad93038a482
pphxiong <1017551718@qq.com>
2021-12-14 14:33:24 +0800

    bug

--- server/app/swapApiBN.js
+++ server/app/swapApiBN.js
@@ -156,14 +156,14 @@ const checkDeal = async (data) => {
       Number(macdList[macdList.length - 1].column) > 0 &&
       rsiList[rsiList.length - 1].RSI1 > rsiList[rsiList.length - 1].RSI3 &&
       (rsiList[rsiList.length - 2].RSI3 < LONG_CONDITION ||
-        rsiList[rsiList.length - 1].RSI3 > LONG_CONDITION);
+        rsiList[rsiList.length - 1].RSI3 < LONG_CONDITION);
     // (shortRatio >= 0 || shortRatio <= LOSS_MAX);
 
     const MAIN_OPEN_SHORT_CONDITION =
       Number(macdList[macdList.length - 1].column) < 0 &&
       rsiList[rsiList.length - 1].RSI1 < rsiList[rsiList.length - 1].RSI3 &&
       (rsiList[rsiList.length - 2].RSI3 > SHORT_CONDITION ||
-        rsiList[rsiList.length - 1].RSI3 < SHORT_CONDITION);
+        rsiList[rsiList.length - 1].RSI3 > SHORT_CONDITION);
     // (longRatio >= 0 || longRatio <= LOSS_MAX);
 
     const MAIN_OPEN_LONG_CONDITION1 = MAIN_OPEN_LONG_CONDITION;
