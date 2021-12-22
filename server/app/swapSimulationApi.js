a12cbb63789b7c75ce946ac82eda1470e8fa6ec1
pphxiong <1017551718@qq.com>
2021-12-22 09:36:43 +0800

    bug

--- server/app/swapSimulationApi.js
+++ server/app/swapSimulationApi.js
@@ -91,9 +91,9 @@ let rsi1 = 6;
 let rsi2 = 14;
 let rsi3 = 24;
 
-const DEFAULT_CONDITION = 50;
-let LONG_CONDITION = 80;
-let SHORT_CONDITION = 30;
+const DEFAULT_CONDITION = 47.8;
+let LONG_CONDITION = DEFAULT_CONDITION;
+let SHORT_CONDITION = DEFAULT_CONDITION;
 
 let lastMode = 0;
 
@@ -789,7 +789,8 @@ const checkDeal = async (data, isAutoReset = true) => {
       // Number(macdList[macdList.length - 1].column) > 0 &&
       // Number(macdList[macdList.length - 2].column) < 0;
       rsiList[rsiList.length - 1].RSI2 > rsiList[rsiList.length - 1].RSI3 &&
-      rsiList[rsiList.length - 2].RSI2 < rsiList[rsiList.length - 2].RSI3;
+      rsiList[rsiList.length - 2].RSI2 < rsiList[rsiList.length - 2].RSI3 &&
+      rsiList[rsiList.length - 1].RSI3 > LONG_CONDITION;
     // rsiList[rsiList.length - 2].RSI1 > rsiList[rsiList.length - 2].RSI3 &&
     // rsiList[rsiList.length - 2].RSI1 < LONG_CONDITION;
     // (shortRatio >= 0 || shortRatio <= LOSS_MAX);
@@ -799,7 +800,8 @@ const checkDeal = async (data, isAutoReset = true) => {
       // Number(macdList[macdList.length - 2].column) > 0;
       // Number(macdList[macdList.length - 1].column) < 0 &&
       rsiList[rsiList.length - 1].RSI2 < rsiList[rsiList.length - 1].RSI3 &&
-      rsiList[rsiList.length - 2].RSI2 > rsiList[rsiList.length - 2].RSI3;
+      rsiList[rsiList.length - 2].RSI2 > rsiList[rsiList.length - 2].RSI3 &&
+      rsiList[rsiList.length - 1].RSI3 < SHORT_CONDITION;
     // rsiList[rsiList.length - 2].RSI1 > SHORT_CONDITION;
     // (longRatio >= 0 || longRatio <= LOSS_MAX);
 
