0b6e9144b27377830a8eca8e9e2bc8d985926d0d
pphxiong <1017551718@qq.com>
2021-12-22 14:31:43 +0800

    bug

--- server/app/swapSimulationApi.js
+++ server/app/swapSimulationApi.js
@@ -789,7 +789,7 @@ const checkDeal = async (data, isAutoReset = true) => {
       Number(macdList[macdList.length - 1].column) > 0 &&
       Number(macdList[macdList.length - 2].column) < 0 &&
       // rsiList[rsiList.length - 1].RSI3 > LONG_CONDITION &&
-      rsiList[rsiList.length - 1].RSI3 > LONG_CONDITION;
+      rsiList[rsiList.length - 2].RSI3 < LONG_CONDITION;
     // rsiList[rsiList.length - 2].RSI1 > rsiList[rsiList.length - 2].RSI3 &&
 
     // rsiList[rsiList.length - 2].RSI1 > rsiList[rsiList.length - 2].RSI3 &&
@@ -802,7 +802,7 @@ const checkDeal = async (data, isAutoReset = true) => {
       // Number(macdList[macdList.length - 1].column) < 0 &&
       // rsiList[rsiList.length - 1].RSI3 < rsiList[rsiList.length - 2].RSI3 &&
       // rsiList[rsiList.length - 1].RSI3 < SHORT_CONDITION &&
-      rsiList[rsiList.length - 1].RSI3 < SHORT_CONDITION;
+      rsiList[rsiList.length - 2].RSI3 > SHORT_CONDITION;
     // (longRatio >= 0 || longRatio <= LOSS_MAX);
 
     const MAIN_OPEN_LONG_CONDITION = MAIN_LONG_BASIC_CONDITION;
