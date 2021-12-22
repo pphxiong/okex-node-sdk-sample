5e57f7a8fb12cfad1ce2319b34e418665793ec9a
pphxiong <1017551718@qq.com>
2021-12-22 11:22:35 +0800

    bug

--- server/app/swapSimulationApi.js
+++ server/app/swapSimulationApi.js
@@ -788,9 +788,10 @@ const checkDeal = async (data, isAutoReset = true) => {
     const MAIN_LONG_BASIC_CONDITION =
       // Number(macdList[macdList.length - 1].column) > 0 &&
       // Number(macdList[macdList.length - 2].column) < 0;
-      rsiList[rsiList.length - 1].RSI3 > LONG_CONDITION;
+      rsiList[rsiList.length - 1].RSI3 > LONG_CONDITION &&
+      rsiList[rsiList.length - 2].RSI3 < LONG_CONDITION;
     // rsiList[rsiList.length - 2].RSI1 > rsiList[rsiList.length - 2].RSI3 &&
-    // rsiList[rsiList.length - 1].RSI3 > LONG_CONDITION;
+
     // rsiList[rsiList.length - 2].RSI1 > rsiList[rsiList.length - 2].RSI3 &&
     // rsiList[rsiList.length - 2].RSI1 < LONG_CONDITION;
     // (shortRatio >= 0 || shortRatio <= LOSS_MAX);
@@ -800,8 +801,8 @@ const checkDeal = async (data, isAutoReset = true) => {
       // Number(macdList[macdList.length - 2].column) > 0;
       // Number(macdList[macdList.length - 1].column) < 0 &&
       // rsiList[rsiList.length - 1].RSI3 < rsiList[rsiList.length - 2].RSI3 &&
-      rsiList[rsiList.length - 1].RSI3 < SHORT_CONDITION;
-    // rsiList[rsiList.length - 2].RSI1 > SHORT_CONDITION;
+      rsiList[rsiList.length - 1].RSI3 < SHORT_CONDITION &&
+      rsiList[rsiList.length - 2].RSI3 > SHORT_CONDITION;
     // (longRatio >= 0 || longRatio <= LOSS_MAX);
 
     const MAIN_OPEN_LONG_CONDITION = MAIN_LONG_BASIC_CONDITION;
