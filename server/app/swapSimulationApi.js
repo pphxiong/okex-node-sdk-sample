ccc9b5e8afbcfa87a11e21c731aae02fdc47429f
pphxiong <1017551718@qq.com>
2021-12-22 09:05:55 +0800

    bug

--- server/app/swapSimulationApi.js
+++ server/app/swapSimulationApi.js
@@ -786,17 +786,20 @@ const checkDeal = async (data, isAutoReset = true) => {
     // });
 
     const MAIN_LONG_BASIC_CONDITION =
-      // Number(macdList[macdList.length - 1].column) > 0;
-      rsiList[rsiList.length - 1].RSI3 > SHORT_CONDITION &&
-      rsiList[rsiList.length - 2].RSI3 < SHORT_CONDITION;
+      Number(macdList[macdList.length - 1].column) > 0 &&
+      Number(macdList[macdList.length - 2].column) < 0;
+    // rsiList[rsiList.length - 1].RSI3 > SHORT_CONDITION &&
+    // rsiList[rsiList.length - 2].RSI3 < SHORT_CONDITION;
     // rsiList[rsiList.length - 2].RSI1 > rsiList[rsiList.length - 2].RSI3 &&
     // rsiList[rsiList.length - 2].RSI1 < LONG_CONDITION;
     // (shortRatio >= 0 || shortRatio <= LOSS_MAX);
 
     const MAIN_SHORT_BASIC_CONDITION =
-      // Number(macdList[macdList.length - 1].column) < 0 &&
-      rsiList[rsiList.length - 1].RSI3 < LONG_CONDITION &&
-      rsiList[rsiList.length - 2].RSI3 > LONG_CONDITION;
+      Number(macdList[macdList.length - 1].column) < 0 &&
+      Number(macdList[macdList.length - 2].column) > 0;
+    // Number(macdList[macdList.length - 1].column) < 0 &&
+    // rsiList[rsiList.length - 1].RSI3 < LONG_CONDITION &&
+    // rsiList[rsiList.length - 2].RSI3 > LONG_CONDITION;
     // rsiList[rsiList.length - 2].RSI1 > SHORT_CONDITION;
     // (longRatio >= 0 || longRatio <= LOSS_MAX);
 
