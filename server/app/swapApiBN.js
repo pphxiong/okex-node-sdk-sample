da896135620fa89f4d8bfb58654dd9331e7d0649
pphxiong <1017551718@qq.com>
2022-01-06 18:12:18 +0800

    bug

--- server/app/swapApiBN.js
+++ server/app/swapApiBN.js
@@ -830,7 +830,7 @@ const countdownCancelAll = async (time) => {
 };
 
 const startInterval = async () => {
-  const params = { symbol: BN_SYMBOL, limit: 30, period: "30m" };
+  const params = { symbol: BN_SYMBOL, limit: 30, period: "1d" };
   const accountResult = await cAuthClientBN.swap.topLongShortAccountRatio(
     params
   );
