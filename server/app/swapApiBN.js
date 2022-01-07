f7a778ac801645e5215ea502e44efa2b25402e51
pphxiong <1017551718@qq.com>
2022-01-07 12:39:28 +0800

    bug

--- server/app/swapApiBN.js
+++ server/app/swapApiBN.js
@@ -899,10 +899,8 @@ const startInterval = async () => {
   const newResult = [];
   accountResult.reduce((pre, cur, index) => {
     const obj = {
-      current: {
-        account: cur,
-        position: positionResult[index],
-      },
+      originAccount: cur,
+      originPosition: positionResult[index],
       account: cur.longShortRatio / pre.longShortRatio,
       position:
         positionResult[index].longShortRatio /
