8cd8bc0270c49525e7ebb72ea4fcb319e3f328d7
pphxiong <1017551718@qq.com>
2022-01-08 14:18:57 +0800

    bug

--- server/app/swapApiBN.js
+++ server/app/swapApiBN.js
@@ -872,96 +872,96 @@ const dealOrderHandler = async () => {
       (item) => item.positionAmt && Math.abs(Number(item.positionAmt)) > 0
     );
 
-    // const pList = [];
-    // holdings.forEach((holding) => {
-    //   const { leverage, entryPrice, positionAmt, positionSide } = holding;
-    //   if (positionAmt && Math.abs(Number(positionAmt)) > 0) {
-    //     let positionRatio =
-    //       ((Number(mark_price) - Number(entryPrice)) * Number(leverage)) /
-    //       Number(mark_price);
+    const pList = [];
+    holdings.forEach((holding) => {
+      const { leverage, entryPrice, positionAmt, positionSide } = holding;
+      if (positionAmt && Math.abs(Number(positionAmt)) > 0) {
+        let positionRatio =
+          ((Number(mark_price) - Number(entryPrice)) * Number(leverage)) /
+          Number(mark_price);
 
-    //     positionRatio = positionSide == "LONG" ? positionRatio : -positionRatio;
+        positionRatio = positionSide == "LONG" ? positionRatio : -positionRatio;
 
-    //     if (positionRatio > 0) {
-    //       const closePayload = {
-    //         position: Math.abs(Number(positionAmt)),
-    //         positionSide,
-    //         mark_price,
-    //         time: moment().format("YYYY-MM-DD HH:mm:ss"),
-    //       };
-    //       pList.push(closePosition(closePayload, true));
-    //       // await closePosition(closePayload, true);
-    //     }
-    //   }
-    // });
+        if (positionRatio > 0) {
+          const closePayload = {
+            position: Math.abs(Number(positionAmt)),
+            positionSide,
+            mark_price,
+            time: moment().format("YYYY-MM-DD HH:mm:ss"),
+          };
+          pList.push(closePosition(closePayload, true));
+          // await closePosition(closePayload, true);
+        }
+      }
+    });
 
     // if (pList.length) await Promise.all(pList);
 
-    const params = { symbol: BN_SYMBOL, limit: 10, period: "2h" };
-    const accountResult = await cAuthClientBN.swap.topLongShortAccountRatio(
-      params
-    );
-    const positionResult = await cAuthClientBN.swap.topLongShortPositionRatio(
-      params
-    );
-    const newResult = [];
-    accountResult.reduce((pre, cur, index) => {
-      const obj = {
-        originAccount: cur,
-        originPosition: positionResult[index],
-        account: cur.longShortRatio / pre.longShortRatio,
-        accountLongShortRatioChange: cur.longShortRatio - pre.longShortRatio,
-        positionLongShortRatioChange:
-          positionResult[index].longShortRatio -
-          positionResult[index - 1].longShortRatio,
-        position:
-          positionResult[index].longShortRatio /
-          positionResult[index - 1].longShortRatio,
-        ratio:
-          positionResult[index].longShortRatio /
-          positionResult[index - 1].longShortRatio /
-          (cur.longShortRatio / pre.longShortRatio),
-        timestamp: moment(cur.timestamp).format("YYYY-MM-DD HH:mm:ss"),
-      };
-      if (obj.positionLongShortRatioChange > obj.accountLongShortRatioChange) {
-        obj.long = true;
-      } else if (
-        obj.positionLongShortRatioChange < obj.accountLongShortRatioChange
-      ) {
-        obj.short = true;
-      }
-      newResult.push(obj);
-      return cur;
-    });
+    // const params = { symbol: BN_SYMBOL, limit: 10, period: "2h" };
+    // const accountResult = await cAuthClientBN.swap.topLongShortAccountRatio(
+    //   params
+    // );
+    // const positionResult = await cAuthClientBN.swap.topLongShortPositionRatio(
+    //   params
+    // );
+    // const newResult = [];
+    // accountResult.reduce((pre, cur, index) => {
+    //   const obj = {
+    //     originAccount: cur,
+    //     originPosition: positionResult[index],
+    //     account: cur.longShortRatio / pre.longShortRatio,
+    //     accountLongShortRatioChange: cur.longShortRatio - pre.longShortRatio,
+    //     positionLongShortRatioChange:
+    //       positionResult[index].longShortRatio -
+    //       positionResult[index - 1].longShortRatio,
+    //     position:
+    //       positionResult[index].longShortRatio /
+    //       positionResult[index - 1].longShortRatio,
+    //     ratio:
+    //       positionResult[index].longShortRatio /
+    //       positionResult[index - 1].longShortRatio /
+    //       (cur.longShortRatio / pre.longShortRatio),
+    //     timestamp: moment(cur.timestamp).format("YYYY-MM-DD HH:mm:ss"),
+    //   };
+    //   if (obj.positionLongShortRatioChange > obj.accountLongShortRatioChange) {
+    //     obj.long = true;
+    //   } else if (
+    //     obj.positionLongShortRatioChange < obj.accountLongShortRatioChange
+    //   ) {
+    //     obj.short = true;
+    //   }
+    //   newResult.push(obj);
+    //   return cur;
+    // });
 
-    const latestResult = newResult[newResult.length - 1];
-    const ratio = Number(latestResult.ratio);
-    const positionSide = ratio >= 1 ? "LONG" : "SHORT";
+    // const latestResult = newResult[newResult.length - 1];
+    // const ratio = Number(latestResult.ratio);
+    // const positionSide = ratio >= 1 ? "LONG" : "SHORT";
 
-    const availPosition = (
-      (Number(availableBalance) * LEVERAGE) /
-      mark_price /
-      POSITION_RATIO
-    ).toFixed(3);
+    // const availPosition = (
+    //   (Number(availableBalance) * LEVERAGE) /
+    //   mark_price /
+    //   POSITION_RATIO
+    // ).toFixed(3);
 
-    INIT_POSITION = Number(availPosition);
-    const position = INIT_POSITION;
-    // let position;
-    // if (ratio >= 1) {
-    //   position = (INIT_POSITION * ratio).toFixed(3);
-    // } else {
-    //   position = (INIT_POSITION * (2 - ratio)).toFixed(3);
-    // }
+    // INIT_POSITION = Number(availPosition);
+    // const position = INIT_POSITION;
+    // // let position;
+    // // if (ratio >= 1) {
+    // //   position = (INIT_POSITION * ratio).toFixed(3);
+    // // } else {
+    // //   position = (INIT_POSITION * (2 - ratio)).toFixed(3);
+    // // }
 
-    const openPayload = {
-      position: Number(position),
-      positionSide,
-      mark_price,
-      time: moment().format("YYYY-MM-DD HH:mm:ss"),
-    };
-    await openPosition(openPayload, true);
-    console.log("pList::", pList);
-    console.log("accountAndPosition::", latestResult);
+    // const openPayload = {
+    //   position: Number(position),
+    //   positionSide,
+    //   mark_price,
+    //   time: moment().format("YYYY-MM-DD HH:mm:ss"),
+    // };
+    // await openPosition(openPayload, true);
+    // console.log("pList::", pList);
+    // console.log("accountAndPosition::", latestResult);
   } catch (e) {
     restart("dealOrder...");
   }
