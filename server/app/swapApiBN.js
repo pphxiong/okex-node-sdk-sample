ec0b5f7c47b974b3bc99bb8eab93ba50b97c19a0
pphxiong <1017551718@qq.com>
2022-01-11 10:49:08 +0800

    new

--- server/app/swapApiBN.js
+++ server/app/swapApiBN.js
@@ -811,7 +811,7 @@ const countdownCancelAll = async (time) => {
 };
 
 const fnConsoleDealOrder = async () => {
-  const params = { symbol: BN_SYMBOL, limit: 50, period: "30m" };
+  const params = { symbol: BN_SYMBOL, limit: 30, period: "30m" };
   const accountResult = await cAuthClientBN.swap.topLongShortAccountRatio(
     params
   );
@@ -848,8 +848,8 @@ const fnConsoleDealOrder = async () => {
   });
   console.log(
     "newResult::",
-    // newResult
-    newResult.filter((item) => item.long || item.short)
+    newResult
+    // newResult.filter((item) => item.long || item.short)
   );
 };
 
