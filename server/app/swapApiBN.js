cee5bd27d22e00be54f6b1fedc7c2d849350f19f
pphxiong <1017551718@qq.com>
2022-01-13 09:33:12 +0800

    bug

--- server/app/swapApiBN.js
+++ server/app/swapApiBN.js
@@ -817,7 +817,7 @@ const fnGetAverage = (arr, timestamp) => {
     // console.log(i); // 0~29
     // console.log(arr[i]); //arr[0]~arr[29]
     // 把数组中的所有数字累加
-    sum += arr[i];
+    sum += arr[i].longShortRatio;
     if ((i + 1) % 6 == 0) {
       // 已经加够6个数字，要计算平均值，并且放到新数组
       newArr.push(sum / 6); // 计算出的平均值放到新数组中
@@ -836,11 +836,11 @@ const fnConsoleDealOrder = async () => {
     params
   );
   accountResult = fnGetAverage(
-    accountResult.map((item) => Number(item.longShortRatio)),
+    accountResult,
     accountResult[accountResult.length - 1].timestamp
   );
   positionResult = fnGetAverage(
-    positionResult.map((item) => Number(item.longShortRatio)),
+    accountResult,
     positionResult[positionResult.length - 1].timestamp
   );
   const newResult = [];
@@ -892,11 +892,11 @@ const dealOrderHandler = async () => {
       params
     );
     accountResult = fnGetAverage(
-      accountResult.map((item) => Number(item.longShortRatio)),
+      accountResult,
       accountResult[accountResult.length - 1].timestamp
     );
     positionResult = fnGetAverage(
-      positionResult.map((item) => Number(item.longShortRatio)),
+      positionResult,
       positionResult[positionResult.length - 1].timestamp
     );
     const newResult = [];
