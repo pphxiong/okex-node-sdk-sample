e37ffdce3409df48bb4106bf381761d541b020f4
pphxiong <1017551718@qq.com>
2022-01-18 19:02:37 +0800

    bug

--- server/app/swapApiBN.js
+++ server/app/swapApiBN.js
@@ -351,16 +351,16 @@ const dealOrderHandler = async () => {
 
       const { availableBalance } = await cAuthClientBN.swap.getPosition();
 
-      // const availPosition = (
-      //   (Number(availableBalance) * LEVERAGE) /
-      //   mark_price /
-      //   POSITION_RATIO
-      // ).toFixed(3);
+      const availPosition = (
+        (Number(availableBalance) * LEVERAGE) /
+        mark_price /
+        POSITION_RATIO
+      ).toFixed(3);
 
       // INIT_POSITION = Number(availPosition);
       const position = INIT_POSITION;
 
-      if (Number(position)) {
+      if (Number(position) && Number(availPosition) > Number(position)) {
         const openPayload = {
           position: Number(position),
           positionSide,
