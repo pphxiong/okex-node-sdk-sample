3daa99767f91e0ee003b58925231565ecfe10e37
pphxiong <1017551718@qq.com>
2022-03-27 00:37:26 +0800

    boll

--- server/app/swapSimulationApi.js
+++ server/app/swapSimulationApi.js
@@ -724,7 +724,7 @@ const checkDeal = async (data, isAutoReset = true) => {
     isForceDeal = false;
     const {macdList, rsiList, bollList} = data;
 
-    console.log(bollList[bollList.length - 1]);
+    // console.log(bollList[bollList.length - 1]);
 
     macdList.slice(-3);
     const mark_price = macdList[macdList.length - 1].close;
@@ -1045,7 +1045,6 @@ const checkDeal = async (data, isAutoReset = true) => {
     // }
 
     const patchPosition = async (holding, direction) => {
-      console.log('patchPosition', holding);
       let positionAmt = Number(holding.positionAmt) * 2;
       const price =
         (Number(mark_price) * Number(holding.positionAmt) +
@@ -1385,91 +1384,6 @@ const checkDeal = async (data, isAutoReset = true) => {
     }
   }
 };
-
-app.post('/swap/startHearBeat', async (req, response) => {
-  console.log(req.query);
-  send(response, {errcode: 0, errmsg: 'ok', data: {}});
-  return;
-  //1.通过判断url路径和请求方式来判断是否是表单提交
-  if (req.url === '/swap/startHearBeat' && req.method === 'POST') {
-    //创建空字符叠加数据片段
-    let data = '';
-
-    //2.注册data事件接收数据（每当收到一段表单提交的数据，该方法会执行一次）
-    req.on('data', function (chunk) {
-      // chunk 默认是一个二进制数据，和 data 拼接会自动 toString
-      data += chunk;
-    });
-
-    // 3.当接收表单提交的数据完毕之后，就可以进一步处理了
-    //注册end事件，所有数据接收完成会执行一次该方法
-    req.on('end', async () => {
-      const dataObject = querystring.parse(data);
-
-      const {
-        time,
-        date,
-        interval = INTERVAL,
-        limit = 1500,
-        isAutoReset = true,
-        initList = [],
-      } = dataObject;
-      try {
-        dealDetailList = [];
-        mostLoss = INIT_MOST_LOSS;
-
-        if (isAutoReset) {
-          totalProfit = 0;
-          maxWinRatio = 0;
-          currentPosition = {};
-          longPosition = {};
-          shortPosition = {};
-        }
-
-        // const mock = require(`./mock/${date}.js`);
-        // const list = mock.mockData
-
-        const payload = {
-          interval,
-          limit,
-          startTime: time,
-        };
-        const data = await cAuthClientBN.common.getHistory(BN_SYMBOL, payload);
-        const list = data;
-
-        const newList = JSON.parse(JSON.stringify(initList.concat(list)));
-
-        const macdList = getCurrentMacd(newList, lastMacd);
-        const rsiList = getCurrentRSI(newList, lastRSI);
-
-        const result = {
-          macdList,
-          rsiList,
-        };
-        // lastMacd = macdList[macdList.length-1]
-        // lastRSI = rsiList[rsiList.length-1]
-
-        await checkDeal(result, isAutoReset);
-        send(response, {
-          errcode: 0,
-          errmsg: 'ok',
-          data: {
-            // history: list,
-            // index: result,
-            totalProfit,
-            currentPosition,
-            dealDetailList,
-            mostLoss,
-            initList: list.slice(-200),
-          },
-        });
-      } catch (e) {
-        console.log(e);
-        restart('startHearBeat');
-      }
-    });
-  }
-});
 
 const readData = async () => {
   let dataConfig = JSON.parse(
