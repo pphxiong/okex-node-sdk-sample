706f1447f741e9b0001ccda5a62dac3042998f30
pphxiong <1017551718@qq.com>
2022-03-27 16:42:31 +0800

    boll

--- server/app/swapSimulationApi.js
+++ server/app/swapSimulationApi.js
@@ -778,15 +778,17 @@ const checkDeal = async (data, isAutoReset = true) => {
 
     const MAIN_LONG_BASIC_CONDITION =
       Number(macdList[macdList.length - 2].close) <
-        Number(bollList[bollList.length - 2].MA) &&
-      Number(macdList[macdList.length - 1].close) >
-        Number(bollList[bollList.length - 1].MA);
+      Number(bollList[bollList.length - 2].DN);
+    // &&
+    // Number(macdList[macdList.length - 1].close) >
+    //   Number(bollList[bollList.length - 1].MA);
 
     const MAIN_SHORT_BASIC_CONDITION =
       Number(macdList[macdList.length - 2].close) >
-        Number(bollList[bollList.length - 2].MA) &&
-      Number(macdList[macdList.length - 1].close) <
-        Number(bollList[bollList.length - 1].MA);
+      Number(bollList[bollList.length - 2].UP);
+    // &&
+    // Number(macdList[macdList.length - 1].close) <
+    //   Number(bollList[bollList.length - 1].MA);
 
     const MAIN_OPEN_LONG_CONDITION = MAIN_LONG_BASIC_CONDITION;
 
