5a162870b58b0008062813187ab82dc84141a599
pphxiong <1017551718@qq.com>
2022-03-27 16:35:50 +0800

    boll

--- server/app/swapSimulationApi.js
+++ server/app/swapSimulationApi.js
@@ -725,7 +725,7 @@ const checkDeal = async (data, isAutoReset = true) => {
       {
         macdList: data.macdList.slice(i, i + 10),
         rsiList: data.rsiList.slice(i, i + 10),
-        bollList: data.bollList.slice(i, i + 10),
+        bollList: data.bollList,
       },
       isAutoReset
       // && i == data.macdList.length - 10
