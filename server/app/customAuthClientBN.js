ec8bfc2d4316aaa77cac3ff89624906fcbaaf845
pphxiong <1017551718@qq.com>
2022-03-28 19:04:20 +0800

    bug

--- server/app/customAuthClientBN.js
+++ server/app/customAuthClientBN.js
@@ -145,7 +145,7 @@ function customAuthClient(
         return commonGet(`/fapi/v1/premiumIndex?symbol=${symbol}`);
       },
       getHistory: function (symbol, params) {
-        return commonGet(
+        return getWithSign(
           `/fapi/v1/klines?symbol=${symbol}&` + querystring.stringify(params)
         );
       },
