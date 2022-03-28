a1625700b9b3a4bdeacdb88246416a39232e849d
pphxiong <1017551718@qq.com>
2022-03-28 10:55:41 +0800

    bug

--- server/app/swapSimulationApi.js
+++ server/app/swapSimulationApi.js
@@ -779,13 +779,13 @@ const checkDeal = async (data, isAutoReset = true) => {
     const latestRsiList = rsiList.slice(-6);
 
     const MAIN_LONG_BASIC_CONDITION =
-      Number(macdList[macdList.length - 2].low) <
+      Number(macdList[macdList.length - 2].close) <
         Number(bollList[bollList.length - 2].DN) &&
       Number(macdList[macdList.length - 1].close) >
         Number(bollList[bollList.length - 1].DN);
 
     const MAIN_SHORT_BASIC_CONDITION =
-      Number(macdList[macdList.length - 2].high) >
+      Number(macdList[macdList.length - 2].close) >
         Number(bollList[bollList.length - 2].UP) &&
       Number(macdList[macdList.length - 1].close) <
         Number(bollList[bollList.length - 1].UP);
@@ -799,10 +799,18 @@ const checkDeal = async (data, isAutoReset = true) => {
     const MAIN_OPEN_SHORT_CONDITION1 = MAIN_OPEN_SHORT_CONDITION;
 
     const MAIN_CLOSE_LONG_CONDITION1 =
-      MAIN_OPEN_SHORT_CONDITION1; /* && longRatio > 0 */
+      MAIN_OPEN_SHORT_CONDITION1 ||
+      (Number(macdList[macdList.length - 2].close) >
+        Number(bollList[bollList.length - 2].DN) &&
+        Number(macdList[macdList.length - 1].close) <
+          Number(bollList[bollList.length - 1].DN)); /* && longRatio > 0 */
 
     const MAIN_CLOSE_SHORT_CONDITION1 =
-      MAIN_OPEN_LONG_CONDITION1; /* && shortRatio > 0 */
+      MAIN_OPEN_LONG_CONDITION1 ||
+      (Number(macdList[macdList.length - 2].close) <
+        Number(bollList[bollList.length - 2].UP) &&
+        Number(macdList[macdList.length - 1].close) >
+          Number(bollList[bollList.length - 1].UP)); /* && shortRatio > 0 */
 
     if (modeChange) lastMode = lastMode ? 0 : 1;
 
