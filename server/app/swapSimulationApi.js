5ad58425c49927d06c5d510a3e43c38c9c396b37
pphxiong <1017551718@qq.com>
2022-03-28 09:36:28 +0800

    bug

--- server/app/swapSimulationApi.js
+++ server/app/swapSimulationApi.js
@@ -780,7 +780,7 @@ const checkDeal = async (data, isAutoReset = true) => {
     const latestRsiList = rsiList.slice(-6);
 
     const MAIN_LONG_BASIC_CONDITION =
-      Number(macdList[macdList.length - 2].low) <
+      Number(macdList[macdList.length - 2].close) <
         Number(bollList[bollList.length - 2].DN) &&
       Number(macdList[macdList.length - 1].close) >
         Number(bollList[bollList.length - 1].DN);
@@ -789,7 +789,7 @@ const checkDeal = async (data, isAutoReset = true) => {
     //   Number(bollList[bollList.length - 1].MA);
 
     const MAIN_SHORT_BASIC_CONDITION =
-      Number(macdList[macdList.length - 2].high) >
+      Number(macdList[macdList.length - 2].close) >
         Number(bollList[bollList.length - 2].UP) &&
       Number(macdList[macdList.length - 1].close) <
         Number(bollList[bollList.length - 1].UP);
