f2caa434b9a427ca31251d6712d8117cbf666a07
pphxiong <1017551718@qq.com>
2022-03-28 11:07:42 +0800

    bug

--- server/app/swapSimulationApi.js
+++ server/app/swapSimulationApi.js
@@ -779,16 +779,24 @@ const checkDeal = async (data, isAutoReset = true) => {
     const latestRsiList = rsiList.slice(-6);
 
     const MAIN_LONG_BASIC_CONDITION =
-      Number(macdList[macdList.length - 2].close) <
+      (Number(macdList[macdList.length - 2].close) <
         Number(bollList[bollList.length - 2].DN) &&
-      Number(macdList[macdList.length - 1].close) >
-        Number(bollList[bollList.length - 1].DN);
+        Number(macdList[macdList.length - 1].close) >
+          Number(bollList[bollList.length - 1].DN)) ||
+      (Number(macdList[macdList.length - 2].close) <
+        Number(bollList[bollList.length - 2].UP) &&
+        Number(macdList[macdList.length - 1].close) >
+          Number(bollList[bollList.length - 1].UP));
 
     const MAIN_SHORT_BASIC_CONDITION =
-      Number(macdList[macdList.length - 2].close) >
+      (Number(macdList[macdList.length - 2].close) >
         Number(bollList[bollList.length - 2].UP) &&
-      Number(macdList[macdList.length - 1].close) <
-        Number(bollList[bollList.length - 1].UP);
+        Number(macdList[macdList.length - 1].close) <
+          Number(bollList[bollList.length - 1].UP)) ||
+      (Number(macdList[macdList.length - 2].close) >
+        Number(bollList[bollList.length - 2].DN) &&
+        Number(macdList[macdList.length - 1].close) <
+          Number(bollList[bollList.length - 1].DN));
 
     const MAIN_OPEN_LONG_CONDITION = MAIN_LONG_BASIC_CONDITION;
 
@@ -799,18 +807,10 @@ const checkDeal = async (data, isAutoReset = true) => {
     const MAIN_OPEN_SHORT_CONDITION1 = MAIN_OPEN_SHORT_CONDITION;
 
     const MAIN_CLOSE_LONG_CONDITION1 =
-      MAIN_OPEN_SHORT_CONDITION1 ||
-      (Number(macdList[macdList.length - 2].close) >
-        Number(bollList[bollList.length - 2].DN) &&
-        Number(macdList[macdList.length - 1].close) <
-          Number(bollList[bollList.length - 1].DN)); /* && longRatio > 0 */
+      MAIN_OPEN_SHORT_CONDITION1; /* && longRatio > 0 */
 
     const MAIN_CLOSE_SHORT_CONDITION1 =
-      MAIN_OPEN_LONG_CONDITION1 ||
-      (Number(macdList[macdList.length - 2].close) <
-        Number(bollList[bollList.length - 2].UP) &&
-        Number(macdList[macdList.length - 1].close) >
-          Number(bollList[bollList.length - 1].UP)); /* && shortRatio > 0 */
+      MAIN_OPEN_LONG_CONDITION1; /* && shortRatio > 0 */
 
     if (modeChange) lastMode = lastMode ? 0 : 1;
 
