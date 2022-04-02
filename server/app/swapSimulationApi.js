6b73116d3d9983a3c73d81ca37b62090929af31a
pphxiong <1017551718@qq.com>
2022-04-02 14:49:07 +0800

    bug

--- server/app/swapSimulationApi.js
+++ server/app/swapSimulationApi.js
@@ -784,16 +784,18 @@ const checkDeal = async (data, isAutoReset = true) => {
     }
 
     const MAIN_LONG_BASIC_CONDITION =
-      Number(macdList[macdList.length - 2].low) <
+      Number(macdList[macdList.length - 2].close) <
         Number(bollList[bollList.length - 2].DN) &&
       Number(macdList[macdList.length - 1].close) >
-        Number(bollList[bollList.length - 1].DN);
+        Number(bollList[bollList.length - 1].DN) &&
+      longRatio >= 0;
 
     const MAIN_SHORT_BASIC_CONDITION =
-      Number(macdList[macdList.length - 2].high) >
+      Number(macdList[macdList.length - 2].close) >
         Number(bollList[bollList.length - 2].UP) &&
       Number(macdList[macdList.length - 1].close) <
-        Number(bollList[bollList.length - 1].UP);
+        Number(bollList[bollList.length - 1].UP) &&
+      shortRatio >= 0;
 
     const MAIN_OPEN_LONG_CONDITION = MAIN_LONG_BASIC_CONDITION;
 
@@ -922,7 +924,7 @@ const checkDeal = async (data, isAutoReset = true) => {
 
           let closePositionAmt = longHolding.positionAmt;
           if (longRatio < 0)
-            closePositionAmt = Math.min(INIT_POSITION * 3, closePositionAmt);
+            closePositionAmt = Math.min(INIT_POSITION * 1, closePositionAmt);
           const currentProfit =
             (longRatio * closePositionAmt) / LEVERAGE -
             0.018 * 0.01 * closePositionAmt;
@@ -991,7 +993,7 @@ const checkDeal = async (data, isAutoReset = true) => {
           if (shortRatio < 0) modeChange = true;
           let closePositionAmt = shortHolding.positionAmt;
           if (shortRatio < 0)
-            closePositionAmt = Math.min(INIT_POSITION * 3, closePositionAmt);
+            closePositionAmt = Math.min(INIT_POSITION * 1, closePositionAmt);
           const currentProfit =
             (shortRatio * closePositionAmt) / LEVERAGE -
             0.018 * 0.01 * closePositionAmt;
