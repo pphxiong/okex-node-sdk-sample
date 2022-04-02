e07d8c500f6440b843428dcbcea0ecd673e89e40
pphxiong <1017551718@qq.com>
2022-04-02 11:06:06 +0800

    bug

--- server/app/swapSimulationApi.js
+++ server/app/swapSimulationApi.js
@@ -922,7 +922,7 @@ const checkDeal = async (data, isAutoReset = true) => {
 
           let closePositionAmt = longHolding.positionAmt;
           if (longRatio < 0)
-            closePositionAmt = Math.min(INIT_POSITION * 2, closePositionAmt);
+            closePositionAmt = Math.min(INIT_POSITION * 1, closePositionAmt);
           const currentProfit =
             (longRatio * closePositionAmt) / LEVERAGE -
             0.018 * 0.01 * closePositionAmt;
@@ -991,7 +991,7 @@ const checkDeal = async (data, isAutoReset = true) => {
           if (shortRatio < 0) modeChange = true;
           let closePositionAmt = shortHolding.positionAmt;
           if (shortRatio < 0)
-            closePositionAmt = Math.min(INIT_POSITION * 2, closePositionAmt);
+            closePositionAmt = Math.min(INIT_POSITION * 1, closePositionAmt);
           const currentProfit =
             (shortRatio * closePositionAmt) / LEVERAGE -
             0.018 * 0.01 * closePositionAmt;
