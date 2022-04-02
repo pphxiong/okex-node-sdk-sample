71699e516a59da9096bd07cd6da996f81007d2f1
pphxiong <1017551718@qq.com>
2022-04-02 15:46:40 +0800

    bug

--- server/app/swapSimulationApi.js
+++ server/app/swapSimulationApi.js
@@ -784,16 +784,20 @@ const checkDeal = async (data, isAutoReset = true) => {
     }
 
     const MAIN_LONG_BASIC_CONDITION =
+      // Number(macdList[macdList.length - 2].close) <
+      //   Number(bollList[bollList.length - 2].DN) &&
+      // Number(macdList[macdList.length - 1].close) >
+      // Number(bollList[bollList.length - 1].DN);
       Number(macdList[macdList.length - 2].close) <
-        Number(bollList[bollList.length - 2].DN) &&
+        Number(bollList[bollList.length - 2].UP) &&
       Number(macdList[macdList.length - 1].close) >
-        Number(bollList[bollList.length - 1].DN);
+        Number(bollList[bollList.length - 1].UP);
 
     const MAIN_SHORT_BASIC_CONDITION =
       Number(macdList[macdList.length - 2].close) >
-        Number(bollList[bollList.length - 2].UP) &&
+        Number(bollList[bollList.length - 2].DN) &&
       Number(macdList[macdList.length - 1].close) <
-        Number(bollList[bollList.length - 1].UP);
+        Number(bollList[bollList.length - 1].DN);
 
     const MAIN_OPEN_LONG_CONDITION = MAIN_LONG_BASIC_CONDITION;
 
