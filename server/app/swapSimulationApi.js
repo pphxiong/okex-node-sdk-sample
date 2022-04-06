45b6c321858bcc197d29716e7d24993a3d59b980
pphxiong <1017551718@qq.com>
2022-04-06 13:33:25 +0800

    new

--- server/app/swapSimulationApi.js
+++ server/app/swapSimulationApi.js
@@ -1101,7 +1101,9 @@ const checkDeal = async (data, isAutoReset = true) => {
           // closeShort()
           // const openPositionAmt = shortRatio < LOSS_MAX ? INIT_POSITION * 2 : INIT_POSITION
           // let openPositionAmt = INIT_POSITION + longPosition.positionAmt;
-          let openPositionAmt = longPosition.positionAmt * 2;
+          let openPositionAmt = longPosition.positionAmt
+            ? longPosition.positionAmt * 2
+            : INIT_POSITION;
           // const ratio = shortRatio;
           // const increasePosition = INCREASE_FI_LIST[fiIndex + 1];
           // const decreasePosition = INCREASE_FI_LIST[0];
@@ -1178,7 +1180,9 @@ const checkDeal = async (data, isAutoReset = true) => {
           // closeLong()
           // const openPositionAmt = longRatio < LOSS_MAX ? INIT_POSITION * 2 : INIT_POSITION
           // let openPositionAmt = INIT_POSITION + shortPosition.positionAmt;
-          let openPositionAmt = shortPosition.positionAmt * 2;
+          let openPositionAmt = shortPosition.positionAmt
+            ? shortPosition.positionAmt * 2
+            : INIT_POSITION;
           // const ratio = longRatio;
           // const increasePosition = INCREASE_FI_LIST[fiIndex + 1];
           // const decreasePosition = INCREASE_FI_LIST[0];
