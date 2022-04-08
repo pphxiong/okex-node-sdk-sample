832d58bf053feb5743342e5513be42bcd8873b04
pphxiong <1017551718@qq.com>
2022-04-08 09:49:28 +0800

    new

--- server/app/swapSimulationApi.js
+++ server/app/swapSimulationApi.js
@@ -803,19 +803,19 @@ const checkDeal = async (data, isAutoReset = true) => {
 
     const MAIN_OPEN_SHORT_CONDITION1 = MAIN_OPEN_SHORT_CONDITION;
 
-    const MAIN_CLOSE_LONG_CONDITION1 = MAIN_OPEN_SHORT_CONDITION1;
-    // ||
-    // (Number(macdList[macdList.length - 1].close) <
-    //   Number(bollList[bollList.length - 1].MA) &&
-    //   Number(macdList[macdList.length - 1].open) >
-    //     Number(bollList[bollList.length - 1].MA));
+    const MAIN_CLOSE_LONG_CONDITION1 =
+      MAIN_OPEN_SHORT_CONDITION1 ||
+      (Number(macdList[macdList.length - 1].close) <
+        Number(bollList[bollList.length - 1].MA) &&
+        Number(macdList[macdList.length - 1].open) >
+          Number(bollList[bollList.length - 1].MA));
 
-    const MAIN_CLOSE_SHORT_CONDITION1 = MAIN_OPEN_LONG_CONDITION1;
-    // ||
-    // (Number(macdList[macdList.length - 1].close) >
-    //   Number(bollList[bollList.length - 1].MA) &&
-    //   Number(macdList[macdList.length - 1].open) <
-    //     Number(bollList[bollList.length - 1].MA));
+    const MAIN_CLOSE_SHORT_CONDITION1 =
+      MAIN_OPEN_LONG_CONDITION1 ||
+      (Number(macdList[macdList.length - 1].close) >
+        Number(bollList[bollList.length - 1].MA) &&
+        Number(macdList[macdList.length - 1].open) <
+          Number(bollList[bollList.length - 1].MA));
 
     if (modeChange) lastMode = lastMode ? 0 : 1;
 
