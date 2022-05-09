6bc1defa6175f9f1b53e6b4377e6125008462779
pphxiong <1017551718@qq.com>
2022-05-09 11:12:37 +0800

    bug

--- server/app/swapApiBN.js
+++ server/app/swapApiBN.js
@@ -132,35 +132,45 @@ const checkDeal = async (data) => {
     }
 
     const MAIN_OPEN_LONG_CONDITION =
-      ((Number(macdList[macdList.length - 2].close) <
+      Number(macdList[macdList.length - 2].close) <
         Number(bollList[bollList.length - 2].DN) &&
-        Number(macdList[macdList.length - 1].close) >
-          Number(bollList[bollList.length - 1].DN)) ||
-        (shortRatio < 0 &&
-          Number(macdList[macdList.length - 2].close) <
-            Number(bollList[bollList.length - 2].UP) &&
-          Number(macdList[macdList.length - 1].close) >
-            Number(bollList[bollList.length - 1].UP))) &&
+      Number(macdList[macdList.length - 1].close) >
+        Number(bollList[bollList.length - 1].DN) &&
+      (!longHolding ||
+        Math.abs(Number(longHolding.positionAmt)) <= INIT_POSITION * 3);
+
+    const EXTRA_OPEN_LONG_CONDITION =
+      shortRatio < 0 &&
+      Number(macdList[macdList.length - 2].close) <
+        Number(bollList[bollList.length - 2].UP) &&
+      Number(macdList[macdList.length - 1].close) >
+        Number(bollList[bollList.length - 1].UP) &&
       (!longHolding ||
         Math.abs(Number(longHolding.positionAmt)) <= INIT_POSITION * 3);
 
     const MAIN_OPEN_SHORT_CONDITION =
-      ((Number(macdList[macdList.length - 2].close) >
+      Number(macdList[macdList.length - 2].close) >
         Number(bollList[bollList.length - 2].UP) &&
-        Number(macdList[macdList.length - 1].close) <
-          Number(bollList[bollList.length - 1].UP)) ||
-        (longRatio < 0 &&
-          Number(macdList[macdList.length - 2].close) >
-            Number(bollList[bollList.length - 2].DN) &&
-          Number(macdList[macdList.length - 1].close) <
-            Number(bollList[bollList.length - 1].DN))) &&
+      Number(macdList[macdList.length - 1].close) <
+        Number(bollList[bollList.length - 1].UP) &&
       (!shortHolding ||
         Math.abs(Number(shortHolding.positionAmt)) <= INIT_POSITION * 3);
 
-    const MAIN_OPEN_LONG_CONDITION1 = MAIN_OPEN_LONG_CONDITION;
-    const MAIN_OPEN_SHORT_CONDITION1 = MAIN_OPEN_SHORT_CONDITION;
-    const MAIN_CLOSE_LONG_CONDITION1 = MAIN_OPEN_SHORT_CONDITION1;
-    const MAIN_CLOSE_SHORT_CONDITION1 = MAIN_OPEN_LONG_CONDITION1;
+    const EXTRA_OPEN_SHORT_CONDITION =
+      longRatio < 0 &&
+      Number(macdList[macdList.length - 2].close) >
+        Number(bollList[bollList.length - 2].DN) &&
+      Number(macdList[macdList.length - 1].close) <
+        Number(bollList[bollList.length - 1].DN) &&
+      (!shortHolding ||
+        Math.abs(Number(shortHolding.positionAmt)) <= INIT_POSITION * 3);
+
+    const MAIN_OPEN_LONG_CONDITION1 =
+      MAIN_OPEN_LONG_CONDITION || EXTRA_OPEN_LONG_CONDITION;
+    const MAIN_OPEN_SHORT_CONDITION1 =
+      MAIN_OPEN_SHORT_CONDITION || EXTRA_OPEN_SHORT_CONDITION;
+    const MAIN_CLOSE_LONG_CONDITION1 = MAIN_OPEN_SHORT_CONDITION;
+    const MAIN_CLOSE_SHORT_CONDITION1 = MAIN_OPEN_LONG_CONDITION;
 
     const MAIN_OPEN_LONG_CONDITION2 = MAIN_OPEN_SHORT_CONDITION1;
     const MAIN_OPEN_SHORT_CONDITION2 = MAIN_OPEN_LONG_CONDITION1;
