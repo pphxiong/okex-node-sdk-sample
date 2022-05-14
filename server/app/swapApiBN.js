3e7952cc98b1275fbd6c1493f45a8cb822736cce
pphxiong <1017551718@qq.com>
2022-05-14 10:24:03 +0800

    bug

--- server/app/swapApiBN.js
+++ server/app/swapApiBN.js
@@ -135,8 +135,13 @@ const checkDeal = async (data) => {
       (Number(macdList[macdList.length - 2].close) <
         Number(bollList[bollList.length - 2].DN) &&
         Number(macdList[macdList.length - 1].close) >
-          Number(bollList[bollList.length - 1].DN)) ||
+          Number(bollList[bollList.length - 1].DN) &&
+        (!longHolding ||
+          (Math.abs(Number(longHolding.positionAmt)) <= INIT_POSITION * 2 &&
+            longRatio < 0))) ||
       (longRatio > 0 &&
+        Number(macdList[macdList.length - 3].close) >
+          Number(bollList[bollList.length - 3].DN) &&
         Number(macdList[macdList.length - 2].low) <
           Number(bollList[bollList.length - 2].DN) &&
         Number(macdList[macdList.length - 1].close) >
@@ -144,7 +149,7 @@ const checkDeal = async (data) => {
 
     const EXTRA_OPEN_LONG_CONDITION =
       shortHolding &&
-      Math.abs(Number(shortHolding.positionAmt)) >= INIT_POSITION * 2 &&
+      Math.abs(Number(shortHolding.positionAmt)) >= INIT_POSITION * 3 &&
       shortRatio < 0 &&
       Number(macdList[macdList.length - 2].close) <
         Number(bollList[bollList.length - 2].UP) &&
@@ -155,8 +160,13 @@ const checkDeal = async (data) => {
       (Number(macdList[macdList.length - 2].close) >
         Number(bollList[bollList.length - 2].UP) &&
         Number(macdList[macdList.length - 1].close) <
-          Number(bollList[bollList.length - 1].UP)) ||
+          Number(bollList[bollList.length - 1].UP) &&
+        (!shortHolding ||
+          (Math.abs(Number(shortHolding.positionAmt)) <= INIT_POSITION * 2 &&
+            shortRatio < 0))) ||
       (shortRatio > 0 &&
+        Number(macdList[macdList.length - 3].close) <
+          Number(bollList[bollList.length - 3].UP) &&
         Number(macdList[macdList.length - 2].high) >
           Number(bollList[bollList.length - 2].UP) &&
         Number(macdList[macdList.length - 1].close) <
@@ -164,7 +174,7 @@ const checkDeal = async (data) => {
 
     const EXTRA_OPEN_SHORT_CONDITION =
       longHolding &&
-      Math.abs(Number(longHolding.positionAmt)) >= INIT_POSITION * 2 &&
+      Math.abs(Number(longHolding.positionAmt)) >= INIT_POSITION * 3 &&
       longRatio < 0 &&
       Number(macdList[macdList.length - 2].close) >
         Number(bollList[bollList.length - 2].DN) &&
@@ -172,13 +182,11 @@ const checkDeal = async (data) => {
         Number(bollList[bollList.length - 1].DN);
 
     const MAIN_OPEN_LONG_CONDITION1 =
-      MAIN_OPEN_LONG_CONDITION && !EXTRA_OPEN_LONG_CONDITION;
+      MAIN_OPEN_LONG_CONDITION || EXTRA_OPEN_LONG_CONDITION;
     const MAIN_OPEN_SHORT_CONDITION1 =
-      MAIN_OPEN_SHORT_CONDITION && !EXTRA_OPEN_SHORT_CONDITION;
-    const MAIN_CLOSE_LONG_CONDITION1 =
-      MAIN_OPEN_SHORT_CONDITION && !EXTRA_OPEN_SHORT_CONDITION;
-    const MAIN_CLOSE_SHORT_CONDITION1 =
-      MAIN_OPEN_LONG_CONDITION && !EXTRA_OPEN_LONG_CONDITION;
+      MAIN_OPEN_SHORT_CONDITION || EXTRA_OPEN_SHORT_CONDITION;
+    const MAIN_CLOSE_LONG_CONDITION1 = MAIN_OPEN_SHORT_CONDITION1;
+    const MAIN_CLOSE_SHORT_CONDITION1 = MAIN_OPEN_LONG_CONDITION1;
 
     const MAIN_OPEN_LONG_CONDITION2 = MAIN_OPEN_SHORT_CONDITION1;
     const MAIN_OPEN_SHORT_CONDITION2 = MAIN_OPEN_LONG_CONDITION1;
