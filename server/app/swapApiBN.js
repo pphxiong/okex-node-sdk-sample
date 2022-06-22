01a87d46eb3c651b6ca77afc47bf66520b54b4c8
pphxiong <1017551718@qq.com>
2022-06-22 21:10:58 +0800

    bug

--- server/app/swapApiBN.js
+++ server/app/swapApiBN.js
@@ -132,13 +132,12 @@ const checkDeal = async (data) => {
     }
 
     const MAIN_OPEN_LONG_CONDITION =
-      true ||
-      (Number(macdList[macdList.length - 2].close) <
+      Number(macdList[macdList.length - 2].close) <
         Number(bollList[bollList.length - 2].DN) &&
-        Number(macdList[macdList.length - 1].close) >
-          Number(bollList[bollList.length - 1].DN) &&
-        Number(macdList[macdList.length - 1].close) >
-          Number(macdList[macdList.length - 1].open));
+      Number(macdList[macdList.length - 1].close) >
+        Number(bollList[bollList.length - 1].DN) &&
+      Number(macdList[macdList.length - 1].close) >
+        Number(macdList[macdList.length - 1].open);
 
     const EXTRA_OPEN_LONG_CONDITION =
       Number(macdList[macdList.length - 3].close) >
@@ -191,12 +190,13 @@ const checkDeal = async (data) => {
     }
 
     const MAIN_OPEN_SHORT_CONDITION =
-      Number(macdList[macdList.length - 2].close) >
+      true ||
+      (Number(macdList[macdList.length - 2].close) >
         Number(bollList[bollList.length - 2].UP) &&
-      Number(macdList[macdList.length - 1].close) <
-        Number(bollList[bollList.length - 1].UP) &&
-      Number(macdList[macdList.length - 1].close) <
-        Number(macdList[macdList.length - 1].open);
+        Number(macdList[macdList.length - 1].close) <
+          Number(bollList[bollList.length - 1].UP) &&
+        Number(macdList[macdList.length - 1].close) <
+          Number(macdList[macdList.length - 1].open));
 
     const EXTRA_OPEN_SHORT_CONDITION =
       Number(macdList[macdList.length - 3].close) <
