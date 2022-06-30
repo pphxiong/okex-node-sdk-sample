6dc2a2f8e7774436c2c1a3123e9d3dfb9b5b4d31
pphxiong <1017551718@qq.com>
2022-06-30 11:07:47 +0800

    bug

--- server/app/swapApiBN.js
+++ server/app/swapApiBN.js
@@ -293,30 +293,30 @@ const checkDeal = async (data) => {
       MAIN_OPEN_SHORT_CONDITION &&
       shortHolding &&
       ((!longHolding &&
-        Math.abs(shortHolding.positionAmt) >= INIT_POSITION * 4) ||
+        Math.abs(shortHolding.positionAmt) >= INIT_POSITION * 3) ||
         (longHolding &&
           Math.abs(shortHolding.positionAmt) >=
-            Math.abs(longHolding.positionAmt) + INIT_POSITION * 4));
+            Math.abs(longHolding.positionAmt) + INIT_POSITION * 3));
 
     const BATCH_SHORT_CONDITION =
       MAIN_OPEN_LONG_CONDITION &&
       longHolding &&
       ((!shortHolding &&
-        Math.abs(longHolding.positionAmt) >= INIT_POSITION * 4) ||
+        Math.abs(longHolding.positionAmt) >= INIT_POSITION * 3) ||
         (shortHolding &&
           Math.abs(longHolding.positionAmt) >=
-            Math.abs(shortHolding.positionAmt) + INIT_POSITION * 4));
+            Math.abs(shortHolding.positionAmt) + INIT_POSITION * 3));
 
     const MAIN_OPEN_LONG_CONDITION1 =
       (MAIN_OPEN_LONG_CONDITION &&
         (!longHolding ||
-          Math.abs(longHolding.positionAmt) < INIT_POSITION * 5)) ||
+          Math.abs(longHolding.positionAmt) <= INIT_POSITION * 3)) ||
       BATCH_LONG_CONDITION;
 
     const MAIN_OPEN_SHORT_CONDITION1 =
       (MAIN_OPEN_SHORT_CONDITION &&
         (!shortHolding ||
-          Math.abs(shortHolding.positionAmt) < INIT_POSITION * 5)) ||
+          Math.abs(shortHolding.positionAmt) <= INIT_POSITION * 3)) ||
       BATCH_SHORT_CONDITION;
 
     const MAIN_CLOSE_LONG_CONDITION1 =
