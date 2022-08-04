058d77f9f765ee5fd64769cdb48f8c1799d1bdb5
pphxiong <1017551718@qq.com>
2022-08-04 08:51:06 +0800

    new

--- server/app/swapApiBN.js
+++ server/app/swapApiBN.js
@@ -173,24 +173,17 @@ const checkDeal = async (data) => {
       Number(macdList[macdList.length - 1].close) <
         Number(bollList[bollList.length - 1].MA);
 
-    const MAIN_OPEN_LONG_CONDITION1 = MAIN_OPEN_LONG_CONDITION && !longHolding;
-    // && currentMacd.close > currentMacd.ema60;
+    const MAIN_OPEN_LONG_CONDITION1 =
+      (MAIN_OPEN_LONG_CONDITION || OUT_HIGH_CONDITION) && !longHolding;
 
     const MAIN_OPEN_SHORT_CONDITION1 =
-      MAIN_OPEN_SHORT_CONDITION && !shortHolding;
-    // && currentMacd.close < currentMacd.ema60;
+      (MAIN_OPEN_SHORT_CONDITION || OUT_LOW_CONDITION) && !shortHolding;
 
     const MAIN_CLOSE_LONG_CONDITION1 =
-      longHolding &&
-      (MAIN_OPEN_SHORT_CONDITION ||
-        MAIN_OPEN_LONG_CONDITION ||
-        OUT_LOW_CONDITION);
+      longHolding && (MAIN_OPEN_SHORT_CONDITION || MAIN_OPEN_LONG_CONDITION);
 
     const MAIN_CLOSE_SHORT_CONDITION1 =
-      shortHolding &&
-      (MAIN_OPEN_LONG_CONDITION ||
-        MAIN_OPEN_SHORT_CONDITION ||
-        OUT_HIGH_CONDITION);
+      shortHolding && (MAIN_OPEN_LONG_CONDITION || MAIN_OPEN_SHORT_CONDITION);
 
     const MAIN_OPEN_LONG_CONDITION2 = MAIN_OPEN_SHORT_CONDITION1;
     const MAIN_OPEN_SHORT_CONDITION2 = MAIN_OPEN_LONG_CONDITION1;
