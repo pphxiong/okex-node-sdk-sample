91a862690ae2003ddd3698cbe706cec2e849aa79
pphxiong <1017551718@qq.com>
2022-08-08 17:21:00 +0800

    new

--- server/app/swapSimulationApi.js
+++ server/app/swapSimulationApi.js
@@ -867,17 +867,16 @@ const checkDeal = async (data, isAutoReset = true) => {
       Number(macdList[macdList.length - 1].close) <
         Number(macdList[macdList.length - 1].open);
 
-    const MAIN_OPEN_LONG_CONDITION1 = MAIN_OPEN_LONG_CONDITION && !longHolding;
+    const MAIN_OPEN_LONG_CONDITION1 =
+      CENTER_CROSS_LONG_CONDITION && !longHolding;
 
     const MAIN_OPEN_SHORT_CONDITION1 =
-      MAIN_OPEN_SHORT_CONDITION && !shortHolding;
+      CENTER_CROSS_SHORT_CONDITION && !shortHolding;
 
-    const MAIN_CLOSE_LONG_CONDITION1 =
-      longHolding && (MAIN_OPEN_SHORT_CONDITION || CENTER_CROSS_LONG_CONDITION);
+    const MAIN_CLOSE_LONG_CONDITION1 = longHolding && MAIN_OPEN_SHORT_CONDITION;
 
     const MAIN_CLOSE_SHORT_CONDITION1 =
-      shortHolding &&
-      (MAIN_OPEN_LONG_CONDITION || CENTER_CROSS_SHORT_CONDITION);
+      shortHolding && MAIN_OPEN_LONG_CONDITION;
 
     const MAIN_OPEN_LONG_CONDITION2 = MAIN_OPEN_SHORT_CONDITION1;
     const MAIN_OPEN_SHORT_CONDITION2 = MAIN_OPEN_LONG_CONDITION1;
