ff424954ec2c8a2d82c5856cd1ffd8beea73889a
pphxiong <1017551718@qq.com>
2022-08-09 17:06:54 +0800

    new

--- server/app/swapSimulationApi.js
+++ server/app/swapSimulationApi.js
@@ -821,6 +821,10 @@ const checkDeal = async (data, isAutoReset = true) => {
       Number(macdList[macdList.length - 1].close) <
         Number(bollList[bollList.length - 1].DN);
 
+    const MACD_UP_CONDITION =
+      Number(bollList[bollList.length - 2].MA) <
+      Number(bollList[bollList.length - 1].MA);
+
     const CENTER_CROSS_LONG_CONDITION =
       Number(macdList[macdList.length - 2].close) <
         Number(bollList[bollList.length - 2].MA) &&
@@ -840,6 +844,10 @@ const checkDeal = async (data, isAutoReset = true) => {
         Number(bollList[bollList.length - 2].UP) &&
       Number(macdList[macdList.length - 1].close) >
         Number(bollList[bollList.length - 1].UP);
+
+    const MACD_DOWN_CONDITION =
+      Number(bollList[bollList.length - 2].MA) >
+      Number(bollList[bollList.length - 1].MA);
 
     const CENTER_CROSS_SHORT_CONDITION =
       Number(macdList[macdList.length - 2].close) >
@@ -868,18 +876,16 @@ const checkDeal = async (data, isAutoReset = true) => {
         Number(macdList[macdList.length - 1].open);
 
     const MAIN_OPEN_LONG_CONDITION1 =
-      CENTER_CROSS_SHORT_CONDITION && !longHolding;
+      CENTER_CROSS_SHORT_CONDITION && MACD_UP_CONDITION && !longHolding;
 
     const MAIN_OPEN_SHORT_CONDITION1 =
-      CENTER_CROSS_LONG_CONDITION && !shortHolding;
+      CENTER_CROSS_LONG_CONDITION && MACD_DOWN_CONDITION && !shortHolding;
 
     const MAIN_CLOSE_LONG_CONDITION1 =
-      longHolding &&
-      (CENTER_CROSS_SHORT_CONDITION || OUT_LOW_CONDITION || OUT_HIGH_CONDITION);
+      longHolding && CENTER_CROSS_LONG_CONDITION && MACD_DOWN_CONDITION;
 
     const MAIN_CLOSE_SHORT_CONDITION1 =
-      shortHolding &&
-      (CENTER_CROSS_LONG_CONDITION || OUT_HIGH_CONDITION || OUT_LOW_CONDITION);
+      shortHolding && CENTER_CROSS_SHORT_CONDITION && MACD_UP_CONDITION;
 
     const MAIN_OPEN_LONG_CONDITION2 = MAIN_OPEN_SHORT_CONDITION1;
     const MAIN_OPEN_SHORT_CONDITION2 = MAIN_OPEN_LONG_CONDITION1;
