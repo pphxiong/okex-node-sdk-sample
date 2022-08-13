4fffdb477c7b194a579b5482dd848d2aa7055913
pphxiong <1017551718@qq.com>
2022-08-14 07:37:47 +0800

    continuous

--- server/app/swapApiBN.js
+++ server/app/swapApiBN.js
@@ -193,17 +193,31 @@ const checkDeal = async (data) => {
       Number(macdList[macdList.length - 1].close) <
         Number(macdList[macdList.length - 1].open);
 
+    const CONTINOUS_LONG_CONDITION =
+      Number(macdList[macdList.length - 2].close) >
+        Number(bollList[bollList.length - 2].MA) &&
+      Number(macdList[macdList.length - 1].close) >
+        Number(bollList[bollList.length - 1].MA);
+
+    const CONTINOUS_SHORT_CONDITION =
+      Number(macdList[macdList.length - 2].close) <
+        Number(bollList[bollList.length - 2].MA) &&
+      Number(macdList[macdList.length - 1].close) <
+        Number(bollList[bollList.length - 1].MA);
+
     const MAIN_OPEN_LONG_CONDITION1 =
-      CENTER_CROSS_LONG_CONDITION && !longHolding;
+      (CENTER_CROSS_LONG_CONDITION || CONTINOUS_LONG_CONDITION) && !longHolding;
 
     const MAIN_OPEN_SHORT_CONDITION1 =
-      CENTER_CROSS_SHORT_CONDITION && !shortHolding;
+      (CENTER_CROSS_SHORT_CONDITION || CONTINOUS_SHORT_CONDITION) &&
+      !shortHolding;
 
     const MAIN_CLOSE_LONG_CONDITION1 =
-      longHolding && CENTER_CROSS_SHORT_CONDITION;
+      longHolding &&
+      (CENTER_CROSS_SHORT_CONDITION || CONTINOUS_SHORT_CONDITION);
 
     const MAIN_CLOSE_SHORT_CONDITION1 =
-      shortHolding && CENTER_CROSS_LONG_CONDITION;
+      shortHolding && (CENTER_CROSS_LONG_CONDITION || CONTINOUS_LONG_CONDITION);
 
     const MAIN_OPEN_LONG_CONDITION2 = MAIN_OPEN_SHORT_CONDITION1;
     const MAIN_OPEN_SHORT_CONDITION2 = MAIN_OPEN_LONG_CONDITION1;
