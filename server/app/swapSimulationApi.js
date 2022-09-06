42a860cff8cd5b7a8dfee088f2e0904e07f65a15
pphxiong <1017551718@qq.com>
2022-09-06 11:08:32 +0800

    new

--- server/app/swapSimulationApi.js
+++ server/app/swapSimulationApi.js
@@ -31,7 +31,7 @@ const BN_SYMBOL = "ETHUSDT";
 const LEVERAGE = 10;
 const INTERVAL = "5m";
 const BAO_RATIO = (-0.5 * LEVERAGE) / 10;
-const LOSS_MAX = (-1 * LEVERAGE) / 10;
+const LOSS_MAX = (-0.618 * LEVERAGE) / 10;
 const WIN_MAX = ((0.1 / 2) * LEVERAGE) / 10;
 // const BAO_RATIO = LOSS_MAX * 2;
 const CAPITAL_RATIO = 1;
@@ -815,7 +815,7 @@ const checkDeal = async (data, isAutoReset = true) => {
       Number(macdList[macdList.length - 1].close) <
         Number(bollList[bollList.length - 1].UP) &&
       Number(macdList[macdList.length - 1].close) >
-        Number(bollList[bollList.length - 1].MA);
+        Number(bollList[bollList.length - 1].DN);
 
     const OUT_LOW_CONDITION =
       Number(macdList[macdList.length - 2].close) >
@@ -827,9 +827,7 @@ const checkDeal = async (data, isAutoReset = true) => {
       Number(macdList[macdList.length - 2].close) <
         Number(bollList[bollList.length - 2].MA) &&
       Number(macdList[macdList.length - 1].close) >
-        Number(bollList[bollList.length - 1].MA) &&
-      Number(macdList[macdList.length - 1].close) <
-        Number(bollList[bollList.length - 1].UP);
+        Number(bollList[bollList.length - 1].MA);
 
     const UP_BOLL_CONDITION =
       Number(bollList[bollList.length - 2].MA) <
@@ -841,7 +839,7 @@ const checkDeal = async (data, isAutoReset = true) => {
       Number(macdList[macdList.length - 1].close) >
         Number(bollList[bollList.length - 1].DN) &&
       Number(macdList[macdList.length - 1].close) <
-        Number(bollList[bollList.length - 1].MA);
+        Number(bollList[bollList.length - 1].UP);
 
     const OUT_HIGH_CONDITION =
       Number(macdList[macdList.length - 2].close) <
@@ -853,9 +851,7 @@ const checkDeal = async (data, isAutoReset = true) => {
       Number(macdList[macdList.length - 2].close) >
         Number(bollList[bollList.length - 2].MA) &&
       Number(macdList[macdList.length - 1].close) <
-        Number(bollList[bollList.length - 1].MA) &&
-      Number(macdList[macdList.length - 1].close) >
-        Number(bollList[bollList.length - 1].DN);
+        Number(bollList[bollList.length - 1].MA);
 
     const DOWN_BOLL_CONDITION =
       Number(bollList[bollList.length - 2].MA) >
