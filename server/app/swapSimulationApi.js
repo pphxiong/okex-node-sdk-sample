506c9859c299835790e3795f0ac1d9c2736103bd
pphxiong <1017551718@qq.com>
2022-09-13 09:08:28 +0800

    bug

--- server/app/swapSimulationApi.js
+++ server/app/swapSimulationApi.js
@@ -1338,7 +1338,8 @@ const checkDeal = async (data, isAutoReset = true) => {
           // const openPositionAmt =
           //   shortRatio < LOSS_MAX ? INIT_POSITION * 2 : INIT_POSITION;
           // let openPositionAmt = INIT_POSITION + longPosition.positionAmt;
-          let openPositionAmt = INIT_POSITION * NEW_POSITION_RATIO;
+          let openPositionAmt =
+            INIT_POSITION * NEW_POSITION_RATIO + longPosition.positionAmt;
           // openPositionAmt = Math.min(openPositionAmt, INIT_POSITION * 16);
           // const ratio = shortRatio;
           // const increasePosition = INCREASE_FI_LIST[fiIndex + 1];
@@ -1417,7 +1418,9 @@ const checkDeal = async (data, isAutoReset = true) => {
           // const openPositionAmt =
           //   longRatio < LOSS_MAX ? INIT_POSITION * 2 : INIT_POSITION;
           // let openPositionAmt = INIT_POSITION + shortPosition.positionAmt;
-          let openPositionAmt = INIT_POSITION * NEW_POSITION_RATIO;
+          let openPositionAmt =
+            INIT_POSITION * NEW_POSITION_RATIO +
+            Math.abs(Number(shortPosition.positionAmt));
           // openPositionAmt = Math.min(openPositionAmt, INIT_POSITION * 16);
           // const ratio = longRatio;
           // const increasePosition = INCREASE_FI_LIST[fiIndex + 1];
