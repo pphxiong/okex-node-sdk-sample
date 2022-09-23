0562218a0261165c67a30ad1b39d55707bb52d0e
pphxiong <1017551718@qq.com>
2022-09-23 09:10:56 +0800

    new

--- server/app/swapApiBN.js
+++ server/app/swapApiBN.js
@@ -235,12 +235,11 @@ const checkDeal = async (data) => {
 
     const MAIN_OPEN_LONG_CONDITION1 =
       LOW_CONVERSE_CONDITION &&
-      (!longHolding || Math.abs(longHolding.positionAmt) <= INIT_POSITION * 2);
+      (!longHolding || Math.abs(longHolding.positionAmt) < INIT_POSITION * 2);
 
     const MAIN_OPEN_SHORT_CONDITION1 =
       UP_CONVERSE_CONDITION &&
-      (!shortHolding ||
-        Math.abs(shortHolding.positionAmt) <= INIT_POSITION * 2);
+      (!shortHolding || Math.abs(shortHolding.positionAmt) < INIT_POSITION * 2);
 
     const MAIN_CLOSE_LONG_CONDITION1 =
       longHolding &&
