c5949e31ea73d71b39acf27216027f618d3b9a92
pphxiong <1017551718@qq.com>
2022-09-25 14:23:36 +0800

    bug

--- server/app/swapApiBN.js
+++ server/app/swapApiBN.js
@@ -240,13 +240,13 @@ const checkDeal = async (data) => {
     // (!shortHolding || Math.abs(shortHolding.positionAmt) < INIT_POSITION * 2);
 
     const MAIN_CLOSE_LONG_CONDITION1 =
-      longHolding && (CENTER_CROSS_LONG_CONDITION || LOW_CONVERSE_CONDITION);
+      longHolding && CENTER_CROSS_LONG_CONDITION;
     // ||
     // (LOW_CONVERSE_CONDITION &&
     //   Math.abs(longHolding.positionAmt) >= INIT_POSITION * 2)
 
     const MAIN_CLOSE_SHORT_CONDITION1 =
-      shortHolding && (CENTER_CROSS_SHORT_CONDITION || UP_CONVERSE_CONDITION);
+      shortHolding && CENTER_CROSS_SHORT_CONDITION;
     // ||
     // (UP_CONVERSE_CONDITION &&
     //   Math.abs(shortHolding.positionAmt) <= INIT_POSITION * 2)
