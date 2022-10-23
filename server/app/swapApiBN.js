18ef83f7066de4fc6b2741fb93de2160604f6e2d
pphxiong <1017551718@qq.com>
2022-10-23 14:55:38 +0800

    new

--- server/app/swapApiBN.js
+++ server/app/swapApiBN.js
@@ -252,45 +252,43 @@ const checkDeal = async (data) => {
       Number(macdList[macdList.length - 1].open) >
       Number(bollList[bollList.length - 1].MA);
 
-    const OPEN_LONG_CONDITION_LAST =
+    const CENTER_OPEN_LONG_CONDITION_LAST =
       Number(macdList[macdList.length - 2].open) <
-        Number(bollList[bollList.length - 2].MA) &&
+      Number(bollList[bollList.length - 2].MA);
+
+    const CENTER_OPEN_SHORT_CONDITION_LAST =
+      Number(macdList[macdList.length - 2].open) >
+      Number(bollList[bollList.length - 2].MA);
+
+    const MACD_UP_CONDITION_LAST =
       Number(macdList[macdList.length - 2].close) >
         Number(macdList[macdList.length - 2].open) &&
-      Number(macdList[macdList.length - 2].column) < 0 &&
-      Number(macdList[macdList.length - 1].open) >
-        Number(bollList[bollList.length - 1].MA) &&
-      MACD_UP_CONDITION;
+      Number(macdList[macdList.length - 2].column) > 0;
 
-    const OPEN_SHORT_CONDITION_LAST =
-      Number(macdList[macdList.length - 2].open) >
-        Number(bollList[bollList.length - 2].MA) &&
+    const MACD_DOWN_CONDITION_LAST =
       Number(macdList[macdList.length - 2].close) <
         Number(macdList[macdList.length - 2].open) &&
-      Number(macdList[macdList.length - 2].column) > 0 &&
-      Number(macdList[macdList.length - 1].open) <
-        Number(bollList[bollList.length - 1].MA) &&
-      MACD_DOWN_CONDITION;
+      Number(macdList[macdList.length - 2].column) < 0;
 
     const MAIN_OPEN_LONG_CONDITION1 =
       ((MACD_UP_CONDITION && CENTER_OPEN_LONG_CONDITION) ||
-        OPEN_LONG_CONDITION_LAST) &&
+        (MACD_UP_CONDITION_LAST && CENTER_OPEN_LONG_CONDITION_LAST)) &&
       !longHolding;
 
     const MAIN_OPEN_SHORT_CONDITION1 =
       ((MACD_DOWN_CONDITION && CENTER_OPEN_SHORT_CONDITION) ||
-        OPEN_SHORT_CONDITION_LAST) &&
+        (MACD_DOWN_CONDITION_LAST && CENTER_OPEN_SHORT_CONDITION_LAST)) &&
       !shortHolding;
 
     const MAIN_CLOSE_LONG_CONDITION1 =
       longHolding &&
       ((MACD_DOWN_CONDITION && CENTER_OPEN_SHORT_CONDITION) ||
-        OPEN_SHORT_CONDITION_LAST);
+        (MACD_DOWN_CONDITION_LAST && CENTER_OPEN_SHORT_CONDITION_LAST));
 
     const MAIN_CLOSE_SHORT_CONDITION1 =
       shortHolding &&
       ((MACD_UP_CONDITION && CENTER_OPEN_LONG_CONDITION) ||
-        OPEN_LONG_CONDITION_LAST);
+        (MACD_UP_CONDITION_LAST && CENTER_OPEN_LONG_CONDITION_LAST));
 
     const MAIN_OPEN_LONG_CONDITION2 = MAIN_OPEN_SHORT_CONDITION1;
     const MAIN_OPEN_SHORT_CONDITION2 = MAIN_OPEN_LONG_CONDITION1;
