c00782b675ff324310c0c85915f94cd412410435
pphxiong <1017551718@qq.com>
2022-11-10 19:23:42 +0800

    new

--- server/app/swapSimulationApi.js
+++ server/app/swapSimulationApi.js
@@ -869,19 +869,19 @@ const checkDeal = async (data, isAutoReset = true) => {
 
     const LOW_CONVERSE_CONDITION =
       Number(macdList[macdList.length - 2].close) <
-        Number(bollList[bollList.length - 2].DN) &&
-      Number(macdList[macdList.length - 1].close) >
-        Number(bollList[bollList.length - 1].DN) &&
+        Number(bollList[bollList.length - 2].MA) &&
+      // Number(macdList[macdList.length - 1].close) >
+      //   Number(bollList[bollList.length - 1].DN) &&
       Number(macdList[macdList.length - 1].close) <
         Number(bollList[bollList.length - 1].MA);
 
     const CENTER_CROSS_SHORT_CONDITION =
       Number(macdList[macdList.length - 2].close) >
         Number(bollList[bollList.length - 2].MA) &&
-      Number(macdList[macdList.length - 1].close) <
-        Number(bollList[bollList.length - 1].MA) &&
+      // Number(macdList[macdList.length - 1].close) <
+      //   Number(bollList[bollList.length - 1].MA) &&
       Number(macdList[macdList.length - 1].close) >
-        Number(bollList[bollList.length - 1].DN);
+        Number(bollList[bollList.length - 1].MA);
 
     const MAIN_OPEN_LONG_CONDITION1 = !longHolding && UP_CONVERSE_CONDITION;
 
