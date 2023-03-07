246132567e43cc0d31c03ec183caf54d795b1766
pphxiong <1017551718@qq.com>
2023-03-07 19:15:07 +0800

    bug

--- server/app/swapApiBN.js
+++ server/app/swapApiBN.js
@@ -173,17 +173,33 @@ const checkDeal = async (data) => {
       Number(macdList[macdList.length - 1].close) <
         Number(bollList[bollList.length - 1].MA);
 
-    const MAIN_OPEN_LONG_CONDITION1 =
-      !longHolding && CENTER_CROSS_LONG_CONDITION;
+    const CLOSE_MORE_HIGH_CONDITION =
+      Number(macdList[macdList.length - 1].close) >
+      Number(bollList[bollList.length - 1].UP);
 
-    const MAIN_OPEN_SHORT_CONDITION1 =
-      !shortHolding && CENTER_CROSS_SHORT_CONDITION;
+    const CLOSE_LESS_HIGH_CONDITION =
+      Number(macdList[macdList.length - 1].close) <
+      Number(bollList[bollList.length - 1].UP);
+
+    const CLOSE_MORE_LOW_CONDITION =
+      Number(macdList[macdList.length - 1].close) >
+      Number(bollList[bollList.length - 1].DN);
+
+    const CLOSE_LESS_LOW_CONDITION =
+      Number(macdList[macdList.length - 1].close) <
+      Number(bollList[bollList.length - 1].DN);
+
+    const MAIN_OPEN_LONG_CONDITION1 = CLOSE_LESS_LOW_CONDITION;
+
+    const MAIN_OPEN_SHORT_CONDITION1 = CLOSE_MORE_HIGH_CONDITION;
 
     const MAIN_CLOSE_LONG_CONDITION1 =
-      longHolding && CENTER_CROSS_SHORT_CONDITION;
+      longHolding &&
+      ((CLOSE_MORE_LOW_CONDITION && longRatio > 0) || CONVERSE_LOW_CONDITION);
 
     const MAIN_CLOSE_SHORT_CONDITION1 =
-      shortHolding && CENTER_CROSS_LONG_CONDITION;
+      shortHolding &&
+      ((CLOSE_LESS_HIGH_CONDITION && shortRatio > 0) || CONVERSE_UP_CONDITION);
 
     const MAIN_OPEN_LONG_CONDITION2 =
       !longHolding && CENTER_CROSS_SHORT_CONDITION;
