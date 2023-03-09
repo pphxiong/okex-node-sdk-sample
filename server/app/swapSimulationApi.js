e66fbb3b4777f78cee84d92412ad495b9abc66a7
pphxiong <1017551718@qq.com>
2023-03-09 11:20:26 +0800

    new

--- server/app/swapSimulationApi.js
+++ server/app/swapSimulationApi.js
@@ -892,17 +892,13 @@ const checkDeal = async (data, isAutoReset = true) => {
           Math.abs(shortHolding.positionAmt));
     }
 
-    const MAIN_OPEN_LONG_CONDITION1 =
-      CENTER_CROSS_SHORT_CONDITION && longRatio >= 0;
+    const MAIN_OPEN_LONG_CONDITION1 = CENTER_CROSS_LONG_CONDITION;
 
-    const MAIN_OPEN_SHORT_CONDITION1 =
-      CENTER_CROSS_LONG_CONDITION && shortRatio >= 0;
+    const MAIN_OPEN_SHORT_CONDITION1 = CENTER_CROSS_SHORT_CONDITION;
 
-    const MAIN_CLOSE_LONG_CONDITION1 =
-      longHolding && CENTER_CROSS_LONG_CONDITION && totalRatio > 0;
+    const MAIN_CLOSE_LONG_CONDITION1 = longHolding && totalRatio > 0;
 
-    const MAIN_CLOSE_SHORT_CONDITION1 =
-      shortHolding && CENTER_CROSS_SHORT_CONDITION && totalRatio > 0;
+    const MAIN_CLOSE_SHORT_CONDITION1 = shortHolding && totalRatio > 0;
 
     const MAIN_OPEN_LONG_CONDITION2 =
       !longHolding && CENTER_CROSS_SHORT_CONDITION;
