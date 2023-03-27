24d2cf125ee6a0434e9c2933ea413f46bffbadd1
pphxiong <1017551718@qq.com>
2023-03-27 16:32:01 +0800

    bug

--- server/app/swapApiBN.js
+++ server/app/swapApiBN.js
@@ -5,7 +5,7 @@ const customAuthClientBN = require("./customAuthClientBN");
 
 const BN_SYMBOL = "ETHUSDT";
 const DEFAULT_INTERVAL = "1h";
-const INIT_POSITION = 0.4;
+const INIT_POSITION = 0.1;
 let MODE = 1;
 
 const generatePositionList = (init, num) => {
