41cddc643a40ac009f6743a55e355998416ee8b3
pphxiong <1017551718@qq.com>
2023-04-03 13:42:13 +0800

    new

--- server/app/swapSimulationApi.js
+++ server/app/swapSimulationApi.js
@@ -892,15 +892,21 @@ const checkDeal = async (data, isAutoReset = true) => {
           Math.abs(shortHolding.positionAmt));
     }
 
-    const MAIN_OPEN_LONG_CONDITION1 = CENTER_CROSS_LONG_CONDITION;
+    const LAST_LONG_CONDITION =
+      Number(macdList[macdList.length - 1].close) >
+      Number(macdList[macdList.length - 1].open);
 
-    const MAIN_OPEN_SHORT_CONDITION1 = CENTER_CROSS_SHORT_CONDITION;
+    const LAST_SHORT_CONDITION =
+      Number(macdList[macdList.length - 1].close) <
+      Number(macdList[macdList.length - 1].open);
 
-    const MAIN_CLOSE_LONG_CONDITION1 =
-      longHolding && OUT_HIGH_CONDITION && totalRatio > 0;
+    const MAIN_OPEN_LONG_CONDITION1 = LAST_LONG_CONDITION;
 
-    const MAIN_CLOSE_SHORT_CONDITION1 =
-      shortHolding && OUT_LOW_CONDITION && totalRatio > 0;
+    const MAIN_OPEN_SHORT_CONDITION1 = LAST_SHORT_CONDITION;
+
+    const MAIN_CLOSE_LONG_CONDITION1 = longHolding && LAST_SHORT_CONDITION;
+
+    const MAIN_CLOSE_SHORT_CONDITION1 = shortHolding && LAST_LONG_CONDITION;
 
     const MAIN_OPEN_LONG_CONDITION2 =
       !longHolding && CENTER_CROSS_SHORT_CONDITION;
