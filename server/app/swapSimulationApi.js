93913747bba8e5d9aca05e4cfcebde573a1541a2
pphxiong <1017551718@qq.com>
2023-04-10 10:57:16 +0800

    bug

--- server/app/swapSimulationApi.js
+++ server/app/swapSimulationApi.js
@@ -920,33 +920,13 @@ const checkDeal = async (data, isAutoReset = true) => {
       Number(bollList[bollList.length - 5].MA) <
         Number(bollList[bollList.length - 4].MA);
 
-    const MAIN_OPEN_LONG_CONDITION1 =
-      !longHolding &&
-      (CONVERSE_LOW_CONDITION || (shortHolding && OUT_HIGH_CONDITION));
-
-    const MAIN_OPEN_SHORT_CONDITION1 =
-      !shortHolding &&
-      (CONVERSE_UP_CONDITION || (longHolding && OUT_LOW_CONDITION));
-
-    const MAIN_CLOSE_LONG_CONDITION1 =
-      longHolding &&
-      ((!shortHolding && CONVERSE_UP_CONDITION) ||
-        (shortHolding && CENTER_CROSS_SHORT_CONDITION));
-
-    const MAIN_CLOSE_SHORT_CONDITION1 =
-      shortHolding &&
-      ((!longHolding && CONVERSE_LOW_CONDITION) ||
-        (longHolding && CENTER_CROSS_LONG_CONDITION));
-
     // const MAIN_OPEN_LONG_CONDITION1 =
     //   !longHolding &&
-    //   (CONVERSE_LOW_CONDITION ||
-    //     (shortHolding && (OUT_HIGH_CONDITION || CENTER_CROSS_LONG_CONDITION)));
+    //   (CONVERSE_LOW_CONDITION || (shortHolding && OUT_HIGH_CONDITION));
 
     // const MAIN_OPEN_SHORT_CONDITION1 =
     //   !shortHolding &&
-    //   (CONVERSE_UP_CONDITION ||
-    //     (longHolding && (OUT_LOW_CONDITION || CENTER_CROSS_SHORT_CONDITION)));
+    //   (CONVERSE_UP_CONDITION || (longHolding && OUT_LOW_CONDITION));
 
     // const MAIN_CLOSE_LONG_CONDITION1 =
     //   longHolding &&
@@ -957,6 +937,24 @@ const checkDeal = async (data, isAutoReset = true) => {
     //   shortHolding &&
     //   ((!longHolding && CONVERSE_LOW_CONDITION) ||
     //     (longHolding && CENTER_CROSS_LONG_CONDITION));
+
+    const MAIN_OPEN_LONG_CONDITION1 =
+      !longHolding &&
+      (CENTER_CROSS_LONG_CONDITION || (shortHolding && OUT_HIGH_CONDITION));
+
+    const MAIN_OPEN_SHORT_CONDITION1 =
+      !shortHolding &&
+      (CENTER_CROSS_SHORT_CONDITION || (longHolding && OUT_LOW_CONDITION));
+
+    const MAIN_CLOSE_LONG_CONDITION1 =
+      longHolding &&
+      ((!shortHolding && CONVERSE_UP_CONDITION) ||
+        (shortHolding && CENTER_CROSS_SHORT_CONDITION));
+
+    const MAIN_CLOSE_SHORT_CONDITION1 =
+      shortHolding &&
+      ((!longHolding && CONVERSE_LOW_CONDITION) ||
+        (longHolding && CENTER_CROSS_LONG_CONDITION));
 
     const MAIN_OPEN_LONG_CONDITION2 =
       !longHolding && CENTER_CROSS_SHORT_CONDITION;
