afbf7f83d0d87af2ea02d0aa7d0d1ad7078b071e
pphxiong <1017551718@qq.com>
2023-04-14 16:38:52 +0800

    new

--- server/app/swapSimulationApi.js
+++ server/app/swapSimulationApi.js
@@ -949,12 +949,12 @@ const checkDeal = async (data, isAutoReset = true) => {
     const MAIN_CLOSE_LONG_CONDITION1 =
       longHolding &&
       ((!shortHolding && CONVERSE_UP_CONDITION) ||
-        (shortHolding && CENTER_CROSS_SHORT_CONDITION));
+        (shortHolding && CENTER_CROSS_LONG_CONDITION));
 
     const MAIN_CLOSE_SHORT_CONDITION1 =
       shortHolding &&
       ((!longHolding && CONVERSE_LOW_CONDITION) ||
-        (longHolding && CENTER_CROSS_LONG_CONDITION));
+        (longHolding && CENTER_CROSS_SHORT_CONDITION));
 
     const MAIN_OPEN_LONG_CONDITION2 =
       !longHolding && CENTER_CROSS_SHORT_CONDITION;
