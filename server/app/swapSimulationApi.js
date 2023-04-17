02dd4f1a1976e2eb7880160ed8e81925d5dd1128
pphxiong <1017551718@qq.com>
2023-04-17 18:35:46 +0800

    bug

--- server/app/swapSimulationApi.js
+++ server/app/swapSimulationApi.js
@@ -837,16 +837,16 @@ const checkDeal = async (data, isAutoReset = true) => {
         Number(bollList[bollList.length - 1].MA);
 
     const OUT_HIGH_CONDITION =
-      Number(macdList[macdList.length - 2].close) <
-        Number(bollList[bollList.length - 2].UP) &&
+      // Number(macdList[macdList.length - 2].close) <
+      //   Number(bollList[bollList.length - 2].UP) &&
       Number(macdList[macdList.length - 1].close) >
-        Number(bollList[bollList.length - 1].UP);
+      Number(bollList[bollList.length - 1].UP);
 
     const OUT_LOW_CONDITION =
-      Number(macdList[macdList.length - 2].close) >
-        Number(bollList[bollList.length - 2].DN) &&
+      // Number(macdList[macdList.length - 2].close) >
+      //   Number(bollList[bollList.length - 2].DN) &&
       Number(macdList[macdList.length - 1].close) <
-        Number(bollList[bollList.length - 1].DN);
+      Number(bollList[bollList.length - 1].DN);
 
     const CONVERSE_UP_CONDITION =
       Number(macdList[macdList.length - 2].close) >
