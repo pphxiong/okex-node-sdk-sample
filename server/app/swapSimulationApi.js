bbf3bf485e0e6f2221a55c3477b58855ccb50b77
pphxiong <1017551718@qq.com>
2023-04-17 09:06:54 +0800

    bug

--- server/app/swapSimulationApi.js
+++ server/app/swapSimulationApi.js
@@ -939,22 +939,32 @@ const checkDeal = async (data, isAutoReset = true) => {
     //     (longHolding && CENTER_CROSS_LONG_CONDITION));
 
     const MAIN_OPEN_LONG_CONDITION1 =
-      !longHolding &&
-      (CONVERSE_LOW_CONDITION || (shortHolding && OUT_HIGH_CONDITION));
+      (CONVERSE_LOW_CONDITION &&
+        (!longHolding ||
+          Math.abs(longHolding.positionAmt) < INIT_POSITION * 3)) ||
+      (shortHolding &&
+        OUT_HIGH_CONDITION &&
+        Math.abs(shortHolding.positionAmt) >= INIT_POSITION * 3);
 
     const MAIN_OPEN_SHORT_CONDITION1 =
-      !shortHolding &&
-      (CONVERSE_UP_CONDITION || (longHolding && OUT_LOW_CONDITION));
+      (CONVERSE_UP_CONDITION &&
+        (!shortHolding ||
+          Math.abs(shortHolding.positionAmt) < INIT_POSITION * 3)) ||
+      (longHolding &&
+        OUT_LOW_CONDITION &&
+        Math.abs(longHolding.positionAmt) >= INIT_POSITION * 3);
 
     const MAIN_CLOSE_LONG_CONDITION1 =
       longHolding &&
-      ((!shortHolding && CONVERSE_UP_CONDITION) ||
-        (shortHolding && CENTER_CROSS_SHORT_CONDITION));
+      CONVERSE_UP_CONDITION &&
+      (!shortHolding ||
+        Math.abs(shortHolding.positionAmt) > Math.abs(longHolding.positionAmt));
 
     const MAIN_CLOSE_SHORT_CONDITION1 =
       shortHolding &&
-      ((!longHolding && CONVERSE_LOW_CONDITION) ||
-        (longHolding && CENTER_CROSS_LONG_CONDITION));
+      CONVERSE_LOW_CONDITION &&
+      (!longHolding ||
+        Math.abs(longHolding.positionAmt) > Math.abs(shortHolding.positionAmt));
 
     const MAIN_OPEN_LONG_CONDITION2 =
       !longHolding && CENTER_CROSS_SHORT_CONDITION;
