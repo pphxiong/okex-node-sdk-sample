2dfb8ab92e259a934307fd3019c44d68adb18049
pphxiong <1017551718@qq.com>
2023-04-26 18:06:37 +0800

    new

--- server/app/swapApiBN.js
+++ server/app/swapApiBN.js
@@ -210,23 +210,15 @@ const checkDeal = async (data) => {
         Number(bollList[bollList.length - 2].MA);
 
     const MAIN_OPEN_LONG_CONDITION1 =
-      (CONVERSE_LOW_CONDITION &&
-        (!longHolding ||
-          Math.abs(longHolding.positionAmt) <=
-            INIT_POSITION * MAX_OPEN_POSITION_RATIO)) ||
-      (shortHolding &&
-        OUT_HIGH_CONDITION &&
-        Math.abs(shortHolding.positionAmt) >
+      CONVERSE_LOW_CONDITION &&
+      (!longHolding ||
+        Math.abs(longHolding.positionAmt) <=
           INIT_POSITION * MAX_OPEN_POSITION_RATIO);
 
     const MAIN_OPEN_SHORT_CONDITION1 =
-      (CONVERSE_UP_CONDITION &&
-        (!shortHolding ||
-          Math.abs(shortHolding.positionAmt) <=
-            INIT_POSITION * MAX_OPEN_POSITION_RATIO)) ||
-      (longHolding &&
-        OUT_LOW_CONDITION &&
-        Math.abs(longHolding.positionAmt) >
+      CONVERSE_UP_CONDITION &&
+      (!shortHolding ||
+        Math.abs(shortHolding.positionAmt) <=
           INIT_POSITION * MAX_OPEN_POSITION_RATIO);
 
     const MAIN_CLOSE_LONG_CONDITION1 = longHolding && CONVERSE_UP_CONDITION;
