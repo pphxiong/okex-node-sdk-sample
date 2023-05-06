f5b818b52fe4fa3176bbfdc53019ba70e33eec75
pphxiong <1017551718@qq.com>
2023-05-06 09:43:38 +0800

    bug

--- server/app/swapSimulationApi.js
+++ server/app/swapSimulationApi.js
@@ -921,48 +921,42 @@ const checkDeal = async (data, isAutoReset = true) => {
       Number(bollList[bollList.length - 5].MA) <
         Number(bollList[bollList.length - 4].MA);
 
-    // const MAIN_OPEN_LONG_CONDITION1 =
-    //   !longHolding &&
-    //   (CONVERSE_LOW_CONDITION || (shortHolding && OUT_HIGH_CONDITION));
+    const MAIN_OPEN_LONG_CONDITION1 =
+      !longHolding && CENTER_CROSS_LONG_CONDITION;
 
-    // const MAIN_OPEN_SHORT_CONDITION1 =
-    //   !shortHolding &&
-    //   (CONVERSE_UP_CONDITION || (longHolding && OUT_LOW_CONDITION));
+    const MAIN_OPEN_SHORT_CONDITION1 =
+      !shortHolding && CENTER_CROSS_SHORT_CONDITION;
 
-    // const MAIN_CLOSE_LONG_CONDITION1 =
-    //   longHolding &&
-    //   ((!shortHolding && CONVERSE_UP_CONDITION) ||
-    //     (shortHolding && CENTER_CROSS_SHORT_CONDITION));
+    const MAIN_CLOSE_LONG_CONDITION1 =
+      longHolding && CENTER_CROSS_SHORT_CONDITION;
 
-    // const MAIN_CLOSE_SHORT_CONDITION1 =
-    //   shortHolding &&
-    //   ((!longHolding && CONVERSE_LOW_CONDITION) ||
-    //     (longHolding && CENTER_CROSS_LONG_CONDITION));
+    const MAIN_CLOSE_SHORT_CONDITION1 =
+      shortHolding && CENTER_CROSS_LONG_CONDITION;
 
-    const latestMACD = macdList[macdList.length - 1];
-    const HIGH_20_CONDITION = macdList.every(
-      (item) => latestMACD.close >= item.close
-    );
+    // const latestMACD = macdList[macdList.length - 1];
+    // const HIGH_20_CONDITION = macdList.every(
+    //   (item) => latestMACD.close >= item.close
+    // );
 
-    const LOW_20_CONDITION = macdList.every(
-      (item) => latestMACD.close <= item.close
-    );
+    // const LOW_20_CONDITION = macdList.every(
+    //   (item) => latestMACD.close <= item.close
+    // );
 
-    const HIGH_10_CONDITION = macdList.every(
-      (item) => latestMACD.close >= item.close
-    );
+    // const HIGH_10_CONDITION = macdList.every(
+    //   (item) => latestMACD.close >= item.close
+    // );
 
-    const LOW_10_CONDITION = macdList.every(
-      (item) => latestMACD.close <= item.close
-    );
+    // const LOW_10_CONDITION = macdList.every(
+    //   (item) => latestMACD.close <= item.close
+    // );
 
-    const MAIN_OPEN_LONG_CONDITION1 = !longHolding && HIGH_20_CONDITION;
+    // const MAIN_OPEN_LONG_CONDITION1 = !longHolding && HIGH_20_CONDITION;
 
-    const MAIN_OPEN_SHORT_CONDITION1 = !shortHolding && LOW_20_CONDITION;
+    // const MAIN_OPEN_SHORT_CONDITION1 = !shortHolding && LOW_20_CONDITION;
 
-    const MAIN_CLOSE_LONG_CONDITION1 = longHolding && LOW_10_CONDITION;
+    // const MAIN_CLOSE_LONG_CONDITION1 = longHolding && LOW_10_CONDITION;
 
-    const MAIN_CLOSE_SHORT_CONDITION1 = shortHolding && HIGH_10_CONDITION;
+    // const MAIN_CLOSE_SHORT_CONDITION1 = shortHolding && HIGH_10_CONDITION;
 
     const MAIN_OPEN_LONG_CONDITION2 =
       !longHolding && CENTER_CROSS_SHORT_CONDITION;
