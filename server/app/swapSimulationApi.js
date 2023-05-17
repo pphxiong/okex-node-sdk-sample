20276593036935ccd37c66f7cdeab7a0202ef5bd
pphxiong <1017551718@qq.com>
2023-05-17 09:23:18 +0800

    bug

--- server/app/swapSimulationApi.js
+++ server/app/swapSimulationApi.js
@@ -992,18 +992,16 @@ const checkDeal = async (data, isAutoReset = true) => {
       CENTER_CROSS_LONG_CONDITION;
 
     const MAIN_OPEN_LONG_CONDITION1 =
-      (CONVERSE_LOW_CONDITION &&
-        (!longHolding ||
-          Math.abs(longHolding.positionAmt) <=
-            INIT_POSITION * MAX_OPEN_POSITION_RATIO)) ||
-      BATCH_LONG_OPEN_CONDITION;
+      CONVERSE_LOW_CONDITION &&
+      (!longHolding ||
+        Math.abs(longHolding.positionAmt) <=
+          INIT_POSITION * MAX_OPEN_POSITION_RATIO);
 
     const MAIN_OPEN_SHORT_CONDITION1 =
-      (CONVERSE_UP_CONDITION &&
-        (!shortHolding ||
-          Math.abs(shortHolding.positionAmt) <=
-            INIT_POSITION * MAX_OPEN_POSITION_RATIO)) ||
-      BATCH_SHORT_OPEN_CONDITION;
+      CONVERSE_UP_CONDITION &&
+      (!shortHolding ||
+        Math.abs(shortHolding.positionAmt) <=
+          INIT_POSITION * MAX_OPEN_POSITION_RATIO);
 
     const MAIN_CLOSE_LONG_CONDITION1 =
       longHolding && (CONVERSE_UP_CONDITION || BATCH_LONG_CLOSE_CONDITION);
@@ -1426,8 +1424,8 @@ const checkDeal = async (data, isAutoReset = true) => {
           // const openPositionAmt =
           //   shortRatio < LOSS_MAX ? INIT_POSITION * 2 : INIT_POSITION;
           let openPositionAmt = INIT_POSITION;
-          if (BATCH_LONG_OPEN_CONDITION)
-            openPositionAmt = INIT_POSITION * (MAX_OPEN_POSITION_RATIO + 1);
+          // if (BATCH_LONG_OPEN_CONDITION)
+          //   openPositionAmt = INIT_POSITION * (MAX_OPEN_POSITION_RATIO + 1);
           // let openPositionAmt = longHolding
           //   ? Math.abs(longHolding.positionAmt)
           //   : INIT_POSITION * 1;
@@ -1520,8 +1518,8 @@ const checkDeal = async (data, isAutoReset = true) => {
           // const openPositionAmt =
           //   longRatio < LOSS_MAX ? INIT_POSITION * 2 : INIT_POSITION;
           let openPositionAmt = INIT_POSITION;
-          if (BATCH_SHORT_OPEN_CONDITION)
-            openPositionAmt = INIT_POSITION * (MAX_OPEN_POSITION_RATIO + 1);
+          // if (BATCH_SHORT_OPEN_CONDITION)
+          //   openPositionAmt = INIT_POSITION * (MAX_OPEN_POSITION_RATIO + 1);
           // let openPositionAmt = shortHolding
           //   ? Math.abs(shortHolding.positionAmt)
           //   : INIT_POSITION * 1;
