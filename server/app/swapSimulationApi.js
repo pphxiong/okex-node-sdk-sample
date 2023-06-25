fa5d9c2a59a68084364f23638ae7a535d42bbdb4
pphxiong <1017551718@qq.com>
2023-06-25 13:46:52 +0800

    new

--- server/app/swapSimulationApi.js
+++ server/app/swapSimulationApi.js
@@ -1011,11 +1011,11 @@ const checkDeal = async (data, isAutoReset = true) => {
 
 		const MAIN_OPEN_LONG_CONDITION1 =
 			// !longHolding &&
-			CONVERSE_UP_CONDITION || (shortHolding && OUT_LOW_CONDITION);
+			OUT_HIGH_CONDITION || (shortHolding && OUT_LOW_CONDITION);
 
 		const MAIN_OPEN_SHORT_CONDITION1 =
 			// !shortHolding &&
-			CONVERSE_LOW_CONDITION || (longHolding && OUT_HIGH_CONDITION);
+			OUT_LOW_CONDITION || (longHolding && OUT_HIGH_CONDITION);
 
 		const CLOSE_ALL_LONG_CONDITION =
 			longHolding &&
@@ -1034,10 +1034,9 @@ const checkDeal = async (data, isAutoReset = true) => {
 				MAX_OPEN_POSITION_RATIO;
 
 		const MAIN_CLOSE_LONG_CONDITION1 =
-			longHolding && (CONVERSE_LOW_CONDITION || CLOSE_ALL_LONG_CONDITION);
+			longHolding && (OUT_LOW_CONDITION || CLOSE_ALL_LONG_CONDITION);
 		const MAIN_CLOSE_SHORT_CONDITION1 =
-			shortHolding &&
-			(CONVERSE_UP_CONDITION || CLOSE_ALL_SHORT_CONDITION);
+			shortHolding && (OUT_HIGH_CONDITION || CLOSE_ALL_SHORT_CONDITION);
 
 		const MAIN_OPEN_LONG_CONDITION2 =
 			!longHolding && CENTER_CROSS_SHORT_CONDITION;
