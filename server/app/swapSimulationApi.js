aa5ba3f7ce335b1f532295ddba9092ed39145420
pphxiong <1017551718@qq.com>
2023-06-29 16:48:29 +0800

    bug

--- server/app/swapSimulationApi.js
+++ server/app/swapSimulationApi.js
@@ -1011,19 +1011,20 @@ const checkDeal = async (data, isAutoReset = true) => {
 
 		const MAIN_OPEN_LONG_CONDITION1 =
 			// !longHolding &&
-			CONVERSE_LOW_CONDITION;
+			CONVERSE_UP_CONDITION;
 
 		const MAIN_OPEN_SHORT_CONDITION1 =
 			// !shortHolding &&
-			CONVERSE_UP_CONDITION;
+			CONVERSE_LOW_CONDITION;
 
 		const CLOSE_ALL_LONG_CONDITION = false;
 
 		const CLOSE_ALL_SHORT_CONDITION = false;
 
-		const MAIN_CLOSE_LONG_CONDITION1 = longHolding && CONVERSE_UP_CONDITION;
+		const MAIN_CLOSE_LONG_CONDITION1 =
+			longHolding && CONVERSE_LOW_CONDITION;
 		const MAIN_CLOSE_SHORT_CONDITION1 =
-			shortHolding && CONVERSE_LOW_CONDITION;
+			shortHolding && CONVERSE_UP_CONDITION;
 
 		const MAIN_OPEN_LONG_CONDITION2 =
 			!longHolding && CENTER_CROSS_SHORT_CONDITION;
