c4e98301f417c25a5e896d89e25705da30d73b06
pphxiong <1017551718@qq.com>
2023-07-04 09:12:12 +0800

    new

--- server/app/swapSimulationApi.js
+++ server/app/swapSimulationApi.js
@@ -1028,21 +1028,18 @@ const checkDeal = async (data, isAutoReset = true) => {
 				Math.abs(Number(longHolding.positionAmt)) +
 				Math.abs(Number(shortHolding.positionAmt));
 		}
-		const CLOSE_ALL_LONG_CONDITION =
-			longHolding && CONVERSE_UP_CONDITION && longRatio > 0;
-		const CLOSE_ALL_SHORT_CONDITION =
-			shortHolding && CONVERSE_LOW_CONDITION && shortRatio > 0;
+		const CLOSE_ALL_LONG_CONDITION = false;
+		const CLOSE_ALL_SHORT_CONDITION = false;
 
-		const MAIN_OPEN_LONG_CONDITION1 = CONVERSE_LOW_CONDITION;
+		const MAIN_OPEN_LONG_CONDITION1 = CONVERSE_UP_CONDITION;
 
-		const MAIN_OPEN_SHORT_CONDITION1 = CONVERSE_UP_CONDITION;
+		const MAIN_OPEN_SHORT_CONDITION1 = CONVERSE_LOW_CONDITION;
 
 		const MAIN_CLOSE_LONG_CONDITION1 =
-			longHolding && (CENTER_CROSS_LONG_CONDITION || OUT_LOW_CONDITION);
+			longHolding && (CENTER_CROSS_SHORT_CONDITION || OUT_HIGH_CONDITION);
 
 		const MAIN_CLOSE_SHORT_CONDITION1 =
-			shortHolding &&
-			(CENTER_CROSS_SHORT_CONDITION || OUT_HIGH_CONDITION);
+			shortHolding && (CENTER_CROSS_LONG_CONDITION || OUT_LOW_CONDITION);
 
 		const MAIN_OPEN_LONG_CONDITION2 =
 			!longHolding && CENTER_CROSS_SHORT_CONDITION;
