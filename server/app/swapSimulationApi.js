6bf594317e7915f4057606c599e533f61d05b403
pphxiong <1017551718@qq.com>
2023-07-17 12:41:54 +0800

    neww

--- server/app/swapSimulationApi.js
+++ server/app/swapSimulationApi.js
@@ -1033,14 +1033,14 @@ const checkDeal = async (data, isAutoReset = true) => {
 		const RSI_SHORT = rsiList[rsiList.length - 1].RSI3 < 50;
 
 		const MACD_LONG_REVERSE =
-			Number(macdList[macdList.length - 1].column) >= 0 &&
+			Number(macdList[macdList.length - 2].column) >= 0 &&
 			Number(macdList[macdList.length - 2].column) <
 				Number(macdList[macdList.length - 1].column) &&
 			Number(macdList[macdList.length - 1].close) <
 				Number(macdList[macdList.length - 1].open);
 
 		const MACD_SHORT_REVERSE =
-			Number(macdList[macdList.length - 1].column) < 0 &&
+			Number(macdList[macdList.length - 2].column) < 0 &&
 			Number(macdList[macdList.length - 2].column) >
 				Number(macdList[macdList.length - 1].column) &&
 			Number(macdList[macdList.length - 1].close) >
@@ -1049,9 +1049,18 @@ const checkDeal = async (data, isAutoReset = true) => {
 		const MACD_LONG = Number(macdList[macdList.length - 1].column) >= 0;
 		const MACD_SHORT = Number(macdList[macdList.length - 1].column) < 0;
 
-		const MAIN_OPEN_LONG_CONDITION1 = !longHolding && MACD_LONG_REVERSE;
+		const PRICE_LOW_MA =
+			Number(macdList[macdList.length - 1].close) <
+			Number(bollList[bollList.length - 1].MA);
+		const PRICE_UP_MA =
+			Number(macdList[macdList.length - 1].close) >
+			Number(bollList[bollList.length - 1].MA);
 
-		const MAIN_OPEN_SHORT_CONDITION1 = !shortHolding && MACD_SHORT_REVERSE;
+		const MAIN_OPEN_LONG_CONDITION1 =
+			!longHolding && MACD_LONG_REVERSE && PRICE_LOW_MA;
+
+		const MAIN_OPEN_SHORT_CONDITION1 =
+			!shortHolding && MACD_SHORT_REVERSE && PRICE_UP_MA;
 
 		const CLOSE_ALL_LONG_CONDITION = false;
 		const CLOSE_ALL_SHORT_CONDITION = false;
