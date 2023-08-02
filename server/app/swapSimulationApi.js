2c6082cbc68264b4555a10482cff109e567eb48e
pphxiong <1017551718@qq.com>
2023-08-02 15:33:40 +0800

    bug

--- server/app/swapSimulationApi.js
+++ server/app/swapSimulationApi.js
@@ -27,7 +27,7 @@ function getRandomNumberByRange(start, end) {
 }
 
 // const OK_INSTRUMENT_ID = "ETH-USDT-SWAP";
-const BN_SYMBOL = 'LTCUSDT';
+const BN_SYMBOL = 'DOGEUSDT';
 const LEVERAGE = 10;
 const INTERVAL = '1h';
 const BAO_RATIO = (-0.8 * LEVERAGE) / 10;
