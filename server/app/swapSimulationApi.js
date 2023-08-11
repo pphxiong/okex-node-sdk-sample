8580337099ba2bc8a4132d0493900b06cb1ab1d0
pphxiong <1017551718@qq.com>
2023-08-11 09:54:40 +0800

    btc

--- server/app/swapSimulationApi.js
+++ server/app/swapSimulationApi.js
@@ -1092,11 +1092,11 @@ const checkDeal = async (data, isAutoReset = true) => {
 
 		const MAIN_CLOSE_LONG_CONDITION1 =
 			longHolding &&
-			((!shortHolding && CONVERSE_UP_CONDITION) ||
+			((!shortHolding && OUT_HIGH_CONDITION) ||
 				(shortHolding && CENTER_CROSS_SHORT_CONDITION));
 		const MAIN_CLOSE_SHORT_CONDITION1 =
 			shortHolding &&
-			((!longHolding && CONVERSE_LOW_CONDITION) ||
+			((!longHolding && OUT_LOW_CONDITION) ||
 				(longHolding && CENTER_CROSS_LONG_CONDITION));
 
 		// const MAIN_OPEN_LONG_CONDITION1 =
