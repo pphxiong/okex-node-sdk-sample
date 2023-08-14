5a44b55e792b97a66404490c3ee18a30d2bba891
pphxiong <1017551718@qq.com>
2023-08-14 11:12:02 +0800

    bug

--- server/app/swapSimulationApi.js
+++ server/app/swapSimulationApi.js
@@ -350,10 +350,9 @@ const checkDeal = async (data) => {
 			'minute'
 		);
 		const isFiveM =
-			true ||
-			(minuteDiff < 90 &&
-				minuteList.includes(lastMinuteCharacter) &&
-				!secondList.includes(lastSecondCharacter));
+			minuteDiff < 90 &&
+			minuteList.includes(lastMinuteCharacter) &&
+			!secondList.includes(lastSecondCharacter);
 
 		// if (isFiveM && avail < INIT_POSITION * NEW_POSITION_RATIO) {
 		//   if (openLongCondition) {
