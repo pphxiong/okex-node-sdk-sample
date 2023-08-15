fdfc3fd81fb070ced1c96ffee9615f9154fb0333
pphxiong <1017551718@qq.com>
2023-08-15 11:01:27 +0800

    bug

--- server/app/swapSimulationTest.js
+++ server/app/swapSimulationTest.js
@@ -1089,25 +1089,19 @@ const checkDeal = async (data, isAutoReset = true) => {
 
 		const MAIN_OPEN_LONG_CONDITION1 =
 			!longHolding &&
-			(CONVERSE_LOW_CONDITION ||
-				(shortHolding && OUT_HIGH_CONDITION) ||
-				CONTINUOUS_LOW_CONDITION);
+			(CONVERSE_LOW_CONDITION || (shortHolding && OUT_HIGH_CONDITION));
 		const MAIN_OPEN_SHORT_CONDITION1 =
 			!shortHolding &&
-			(CONVERSE_UP_CONDITION ||
-				(longHolding && OUT_LOW_CONDITION) ||
-				CONTINUOUS_HIGH_CONDITION);
+			(CONVERSE_UP_CONDITION || (longHolding && OUT_LOW_CONDITION));
 
 		const MAIN_CLOSE_LONG_CONDITION1 =
 			longHolding &&
 			((!shortHolding && CONVERSE_UP_CONDITION) ||
-				(shortHolding && CENTER_CROSS_SHORT_CONDITION) ||
-				CONTINUOUS_HIGH_CONDITION);
+				(shortHolding && CENTER_CROSS_SHORT_CONDITION));
 		const MAIN_CLOSE_SHORT_CONDITION1 =
 			shortHolding &&
 			((!longHolding && CONVERSE_LOW_CONDITION) ||
-				(longHolding && CENTER_CROSS_LONG_CONDITION) ||
-				CONTINUOUS_LOW_CONDITION);
+				(longHolding && CENTER_CROSS_LONG_CONDITION));
 
 		// const MAIN_OPEN_LONG_CONDITION1 =
 		// 	!longHolding && CENTER_CROSS_LONG_CONDITION;
