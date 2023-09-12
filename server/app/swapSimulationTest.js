5907cc54dfbcb7a829f90dbb8cc10e410863bea6
pphxiong <1017551718@qq.com>
2023-09-12 15:17:10 +0800

    new

--- server/app/swapSimulationTest.js
+++ server/app/swapSimulationTest.js
@@ -1111,13 +1111,23 @@ const checkDeal = async (data, isAutoReset = true) => {
 		const CLOSE_ALL_LONG_CONDITION = false;
 		const CLOSE_ALL_SHORT_CONDITION = false;
 
-		const MAIN_OPEN_LONG_CONDITION1 = !longHolding && OUT_LOW_CONDITION;
-		const MAIN_OPEN_SHORT_CONDITION1 = !shortHolding && OUT_HIGH_CONDITION;
+		const MAIN_OPEN_LONG_CONDITION1 =
+			!longHolding &&
+			((!shortHolding && OUT_LOW_CONDITION) ||
+				(shortHolding && OUT_HIGH_CONDITION));
+		const MAIN_OPEN_SHORT_CONDITION1 =
+			!shortHolding &&
+			((!longHolding && OUT_HIGH_CONDITION) ||
+				(longHolding && OUT_LOW_CONDITION));
 
 		const MAIN_CLOSE_LONG_CONDITION1 =
-			longHolding && CONVERSE_LOW_CONDITION;
+			longHolding &&
+			((!shortHolding && CONVERSE_LOW_CONDITION) ||
+				(shortHolding && CONVERSE_UP_CONDITION));
 		const MAIN_CLOSE_SHORT_CONDITION1 =
-			shortHolding && CONVERSE_UP_CONDITION;
+			shortHolding &&
+			((!longHolding && CONVERSE_UP_CONDITION) ||
+				(longHolding && CONVERSE_LOW_CONDITION));
 
 		// const MAIN_OPEN_LONG_CONDITION1 =
 		// 	!longHolding &&
