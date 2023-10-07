4e96245a2d9eaa5a6a03a530eacc34dfb33e14fd
pphxiong <1017551718@qq.com>
2023-10-07 16:28:03 +0800

    new

--- server/app/swapSimulationTest.js
+++ server/app/swapSimulationTest.js
@@ -1212,12 +1212,12 @@ const checkDeal = async (data, isAutoReset = true) => {
 			!longHolding &&
 			(OUT_LOW_CONDITION ||
 				(shortHolding && CONVERSE_UP_CONDITION) ||
-				(!shortHolding && CENTER_CROSS_LONG_CONDITION));
+				/* !shortHolding && */ CENTER_CROSS_LONG_CONDITION);
 		const MAIN_OPEN_SHORT_CONDITION1 =
 			!shortHolding &&
 			(OUT_HIGH_CONDITION ||
 				(longHolding && CONVERSE_LOW_CONDITION) ||
-				(!longHolding && CENTER_CROSS_SHORT_CONDITION));
+				/* !longHolding  && */ CENTER_CROSS_SHORT_CONDITION);
 
 		const MAIN_CLOSE_LONG_CONDITION1 =
 			longHolding &&
