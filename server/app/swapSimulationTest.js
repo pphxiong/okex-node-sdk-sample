f3fc49db8400968b630d9e7cc24d5318d2689da1
pphxiong <1017551718@qq.com>
2023-10-07 15:50:26 +0800

    new

--- server/app/swapSimulationTest.js
+++ server/app/swapSimulationTest.js
@@ -1208,27 +1208,27 @@ const checkDeal = async (data, isAutoReset = true) => {
 			Number(macdList[macdList.length - 1].close) <
 				Number(bollList[bollList.length - 1].DN);
 
-		const MAIN_OPEN_LONG_CONDITION1 =
-			!longHolding &&
-			(OUT_LOW_CONDITION ||
-				(shortHolding && CONVERSE_UP_CONDITION) ||
-				(!shortHolding && CENTER_CROSS_LONG_CONDITION));
-		const MAIN_OPEN_SHORT_CONDITION1 =
-			!shortHolding &&
-			(OUT_HIGH_CONDITION ||
-				(longHolding && CONVERSE_LOW_CONDITION) ||
-				(!longHolding && CENTER_CROSS_SHORT_CONDITION));
+		// const MAIN_OPEN_LONG_CONDITION1 =
+		// 	!longHolding &&
+		// 	(OUT_LOW_CONDITION ||
+		// 		(shortHolding && CONVERSE_UP_CONDITION) ||
+		// 		(!shortHolding && CENTER_CROSS_LONG_CONDITION));
+		// const MAIN_OPEN_SHORT_CONDITION1 =
+		// 	!shortHolding &&
+		// 	(OUT_HIGH_CONDITION ||
+		// 		(longHolding && CONVERSE_LOW_CONDITION) ||
+		// 		(!longHolding && CENTER_CROSS_SHORT_CONDITION));
 
-		const MAIN_CLOSE_LONG_CONDITION1 =
-			longHolding &&
-			((!shortHolding && OUT_HIGH_CONDITION) ||
-				(shortHolding &&
-					(CENTER_CROSS_SHORT_CONDITION || OUT_LOW_CONDITION)));
-		const MAIN_CLOSE_SHORT_CONDITION1 =
-			shortHolding &&
-			((!longHolding && OUT_LOW_CONDITION) ||
-				(longHolding &&
-					(CENTER_CROSS_LONG_CONDITION || OUT_HIGH_CONDITION)));
+		// const MAIN_CLOSE_LONG_CONDITION1 =
+		// 	longHolding &&
+		// 	((!shortHolding && OUT_HIGH_CONDITION) ||
+		// 		(shortHolding &&
+		// 			(CENTER_CROSS_SHORT_CONDITION || OUT_LOW_CONDITION)));
+		// const MAIN_CLOSE_SHORT_CONDITION1 =
+		// 	shortHolding &&
+		// 	((!longHolding && OUT_LOW_CONDITION) ||
+		// 		(longHolding &&
+		// 			(CENTER_CROSS_LONG_CONDITION || OUT_HIGH_CONDITION)));
 
 		// const MAIN_OPEN_LONG_CONDITION1 =
 		// 	!longHolding &&
@@ -1284,15 +1284,15 @@ const checkDeal = async (data, isAutoReset = true) => {
 		// 		(longHolding &&
 		// 			CONVERSE_UP_CONDITION));
 
-		// const MAIN_OPEN_LONG_CONDITION1 =
-		// 	!longHolding && CENTER_CROSS_LONG_CONDITION;
-		// const MAIN_OPEN_SHORT_CONDITION1 =
-		// 	!shortHolding && CENTER_CROSS_SHORT_CONDITION;
+		const MAIN_OPEN_LONG_CONDITION1 =
+			!longHolding && CENTER_CROSS_LONG_CONDITION;
+		const MAIN_OPEN_SHORT_CONDITION1 =
+			!shortHolding && CENTER_CROSS_SHORT_CONDITION;
 
-		// const MAIN_CLOSE_LONG_CONDITION1 =
-		// 	longHolding && CENTER_CROSS_SHORT_CONDITION;
-		// const MAIN_CLOSE_SHORT_CONDITION1 =
-		// 	shortHolding && CENTER_CROSS_LONG_CONDITION;
+		const MAIN_CLOSE_LONG_CONDITION1 =
+			longHolding && CENTER_CROSS_SHORT_CONDITION;
+		const MAIN_CLOSE_SHORT_CONDITION1 =
+			shortHolding && CENTER_CROSS_LONG_CONDITION;
 
 		const MAIN_OPEN_LONG_CONDITION2 =
 			!longHolding && CENTER_CROSS_SHORT_CONDITION;
