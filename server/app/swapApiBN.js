a9051496a81e2ebb0c218c173b2e79a9b14415e1
pphxiong <1017551718@qq.com>
2023-11-16 16:44:44 +0800

    bug

--- server/app/swapApiBN.js
+++ server/app/swapApiBN.js
@@ -184,8 +184,8 @@ async function checkByStep(data, ethData) {
 
 	console.log('************************************', currentTime);
 	console.log('isFiveM', isFiveM, lastMinuteCharacter);
-	console.log('macdList', macdList.slice(-2));
-	console.log('bollList', bollList.slice(-2));
+	// console.log('macdList', macdList.slice(-2));
+	// console.log('bollList', bollList.slice(-2));
 	console.log('longRatio', longRatio, 'shortRatio', shortRatio);
 	console.log(
 		'longPositionAmt',
