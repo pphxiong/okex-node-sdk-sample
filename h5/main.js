4c0a821ad19b42ea56aec94ea14234cf34c3e092
pphxiong <1017551718@qq.com>
2023-11-17 10:45:39 +0800

    bug

--- h5/main.js
+++ h5/main.js
@@ -1,4 +1,6 @@
 const express = require('express');
+// 跨域支持 官方文档：https://github.com/expressjs/cors
+const cors = require('cors');
 
 // // 引入外部路由文件
 // const appRouter = require('./router/appRouter')
@@ -7,6 +9,8 @@ const app = express();
 const port = 80;
 
 app.use(express.json());
+// 开启所有请求都支持跨域
+app.use(cors());
 
 // 注册路由, 注册的路由模块接口访问都需要加上 注册路由的前缀
 // app.use('/app', appRouter)
