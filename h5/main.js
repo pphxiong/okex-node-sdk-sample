e06ae45a59af448ca9784763208f916d319176e3
pphxiong <1017551718@qq.com>
2023-11-17 10:40:42 +0800

    bug

---
+++ h5/main.js
@@ -0,0 +1,24 @@
+const express = require('express');
+
+// // 引入外部路由文件
+// const appRouter = require('./router/appRouter')
+// const adminRouter = require('./router/adminRouter')
+const app = express();
+const port = 80;
+
+app.use(express.json());
+
+// 注册路由, 注册的路由模块接口访问都需要加上 注册路由的前缀
+// app.use('/app', appRouter)
+// app.use('/admin', adminRouter)
+
+// 处理 / 根请求
+app.get('/', (req, res) => {
+	const filePath = __dirname + '/index.html';
+
+	res.sendFile(filePath);
+});
+
+app.listen(port, () => {
+	console.log(`node服务已启动 端口号为： ${port}`);
+});
