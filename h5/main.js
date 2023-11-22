757363b36a5bb1a7346524640813cd7789001b0b
pphxiong <1017551718@qq.com>
2023-11-22 15:33:04 +0800

    new

--- h5/main.js
+++ h5/main.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 
 const express = require('express');
+// 用来解决 history 模式404问题
+const history = require('connect-history-api-fallback');
 // 跨域支持 官方文档：https://github.com/expressjs/cors
 const cors = require('cors');
 
@@ -10,25 +12,19 @@ const cors = require('cors');
 const app = express();
 const port = 80;
 
+app.use(history());
 app.use(express.json());
-app.use(express.static('www'));
+app.use(express.static(__dirname + '/www'));
+
 // 开启所有请求都支持跨域
 app.use(cors());
 
 // 处理 / 根请求
-app.get('/static', (req, res) => {
-	console.log(11, req.url);
-	const filePath = `${__dirname}/${req.url}`;
-
-	// fs.readFile(`${__dirname}/www${req.url}`, (err, data) => {
-	// 	if (err) {
-	// 		throw err;
-	// 	} else {
-	// 		res.end(data);
-	// 	}
-	// });
-	res.sendFile(filePath);
-});
+// app.get('/static', (req, res) => {
+// 	console.log(11, req.url);
+// 	const filePath = `${__dirname}/${req.url}`;
+// 	res.sendFile(filePath);
+// });
 
 app.listen(port, () => {
 	console.log(`node服务已启动 端口号为： ${port}`);
