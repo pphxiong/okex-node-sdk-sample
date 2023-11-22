015042b49a4064ceb39f02afb333e638df811818
pphxiong <1017551718@qq.com>
2023-11-22 15:08:33 +0800

    bug

--- h5/main.js
+++ h5/main.js
@@ -1,22 +1,52 @@
-const express = require('express');
-// 跨域支持 官方文档：https://github.com/expressjs/cors
-const cors = require('cors');
+/*
+1.html中所有的外部链接（href、src)，都会变成网络请求
+*/
 
-// // 引入外部路由文件
-// const appRouter = require('./router/appRouter')
-// const adminRouter = require('./router/adminRouter')
-const app = express();
-const port = 80;
+// 1导入模块
+const http = require('http');
+const fs = require('fs');
 
-app.use(express.json());
-app.use(express.static('www'));
-app.use(express.static('www/static'));
-// 开启所有请求都支持跨域
-app.use(cors());
+// 2创建服务器
+const app = http.createServer((req, res) => {
+	// (1)req请求报文
+	console.log(req.url);
+	// (2)处理数据
+	if (req.url === '/') {
+		// (3)res响应报文
+		fs.readFile(`${__dirname}/www/index.html`, (err, data) => {
+			if (err) {
+				throw err;
+			} else {
+				res.end(data);
+			}
+		});
+	} else if (req.url.startsWith('/static')) {
+		fs.readFile(`${__dirname}/www${req.url}`, (err, data) => {
+			if (err) {
+				throw err;
+			} else {
+				res.end(data);
+			}
+		});
+	} else {
+		res.end('404 NOT FOUND');
+	}
+});
 
-// 注册路由, 注册的路由模块接口访问都需要加上 注册路由的前缀
-// app.use('/app', appRouter)
-// app.use('/admin', adminRouter)
+// const express = require('express');
+// // 跨域支持 官方文档：https://github.com/expressjs/cors
+// const cors = require('cors');
+
+// // // 引入外部路由文件
+// // const appRouter = require('./router/appRouter')
+// // const adminRouter = require('./router/adminRouter')
+// const app = express();
+// const port = 80;
+
+// app.use(express.json());
+// app.use(express.static('www'));
+// // 开启所有请求都支持跨域
+// app.use(cors());
 
 // 处理 / 根请求
 // app.get('/', (req, res) => {
