4771348a82f02eba2dc8cd6688cd71f25379483a
pphxiong <1017551718@qq.com>
2023-12-13 17:09:20 +0800

    bug

--- server/app/swapSimulationTest.js
+++ server/app/swapSimulationTest.js
@@ -988,10 +988,11 @@ const checkDeal = async (data, ethData, isAutoReset = true) => {
 
 		const PATCH_CONDITION =
 			// false &&
-			CLOSE_LOSS_CONDITION && longRatio > 0;
-		// (Math.abs(Number(shortHolding.positionAmt)) * eth_mark_price) /
-		// 	LEVERAGE <
-		// 	INIT_ASSETS;
+			CLOSE_LOSS_CONDITION &&
+			longRatio > 0 &&
+			(Math.abs(Number(shortHolding.positionAmt)) * eth_mark_price) /
+				LEVERAGE <
+				INIT_ASSETS;
 
 		// if (REVERSE_MODE_CONDITION) MODE = MODE == 1 ? 2 : 1;
 		if (CLOSE_WIN_CONDITION) {
@@ -1364,18 +1365,18 @@ const checkDeal = async (data, ethData, isAutoReset = true) => {
 							eth_mark_price
 						).toFixed(3)
 					);
-					// if (PATCH_CONDITION) {
-					// 	const currentAssets =
-					// 		(Math.abs(Number(shortHolding.positionAmt)) *
-					// 			eth_mark_price) /
-					// 		LEVERAGE;
-					// 	openPositionAmt = Number(
-					// 		(
-					// 			((INIT_ASSETS - currentAssets) * LEVERAGE) /
-					// 			eth_mark_price
-					// 		).toFixed(3)
-					// 	);
-					// }
+					if (PATCH_CONDITION) {
+						const currentAssets =
+							(Math.abs(Number(shortHolding.positionAmt)) *
+								eth_mark_price) /
+							LEVERAGE;
+						openPositionAmt = Number(
+							(
+								((INIT_ASSETS - currentAssets) * LEVERAGE) /
+								eth_mark_price
+							).toFixed(3)
+						);
+					}
 					// if (BATCH_SHORT_OPEN_CONDITION)
 					//   openPositionAmt = INIT_POSITION * (MAX_OPEN_POSITION_RATIO + 1);
 					// let openPositionAmt = shortHolding
