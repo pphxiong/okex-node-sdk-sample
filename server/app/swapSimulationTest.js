8e600e23c539b5f9615abae96d0c0c298e5f5eb7
pphxiong <1017551718@qq.com>
2023-12-14 16:06:46 +0800

    bug

--- server/app/swapSimulationTest.js
+++ server/app/swapSimulationTest.js
@@ -1372,7 +1372,7 @@ const checkDeal = async (data, ethData, isAutoReset = true) => {
 							LEVERAGE;
 						openPositionAmt = Number(
 							(
-								(((INIT_ASSETS - currentAssets) / 2) *
+								((((INIT_ASSETS - currentAssets) * 3) / 5) *
 									LEVERAGE) /
 								eth_mark_price
 							).toFixed(3)
