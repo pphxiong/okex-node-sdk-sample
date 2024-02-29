dc5d660827b2f1fa1b740f0b3dd356afc09a54b8
pphxiong <1017551718@qq.com>
2024-02-29 16:05:08 +0800

    bug

--- server/app/swapApiBN.js
+++ server/app/swapApiBN.js
@@ -10,7 +10,7 @@ const INIT_POSITION = 100;
 
 let MODE = 1;
 const WIN_MAX = 1 * 0.0618 * 2;
-const LOSS_MAX = -1 * 0.182;
+const LOSS_MAX = -1 * 0.0618;
 const MAX_OFFSET_RATIO = 0.0618;
 const LEVERAGE = 20;
 const INIT_ASSETS = (300 * 1.2) / 2;
