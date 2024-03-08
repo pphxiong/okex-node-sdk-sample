ee507168f2542337f5b611e062efe7a335aa94c0
pphxiong <1017551718@qq.com>
2024-03-08 09:25:29 +0800

    new

--- server/app/swapApiBN.js
+++ server/app/swapApiBN.js
@@ -13,7 +13,7 @@ const WIN_MAX = 1 * 0.0618 * 4;
 const LOSS_MAX = -1 * 0.0618;
 const MAX_OFFSET_RATIO = 0.0618 * 2;
 const LEVERAGE = 20;
-const INIT_ASSETS = 40 / 2;
+const INIT_ASSETS = 40 * 2;
 const INIT_ASSETS_RATIO = 1;
 const MAX_SHORT_ASSETS_RATIO = 1 / 2;
 
