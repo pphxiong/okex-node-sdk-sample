f5e57126ca3e85f0328a8097e56e061ee77ad298
pphxiong <1017551718@qq.com>
2024-04-28 13:54:48 +0800

    b

--- server/app/swapApiBN.js
+++ server/app/swapApiBN.js
@@ -78,6 +78,7 @@ const fnIsCurrentContinousLower = (macdList, i) => {
 };
 
 const fnIsMacdReverse = (macdList) => {
+	console.log(3, macdList.slice(-10));
 	const isUpper =
 		macdList[macdList.length - 1].column >
 		macdList[macdList.length - 2].column;
