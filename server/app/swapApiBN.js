7ddb5338bed451eba4434008bd31d9c3a579d2e9
pphxiong <1017551718@qq.com>
2024-08-05 10:53:55 +0800

    n

--- server/app/swapApiBN.js
+++ server/app/swapApiBN.js
@@ -188,7 +188,7 @@ const fnGetPositionAndDeal = async (currentResult, lastResult) => {
 		minSymbol: minSymbolLast,
 		symbolRatioList: symbolRatioListLast,
 	} = lastResult;
-	const { longNum, shortNum } = fnGetConditionNum(symbolRatioList);
+	const { longNum } = fnGetConditionNum(symbolRatioList);
 	const { longNum: longNumLast } = fnGetConditionNum(symbolRatioListLast);
 	const currentCondition = longNum >= 3;
 	const lastCondition = longNumLast >= 3;
@@ -265,8 +265,6 @@ const fnGetPositionAndDeal = async (currentResult, lastResult) => {
 		minSymbolLast,
 		'longNum',
 		longNum,
-		'shortNum',
-		shortNum,
 		'longNumLast',
 		longNumLast
 	);
