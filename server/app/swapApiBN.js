1b4af9590f1f3c953157d735da1a2d03411a7d16
pphxiong <1017551718@qq.com>
2024-08-10 04:29:48 +0800

    n

--- server/app/swapApiBN.js
+++ server/app/swapApiBN.js
@@ -1784,7 +1784,7 @@ const countdownCancelAll = async (symbol, time = 1000 * 2) => {
 const fnGetSymbolResult = async (symbol, payload) => {
   const list = await cAuthClientBN.common.getHistory(symbol, payload);
   const newList = JSON.parse(JSON.stringify(list));
-  newList.pop();
+  // newList.pop();
 
   const bollList = getCurrentBOLL(newList);
   const macdList = getCurrentMacd(newList);
