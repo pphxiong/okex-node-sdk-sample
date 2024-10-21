3f4ee3bad920bbcfd2d40adafc8db8e64e7d8d81
pphxiong <1017551718@qq.com>
2024-10-21 11:09:27 +0800

    n

--- server/app/swapSimulationTest.js
+++ server/app/swapSimulationTest.js
@@ -13,7 +13,7 @@ const fs = require('fs');
 
 const isContinousLong = (list, k) => {
 	let isC = true;
-	for (let i = list.length - 1; i >= list.length - k; i -= 1) {
+	for (let i = list.length - 1; i > list.length - k; i -= 1) {
 		if (list[i].close < list[i].open) {
 			isC = false;
 			break;
@@ -24,7 +24,7 @@ const isContinousLong = (list, k) => {
 
 const isContinousShort = (list, k) => {
 	let isC = true;
-	for (let i = list.length - 1; i >= list.length - k; i -= 1) {
+	for (let i = list.length - 1; i > list.length - k; i -= 1) {
 		if (list[i].close > list[i].open) {
 			isC = false;
 			break;
