de47c40fe96e3a6026211424263a95fb94c8ac5f
pphxiong <1017551718@qq.com>
2024-10-21 11:14:03 +0800

    n

--- server/app/swapSimulationTest.js
+++ server/app/swapSimulationTest.js
@@ -836,14 +836,14 @@ const checkDeal = async (data, isAutoReset = true) => {
 		// (longRatio >= 0 || longRatio <= LOSS_MAX);
 
 		const MAIN_OPEN_LONG_CONDITION1 =
-			!longHolding && isContinousShort(macdList, 5);
+			!longHolding && isContinousShort(macdList, 4);
 		const MAIN_OPEN_SHORT_CONDITION1 =
-			!shortHolding && isContinousLong(macdList, 5);
+			!shortHolding && isContinousLong(macdList, 4);
 
 		const MAIN_CLOSE_LONG_CONDITION1 =
-			longHolding && isContinousLong(macdList, 3);
+			longHolding && isContinousLong(macdList, 2);
 		const MAIN_CLOSE_SHORT_CONDITION1 =
-			shortHolding && isContinousShort(macdList, 3);
+			shortHolding && isContinousShort(macdList, 2);
 
 		if (modeChange) lastMode = lastMode ? 0 : 1;
 
