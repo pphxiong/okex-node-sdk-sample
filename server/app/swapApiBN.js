ccc69c76cfd925a5c816dddf2b6fa6e5bde9327e
pphxiong <1017551718@qq.com>
2024-10-22 17:41:27 +0800

    n

--- server/app/swapApiBN.js
+++ server/app/swapApiBN.js
@@ -64,7 +64,7 @@ const MAX_OFFSET_RATIO = Math.abs(LOSS_MAX);
 
 let RESTART_TIME = 0;
 let MODE = 1;
-const DEFAULT_INTERVAL = '5m';
+const DEFAULT_INTERVAL = '1h';
 const INIT_POSITION = 100;
 let rsi1 = 8;
 let rsi2 = 12;
