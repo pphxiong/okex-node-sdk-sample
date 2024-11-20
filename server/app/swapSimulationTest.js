07e9057554ef0044cd517be7f5161c151ae38522
pphxiong <1017551718@qq.com>
2024-11-20 17:27:29 +0800

    n

--- server/app/swapSimulationTest.js
+++ server/app/swapSimulationTest.js
@@ -877,13 +877,21 @@ const checkDeal = async (data, isAutoReset = true) => {
 			longHolding &&
 			((rsiList[rsiList.length - 1].RSI2 < OPENCONTINOUS + 20 &&
 				rsiList[rsiList.length - 2].RSI2 > OPENCONTINOUS + 20 &&
-				(longRatio > 0 || (longRatio < 0 && shortRatio < 0))) ||
+				(longRatio > 0 ||
+					(longRatio < 0 &&
+						shortRatio < 0 &&
+						rsiList[rsiList.length - 1].RSI2 < CLOSECONTINOUS &&
+						rsiList[rsiList.length - 2].RSI2 > CLOSECONTINOUS))) ||
 				isForceDeal);
 		const MAIN_CLOSE_SHORT_CONDITION1 =
 			shortHolding &&
 			((rsiList[rsiList.length - 1].RSI2 > CLOSECONTINOUS - 20 &&
 				rsiList[rsiList.length - 2].RSI2 < CLOSECONTINOUS - 20 &&
-				(shortRatio > 0 || (longRatio < 0 && shortRatio < 0))) ||
+				(shortRatio > 0 ||
+					(longRatio < 0 &&
+						shortRatio < 0 &&
+						rsiList[rsiList.length - 1].RSI2 > OPENCONTINOUS &&
+						rsiList[rsiList.length - 2].RSI2 < OPENCONTINOUS))) ||
 				isForceDeal);
 
 		if (modeChange) lastMode = lastMode ? 0 : 1;
