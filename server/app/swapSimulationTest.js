191e66306f2853f7c9f4177ddb965b7a446bffac
pphxiong <1017551718@qq.com>
2024-11-20 13:18:06 +0800

    n

--- server/app/swapSimulationTest.js
+++ server/app/swapSimulationTest.js
@@ -860,23 +860,24 @@ const checkDeal = async (data, isAutoReset = true) => {
 
 		const MAIN_OPEN_LONG_CONDITION1 =
 			!longHolding &&
-			rsiList[rsiList.length - 1].RSI2 < CLOSECONTINOUS &&
-			rsiList[rsiList.length - 1].RSI2 > CLOSECONTINOUS - 20;
-
+			rsiList[rsiList.length - 1].RSI2 > OPENCONTINOUS &&
+			rsiList[rsiList.length - 2].RSI2 < OPENCONTINOUS;
 		const MAIN_OPEN_SHORT_CONDITION1 =
 			!shortHolding &&
-			rsiList[rsiList.length - 1].RSI2 > OPENCONTINOUS &&
-			rsiList[rsiList.length - 1].RSI2 < OPENCONTINOUS + 20;
+			rsiList[rsiList.length - 1].RSI2 < CLOSECONTINOUS &&
+			rsiList[rsiList.length - 2].RSI2 > CLOSECONTINOUS;
 
 		const MAIN_CLOSE_LONG_CONDITION1 =
 			longHolding &&
-			(rsiList[rsiList.length - 1].RSI2 > CLOSECONTINOUS + 10 ||
-				rsiList[rsiList.length - 1].RSI2 < CLOSECONTINOUS - 20 ||
+			((rsiList[rsiList.length - 1].RSI2 < OPENCONTINOUS &&
+				rsiList[rsiList.length - 2].RSI2 > OPENCONTINOUS &&
+				(longRatio > 0 || (longRatio < 0 && shortRatio < 0))) ||
 				isForceDeal);
 		const MAIN_CLOSE_SHORT_CONDITION1 =
 			shortHolding &&
-			(rsiList[rsiList.length - 1].RSI2 < OPENCONTINOUS - 10 ||
-				rsiList[rsiList.length - 1].RSI2 > OPENCONTINOUS + 20 ||
+			((rsiList[rsiList.length - 1].RSI2 > CLOSECONTINOUS &&
+				rsiList[rsiList.length - 2].RSI2 < CLOSECONTINOUS &&
+				(shortRatio > 0 || (longRatio < 0 && shortRatio < 0))) ||
 				isForceDeal);
 
 		if (modeChange) lastMode = lastMode ? 0 : 1;
