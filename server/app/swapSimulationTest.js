67594e2985607674d2d94f2fcd7906621d299bb7
pphxiong <1017551718@qq.com>
2024-11-20 10:49:43 +0800

    n

--- server/app/swapSimulationTest.js
+++ server/app/swapSimulationTest.js
@@ -870,12 +870,12 @@ const checkDeal = async (data, isAutoReset = true) => {
 
 		const MAIN_CLOSE_LONG_CONDITION1 =
 			longHolding &&
-			(rsiList[rsiList.length - 1].RSI2 > CLOSECONTINOUS + 20 ||
+			(rsiList[rsiList.length - 1].RSI2 > CLOSECONTINOUS + 10 ||
 				rsiList[rsiList.length - 1].RSI2 < CLOSECONTINOUS - 20 ||
 				isForceDeal);
 		const MAIN_CLOSE_SHORT_CONDITION1 =
 			shortHolding &&
-			(rsiList[rsiList.length - 1].RSI2 < OPENCONTINOUS - 20 ||
+			(rsiList[rsiList.length - 1].RSI2 < OPENCONTINOUS - 10 ||
 				rsiList[rsiList.length - 1].RSI2 > OPENCONTINOUS + 20 ||
 				isForceDeal);
 
