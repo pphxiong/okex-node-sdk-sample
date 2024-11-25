34097663a63d278aa1ca62ad03656f9300097396
pphxiong <1017551718@qq.com>
2024-11-25 20:50:22 +0800

    n

--- server/app/swapSimulationTest.js
+++ server/app/swapSimulationTest.js
@@ -1102,7 +1102,7 @@ const fnIsMacdReverse = (macdList) => {
 };
 
 const checkDeal = async (data, isAutoReset = true) => {
-  console.log(334, data.bollList.length);
+  console.log(334, data.bollList.length, data.macdList.length);
   for (let i = 0; i < data.bollList.length - 3; i++) {
     checkByStep(
       {
