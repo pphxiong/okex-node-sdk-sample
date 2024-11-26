291d3a559f84f9699febe4e2dfbc8ddc4d2ac1fe
pphxiong <1017551718@qq.com>
2024-11-26 16:52:02 +0800

    n

--- server/app/swapSimulationTest.js
+++ server/app/swapSimulationTest.js
@@ -1300,30 +1300,30 @@ const checkDeal = async (data, isForceDeal = false) => {
 		const MAIN_OPEN_LONG_CONDITION1 =
 			!longHolding &&
 			macdList[macdList.length - 1].close >
-				bollList[bollList.length - 1].MA &&
+				bollList[bollList.length - 1].DN &&
 			macdList[macdList.length - 2].close <
-				bollList[bollList.length - 2].MA;
+				bollList[bollList.length - 2].DN;
 		const MAIN_OPEN_SHORT_CONDITION1 =
 			!shortHolding &&
 			macdList[macdList.length - 1].close <
-				bollList[bollList.length - 1].MA &&
+				bollList[bollList.length - 1].UP &&
 			macdList[macdList.length - 2].close >
-				bollList[bollList.length - 2].MA;
+				bollList[bollList.length - 2].UP;
 
 		const MAIN_CLOSE_LONG_CONDITION1 =
 			longHolding &&
 			((macdList[macdList.length - 1].close <
-				bollList[bollList.length - 1].DN &&
+				bollList[bollList.length - 1].UP &&
 				macdList[macdList.length - 2].close >
-					bollList[bollList.length - 2].DN &&
+					bollList[bollList.length - 2].UP &&
 				longRatio > 0) ||
 				isForceDeal);
 		const MAIN_CLOSE_SHORT_CONDITION1 =
 			shortHolding &&
 			((macdList[macdList.length - 1].close >
-				bollList[bollList.length - 1].UP &&
+				bollList[bollList.length - 1].DN &&
 				macdList[macdList.length - 2].close <
-					bollList[bollList.length - 2].UP &&
+					bollList[bollList.length - 2].DN &&
 				shortRatio > 0) ||
 				isForceDeal);
 
