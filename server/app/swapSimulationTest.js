aaf709774c35c2a31f34aa21be03fa3fbd5e5cb9
pphxiong <1017551718@qq.com>
2024-11-27 08:53:04 +0800

    n

--- server/app/swapSimulationTest.js
+++ server/app/swapSimulationTest.js
@@ -670,9 +670,9 @@ app.get('/swap/getLatestProfit', async (req, response) => {
 			isContinousAutoClose === true || isContinousAutoClose === 'true';
 
 		const newList = JSON.parse(JSON.stringify(list));
-		const bollList = getCurrentBOLL(newList).slice(-1400);
 		const macdList = getCurrentMacd(newList).slice(-1400);
 		const rsiList = getCurrentRSI(newList).slice(-1400);
+		const bollList = getCurrentBOLL(newList).slice(-1400);
 
 		const result = {
 			macdList,
