42286f5537be8da0c1376c66db27effd28b78c0c
pphxiong <1017551718@qq.com>
2024-12-27 14:41:57 +0800

    ng

--- server/app/swapSimulationTest.js
+++ server/app/swapSimulationTest.js
@@ -1319,27 +1319,27 @@ const checkDeal = async (data, isForceDeal = false) => {
 			!longHolding &&
 			macdList[macdList.length - 1].ema7 >=
 				macdList[macdList.length - 1].ema99 &&
-			macdList[macdList.length - 1].ema7 <=
-				macdList[macdList.length - 1].ema99;
+			macdList[macdList.length - 2].ema7 <=
+				macdList[macdList.length - 2].ema99;
 		const MAIN_OPEN_SHORT_CONDITION1 =
 			!shortHolding &&
 			macdList[macdList.length - 1].ema7 <=
 				macdList[macdList.length - 1].ema99 &&
-			macdList[macdList.length - 1].ema7 >=
-				macdList[macdList.length - 1].ema99;
+			macdList[macdList.length - 2].ema7 >=
+				macdList[macdList.length - 2].ema99;
 
 		const MAIN_CLOSE_LONG_CONDITION1 =
 			longHolding &&
 			macdList[macdList.length - 1].ema7 <=
 				macdList[macdList.length - 1].ema99 &&
-			macdList[macdList.length - 1].ema7 >=
-				macdList[macdList.length - 1].ema99;
+			macdList[macdList.length - 2].ema7 >=
+				macdList[macdList.length - 2].ema99;
 		const MAIN_CLOSE_SHORT_CONDITION1 =
 			shortHolding &&
 			macdList[macdList.length - 1].ema7 >=
 				macdList[macdList.length - 1].ema99 &&
-			macdList[macdList.length - 1].ema7 <=
-				macdList[macdList.length - 1].ema99;
+			macdList[macdList.length - 2].ema7 <=
+				macdList[macdList.length - 2].ema99;
 
 		if (modeChange) lastMode = lastMode ? 0 : 1;
 
