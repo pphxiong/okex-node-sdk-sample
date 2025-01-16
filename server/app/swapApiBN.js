6cc5459d5d4bde00e0b2be22910f4173008f611c
pphxiong <1017551718@qq.com>
2025-01-16 13:56:23 +0800

    n

--- server/app/swapApiBN.js
+++ server/app/swapApiBN.js
@@ -1351,8 +1351,8 @@ async function checkByStep(data, symbol) {
 	const MAIN_CLOSE_ALL_CONDITION =
 		false && (CLOSE_WIN_CONDITION || CLOSE_LOSS_CONDITION);
 
-	let openLongCondition = MAIN_OPEN_LONG_CONDITION1;
-	let openShortCondition = MAIN_OPEN_SHORT_CONDITION1;
+	let openLongCondition = MAIN_OPEN_LONG_CONDITION1 && false;
+	let openShortCondition = MAIN_OPEN_SHORT_CONDITION1 && false;
 	let closeLongCondition = MAIN_CLOSE_LONG_CONDITION1;
 	let closeShortCondition = MAIN_CLOSE_SHORT_CONDITION1;
 
