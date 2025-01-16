25ba4c669072b5ed5129c54067c1a44536aecb8b
pphxiong <1017551718@qq.com>
2025-01-16 11:23:12 +0800

    n

--- server/app/swapApiBN.js
+++ server/app/swapApiBN.js
@@ -1325,24 +1325,24 @@ async function checkByStep(data, symbol) {
 	const MAIN_OPEN_LONG_CONDITION1 =
 		!longHolding &&
 		macdList[macdList.length - 1].ema7 >=
-			macdList[macdList.length - 1].ema99 &&
+			macdList[macdList.length - 1].ema60 &&
 		macdList[macdList.length - 2].ema7 <=
-			macdList[macdList.length - 2].ema99;
+			macdList[macdList.length - 2].ema60;
 	const MAIN_OPEN_SHORT_CONDITION1 =
 		!shortHolding &&
 		macdList[macdList.length - 1].ema7 <=
-			macdList[macdList.length - 1].ema99 &&
+			macdList[macdList.length - 1].ema60 &&
 		macdList[macdList.length - 2].ema7 >=
-			macdList[macdList.length - 2].ema99;
+			macdList[macdList.length - 2].ema60;
 
 	const MAIN_CLOSE_LONG_CONDITION1 =
 		longHolding &&
 		macdList[macdList.length - 1].ema7 <=
-			macdList[macdList.length - 1].ema99;
+			macdList[macdList.length - 1].ema60;
 	const MAIN_CLOSE_SHORT_CONDITION1 =
 		shortHolding &&
 		macdList[macdList.length - 1].ema7 >=
-			macdList[macdList.length - 1].ema99;
+			macdList[macdList.length - 1].ema60;
 
 	const MAIN_CLOSE_ALL_CONDITION =
 		false && (CLOSE_WIN_CONDITION || CLOSE_LOSS_CONDITION);
