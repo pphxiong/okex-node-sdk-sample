5edeaeb1fec33f7a8d2dd2746c672459ed8f2e9d
pphxiong <1017551718@qq.com>
2025-02-18 11:30:52 +0800

    n

--- server/app/swapSimulationTest.js
+++ server/app/swapSimulationTest.js
@@ -1301,28 +1301,28 @@ const checkDeal = async (data, isForceDeal = true) => {
 
 		const MAIN_OPEN_LONG_CONDITION1 =
 			!longHolding &&
-			rsiList[rsiList.length - 1].RSI1 > 50 &&
-			rsiList[rsiList.length - 1].RSI2 > 50 &&
-			rsiList[rsiList.length - 1].RSI3 > 50 &&
+			(rsiList[rsiList.length - 1].RSI1 < 50 ||
+				rsiList[rsiList.length - 1].RSI2 < 50 ||
+				rsiList[rsiList.length - 1].RSI3 < 50) &&
 			!isForceDeal;
 		const MAIN_OPEN_SHORT_CONDITION1 =
 			!shortHolding &&
-			rsiList[rsiList.length - 1].RSI1 < 50 &&
-			rsiList[rsiList.length - 1].RSI2 < 50 &&
-			rsiList[rsiList.length - 1].RSI3 < 50 &&
+			(rsiList[rsiList.length - 1].RSI1 > 50 ||
+				rsiList[rsiList.length - 1].RSI2 > 50 ||
+				rsiList[rsiList.length - 1].RSI3 > 50) &&
 			!isForceDeal;
 
 		const MAIN_CLOSE_LONG_CONDITION1 =
 			longHolding &&
-			(rsiList[rsiList.length - 1].RSI1 < 50 ||
-				rsiList[rsiList.length - 1].RSI2 < 50 ||
-				rsiList[rsiList.length - 1].RSI3 < 50 ||
+			((rsiList[rsiList.length - 1].RSI1 > 50 &&
+				rsiList[rsiList.length - 1].RSI2 > 50 &&
+				rsiList[rsiList.length - 1].RSI3 > 50) ||
 				isForceDeal);
 		const MAIN_CLOSE_SHORT_CONDITION1 =
 			shortHolding &&
-			(rsiList[rsiList.length - 1].RSI1 > 50 ||
-				rsiList[rsiList.length - 1].RSI2 > 50 ||
-				rsiList[rsiList.length - 1].RSI3 > 50 ||
+			((rsiList[rsiList.length - 1].RSI1 < 50 &&
+				rsiList[rsiList.length - 1].RSI2 < 50 &&
+				rsiList[rsiList.length - 1].RSI3 < 50) ||
 				isForceDeal);
 
 		console.log(234, rsiList[rsiList.length - 1]);
