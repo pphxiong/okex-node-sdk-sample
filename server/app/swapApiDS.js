96fa50786a8fb192b8e1ce97e0e64288ecd3483d
pphxiong <1017551718@qq.com>
2025-03-10 13:50:36 +0800

    n

--- server/app/swapApiDS.js
+++ server/app/swapApiDS.js
@@ -84,7 +84,7 @@ class DogePerpBot extends EventEmitter {
 
 	async loadMarkets() {
 		await this.exchange.loadMarkets();
-		console.log('市场数据加载完成', this.exchange);
+		console.log('市场数据加载完成');
 	}
 
 	setupWebSocket() {
@@ -95,11 +95,12 @@ class DogePerpBot extends EventEmitter {
 			`${this.config.symbol.replace('/', '').toLowerCase()}@bookTicker`,
 		];
 
-		// const ws = new ccxt.pro.binance().stream({
-		// 	method: 'SUBSCRIBE',
-		// 	params: streams,
-		// });
-		// ws.on('data', (data) => this.handleData(data));
+		const ws = new ccxt.pro.binance().stream({
+			method: 'SUBSCRIBE',
+			method: 'publicGetAggTrades',
+			params: streams,
+		});
+		ws.on('data', (data) => this.handleData(data));
 	}
 
 	handleData(data) {
