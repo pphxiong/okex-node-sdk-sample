c66336d2b1a1fa9ef850732af0e4cf77eb093e2c
pphxiong <1017551718@qq.com>
2025-03-12 17:32:19 +0800

    n

--- server/app/swapApiDS.js
+++ server/app/swapApiDS.js
@@ -575,7 +575,9 @@ class DogePerpBot extends EventEmitter {
 			const stopLossPrice =
 				side === 'buy' ? ema - 0.001 * 0.2 : ema + 0.001 * 0.2;
 			const takeProfitPrice =
-				side === 'buy' ? price + distance * 2 : price - distance * 2;
+				side === 'buy'
+					? price + distance * 1.5
+					: price - distance * 1.5;
 
 			const size = await this.calculatePositionSize();
 			const order = await this.exchange.createOrder(
