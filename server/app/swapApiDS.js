97ffd25d8d9d6538f9cd253add852417a848a965
pphxiong <1017551718@qq.com>
2025-03-13 11:05:27 +0800

    bug

--- server/app/swapApiDS.js
+++ server/app/swapApiDS.js
@@ -227,9 +227,16 @@ class RiskManager {
 			`强制平仓 | 方向:${side} 数量:${amount} 均价:${state.entryPrice}`
 		);
 
-		await exchange.createMarketOrder(config.symbol, side, amount, null, {
-			positionSide: side === 'sell' ? 'LONG' : 'SHORT',
-		});
+		await exchange.createOrder(
+			config.symbol,
+			'market',
+			side,
+			amount,
+			null,
+			{
+				positionSide: side === 'sell' ? 'LONG' : 'SHORT',
+			}
+		);
 
 		// 重置状态
 		state.position = 0;
