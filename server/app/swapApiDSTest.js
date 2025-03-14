a8dd3327c907f2a86f2087ac6806533c4f03b21d
pphxiong <1017551718@qq.com>
2025-03-14 15:14:15 +0800

    n

--- server/app/swapApiDSTest.js
+++ server/app/swapApiDSTest.js
@@ -49,13 +49,11 @@ async function fetchOHLCV() {
 				sinceParam
 			);
 			if (!candles.length) break;
-			console.log(candles, 2);
 			sinceParam = candles[candles.length - 1][0] + 1;
-			console.log(sinceParam, 3);
 			allCandles.concat(candles);
 			if (allCandles.length > 1000) break; // 控制数据量
 		}
-
+		console.log(23, allCandles);
 		return allCandles.map((c) => ({
 			timestamp: c[0],
 			open: c[1],
