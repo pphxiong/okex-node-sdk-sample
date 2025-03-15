0bee54bdb18aa5e434259c035630129fc582a79f
pphxiong <1017551718@qq.com>
2025-03-15 08:20:52 +0800

    n

--- server/app/swapApiDSTest.js
+++ server/app/swapApiDSTest.js
@@ -226,7 +226,7 @@ class TradingStrategy {
   }
 
   checkEntrySignal(indicators, candles) {
-    const { high, low } = this.getLatestHighAndLow(candles, 6);
+    const { high, low } = this.getLatestHighAndLow(candles, 10);
     const entryConditions = [
       indicators.price >= high,
       // indicators.emaSlope > this.config.entryRules.emaSlopeThreshold,
