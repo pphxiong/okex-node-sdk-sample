ee00c9b0e19c7ca53b72ddd20507b4270bc881b6
pphxiong <1017551718@qq.com>
2025-03-15 08:47:45 +0800

    n

--- server/app/swapApiDSTest.js
+++ server/app/swapApiDSTest.js
@@ -319,10 +319,10 @@ class Backtester {
   }
 
   executeEntry(indicators) {
-    const stopLoss = indicators.price * 0.99;
+    const stopLoss = indicators.price * 0.995;
     // indicators.price -
     // indicators.atr * STRATEGY_CONFIG.exitRules.stopLossMultiplier;
-    const takeProfit = indicators.price * 1.001;
+    const takeProfit = indicators.price * 1.005;
     // indicators.price +
     // indicators.atr * STRATEGY_CONFIG.exitRules.takeProfitMultiplier;
 
