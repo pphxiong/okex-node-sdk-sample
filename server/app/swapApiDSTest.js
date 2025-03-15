967ee1d2dbba10107c7a9136964bfb21db0259ba
pphxiong <1017551718@qq.com>
2025-03-15 09:10:41 +0800

    n

--- server/app/swapApiDSTest.js
+++ server/app/swapApiDSTest.js
@@ -231,7 +231,7 @@ class TradingStrategy {
       indicators.emaFast > indicators.emaSlow,
       indicators.price > indicators.emaFast,
       // indicators.price >= high,
-      indicators.emaSlope > this.config.entryRules.emaSlopeThreshold,
+      // indicators.emaSlope > this.config.entryRules.emaSlopeThreshold,
       //   indicators.rsiDispersion < this.config.entryRules.rsiDispersionBuy,
       //   indicators.atr >
       //     (indicators.price * this.config.entryRules.atrVolatilityRatio) / 100,
