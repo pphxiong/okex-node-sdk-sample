6ace81d537fec3748731b8478c126920d2cbe4d0
pphxiong <1017551718@qq.com>
2025-03-16 14:42:09 +0800

    n

--- server/app/swapApiDSTest.js
+++ server/app/swapApiDSTest.js
@@ -223,11 +223,13 @@ class WaveBacktester {
 
     return {
       isUpTrend:
-        lastHighs.length >= 2 &&
+        lastHighs.length >= 3 &&
+        lastHighs[2].price < lastHighs[1].price &&
         lastHighs[1].price < lastHighs[0].price &&
         lastLows[1].price < lastLows[0].price,
       isDownTrend:
-        lastHighs.length >= 2 &&
+        lastHighs.length >= 3 &&
+        lastHighs[2].price > lastHighs[1].price &&
         lastHighs[1].price > lastHighs[0].price &&
         lastLows[1].price > lastLows[0].price,
       waveCount: Math.max(
