7f343490130a8db5c4a8cdd45823307b4e6b04dd
pphxiong <1017551718@qq.com>
2025-03-17 11:04:32 +0800

    n

--- server/app/swapApiDS.js
+++ server/app/swapApiDS.js
@@ -41,7 +41,7 @@ const config = {
 	maxOrderAge: 10000, // 限价单最长存活时间(30秒)
 	trailingStop: 0.0025, // 浮动止盈止损(0.25%)
 	stopLoss: 0.008, // 硬止损(0.5%)
-	takeProfit: 0.02, // 硬止盈(1%)
+	takeProfit: 0.012, // 硬止盈(1%)
 	coolingPeriod: 180, // 基础冷却时间(秒)
 	numSegments: 5, // 分段数量
 	icebergRatio: 0.2, // 冰山可见部分比例
