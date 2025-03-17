c66fe64b9ae1d39dae351dbf6a453d4691498149
pphxiong <1017551718@qq.com>
2025-03-17 11:13:55 +0800

    n

--- server/app/swapApiDS.js
+++ server/app/swapApiDS.js
@@ -40,8 +40,8 @@ const config = {
 	tradeAmount: 1000, // 每单交易金额(USDT)
 	maxOrderAge: 10000, // 限价单最长存活时间(30秒)
 	trailingStop: 0.0025, // 浮动止盈止损(0.25%)
-	stopLoss: 0.008, // 硬止损(0.5%)
-	takeProfit: 0.012, // 硬止盈(1%)
+	stopLoss: 0.0025, // 硬止损(0.5%)
+	takeProfit: 0.01, // 硬止盈(1%)
 	coolingPeriod: 180, // 基础冷却时间(秒)
 	numSegments: 5, // 分段数量
 	icebergRatio: 0.2, // 冰山可见部分比例
@@ -438,6 +438,7 @@ class RiskManager {
 		}
 		console.log('***********************************');
 		console.log('entryPrice', state.entryPrice);
+		console.log('currentPrice', currentPrice);
 		console.log('hardStopPrice', hardStopPrice);
 		console.log('hardTakeProfitPrice', hardTakeProfitPrice);
 		// console.log('trailingStopPrice', trailingStopPrice);
