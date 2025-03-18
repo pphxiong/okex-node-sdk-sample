b6c795e6df8677f4730bf29e8dfbe6c2879153e9
pphxiong <1017551718@qq.com>
2025-03-18 14:22:46 +0800

    n

--- server/app/swapApiDSTest.js
+++ server/app/swapApiDSTest.js
@@ -305,9 +305,10 @@ class Backtester {
       盈亏比:        ${profitFactor.toFixed(2)}
       最大单笔盈利:  ${Math.max(...this.trades.map((t) => t.profit)).toFixed(2)}
       最大单笔亏损:  ${Math.min(...this.trades.map((t) => t.profit)).toFixed(2)}
-      详情:    ${this.trades.slice(-20)}
       =============================
     `);
+		console.log('\n最近5笔交易:');
+		console.table(this.trades.slice(-20));
 	}
 }
 
