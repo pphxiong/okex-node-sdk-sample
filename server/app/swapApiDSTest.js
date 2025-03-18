76e7a78c1731a69bf70f87e7da713642098ab276
pphxiong <1017551718@qq.com>
2025-03-18 11:04:11 +0800

    n

--- server/app/swapApiDSTest.js
+++ server/app/swapApiDSTest.js
@@ -82,7 +82,6 @@ class Backtester {
 
 	async calculateIndicators() {
 		try {
-			console.log(34, this.data);
 			// 计算布林带
 			const closes = this.data.map((d) => d.close);
 			const bollinger = await tulind.indicators.bbands.indicator(
@@ -95,6 +94,8 @@ class Backtester {
 				[closes],
 				[config.emaSlope.period]
 			);
+
+			console.log(ema[0]);
 
 			// 计算EMA斜率
 			const emaSlopes = [];
