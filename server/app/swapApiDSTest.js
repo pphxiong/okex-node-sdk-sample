73c0c1bdb7e9639f32857d8699ca5a048e1f335d
pphxiong <1017551718@qq.com>
2025-03-18 17:15:29 +0800

    n

--- server/app/swapApiDSTest.js
+++ server/app/swapApiDSTest.js
@@ -217,7 +217,7 @@ class Backtester {
 						? signal.direction === 'short'
 						: signal.direction === 'long';
 
-				if (isProfitTarget || isStopLoss || isReverse) {
+				if (isReverse) {
 					this.closePosition(position, d);
 					position = null;
 				}
