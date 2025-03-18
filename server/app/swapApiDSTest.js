6ce952976c09a8376578e0027bd75751e17b38fe
pphxiong <1017551718@qq.com>
2025-03-18 12:24:58 +0800

    n

--- server/app/swapApiDSTest.js
+++ server/app/swapApiDSTest.js
@@ -198,12 +198,12 @@ class Backtester {
 		if (!candle.upper || !candle.emaSlope) return null;
 
 		// 多头信号
-		if (/* candle.close <= candle.lower && */ candle.emaSlope > 0.005) {
+		if (/* candle.close <= candle.lower && */ candle.emaSlope > 0) {
 			return { direction: 'long' };
 		}
 
 		// 空头信号
-		if (/* candle.close >= candle.upper && */ candle.emaSlope < -0.005) {
+		if (/* candle.close >= candle.upper && */ candle.emaSlope < 0) {
 			return { direction: 'short' };
 		}
 
