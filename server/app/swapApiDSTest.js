56158685130a2131094072cc8d65a893c79f7285
pphxiong <1017551718@qq.com>
2025-03-18 13:49:39 +0800

    n

--- server/app/swapApiDSTest.js
+++ server/app/swapApiDSTest.js
@@ -141,7 +141,8 @@ class Backtester {
 
 	getPositionSize(price, atr) {
 		const riskAmount = this.balance * config.riskPerTrade;
-		return riskAmount / (atr * 2); // 2倍ATR止损
+		// return riskAmount / (atr * 2); // 2倍ATR止损
+		return 2000;
 	}
 
 	async fetchHistoricalData(start, end) {
