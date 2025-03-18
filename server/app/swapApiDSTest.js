2de48ba2c8405af17595522d907a4ec350b7705a
pphxiong <1017551718@qq.com>
2025-03-18 14:56:33 +0800

    n

--- server/app/swapApiDSTest.js
+++ server/app/swapApiDSTest.js
@@ -133,7 +133,7 @@ class Backtester {
 				}
 				d.atr = atr[i];
 			});
-			this.data.slice(-10).forEach((d) => {
+			this.data.slice(-100).forEach((d) => {
 				console.log(moment(d.timestamp).format('YYYY-MM-DD HH:mm:ss'));
 				console.log(d.emaSlope);
 				console.log(d.atr);
