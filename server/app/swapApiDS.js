72ae126690ac503f1ebe3e53d256d8e32c4ed999
pphxiong <1017551718@qq.com>
2025-03-19 23:19:58 +0800

    n

--- server/app/swapApiDS.js
+++ server/app/swapApiDS.js
@@ -37,9 +37,9 @@ const config = {
   // timeframe: '1m',
   timeframes: ["1h", "15m", "5m"], // 多周期参数
   emaSettings: {
-    "1h": { period: 20, slopeWindow: 5 },
-    "15m": { period: 10, slopeWindow: 3 },
-    "5m": { period: 6, slopeWindow: 2 },
+    "1h": { period: 10, slopeWindow: 5 },
+    "15m": { period: 6, slopeWindow: 3 },
+    "5m": { period: 4, slopeWindow: 2 },
   },
   slopeThreshold: {
     "1h": 0 * 0.01,
