82bc1058de72d63333bdd9725cc161edb2d5fe75
pphxiong <1017551718@qq.com>
2025-03-19 23:27:42 +0800

    n

--- server/app/swapApiDSTest.js
+++ server/app/swapApiDSTest.js
@@ -315,8 +315,8 @@ class Backtester {
         const isReverse =
           position &&
           (position.direction === "long"
-            ? candle["5m"].emaSlope < -config.slopeThreshold["5m"]
-            : candle["5m"].emaSlope > config.slopeThreshold["5m"]);
+            ? candle["15m"].emaSlope < -config.slopeThreshold["15m"]
+            : candle["15m"].emaSlope > config.slopeThreshold["15m"]);
 
         if (isReverse) {
           this.closePosition(position, d);
