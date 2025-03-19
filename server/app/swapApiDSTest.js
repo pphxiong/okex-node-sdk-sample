da906a3bb6f84c910b538a796994f72cf1449ee8
pphxiong <1017551718@qq.com>
2025-03-19 15:43:18 +0800

    n

--- server/app/swapApiDSTest.js
+++ server/app/swapApiDSTest.js
@@ -471,7 +471,17 @@ class Backtester {
 
 	// 步骤2: 计算指标
 	await backtester.calculateIndicators();
-
+	console.log(
+		backtester.data['5m']
+			.slice(-3)
+			.map((d) =>
+				Object.assign(d, {
+					timestamp: moment(d.timestamp).format(
+						'YYYY-MM-DD HH:mm:ss'
+					),
+				})
+			)
+	);
 	// 步骤3: 运行回测
 	backtester.runBacktest();
 
