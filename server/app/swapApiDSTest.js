57444b84abccf6239601d44364d4ff6c17106786
pphxiong <1017551718@qq.com>
2025-03-19 22:39:37 +0800

    n

--- server/app/swapApiDSTest.js
+++ server/app/swapApiDSTest.js
@@ -471,7 +471,7 @@ class Backtester {
   const backtester = new Backtester();
 
   // 步骤1: 加载历史数据
-  await backtester.loadHistoricalData("2025-01-01", "2025-03-19");
+  await backtester.loadHistoricalData("2025-01-01", "2025-02-19");
   // await backtester.loadHistoricalData('2024-10-01', '2024-12-31');
 
   // 步骤2: 计算指标
