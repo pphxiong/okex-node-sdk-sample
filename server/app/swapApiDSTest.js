7afbee6d4617a888f5269d6e1310bf7a9518f037
pphxiong <1017551718@qq.com>
2025-03-19 14:37:21 +0800

    n

--- server/app/swapApiDSTest.js
+++ server/app/swapApiDSTest.js
@@ -35,12 +35,12 @@ const config = {
 	emaSettings: {
 		// '1h': { period: 34, slopeWindow: 5 },
 		'15m': { period: 10, slopeWindow: 5 },
-		'5m': { period: 6, slopeWindow: 2 },
+		'5m': { period: 10, slopeWindow: 5 },
 	},
 	slopeThreshold: {
 		// '1h': 0.025 * 0.01,
 		'15m': 0.05 * 0.01,
-		'5m': 0.06 * 0.01,
+		'5m': 0.05 * 0.01,
 	}, // 斜率阈值
 	// 布林线参数
 	bollinger: {
