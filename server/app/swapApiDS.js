4a9bae0cc90cfd8b9da9a17e34684c2753620f56
pphxiong <1017551718@qq.com>
2025-03-20 22:04:31 +0800

    n

--- server/app/swapApiDS.js
+++ server/app/swapApiDS.js
@@ -196,7 +196,7 @@ async function calculateIndicators() {
           config.emaSettings[tf].slopeWindow;
         emaSlopes.push(slope);
       }
-      console.log(tf, emaSlopes.slice(-5));
+      console.log(tf, emaSlopes.slice(-5), ema[0].slice(-5));
       // 合并指标到数据
       marketData[tf].forEach((d, i) => {
         if (i >= config.bollinger.period) {
