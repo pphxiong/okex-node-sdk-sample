6fca27a9bb64c55fe71bfec5d1ee77ae58ff1c25
pphxiong <1017551718@qq.com>
2025-03-20 21:46:57 +0800

    n

--- server/app/swapApiDS.js
+++ server/app/swapApiDS.js
@@ -57,7 +57,7 @@ const config = {
   },
   emaPeriods: [5, 20, 55], // 三EMA周期
   orderDepth: 0.00015, // 限价单挂单深度 (0.1%)
-  tradeAmount: 1000, // 每单交易金额(USDT)
+  tradeAmount: 100, // 每单交易金额(USDT)
   maxOrderAge: 1000 * 5, // 限价单最长存活时间(30秒)
   trailingStop: 0.0025, // 浮动止盈止损(0.25%)
   stopLoss: 0.01, // 硬止损(0.5%)
