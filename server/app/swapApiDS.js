06b4926b68f0ac7bd0456cc8a54b281aca66f2db
pphxiong <1017551718@qq.com>
2025-03-20 09:39:50 +0800

    n

--- server/app/swapApiDS.js
+++ server/app/swapApiDS.js
@@ -768,7 +768,9 @@ async function handleKlineUpdate(msg, tf) {
 	if (marketData[tf].length >= config.coldStartBars) {
 		marketData[tf].shift();
 	}
+	console.log(newBar);
 	marketData[tf].push(newBar.map(parseKLine)[0]);
+	console.log(2, newBar);
 	// mergeTimeframes();
 	console.log(marketData['5m'].slice(-1)[0]);
 }
