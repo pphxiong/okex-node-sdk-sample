c9e9f429c082695d149167158659137a673bf488
pphxiong <1017551718@qq.com>
2025-03-20 14:13:28 +0800

    n

--- server/app/swapApiDSTest.js
+++ server/app/swapApiDSTest.js
@@ -489,8 +489,8 @@ class Backtester {
 // 执行回测
 (async () => {
 	const backtester = new Backtester();
-	const start = '2024-06-01';
-	const end = '2024-09-30';
+	const start = '2025-01-01';
+	const end = '2025-03-20';
 
 	let i = 0;
 	while (moment(end).isAfter(moment(start).add(i + 10, 'days'))) {
