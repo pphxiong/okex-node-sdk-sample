c737eb9b13ed84dc4eb7ccdbab55c91a36165bc8
pphxiong <1017551718@qq.com>
2025-03-21 17:05:21 +0800

    n

--- server/app/swapApiDSTest.js
+++ server/app/swapApiDSTest.js
@@ -33,7 +33,7 @@ const config = {
 	timeframe: '15m',
 	timeframes: ['1h', '15m', '5m' /* '1m'*/], // 多周期参数
 	emaSettings: {
-		'1h': { period: 8, slopeWindow: 4 },
+		'1h': { period: 10, slopeWindow: 4 },
 		'15m': { period: 2, slopeWindow: 1 },
 		'5m': { period: 1, slopeWindow: 1 },
 	},
@@ -358,10 +358,10 @@ class Backtester {
 				const isReverse =
 					position &&
 					(position.direction === 'long'
-						? candle[config.mediumframe].emaSlope <
-						  -config.slopeThreshold[config.mediumframe]
-						: candle[config.mediumframe].emaSlope >
-						  config.slopeThreshold[config.mediumframe]);
+						? candle[config.slowframe].emaSlope <
+						  -config.slopeThreshold[config.slowframe]
+						: candle[config.slowframe].emaSlope >
+						  config.slopeThreshold[config.slowframe]);
 
 				if (isReverse) {
 					// console.log(
