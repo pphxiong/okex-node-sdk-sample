471fe2a37fd913cdb53ffa53b6ef554356fd2ccb
pphxiong <1017551718@qq.com>
2025-03-23 00:16:20 +0800

    n

--- server/app/swapApiDSTest.js
+++ server/app/swapApiDSTest.js
@@ -25,7 +25,7 @@ const cAuthClientBN = new customAuthClientBN(
 );
 
 const ccxt = require("ccxt");
-const tf = require("@tensorflow/tfjs");
+const tf = require("@tensorflow/tfjs-node");
 const tulind = require("tulind");
 
 // 系统配置
