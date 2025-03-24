23c561c001960e6f1442533fe66c8730d4b78796
pphxiong <1017551718@qq.com>
2025-03-24 10:20:44 +0800

    n

--- server/app/swapApiDSTest.js
+++ server/app/swapApiDSTest.js
@@ -516,9 +516,9 @@ class Backtester {
 		// }
 
 		const longCondition =
-			longs.length < shorts.length && mediumLongs > mediumShorts;
+			longs.length > shorts.length && mediumLongs < mediumShorts;
 		const shortCondition =
-			shorts.length < longs.length && mediumShorts > mediumLongs;
+			shorts.length > longs.length && mediumShorts < mediumLongs;
 
 		// 多头信号
 		if (longCondition) {
