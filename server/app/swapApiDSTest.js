792ccbadd8e0df90e20c2d5193647d042672b0dc
pphxiong <1017551718@qq.com>
2025-03-24 11:26:30 +0800

    n

--- server/app/swapApiDSTest.js
+++ server/app/swapApiDSTest.js
@@ -418,8 +418,8 @@ class Backtester {
 
 				const isReverse =
 					signal && position.direction === 'long'
-						? longs.length < shorts.length
-						: longs.length > shorts.length;
+						? longs.length > shorts.length
+						: longs.length < shorts.length;
 
 				// const isReverse = isProfitTarget || isStopLoss;
 
@@ -523,10 +523,10 @@ class Backtester {
 		// }
 
 		const longCondition =
-			longs.length > shorts.length && mediumLongs > mediumShorts;
+			longs.length < shorts.length && mediumLongs < mediumShorts;
 
 		const shortCondition =
-			longs.length < shorts.length && mediumLongs < mediumShorts;
+			longs.length > shorts.length && mediumLongs > mediumShorts;
 
 		// 多头信号
 		if (longCondition) {
