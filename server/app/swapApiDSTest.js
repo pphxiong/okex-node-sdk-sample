f10672d54c5a4b3ec24b026130c116a81ceaebcf
pphxiong <1017551718@qq.com>
2025-03-25 08:51:01 +0800

    n

--- server/app/swapApiDSTest.js
+++ server/app/swapApiDSTest.js
@@ -532,7 +532,7 @@ class Backtester {
 			candle[config.slowframe].close > candle[config.slowframe].middle;
 
 		const shortCondition =
-			candle[config.fastframe].close > candle[config.fastframe].lower &&
+			candle[config.fastframe].close > candle[config.fastframe].upper &&
 			candle[config.slowframe].close < candle[config.slowframe].middle;
 
 		// 多头信号
