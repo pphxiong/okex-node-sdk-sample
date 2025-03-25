9adc7259b77fe3367e2f8a75bd3fa44b2fbaaa5a
pphxiong <1017551718@qq.com>
2025-03-25 13:47:48 +0800

    n

--- server/app/swapApiDSTest.js
+++ server/app/swapApiDSTest.js
@@ -543,13 +543,11 @@ class Backtester {
 		// }
 
 		const longCondition =
-			candle[config.fastframe].close > candle[config.fastframe].lower &&
-			candle[config.fastframe].close < candle[config.fastframe].middle &&
+			candle[config.fastframe].close > candle[config.fastframe].middle &&
 			candle[config.slowframe].close > candle[config.slowframe].middle;
 
 		const shortCondition =
-			candle[config.fastframe].close < candle[config.fastframe].upper &&
-			candle[config.fastframe].close > candle[config.fastframe].middle &&
+			candle[config.fastframe].close < candle[config.fastframe].middle &&
 			candle[config.slowframe].close < candle[config.slowframe].middle;
 
 		// 多头信号
@@ -653,7 +651,7 @@ class Backtester {
 // 执行回测
 (async () => {
 	const backtester = new Backtester();
-	const start = '2025-03-01';
+	const start = '2025-01-20';
 	const end = '2025-03-25';
 	const interval = 5;
 	let profitTotal = 0;
