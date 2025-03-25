4e8e7fe918cdf8483e285f66a2e6e7eac95a870f
pphxiong <1017551718@qq.com>
2025-03-25 13:43:26 +0800

    n

--- server/app/swapApiDSTest.js
+++ server/app/swapApiDSTest.js
@@ -543,13 +543,11 @@ class Backtester {
 		// }
 
 		const longCondition =
-			candle[config.fastframe].close > candle[config.fastframe].lower &&
-			candle[config.fastframe].close < candle[config.fastframe].middle &&
+			candle[config.fastframe].close > candle[config.fastframe].middle &&
 			candle[config.slowframe].close > candle[config.slowframe].middle;
 
 		const shortCondition =
-			candle[config.fastframe].close < candle[config.fastframe].upper &&
-			candle[config.fastframe].close > candle[config.fastframe].middle &&
+			candle[config.fastframe].close < candle[config.fastframe].middle &&
 			candle[config.slowframe].close < candle[config.slowframe].middle;
 
 		// 多头信号
