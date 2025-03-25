283efc2573281af4a1b27c8e46ceda03c8c83b74
pphxiong <1017551718@qq.com>
2025-03-25 14:26:24 +0800

    n

--- server/app/swapApiDSTest.js
+++ server/app/swapApiDSTest.js
@@ -549,15 +549,25 @@ class Backtester {
 		// }
 
 		const longCondition =
-			secondKline5M.close < secondKline5M.lower &&
-			candle[config.fastframe].close > candle[config.fastframe].lower &&
-			candle[config.fastframe].close < candle[config.fastframe].middle &&
+			((secondKline5M.close < secondKline5M.lower &&
+				candle[config.fastframe].close >
+					candle[config.fastframe].lower &&
+				candle[config.fastframe].close <
+					candle[config.fastframe].middle) ||
+				(secondKline5M.close < secondKline5M.middle &&
+					candle[config.fastframe].close >
+						candle[config.fastframe].middle)) &&
 			candle[config.slowframe].close > candle[config.slowframe].middle;
 
 		const shortCondition =
-			secondKline5M.close > secondKline5M.upper &&
-			candle[config.fastframe].close < candle[config.fastframe].upper &&
-			candle[config.fastframe].close > candle[config.fastframe].middle &&
+			((secondKline5M.close > secondKline5M.upper &&
+				candle[config.fastframe].close <
+					candle[config.fastframe].upper &&
+				candle[config.fastframe].close >
+					candle[config.fastframe].middle) ||
+				(secondKline5M.close > secondKline5M.middle &&
+					candle[config.fastframe].close <
+						candle[config.fastframe].middle)) &&
 			candle[config.slowframe].close < candle[config.slowframe].middle;
 
 		// 多头信号
