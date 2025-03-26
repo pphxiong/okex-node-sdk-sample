e127a8bffa94a7f6ab0f7d9e6cb88172f87e02c4
pphxiong <1017551718@qq.com>
2025-03-26 23:46:47 +0800

    n

--- server/app/swapApiDSTest.js
+++ server/app/swapApiDSTest.js
@@ -369,7 +369,7 @@ class Backtester {
           index * 6,
           (index + 1) * 6
         );
-        console.log(22, adx.slice(-2));
+        if (tf === config.slowframe) console.log(22, adx[0].length);
         // 计算EMA斜率
         const emaSlopes = [];
         for (
