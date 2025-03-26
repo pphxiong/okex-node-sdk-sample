68c4156592ac648bf9b991ffb2472a4ddd41d8d5
pphxiong <1017551718@qq.com>
2025-03-27 00:05:32 +0800

    n

--- server/app/swapApiDSTest.js
+++ server/app/swapApiDSTest.js
@@ -792,9 +792,9 @@ class Backtester {
   // const simPaths = this.generateCorrelatedPaths(histData);
 
   const backtester = new Backtester();
-  const start = "2024-03-20";
+  const start = "2025-03-01";
   const end = "2025-03-26";
-  const interval = 2;
+  const interval = 5;
   let profitTotal = 0;
 
   let i = 0;
