37015b916154e7cf2e96ddda194bb5aae227f345
pphxiong <1017551718@qq.com>
2025-03-27 17:18:09 +0800

    config

--- server/app/swapApiDSTest.js
+++ server/app/swapApiDSTest.js
@@ -902,7 +902,9 @@ class Backtester {
 	let profitTotal = 0;
 
 	let i = 0;
-	while (moment(end).isAfter(moment(start).add(i, 'days'))) {
+	while (
+		moment(end).add(interval, 'days').isAfter(moment(start).add(i, 'days'))
+	) {
 		// while (i === 0) {
 		try {
 			backtester.data = {
