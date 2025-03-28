8217a20022a3022ac407ac3f066f3b983e9bcf29
pphxiong <1017551718@qq.com>
2025-03-28 14:02:48 +0800

    n

--- server/app/swapApiDSTest.js
+++ server/app/swapApiDSTest.js
@@ -983,15 +983,15 @@ class Backtester {
 			// 步骤2: 计算指标
 			await backtester.calculateIndicators();
 
-			// console.log(
-			// 	data[config.slowframe].slice(-10).map((candle) =>
-			// 		Object.assign(candle, {
-			// 			timestamp: moment(candle.timestamp).format(
-			// 				'YYYY-MM-DD HH:mm:ss'
-			// 			),
-			// 		})
-			// 	)
-			// );
+			console.log(
+				data[config.slowframe].slice(-2).map((candle) =>
+					Object.assign(candle, {
+						timestamp: moment(candle.timestamp).format(
+							'YYYY-MM-DD HH:mm:ss'
+						),
+					})
+				)
+			);
 
 			// console.log(data[config.slowframe].length);
 
