18898ec8aaa6e1dc9c0d1f0626cb10a56e74ea33
pphxiong <1017551718@qq.com>
2025-03-29 22:03:32 +0800

    n

--- server/app/swapApiDSTest.js
+++ server/app/swapApiDSTest.js
@@ -843,9 +843,9 @@ class Backtester {
 // 执行回测
 (async () => {
   const backtester = new Backtester();
-  const start = "2025-03-15";
-  const end = "2025-03-28";
-  const interval = 4;
+  const start = "2025-03-21";
+  const end = "2025-03-29";
+  const interval = 1;
   let profitTotal = 0;
 
   let i = 0;
@@ -877,13 +877,13 @@ class Backtester {
       // 步骤2: 计算指标
       await backtester.calculateIndicators();
 
-      console.log(
-        data[config.slowframe].slice(-20).map((candle) =>
-          Object.assign(candle, {
-            timestamp: moment(candle.timestamp).format("YYYY-MM-DD HH:mm:ss"),
-          })
-        )
-      );
+      // console.log(
+      //   data[config.slowframe].slice(-20).map((candle) =>
+      //     Object.assign(candle, {
+      //       timestamp: moment(candle.timestamp).format("YYYY-MM-DD HH:mm:ss"),
+      //     })
+      //   )
+      // );
 
       // console.log(data[config.slowframe].length);
 
