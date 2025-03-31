7389dc0589e1fe70587ba2fe1f45f8a370ef224d
pphxiong <1017551718@qq.com>
2025-03-31 15:36:00 +0800

    n

--- server/app/swapApiDSTest.js
+++ server/app/swapApiDSTest.js
@@ -518,12 +518,12 @@ class Backtester {
 					if (emaSlope < 0) {
 						marketType = '不确定';
 					} else {
-						marketType = '趋势潜在增强';
+						// marketType = '趋势潜在增强';
 					}
 				} else if (rsi > 65 && rsi <= 75) {
-					// if (emaSlope < 0) {
-					// 	marketType = '不确定';
-					// }
+					if (emaSlope < 0) {
+						marketType = '不确定';
+					}
 				} else if (rsi > 75) {
 					marketType = '超买市';
 				}
@@ -538,7 +538,7 @@ class Backtester {
 					if (emaSlope > 0) {
 						marketType = '不确定';
 					} else {
-						marketType = '趋势潜在减弱';
+						// marketType = '趋势潜在减弱';
 					}
 				} else if (rsi >= 25 && rsi < 35) {
 					// if (emaSlope > 0) {
