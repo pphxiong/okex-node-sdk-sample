9e5f3420588e29cb2052ce0c57773d353992a15a
pphxiong <1017551718@qq.com>
2025-03-31 13:40:48 +0800

    n

--- server/app/swapApiDSTest.js
+++ server/app/swapApiDSTest.js
@@ -512,13 +512,13 @@ class Backtester {
 					if (emaSlope > 0) {
 						marketType = '趋势多且增强';
 					} else {
-						marketType = '趋势多且减弱';
+						marketType = '趋势潜在增强';
 					}
 				} else if (rsi > 65) {
 					marketType = '超买市';
 				} else if (rsi <= 65 && rsi > 55) {
 					if (emaSlope > 0) {
-						marketType = '趋势潜在增强';
+						// marketType = '趋势潜在增强';
 					} else {
 						// marketType = '趋势潜在减弱';
 					}
@@ -528,13 +528,13 @@ class Backtester {
 					if (emaSlope < 0) {
 						marketType = '趋势空且增强';
 					} else {
-						marketType = '趋势空且减弱';
+						marketType = '趋势潜在减弱';
 					}
 				} else if (rsi < 35) {
 					marketType = '超卖市';
 				} else if (rsi >= 35 && rsi <= 45) {
 					if (emaSlope < 0) {
-						marketType = '趋势潜在减弱';
+						// marketType = '趋势潜在减弱';
 					} else {
 						// marketType = '趋势潜在增强';
 					}
