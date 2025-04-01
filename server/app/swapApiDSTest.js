b16360b9dc58962a7d77badd82b47e21be73d72d
pphxiong <1017551718@qq.com>
2025-04-01 21:16:31 +0800

    bug

--- server/app/swapApiDSTest.js
+++ server/app/swapApiDSTest.js
@@ -939,8 +939,8 @@ class Backtester {
 // 执行回测
 (async () => {
   const backtester = new Backtester();
-  const start = "2024-01-01";
-  const end = "2024-12-01";
+  const start = "2023-01-01";
+  const end = "2023-12-01";
   const interval = 30;
   let profitTotal = 0;
 
