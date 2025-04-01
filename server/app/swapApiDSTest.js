cc45f781b8fd9204d138ca2a0ced413104b81257
pphxiong <1017551718@qq.com>
2025-04-01 20:50:26 +0800

    bug

--- server/app/swapApiDSTest.js
+++ server/app/swapApiDSTest.js
@@ -519,14 +519,14 @@ class Backtester {
         }
       } else if (adxPlusDI < adxMinusDI) {
         if (rsi >= 40 && rsi <= 55) {
-          if (emaSlope < 0) {
+          if (emaSlope > 0) {
             marketType = "趋势空且增强";
           } else {
             // marketType = '趋势潜在增强';
             marketType = "趋势市";
           }
         } else if (rsi >= 35 && rsi < 40) {
-          if (emaSlope > 0) {
+          if (emaSlope < 0) {
             marketType = "趋势空且增强";
           } else {
             marketType = "趋势空";
