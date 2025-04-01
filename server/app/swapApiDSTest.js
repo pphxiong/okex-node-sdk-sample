930505c12a5448c1d44142fb545c9fa54febfec1
pphxiong <1017551718@qq.com>
2025-04-01 17:28:14 +0800

    m

--- server/app/swapApiDSTest.js
+++ server/app/swapApiDSTest.js
@@ -810,16 +810,16 @@ class Backtester {
 		// 	return { direction: 'short' };
 		// }
 		const longConditions = [
-			// slowMarketType === '趋势多且增强',
+			slowMarketType === '趋势多且增强',
 			slowMarketType === '趋势潜在增强',
-			// slowMarketType === '震荡市开多',
-			// slowMarketType === '潜在转折多',
+			slowMarketType === '震荡市开多',
+			slowMarketType === '潜在转折多',
 		];
 		const shortConditions = [
-			// slowMarketType === '趋势空且增强',
+			slowMarketType === '趋势空且增强',
 			slowMarketType === '趋势潜在减弱',
-			// slowMarketType === '震荡市开空',
-			// slowMarketType === '潜在转折空',
+			slowMarketType === '震荡市开空',
+			slowMarketType === '潜在转折空',
 		];
 
 		const longCondition = longConditions.some((condition) => !!condition);
