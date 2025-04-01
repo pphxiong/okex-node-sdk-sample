4e3a7f63ccb2ee8ca9fe400bd76c7a0d9fcb7c38
pphxiong <1017551718@qq.com>
2025-04-01 17:11:59 +0800

    m

--- server/app/swapApiDSTest.js
+++ server/app/swapApiDSTest.js
@@ -33,21 +33,21 @@ const _ = require('lodash');
 // 策略配置
 const config = {
 	symbol: 'DOGE/USDT',
-	timeframe: '30m',
-	timeframes: ['30m' /* '5m'  '1m'*/], // 多周期参数
+	timeframe: '5m',
+	timeframes: ['5m' /* '5m'  '1m'*/], // 多周期参数
 	emaSettings: {
 		// '30m': { periods: [10, 5], slopeWindow: 5 },
-		'30m': { periods: [10, 5], slopeWindow: 5 },
+		'5m': { periods: [10, 5], slopeWindow: 5 },
 		// '5m': { periods: [10, 5], slopeWindow: 5 },
 	},
 	slopeThreshold: {
 		// '30m': 0,
-		'30m': 0.003 * 0.01,
+		'5m': 0.003 * 0.01,
 		// '5m': 0,
 	}, // 斜率阈值
-	macdParams: { '30m': [12, 26, 9] /* '5m': [12, 26, 9] */ },
-	slowframe: '30m',
-	fastframe: '30m',
+	macdParams: { '5m': [12, 26, 9] /* '5m': [12, 26, 9] */ },
+	slowframe: '5m',
+	fastframe: '5m',
 	kWindowTresholdFast: 3,
 	kWindowTresholdMedium: 5,
 	// 布林线参数
