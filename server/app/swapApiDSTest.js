52f7a33305269b410fe2d2241112ba68da4c5851
pphxiong <1017551718@qq.com>
2025-04-02 15:35:13 +0800

    n

--- server/app/swapApiDSTest.js
+++ server/app/swapApiDSTest.js
@@ -497,7 +497,7 @@ class Backtester {
 	}
 
 	getMarketType(candle, lastCandle) {
-		let marketType = '不确定';
+		let marketType = '';
 		if (!lastCandle) return marketType;
 
 		const {
@@ -818,7 +818,7 @@ class Backtester {
 							'震荡市开空',
 							'趋势潜在减弱',
 							'趋势多且减弱',
-							// '不确定',
+							'不确定',
 							'趋势多只平不开',
 						].includes(slowMarketType),
 					position.slowMarketType === '趋势潜在增强' &&
@@ -829,7 +829,7 @@ class Backtester {
 							'震荡市开空',
 							'趋势潜在减弱',
 							'趋势多且减弱',
-							// '不确定',
+							'不确定',
 							'趋势多只平不开',
 						].includes(slowMarketType),
 					position.slowMarketType === '震荡市开多' &&
