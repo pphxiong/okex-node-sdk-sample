5ed22bdd16695dc392433a083ae199a2ea00752b
pphxiong <1017551718@qq.com>
2025-04-02 11:27:32 +0800

    n

--- server/app/swapApiDSTest.js
+++ server/app/swapApiDSTest.js
@@ -517,7 +517,7 @@ class Backtester {
 		const stronger = emaFast > emaSlow;
 		const weeker = emaFast < emaSlow;
 
-		if (adx < 25 && adx > 20) {
+		if (adx < 20) {
 			// marketType = '趋势市';
 			// 大趋势多
 			if (adxPlusDI > adxMinusDI) {
