535afb31878ebefb147c08e6f751a82f23aaaaae
pphxiong <1017551718@qq.com>
2025-04-02 09:26:29 +0800

    m

--- server/app/swapApiDSTest.js
+++ server/app/swapApiDSTest.js
@@ -541,9 +541,9 @@ class Backtester {
 				} else if (rsi > 75) {
 					marketType = '超买市';
 				} else if (rsi >= 35 && rsi < 45) {
-					marketType = '趋势潜在减弱';
-				} else {
 					marketType = '趋势潜在增强';
+				} else {
+					marketType = '趋势潜在减弱';
 				}
 			} else if (adxPlusDI < adxMinusDI && weeker) {
 				if (rsi >= 35 && rsi <= 45) {
@@ -567,9 +567,9 @@ class Backtester {
 				} else if (rsi < 25) {
 					marketType = '超卖市';
 				} else if (rsi > 55 && rsi <= 65) {
-					marketType = '趋势潜在增强';
-				} else {
 					marketType = '趋势潜在减弱';
+				} else {
+					marketType = '趋势潜在增强';
 				}
 			}
 		} else if (adx < 20) {
@@ -898,14 +898,14 @@ class Backtester {
 		// }
 		const longConditions = [
 			slowMarketType === '趋势多且增强',
-			slowMarketType === '趋势潜在增强',
-			// slowMarketType === '震荡市开多',
+			// slowMarketType === '趋势潜在增强',
+			slowMarketType === '震荡市开多',
 			// slowMarketType === '潜在转折多',
 		];
 		const shortConditions = [
 			slowMarketType === '趋势空且增强',
-			slowMarketType === '趋势潜在减弱',
-			// slowMarketType === '震荡市开空',
+			// slowMarketType === '趋势潜在减弱',
+			slowMarketType === '震荡市开空',
 			// slowMarketType === '潜在转折空',
 		];
 
