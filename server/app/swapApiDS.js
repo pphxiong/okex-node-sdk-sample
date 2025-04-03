6d9bb0b9e2bdd17de87ae2378049622c93fe5080
pphxiong <1017551718@qq.com>
2025-04-03 16:50:34 +0800

    tag前

--- server/app/swapApiDS.js
+++ server/app/swapApiDS.js
@@ -791,7 +791,7 @@ async function initialize() {
 		);
 	});
 
-	const [candlesSlow, candlesFast] = await Promise.all(candlePromises);
+	const [candlesFast] = await Promise.all(candlePromises);
 
 	// candlesSlow.pop();
 	// candlesMedium.pop();
@@ -863,7 +863,7 @@ function getMarketType(candle, lastCandle) {
 			} else if (adxPlusDI < adxMinusDI && adx >= 25) {
 				if (rsi < 60) {
 					marketType = '趋势空且增强';
-				} else {
+				} else if (emaSlope > config.emaSlope.emaSlopeThreshold) {
 					marketType = '趋势多且增强';
 				}
 			} else {
@@ -900,7 +900,7 @@ function getMarketType(candle, lastCandle) {
 			} else if (adxPlusDI > adxMinusDI && adx >= 25) {
 				if (rsi > 40) {
 					marketType = '趋势多且增强';
-				} else {
+				} else if (emaSlope < -config.emaSlope.emaSlopeThreshold) {
 					marketType = '趋势空且增强';
 				}
 			} else {
