944535603aea011a11bc7e170d8c1ad38f97eacb
pphxiong <1017551718@qq.com>
2025-04-03 11:30:41 +0800

    n

--- server/app/swapApiDSTest.js
+++ server/app/swapApiDSTest.js
@@ -549,8 +549,10 @@ class Backtester {
 						marketType = '趋势多且增强';
 					}
 				} else {
-					// if (adx < 20)
-					marketType = adxPlusDI > adxMinusDI ? '趋势空' : '趋势多';
+					marketType = '趋势多';
+					if (adx < 20)
+						marketType =
+							adxPlusDI > adxMinusDI ? '趋势空' : '趋势多';
 				}
 			} else if (close < emaSlow) {
 				if (adx < 20)
@@ -581,8 +583,10 @@ class Backtester {
 						marketType = '趋势空且增强';
 					}
 				} else {
-					// if (adx < 20)
-					marketType = adxPlusDI > adxMinusDI ? '趋势多' : '趋势空';
+					marketType = '趋势空';
+					if (adx < 20)
+						marketType =
+							adxPlusDI > adxMinusDI ? '趋势多' : '趋势空';
 				}
 			} else if (close > emaSlow) {
 				if (adx < 20)
