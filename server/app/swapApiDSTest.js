0e03129d3005bd469eab57aec4b410893d1fb046
pphxiong <1017551718@qq.com>
2025-04-03 16:25:10 +0800

    tag前

--- server/app/swapApiDSTest.js
+++ server/app/swapApiDSTest.js
@@ -1130,8 +1130,8 @@ class Backtester {
 // 执行回测
 (async () => {
 	const backtester = new Backtester();
-	const start = '2021-01-01';
-	const end = '2025-03-30';
+	const start = '2019-01-01';
+	const end = '2020-12-30';
 	const interval = 30;
 	let profitTotal = 0;
 
