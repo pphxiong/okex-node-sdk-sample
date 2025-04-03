3621b05aff415ed0535a2520dfdb73649d31e5ae
pphxiong <1017551718@qq.com>
2025-04-03 11:22:03 +0800

    n

--- server/app/swapApiDS.js
+++ server/app/swapApiDS.js
@@ -856,10 +856,12 @@ function getMarketType(candle, lastCandle) {
 		if (close > emaFast) {
 			if (adxPlusDI > adxMinusDI && adx >= 25) {
 				if (emaSlope > config.emaSlope.emaSlopeThreshold) {
-					if (rsi > 70) {
+					if (rsi > 75) {
 						marketType = '趋势空且增强';
-					} else {
+					} else if (rsi < 60) {
 						marketType = '趋势多且增强';
+					} else {
+						// marketType = '趋势多';
 					}
 				}
 			} else if (adxPlusDI < adxMinusDI && adx >= 25) {
@@ -886,10 +888,12 @@ function getMarketType(candle, lastCandle) {
 		if (close < emaFast) {
 			if (adxPlusDI < adxMinusDI && adx >= 25) {
 				if (emaSlope < -config.emaSlope.emaSlopeThreshold) {
-					if (rsi < 30) {
+					if (rsi < 25) {
 						marketType = '趋势多且增强';
-					} else {
+					} else if (rsi > 40) {
 						marketType = '趋势空且增强';
+					} else {
+						// marketType = '趋势空';
 					}
 				}
 			} else if (adxPlusDI > adxMinusDI && adx >= 25) {
