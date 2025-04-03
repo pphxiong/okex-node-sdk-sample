3f693e889ac8d5f63effad5cd766e0a78e99b1c6
pphxiong <1017551718@qq.com>
2025-04-03 16:12:42 +0800

    n

--- server/app/swapApiDSTest.js
+++ server/app/swapApiDSTest.js
@@ -540,9 +540,15 @@ class Backtester {
 						}
 					}
 				} else if (adxPlusDI < adxMinusDI && adx >= 25) {
-					if (rsi < 60) {
+					if (
+						rsi < 60 &&
+						emaSlope < -config.emaSlope.emaSlopeThreshold
+					) {
 						marketType = '趋势空且增强';
-					} else {
+					} else if (
+						rsi > 45 &&
+						emaSlope > config.emaSlope.emaSlopeThreshold
+					) {
 						marketType = '趋势多且增强';
 					}
 				} else {
@@ -577,9 +583,15 @@ class Backtester {
 						}
 					}
 				} else if (adxPlusDI > adxMinusDI && adx >= 25) {
-					if (rsi > 40) {
+					if (
+						rsi > 40 &&
+						emaSlope > config.emaSlope.emaSlopeThreshold
+					) {
 						marketType = '趋势多且增强';
-					} else {
+					} else if (
+						rsi < 55 &&
+						emaSlope < -config.emaSlope.emaSlopeThreshold
+					) {
 						marketType = '趋势空且增强';
 					}
 				} else {
