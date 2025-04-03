866b5a9196ea1760232ce028bd88f95db9a2b663
pphxiong <1017551718@qq.com>
2025-04-03 10:37:08 +0800

    n

--- server/app/swapApiDSTest.js
+++ server/app/swapApiDSTest.js
@@ -1146,8 +1146,8 @@ class Backtester {
 
 		const profitTotal = this.balance - config.initialBalance;
 
-		// console.log('\n最近20笔交易:');
-		// console.table(this.trades);
+		console.log('\n最近20笔交易:');
+		console.table(this.trades);
 
 		console.log(`
       ========== 回测结果 ==========
