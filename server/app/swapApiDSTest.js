7914e4a42059926830398362ec8406f324f04742
pphxiong <1017551718@qq.com>
2025-04-04 21:26:31 +0800

    bug

--- server/app/swapApiDSTest.js
+++ server/app/swapApiDSTest.js
@@ -508,7 +508,7 @@ class Backtester {
     if (emaFast > emaSlow) {
       if (close > emaFast) {
         if (adxPlusDI > adxMinusDI && adx >= 25) {
-          if (rsi > 70) {
+          if (rsi > 68) {
             marketType = "趋势空且增强";
           } else if (rsi < 60 && emaSlope > config.emaSlope.emaSlopeThreshold) {
             marketType = "趋势多且增强";
@@ -542,7 +542,7 @@ class Backtester {
     if (emaFast < emaSlow) {
       if (close < emaFast) {
         if (adxPlusDI < adxMinusDI && adx >= 25) {
-          if (rsi < 30) {
+          if (rsi < 32) {
             marketType = "趋势多且增强";
           } else if (
             rsi > 40 &&
