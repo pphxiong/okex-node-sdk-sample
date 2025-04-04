2a844da1fbf79da5703b92247e66d62f3402e58e
pphxiong <1017551718@qq.com>
2025-04-04 21:37:25 +0800

    bug

--- server/app/swapApiDSTest.js
+++ server/app/swapApiDSTest.js
@@ -508,7 +508,7 @@ class Backtester {
     if (emaFast > emaSlow) {
       if (close > emaFast) {
         if (adxPlusDI > adxMinusDI && adx >= 25) {
-          if (rsi > 71) {
+          if (rsi > 70) {
             marketType = "趋势空且增强";
           } else if (rsi < 60 && emaSlope > config.emaSlope.emaSlopeThreshold) {
             marketType = "趋势多且增强";
@@ -542,7 +542,7 @@ class Backtester {
     if (emaFast < emaSlow) {
       if (close < emaFast) {
         if (adxPlusDI < adxMinusDI && adx >= 25) {
-          if (rsi < 29) {
+          if (rsi < 30) {
             marketType = "趋势多且增强";
           } else if (
             rsi > 40 &&
