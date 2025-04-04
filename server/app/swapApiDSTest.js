4c75897afe2aa550557b8e5b59b7b71815ff34f0
pphxiong <1017551718@qq.com>
2025-04-04 23:16:54 +0800

    bug

--- server/app/swapApiDSTest.js
+++ server/app/swapApiDSTest.js
@@ -54,7 +54,7 @@ const config = {
   emaSlope: {
     period: 10,
     lookback: 5, // 计算5根K线斜率
-    emaSlopeThreshold: 0.006 * 0.01, // EMA斜率阈值
+    emaSlopeThreshold: 0.004 * 0.01, // EMA斜率阈值
     // emaSlopeThreshold: 0, // EMA斜率阈值
   },
   atrParam: {
