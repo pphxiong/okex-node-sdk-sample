00091e3afdf21dcb79f04b2573136184b2a5183e
pphxiong <1017551718@qq.com>
2025-04-07 08:48:48 +0800

    n

--- server/app/swapApiDSTest.js
+++ server/app/swapApiDSTest.js
@@ -567,8 +567,13 @@ class Backtester {
 		if (emaFast < emaSlow) {
 			if (close < emaFast) {
 				if (adxPlusDI < adxMinusDI && adx >= 25) {
-					if (rsi < 25) {
+					if (
+						rsi < 25 &&
+						emaSlope > config.emaSlope.emaSlopeThreshold
+					) {
 						marketType = '趋势多且增强';
+						// if (emaSlope < -config.emaSlope.emaSlopeThreshold)
+						// 	marketType = '趋势多且增强';
 					} else if (
 						rsi > 40 &&
 						emaSlope < -config.emaSlope.emaSlopeThreshold
