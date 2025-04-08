6adc2814d0938fb820de4d1f900b2acc61ddfe9e
pphxiong <1017551718@qq.com>
2025-04-08 16:15:40 +0800

    bug

--- server/app/swapApiDSTest.js
+++ server/app/swapApiDSTest.js
@@ -559,10 +559,10 @@ class Backtester {
 				marketType = '趋势多';
 				if (adx < 15)
 					marketType = adxPlusDI > adxMinusDI ? '趋势多' : '趋势空';
-				if (adx < 20 && rsi > 45 && adxPlusDI > adxMinusDI) {
+				if (adx < 20 && rsi > 55 && adxPlusDI > adxMinusDI) {
 					marketType = '趋势多且增强';
 				}
-				if (adx < 20 && rsi < 45 && adxPlusDI < adxMinusDI) {
+				if (adx < 20 && rsi < 55 && adxPlusDI < adxMinusDI) {
 					marketType = '趋势空且增强';
 				}
 			}
@@ -601,10 +601,10 @@ class Backtester {
 				marketType = '趋势空';
 				if (adx < 15)
 					marketType = adxPlusDI > adxMinusDI ? '趋势空' : '趋势多';
-				if (adx < 20 && rsi > 55 && adxPlusDI > adxMinusDI) {
+				if (adx < 20 && rsi > 45 && adxPlusDI > adxMinusDI) {
 					marketType = '趋势多且增强';
 				}
-				if (adx < 20 && rsi < 55 && adxPlusDI < adxMinusDI) {
+				if (adx < 20 && rsi < 45 && adxPlusDI < adxMinusDI) {
 					marketType = '趋势空且增强';
 				}
 			}
