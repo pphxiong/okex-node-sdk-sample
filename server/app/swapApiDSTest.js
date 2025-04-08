00b4d322312424782d0712ef5337e8a6944ebd24
pphxiong <1017551718@qq.com>
2025-04-08 10:19:42 +0800

    bug

--- server/app/swapApiDSTest.js
+++ server/app/swapApiDSTest.js
@@ -530,7 +530,7 @@ class Backtester {
 			if (close > emaFast) {
 				if (adx > 50) {
 					if (adxPlusDI > adxMinusDI && rsi > 70) {
-						marketType = '趋势多';
+						marketType = '趋势空且增强';
 					}
 				} else if (adxPlusDI > adxMinusDI && adx >= 25) {
 					if (rsi > 70) {
@@ -575,7 +575,7 @@ class Backtester {
 			if (close < emaFast) {
 				if (adx > 50) {
 					if (adxPlusDI < adxMinusDI && rsi < 30) {
-						marketType = '趋势空';
+						marketType = '趋势多且增强';
 					}
 				} else if (adxPlusDI < adxMinusDI && adx >= 25) {
 					if (rsi < 30) {
