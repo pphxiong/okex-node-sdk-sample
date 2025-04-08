aabee6fab14a585d78a7ec46a04532048a35f87d
pphxiong <1017551718@qq.com>
2025-04-08 22:27:38 +0800

    bug

--- server/app/swapApiDSTest.js
+++ server/app/swapApiDSTest.js
@@ -541,13 +541,13 @@ class Backtester {
         }
       } else if (close < emaSlow) {
         marketType = "趋势多";
-        if (adx < 15) marketType = adxPlusDI > adxMinusDI ? "趋势多" : "趋势空";
-        if (adx < 20 && rsi > 55 && adxPlusDI > adxMinusDI) {
-          marketType = "趋势多且增强";
-        }
-        if (adx < 20 && rsi < 45 && adxPlusDI < adxMinusDI) {
-          marketType = "趋势空且增强";
-        }
+      }
+      if (adx < 15) marketType = adxPlusDI > adxMinusDI ? "趋势多" : "趋势空";
+      if (adx < 20 && rsi > 55 && adxPlusDI > adxMinusDI) {
+        marketType = "趋势多且增强";
+      }
+      if (adx < 20 && rsi < 45 && adxPlusDI < adxMinusDI) {
+        marketType = "趋势空且增强";
       }
     }
 
@@ -578,14 +578,6 @@ class Backtester {
           marketType = "趋势空";
           if (adxPlusDI > adxMinusDI) {
             marketType = "趋势多";
-          }
-          if (adx < 15)
-            marketType = adxPlusDI > adxMinusDI ? "趋势空" : "趋势多";
-          if (adx < 20 && rsi > 55 && adxPlusDI > adxMinusDI) {
-            marketType = "趋势多且增强";
-          }
-          if (adx < 20 && rsi < 45 && adxPlusDI < adxMinusDI) {
-            marketType = "趋势空且增强";
           }
         }
       } else if (close > emaSlow) {
