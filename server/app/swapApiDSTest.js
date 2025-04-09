3b4229388e0ca75c2f0f57cc0a963c0453c1b73a
pphxiong <1017551718@qq.com>
2025-04-09 15:12:59 +0800

    n

--- server/app/swapApiDSTest.js
+++ server/app/swapApiDSTest.js
@@ -555,12 +555,12 @@ class Backtester {
 				}
 			} else if (close < emaSlow) {
 				marketType = '趋势多';
-				if (adx < 15)
+				if (adx < 25)
 					marketType = adxPlusDI > adxMinusDI ? '趋势多' : '趋势空';
-				if (adx < 15 && rsi > 55 && adxPlusDI > adxMinusDI) {
+				if (adx < 20 && rsi > 55 && adxPlusDI > adxMinusDI) {
 					marketType = '趋势多且增强';
 				}
-				if (adx < 15 && rsi < 45 && adxPlusDI < adxMinusDI) {
+				if (adx < 20 && rsi < 45 && adxPlusDI < adxMinusDI) {
 					marketType = '趋势空且增强';
 				}
 				if (adx >= 25 && rsi > 40) marketType = '趋势多且增强';
@@ -601,12 +601,12 @@ class Backtester {
 				}
 			} else if (close > emaSlow) {
 				marketType = '趋势空';
-				if (adx < 15)
+				if (adx < 25)
 					marketType = adxPlusDI > adxMinusDI ? '趋势空' : '趋势多';
-				if (adx < 15 && rsi > 55 && adxPlusDI > adxMinusDI) {
+				if (adx < 20 && rsi > 55 && adxPlusDI > adxMinusDI) {
 					marketType = '趋势多且增强';
 				}
-				if (adx < 15 && rsi < 45 && adxPlusDI < adxMinusDI) {
+				if (adx < 20 && rsi < 45 && adxPlusDI < adxMinusDI) {
 					marketType = '趋势空且增强';
 				}
 			} else {
