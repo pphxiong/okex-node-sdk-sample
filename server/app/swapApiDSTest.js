57506557507125cd45ee7d419f49565b398ddef6
pphxiong <1017551718@qq.com>
2025-04-09 14:53:32 +0800

    n

--- server/app/swapApiDSTest.js
+++ server/app/swapApiDSTest.js
@@ -555,8 +555,8 @@ class Backtester {
 				}
 			} else if (close < emaSlow) {
 				marketType = '趋势多';
-				if (adx < 15)
-					marketType = adxPlusDI > adxMinusDI ? '趋势多' : '趋势空';
+				// if (adx < 15)
+				// 	marketType = adxPlusDI > adxMinusDI ? '趋势多' : '趋势空';
 				if (adx < 20 && rsi > 55 && adxPlusDI > adxMinusDI) {
 					marketType = '趋势多且增强';
 				}
@@ -601,8 +601,8 @@ class Backtester {
 				}
 			} else if (close > emaSlow) {
 				marketType = '趋势空';
-				if (adx < 15)
-					marketType = adxPlusDI > adxMinusDI ? '趋势空' : '趋势多';
+				// if (adx < 15)
+				// 	marketType = adxPlusDI < adxMinusDI ? '趋势空' : '趋势多';
 				if (adx < 20 && rsi > 55 && adxPlusDI > adxMinusDI) {
 					marketType = '趋势多且增强';
 				}
