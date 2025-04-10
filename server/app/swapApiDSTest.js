f1fbd2910ebfa87464fe1e6c7935ee0c78829896
pphxiong <1017551718@qq.com>
2025-04-10 14:57:19 +0800

    new

--- server/app/swapApiDSTest.js
+++ server/app/swapApiDSTest.js
@@ -1018,8 +1018,8 @@ class Backtester {
 
 		const profitTotal = this.balance - config.initialBalance;
 
-		console.log('\n最近20笔交易:');
-		console.table(this.trades);
+		// console.log('\n最近20笔交易:');
+		// console.table(this.trades);
 
 		console.log(`
       ========== 回测结果 ==========
@@ -1066,16 +1066,16 @@ class Backtester {
 // 执行回测
 (async () => {
 	const backtester = new Backtester();
-	const start = '2021-05-01';
-	// const start = '2021-01-01';
+	// const start = '2021-05-01';
+	const start = '2021-01-01';
 	const end = '2025-04-10';
 	const interval = 30;
 	let profitTotal = 0;
 
 	let i = 0;
 	let startTime = moment(start).add(i, 'days');
-	// while (moment(end).isAfter(startTime)) {
-	while (i === 0) {
+	while (moment(end).isAfter(startTime)) {
+		// while (i === 0) {
 		try {
 			backtester.data = {
 				[config.slowframe]: [],
@@ -1102,26 +1102,26 @@ class Backtester {
 			// 步骤2: 计算指标
 			await backtester.calculateIndicators();
 
-			console.log(
-				data[config.slowframe]
-					// .slice(-5)
-					.filter(
-						(item) =>
-							moment(item.timestamp).isAfter(
-								moment('2021-05-14 06:00:00')
-							) &&
-							moment(item.timestamp).isBefore(
-								moment('2021-05-14 07:00:00')
-							)
-					)
-					.map((candle) =>
-						Object.assign(candle, {
-							timestamp: moment(candle.timestamp).format(
-								'YYYY-MM-DD HH:mm:ss'
-							),
-						})
-					)
-			);
+			// console.log(
+			// 	data[config.slowframe]
+			// 		// .slice(-5)
+			// 		.filter(
+			// 			(item) =>
+			// 				moment(item.timestamp).isAfter(
+			// 					moment('2021-05-14 06:00:00')
+			// 				) &&
+			// 				moment(item.timestamp).isBefore(
+			// 					moment('2021-05-14 07:00:00')
+			// 				)
+			// 		)
+			// 		.map((candle) =>
+			// 			Object.assign(candle, {
+			// 				timestamp: moment(candle.timestamp).format(
+			// 					'YYYY-MM-DD HH:mm:ss'
+			// 				),
+			// 			})
+			// 		)
+			// );
 
 			// console.log(data[config.slowframe].length);
 
