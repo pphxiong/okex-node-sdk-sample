cb238d4a0ee97776e3084e86230abfbd424cd005
pphxiong <1017551718@qq.com>
2025-04-10 09:27:28 +0800

    n

--- server/app/swapApiDSTest.js
+++ server/app/swapApiDSTest.js
@@ -548,7 +548,7 @@ class Backtester {
 						rsi > 60 &&
 						emaSlope > config.emaSlope.emaSlopeThreshold
 					) {
-						// marketType = '趋势多且增强';
+						marketType = '趋势空且增强';
 					}
 				} else {
 					marketType = '趋势空';
@@ -594,7 +594,7 @@ class Backtester {
 						rsi < 40 &&
 						emaSlope < -config.emaSlope.emaSlopeThreshold
 					) {
-						// marketType = '趋势空且增强';
+						marketType = '趋势多且增强';
 					}
 				} else {
 					marketType = '趋势多';
