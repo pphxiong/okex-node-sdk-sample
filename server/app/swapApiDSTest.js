b20c5fc3f59c657bf4e65f03c23f1d12ff245175
pphxiong <1017551718@qq.com>
2025-04-10 10:18:12 +0800

    n

--- server/app/swapApiDSTest.js
+++ server/app/swapApiDSTest.js
@@ -1178,24 +1178,26 @@ class Backtester {
 			// 步骤2: 计算指标
 			await backtester.calculateIndicators();
 
-			// console.log(
-			//   data[config.slowframe]
-			//     .slice(-5)
-			//     // .filter(
-			//     // 	(item) =>
-			//     // 		moment(item.timestamp).isAfter(
-			//     // 			moment('2025-04-02 19:30:00')
-			//     // 		) &&
-			//     // 		moment(item.timestamp).isBefore(
-			//     // 			moment('2025-04-04 02:30:00')
-			//     // 		)
-			//     // )
-			//     .map((candle) =>
-			//       Object.assign(candle, {
-			//         timestamp: moment(candle.timestamp).format("YYYY-MM-DD HH:mm:ss"),
-			//       })
-			//     )
-			// );
+			console.log(
+				data[config.slowframe]
+					// .slice(-5)
+					.filter(
+						(item) =>
+							moment(item.timestamp).isAfter(
+								moment('2024-12-19 21:30:00')
+							) &&
+							moment(item.timestamp).isBefore(
+								moment('2024-12-20 07:30:00')
+							)
+					)
+					.map((candle) =>
+						Object.assign(candle, {
+							timestamp: moment(candle.timestamp).format(
+								'YYYY-MM-DD HH:mm:ss'
+							),
+						})
+					)
+			);
 
 			// console.log(data[config.slowframe].length);
 
