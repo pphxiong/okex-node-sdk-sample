2aa501c8c096c80df23e6cee3dd91e01e792cfff
pphxiong <1017551718@qq.com>
2025-04-15 17:16:58 +0800

    n

--- server/app/swapApiDSTest.js
+++ server/app/swapApiDSTest.js
@@ -1042,8 +1042,8 @@ class Backtester {
 
 		const profitTotal = this.balance - config.initialBalance;
 
-		console.log('\n最近20笔交易:');
-		console.table(this.trades);
+		// console.log('\n最近20笔交易:');
+		// console.table(this.trades);
 
 		console.log(`
       ========== 回测结果 ==========
@@ -1090,15 +1090,15 @@ class Backtester {
 // 执行回测
 (async () => {
 	const backtester = new Backtester();
-	// const start = '2021-01-01';
-	const start = '2025-01-01';
+	const start = '2021-01-01';
+	// const start = '2025-01-01';
 	const end = '2025-04-10';
 	const interval = 30;
 	let profitTotal = 0;
 
 	let i = 0;
-	let startTime = moment(start).add(i, 'days');
-	// while (moment(end).isAfter(startTime)) {
+	// let startTime = moment(start).add(i, 'days');
+	while (moment(end).isAfter(startTime)) {
 	while (i === 0) {
 		try {
 			backtester.data = {
