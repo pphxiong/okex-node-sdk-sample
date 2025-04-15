783b22f2bdd2d3b5ea73b2991c0b653b7e946cee
pphxiong <1017551718@qq.com>
2025-04-15 21:51:36 +0800

    n

--- server/app/swapApiDSTest.js
+++ server/app/swapApiDSTest.js
@@ -551,10 +551,10 @@ class Backtester {
         }
 
         if (adx >= 25 && rsi > 40) marketType = "趋势多且增强";
-        if (adx < 15) marketType = adxPlusDI > adxMinusDI ? "趋势多" : "趋势空";
-        if (adx < 25) {
-          marketType = rsi < 40 ? "趋势空" : "趋势多";
-        }
+        // if (adx < 15) marketType = adxPlusDI > adxMinusDI ? "趋势多" : "趋势空";
+        // if (adx < 25) {
+        //   marketType = rsi < 40 ? "趋势空" : "趋势多";
+        // }
         if (adx < 20 && rsi > 55 && adxPlusDI > adxMinusDI) {
           marketType = "趋势多且增强";
         }
