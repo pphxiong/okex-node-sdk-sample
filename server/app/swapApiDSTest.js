862cb52e44f0412b60e52dac144a67d942d11cfe
pphxiong <1017551718@qq.com>
2025-04-15 09:11:07 +0800

    n

--- server/app/swapApiDSTest.js
+++ server/app/swapApiDSTest.js
@@ -548,11 +548,7 @@ class Backtester {
 				} else {
 					if (adx < 20 && adx > 15) {
 						if (rsi > 60) marketType = '趋势空且增强';
-						if (rsi < 45)
-							marketType =
-								Math.abs(adxPlusDI - adxMinusDI) < 10
-									? '趋势多且增强'
-									: '趋势空且增强';
+						if (rsi < 45) marketType = '趋势多且增强';
 					}
 				}
 			} else if (close < emaSlow) {
@@ -606,7 +602,7 @@ class Backtester {
 						if (rsi > 60) marketType = '趋势空且增强';
 						if (rsi < 45)
 							marketType =
-								Math.abs(adxPlusDI - adxMinusDI) < 10
+								Math.abs(adxPlusDI - adxMinusDI) > 10
 									? '趋势多且增强'
 									: '趋势空且增强';
 					}
