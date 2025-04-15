dea166d91b2d6312789e1f14dd915d303fa5015e
pphxiong <1017551718@qq.com>
2025-04-15 09:02:07 +0800

    n

--- server/app/swapApiDSTest.js
+++ server/app/swapApiDSTest.js
@@ -550,13 +550,13 @@ class Backtester {
 						if (rsi > 60)
 							marketType =
 								Math.abs(adxPlusDI - adxMinusDI) > 10
-									? '趋势多且增强'
-									: '趋势空且增强';
+									? '趋势空且增强'
+									: '趋势多且增强';
 						if (rsi < 45)
 							marketType =
 								Math.abs(adxPlusDI - adxMinusDI) > 10
-									? '趋势空且增强'
-									: '趋势多且增强';
+									? '趋势多且增强'
+									: '趋势空且增强';
 					}
 				}
 			} else if (close < emaSlow) {
@@ -610,13 +610,13 @@ class Backtester {
 						if (rsi > 60)
 							marketType =
 								Math.abs(adxPlusDI - adxMinusDI) > 10
-									? '趋势多且增强'
-									: '趋势空且增强';
+									? '趋势空且增强'
+									: '趋势多且增强';
 						if (rsi < 45)
 							marketType =
 								Math.abs(adxPlusDI - adxMinusDI) > 10
-									? '趋势空且增强'
-									: '趋势多且增强';
+									? '趋势多且增强'
+									: '趋势空且增强';
 					}
 				}
 			} else if (close > emaSlow) {
