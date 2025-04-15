497d3cc0796814c368da0406d1d15bd05a757cd1
pphxiong <1017551718@qq.com>
2025-04-15 16:11:44 +0800

    n

--- server/app/swapApiDSTest.js
+++ server/app/swapApiDSTest.js
@@ -545,10 +545,17 @@ class Backtester {
 						marketType = '趋势多且增强';
 					}
 				} else {
-					if (adx < 20 && adx > 10) {
+					if (adx < 20 && adx > 15) {
 						if (
 							rsi > 60 &&
 							emaSlope > config.emaSlope.emaSlopeThreshold * 2
+						)
+							marketType = '趋势空且增强';
+					}
+					if (adx < 25 && adx > 20) {
+						if (
+							rsi > 55 &&
+							emaSlope > config.emaSlope.emaSlopeThreshold
 						)
 							marketType = '趋势空且增强';
 					}
@@ -599,7 +606,7 @@ class Backtester {
 						marketType = '趋势空且增强';
 					}
 				} else {
-					if (adx < 20 && adx > 10) {
+					if (adx < 20 && adx > 15) {
 						if (
 							rsi < 45 &&
 							emaSlope < -config.emaSlope.emaSlopeThreshold * 2
