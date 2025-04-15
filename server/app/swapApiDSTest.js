79278a4ca50bdf95d824269f0845e032f0d4d6c8
pphxiong <1017551718@qq.com>
2025-04-15 22:05:31 +0800

    n

--- server/app/swapApiDSTest.js
+++ server/app/swapApiDSTest.js
@@ -551,7 +551,6 @@ class Backtester {
         }
 
         if (adx >= 25 && rsi > 40) marketType = "趋势多且增强";
-        // if (adx < 15) marketType = adxPlusDI > adxMinusDI ? "趋势多" : "趋势空";
         if (adx < 25) {
           marketType = rsi < 40 ? "趋势空" : "趋势多";
         }
@@ -593,8 +592,17 @@ class Backtester {
           }
         }
       } else if (close > emaSlow) {
-        // marketType = "趋势空";
-        // if (adx < 15) marketType = adxPlusDI > adxMinusDI ? "趋势空" : "趋势多";
+        if (adxPlusDI < adxMinusDI) {
+          if (rsi < 50) marketType = "趋势空且增强";
+        }
+        if (adxPlusDI > adxMinusDI) {
+          if (rsi > 50) marketType = "趋势多且增强";
+        }
+
+        if (adx >= 25 && rsi > 40) marketType = "趋势多且增强";
+        if (adx < 25) {
+          marketType = rsi < 40 ? "趋势空" : "趋势多";
+        }
         if (adx < 20 && rsi > 55 && adxPlusDI > adxMinusDI) {
           marketType = "趋势多且增强";
         }
