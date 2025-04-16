fc24a88ea674a8132fcda8aba0eb4a3930986326
pphxiong <1017551718@qq.com>
2025-04-16 09:24:20 +0800

    n

--- server/app/swapApiDSTest.js
+++ server/app/swapApiDSTest.js
@@ -589,6 +589,7 @@ class Backtester {
 						if (
 							rsi < 25 &&
 							rsi > 20 &&
+							adxPlusDI > 10 &&
 							emaSlope < -config.emaSlope.emaSlopeThreshold * 2 &&
 							adx >= 30
 						)
@@ -1039,8 +1040,8 @@ class Backtester {
 
 		const profitTotal = this.balance - config.initialBalance;
 
-		console.log('\n最近20笔交易:');
-		console.table(this.trades);
+		// console.log('\n最近20笔交易:');
+		// console.table(this.trades);
 
 		console.log(`
       ========== 回测结果 ==========
@@ -1087,16 +1088,16 @@ class Backtester {
 // 执行回测
 (async () => {
 	const backtester = new Backtester();
-	const start = '2025-03-20';
-	// const start = '2025-01-01';
+	// const start = '2025-03-20';
+	const start = '2025-01-01';
 	const end = '2025-04-10';
 	const interval = 30;
 	let profitTotal = 0;
 
 	let i = 0;
 	let startTime = moment(start).add(i, 'days');
-	// while (moment(end).isAfter(startTime)) {
-	while (i === 0) {
+	while (moment(end).isAfter(startTime)) {
+		// while (i === 0) {
 		try {
 			backtester.data = {
 				[config.slowframe]: [],
