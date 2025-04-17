3a89ff0d31d7d1da803e391fe9c58939e449b8ac
pphxiong <1017551718@qq.com>
2025-04-17 17:33:14 +0800

    n

--- server/app/swapApiDSTest.js
+++ server/app/swapApiDSTest.js
@@ -529,34 +529,25 @@ class Backtester {
 		if (emaFast > emaSlow) {
 			if (close > emaFast) {
 				if (adxPlusDI > adxMinusDI) {
-					if (adxMinusDI > 15 || true) {
-						if (rsi > 70) {
-							marketType = adx < 70 ? '趋势空' : '趋势多';
-							if (
-								adx < 70 &&
-								rsi > 75 &&
-								emaSlope >
-									config.emaSlope.emaSlopeThreshold * 2 &&
-								adx >= 30
-							)
-								marketType = '趋势空且增强';
-						} else if (
-							rsi < 60 &&
-							emaSlope > config.emaSlope.emaSlopeThreshold / 2
-						) {
-							marketType = '趋势多且增强';
-						}
-					} else {
-						if (adxMinusDI < 10) marketType = '趋势多';
-						if (adx >= 30) {
-							if (rsi < 75) {
-								marketType = '趋势多且增强';
-							}
-							if (rsi > 85) {
-								if (adxMinusDI > 10)
-									marketType = '趋势空且增强';
-							}
-						}
+					if (rsi > 70) {
+						marketType = adx < 70 ? '趋势空' : '趋势多';
+						if (
+							adx < 70 &&
+							rsi > 75 &&
+							emaSlope > config.emaSlope.emaSlopeThreshold * 2 &&
+							adx >= 30
+						)
+							marketType = '趋势空且增强';
+					} else if (
+						rsi < 60 &&
+						emaSlope > config.emaSlope.emaSlopeThreshold / 2
+					) {
+						marketType = '趋势多且增强';
+					}
+
+					if (adx < 15) {
+						if (rsi > 60) marketType = '趋势空';
+						if (rsi > 65) marketType = '趋势空且增强';
 					}
 				} else {
 					if (adx < 20 && adx > 15) {
@@ -612,14 +603,10 @@ class Backtester {
 						marketType = '趋势空且增强';
 					}
 
-					// if (adx < 25) {
-					// 	if (rsi > 60) marketType = '趋势空';
-					// 	if (rsi > 65) marketType = '趋势空且增强';
+					// if (adx < 20) {
+					// 	if (rsi < 35) marketType = '趋势多';
+					// 	if (rsi < 30) marketType = '趋势多且增强';
 					// }
-					if (adx < 20) {
-						if (rsi < 35) marketType = '趋势多';
-						if (rsi < 30) marketType = '趋势多且增强';
-					}
 					if (adx < 15) {
 						if (rsi < 40) marketType = '趋势多';
 						if (rsi < 35) marketType = '趋势多且增强';
