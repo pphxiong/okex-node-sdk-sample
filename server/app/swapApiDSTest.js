04e3868c75fc446baaa68ddb521561d8969bc1f2
pphxiong <1017551718@qq.com>
2025-04-18 14:28:27 +0800

    n

--- server/app/swapApiDSTest.js
+++ server/app/swapApiDSTest.js
@@ -566,18 +566,16 @@ class Backtester {
 					marketType = rsi < 40 ? '趋势空' : '趋势多';
 				}
 				if (adx < 20) {
-					// if (rsi > 55 && adxPlusDI > adxMinusDI)
-					// 	marketType = '趋势多且增强';
 					if (adxPlusDI < adxMinusDI) {
 						marketType = '趋势空';
 					}
-				}
-				if (adx < 20) {
-					// if (rsi < 45 && adxPlusDI < adxMinusDI)
-					// 	marketType = '趋势空且增强';
 					if (adxPlusDI > adxMinusDI) {
 						marketType = '趋势多';
 					}
+					if (rsi > 55 && adxPlusDI > adxMinusDI)
+						marketType = '趋势多且增强';
+					if (rsi < 45 && adxPlusDI < adxMinusDI)
+						marketType = '趋势空且增强';
 				}
 				// if (adx < 25 && adx > 20) {
 				// 	if (rsi > 50) marketType = '趋势多且增强';
@@ -633,18 +631,10 @@ class Backtester {
 					marketType = rsi > 60 ? '趋势多' : '趋势空';
 				}
 				if (adx < 20) {
-					// if (rsi > 55 && adxPlusDI > adxMinusDI)
-					// 	marketType = '趋势多且增强';
-					if (adxPlusDI < adxMinusDI) {
-						marketType = '趋势空';
-					}
-				}
-				if (adx < 20) {
-					// if (rsi < 45 && adxPlusDI < adxMinusDI)
-					// 	marketType = '趋势空且增强';
-					if (adxPlusDI > adxMinusDI) {
-						marketType = '趋势多';
-					}
+					if (rsi > 55 && adxPlusDI > adxMinusDI)
+						marketType = '趋势多且增强';
+					if (rsi < 45 && adxPlusDI < adxMinusDI)
+						marketType = '趋势空且增强';
 				}
 			} else {
 			}
