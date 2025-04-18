d37dfa045256722d0a5e9c91f388db138c2aadbb
pphxiong <1017551718@qq.com>
2025-04-18 11:02:30 +0800

    n

--- server/app/swapApiDSTest.js
+++ server/app/swapApiDSTest.js
@@ -1134,10 +1134,10 @@ class Backtester {
 					.filter(
 						(item) =>
 							moment(item.timestamp).isAfter(
-								moment('2025-04-07 01:30:00')
+								moment('2025-04-09 10:00:00')
 							) &&
 							moment(item.timestamp).isBefore(
-								moment('2025-04-07 09:45:00')
+								moment('2025-04-09 17:00:00')
 							)
 					)
 					.map((candle) =>
