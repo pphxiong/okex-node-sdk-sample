2d17379accfa2d4e21af0a4c69df9da0f553e7d6
pphxiong <1017551718@qq.com>
2025-04-26 19:13:18 +0800

    n

--- server/app/swapApiDSTest.js
+++ server/app/swapApiDSTest.js
@@ -535,11 +535,10 @@ class Backtester {
 						marketType = '趋势空';
 						if (
 							rsi > 75 &&
-							emaSlope > config.emaSlope.emaSlopeThreshold * 2 &&
-							adx >= 30
+							emaSlope > config.emaSlope.emaSlopeThreshold * 2
 						)
 							marketType =
-								adxMinusDI > 10
+								adxMinusDI > 10 || adx < 30
 									? '趋势空且增强'
 									: '趋势多且增强';
 					} else if (
