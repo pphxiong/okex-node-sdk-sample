7a788eed05f666741901b3020bb2a331f340c927
pphxiong <1017551718@qq.com>
2025-04-28 10:54:23 +0800

    n

--- server/app/swapApiDS.js
+++ server/app/swapApiDS.js
@@ -1031,6 +1031,7 @@ async function strategyLoop(isShowLog = false) {
 
 		// 步骤3: 检查强制平仓
 		if (RiskManager.checkStopConditions(signal)) {
+			console.log(111);
 			await RiskManager.closePosition(signal, orderBook);
 			return;
 		}
@@ -1040,6 +1041,7 @@ async function strategyLoop(isShowLog = false) {
 			if (!signal.buySignal && !signal.sellSignal) {
 				return;
 			}
+			console.log(222);
 			const { klin, slowMarketType } = signal;
 			if (
 				signal.buySignal /* && orderBook.spread < orderBook.ask * 0.001 */
