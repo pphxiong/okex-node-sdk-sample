206dfe9125cdef5357c5f1f15505479e5e5fd3dc
pphxiong <1017551718@qq.com>
2025-04-29 11:46:10 +0800

    n

--- server/app/swapApiDSTest.js
+++ server/app/swapApiDSTest.js
@@ -593,7 +593,7 @@ class Backtester {
 			} else {
 				if (adx < 25 && adx > 20) {
 					if (rsi > 50) marketType = '趋势多且增强-6';
-					if (rsi < 50) marketType = '趋势多且增强-5';
+					if (rsi < 50) marketType = '趋势多且增强-test';
 				}
 			}
 		}
