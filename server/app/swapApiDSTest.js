05fc64baa156190cab4e71bdaea85bd153224d98
pphxiong <1017551718@qq.com>
2025-05-17 11:47:40 +0800

    n

--- server/app/swapApiDSTest.js
+++ server/app/swapApiDSTest.js
@@ -533,18 +533,20 @@ class Backtester {
 				if (adxPlusDI > adxMinusDI) {
 					if (rsi > 70) {
 						marketType = adx >= 30 ? '趋势多' : '趋势空';
-					} else if (rsi > 75) {
-						// marketType = '趋势空';
-						// if (
-						// 	emaSlope > config.emaSlope.emaSlopeThreshold * 2 &&
-						// 	adx >= 30
-						// )
-						// 	marketType =
-						// 		adxMinusDI > 10 && rsi > 80
-						// 			? '趋势空且增强-1'
-						// 			: adxMinusDI < 10
-						// 			? '趋势多且增强-1'
-						// 			: '';
+						// if (rsi > 75) {
+						// 	marketType = '趋势空';
+						// 	if (
+						// 		emaSlope >
+						// 			config.emaSlope.emaSlopeThreshold * 2 &&
+						// 		adx >= 30
+						// 	)
+						// 		marketType =
+						// 			adxMinusDI > 10 && rsi > 80
+						// 				? '趋势空且增强-1'
+						// 				: adxMinusDI < 10
+						// 				? '趋势多且增强-1'
+						// 				: '';
+						// }
 					} else if (
 						rsi < 60 &&
 						emaSlope > config.emaSlope.emaSlopeThreshold / 2
@@ -602,8 +604,8 @@ class Backtester {
 			if (close < emaFast) {
 				if (adxPlusDI < adxMinusDI) {
 					if (rsi < 30) {
-						// marketType = '趋势多';
-						marketType = adx >= 30 ? '趋势空' : '趋势多';
+						marketType = '趋势多';
+						// marketType = adx >= 30 ? '趋势空' : '趋势多';
 					} else if (
 						rsi < 25 &&
 						emaSlope < -config.emaSlope.emaSlopeThreshold * 2
