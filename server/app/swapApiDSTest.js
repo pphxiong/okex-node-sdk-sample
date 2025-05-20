b2b23b0a9487b497fe57cf968c345a9b8e7442aa
pphxiong <1017551718@qq.com>
2025-05-20 21:27:16 +0800

    n

--- server/app/swapApiDSTest.js
+++ server/app/swapApiDSTest.js
@@ -613,7 +613,7 @@ class Backtester {
             rsi < 45 &&
             emaSlope < -config.emaSlope.emaSlopeThreshold
           ) {
-            marketType = adx >= 30 ? "趋势空" : "";
+            // marketType = adx >= 30 ? "趋势空" : "";
           }
         } else {
           if (adx < 20 && adx > 15) {
