b0d296cc34f9501f715cc5b796fd0778560f7db4
pphxiong <1017551718@qq.com>
2025-05-20 12:38:07 +0800

    n

--- server/app/swapApiDSTest.js
+++ server/app/swapApiDSTest.js
@@ -511,18 +511,21 @@ class Backtester {
 		if (close > emaFast) {
 			if (adxPlusDI > adxMinusDI) {
 				if (rsi > 70) {
-					// marketType = adx >= 30 ? "趋势多" : "趋势空";
-					marketType = '趋势空';
-					// if (rsi > 75) {
-					//   marketType = "趋势空";
-					//   if (emaSlope > config.emaSlope.emaSlopeThreshold * 2 && adx >= 30)
-					//     marketType =
-					//       adxMinusDI > 10 && rsi > 80
-					//         ? "趋势空且增强-1"
-					//         : adxMinusDI < 10
-					//         ? "趋势多且增强-1"
-					//         : "";
-					// }
+					marketType = adx >= 30 ? '趋势多' : '趋势空';
+					// marketType = '趋势空';
+					if (rsi > 75) {
+						marketType = '趋势空';
+						if (
+							emaSlope > config.emaSlope.emaSlopeThreshold * 2 &&
+							adx >= 30
+						)
+							marketType =
+								adxMinusDI > 10
+									? '趋势空且增强-1test'
+									: adxMinusDI < 10
+									? '趋势空且增强-2test'
+									: '';
+					}
 				} else if (
 					rsi < 60 &&
 					emaSlope > config.emaSlope.emaSlopeThreshold / 2
