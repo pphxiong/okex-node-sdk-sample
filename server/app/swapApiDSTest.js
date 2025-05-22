f8f1a2ece8068e04593af9a77892dd0bb845c69d
pphxiong <1017551718@qq.com>
2025-05-22 09:41:22 +0800

    n

--- server/app/swapApiDSTest.js
+++ server/app/swapApiDSTest.js
@@ -530,11 +530,10 @@ class Backtester {
 
 		if (emaFast > emaSlow) {
 			if (close > emaFast) {
-				// if (rsi > 70) {
-				// 	marketType = adx >= 30 ? '趋势多' : '趋势空';
-				// }
 				if (adxPlusDI > adxMinusDI) {
-					if (
+					if (rsi > 70) {
+						marketType = adx >= 30 ? '趋势多' : '趋势空';
+					} else if (
 						rsi < 60 &&
 						emaSlope > config.emaSlope.emaSlopeThreshold / 2
 					) {
@@ -556,24 +555,37 @@ class Backtester {
 								: adx > 10
 								? '趋势多且增强-1-8'
 								: '趋势多且增强-1-9';
-						//         if (emaSlope < config.emaSlope.emaSlopeThreshold)
-						// marketType = '趋势空且增强-7test';
+					} else if (
+						rsi < 65 &&
+						emaSlope > config.emaSlope.emaSlopeThreshold
+					) {
+						marketType =
+							adx > 40
+								? '趋势多且增强-2-1'
+								: adx >= 35
+								? '趋势多且增强-2-2'
+								: adx >= 30
+								? '趋势多且增强-2-3'
+								: adx >= 25
+								? '趋势多且增强-2-4'
+								: adx > 20
+								? '趋势多且增强-2-5'
+								: adx > 17.5
+								? '趋势多且增强-2-6'
+								: adx > 15
+								? '趋势多且增强-2-7'
+								: adx > 10
+								? '趋势多且增强-2-8'
+								: '趋势多且增强-2-9';
 					}
-				}
-				if (
-					rsi > 60 &&
-					emaSlope > config.emaSlope.emaSlopeThreshold * 2
-				) {
-					if (adx < 20 && adx > 15) marketType = '趋势空且增强-2-1';
-					if (rsi > 65) {
-						marketType = adx >= 25 ? '趋势多' : '趋势空且增强-2-2';
+				} else {
+					if (adx < 20 && adx > 15) {
+						if (
+							rsi > 60 &&
+							emaSlope > config.emaSlope.emaSlopeThreshold * 2
+						)
+							marketType = '趋势空且增强-2';
 					}
-					if (rsi > 70) {
-						marketType = adx >= 30 ? '趋势多' : '趋势空且增强-2-3';
-					}
-					// if (rsi > 80) {
-					// 	marketType = '趋势空且增强-2-4';
-					// }
 				}
 			} else if (close < emaSlow) {
 				if (adxPlusDI < adxMinusDI) {
