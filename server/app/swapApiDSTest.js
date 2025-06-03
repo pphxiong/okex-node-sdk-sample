6c4361ab6812546b65a9dd03522fdf7c122f4c35
pphxiong <1017551718@qq.com>
2025-06-03 21:34:05 +0800

    n

--- server/app/swapApiDSTest.js
+++ server/app/swapApiDSTest.js
@@ -621,9 +621,9 @@ class Backtester {
                 emaSlope > config.emaSlope.emaSlopeThreshold * 2
                   ? "趋势空且增强-L2-4-1"
                   : emaSlope > config.emaSlope.emaSlopeThreshold
-                  ? "趋势多且增强-L2-4-2"
+                  ? ""
                   : emaSlope > config.emaSlope.emaSlopeThreshold / 2
-                  ? "趋势空且增强-L2-4-3"
+                  ? ""
                   : "";
             }
           } else {
@@ -691,9 +691,9 @@ class Backtester {
               ? "趋势多且增强-L3-1-3"
               : rsi > 47.5
               ? emaSlope > config.emaSlope.emaSlopeThreshold * 2
-                ? "趋势多"
+                ? "趋势多且增强-L3-1-4-1"
                 : emaSlope > config.emaSlope.emaSlopeThreshold
-                ? "趋势多"
+                ? "趋势多且增强-L3-1-4-2"
                 : emaSlope > config.emaSlope.emaSlopeThreshold / 2
                 ? "趋势多且增强-L3-1-4-3"
                 : "趋势多且增强-L3-1-4-4"
