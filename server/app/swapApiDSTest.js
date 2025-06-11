496505c209a7970cb2fdd4d71c0dbb8b11cbd648
pphxiong <1017551718@qq.com>
2025-06-11 14:47:32 +0800

    n

--- server/app/swapApiDSTest.js
+++ server/app/swapApiDSTest.js
@@ -541,14 +541,14 @@ class Backtester {
 		if (lastMacd > 0 && macd < 0) {
 			if (emaFast > emaSlow) {
 				marketType =
-					adx >= 25 ? '趋势空且增强-L-1' : '趋势空且增强-L-1';
+					adx >= 40 ? '趋势空且增强-L-1' : '趋势空且增强-L-1';
 			}
 		}
 
 		if (lastMacd < 0 && macd > 0) {
 			if (emaFast < emaSlow) {
 				marketType =
-					adx >= 25 ? '趋势多且增强-R-1' : '趋势多且增强-R-1';
+					adx >= 40 ? '趋势多且增强-R-1' : '趋势多且增强-R-1';
 			}
 		}
 
