7c855ca1d16005db68c3bee1de9a5dccd34cc56b
pphxiong <1017551718@qq.com>
2025-06-11 17:26:49 +0800

    n

--- server/app/swapApiDSTest.js
+++ server/app/swapApiDSTest.js
@@ -532,70 +532,66 @@ class Backtester {
 		const lastStronger = lastEmaFast > lastEmaSlow;
 		const lastWeeker = lastEmaFast < lastEmaSlow;
 
-		if (macdHistogram > lastMacdHistogram && emaFast > emaSlow) {
+		if (macdHistogram > lastMacdHistogram && macd > 0) {
 			marketType = '趋势多';
 		}
 
-		if (macdHistogram < lastMacdHistogram && emaFast < emaSlow) {
+		if (macdHistogram < lastMacdHistogram && macd < 0) {
 			marketType = '趋势空';
 		}
 
-		if (close > emaFast && emaFast > emaSlow) {
-			if (
-				macd > 0 &&
-				macdHistogram > lastMacdHistogram &&
-				lastMacdHistogram < 0
-			) {
-				marketType =
-					adx >= 60
-						? '趋势多且增强-L-1'
-						: adx >= 50
-						? '趋势多且增强-L-2'
-						: adx >= 40
-						? '趋势多且增强-L-3'
-						: adx >= 35
-						? '趋势多且增强-L-4'
-						: adx >= 30
-						? '趋势多且增强-L-5'
-						: adx >= 25
-						? '趋势多且增强-L-6'
-						: adx >= 20
-						? '趋势多且增强-L-7'
-						: adx >= 15
-						? '趋势多且增强-L-8'
-						: adx >= 10
-						? '趋势多且增强-L-9'
-						: '趋势多且增强-L-10';
-			}
+		if (
+			close > emaSlow &&
+			macdHistogram > lastMacdHistogram &&
+			lastMacdHistogram < 0
+		) {
+			marketType =
+				adx >= 60
+					? '趋势多且增强-L-1'
+					: adx >= 50
+					? '趋势多且增强-L-2'
+					: adx >= 40
+					? '趋势多且增强-L-3'
+					: adx >= 35
+					? '趋势多且增强-L-4'
+					: adx >= 30
+					? '趋势多且增强-L-5'
+					: adx >= 25
+					? '趋势多且增强-L-6'
+					: adx >= 20
+					? '趋势多且增强-L-7'
+					: adx >= 15
+					? '趋势多且增强-L-8'
+					: adx >= 10
+					? '趋势多且增强-L-9'
+					: '趋势多且增强-L-10';
 		}
 
-		if (close < emaFast && emaFast < emaSlow) {
-			if (
-				macd < 0 &&
-				macdHistogram < lastMacdHistogram &&
-				lastMacdHistogram > 0
-			) {
-				marketType =
-					adx >= 60
-						? '趋势空且增强-R-1'
-						: adx >= 50
-						? '趋势空且增强-R-2'
-						: adx >= 40
-						? '趋势空且增强-R-3'
-						: adx >= 35
-						? '趋势空且增强-R-4'
-						: adx >= 30
-						? '趋势空且增强-R-5'
-						: adx >= 25
-						? '趋势空且增强-R-6'
-						: adx >= 20
-						? '趋势空且增强-R-7'
-						: adx >= 15
-						? '趋势空且增强-R-8'
-						: adx >= 10
-						? '趋势空且增强-R-9'
-						: '趋势空且增强-R-10';
-			}
+		if (
+			close < emaSlow &&
+			macdHistogram < lastMacdHistogram &&
+			lastMacdHistogram > 0
+		) {
+			marketType =
+				adx >= 60
+					? '趋势空且增强-R-1'
+					: adx >= 50
+					? '趋势空且增强-R-2'
+					: adx >= 40
+					? '趋势空且增强-R-3'
+					: adx >= 35
+					? '趋势空且增强-R-4'
+					: adx >= 30
+					? '趋势空且增强-R-5'
+					: adx >= 25
+					? '趋势空且增强-R-6'
+					: adx >= 20
+					? '趋势空且增强-R-7'
+					: adx >= 15
+					? '趋势空且增强-R-8'
+					: adx >= 10
+					? '趋势空且增强-R-9'
+					: '趋势空且增强-R-10';
 		}
 
 		return marketType;
