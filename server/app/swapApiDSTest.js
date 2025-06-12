3c6623aa62e4b9e95e0d839e5c4a5e76b36cc5aa
pphxiong <1017551718@qq.com>
2025-06-12 08:59:32 +0800

    n

--- server/app/swapApiDSTest.js
+++ server/app/swapApiDSTest.js
@@ -546,7 +546,7 @@ class Backtester {
 		if (lastMacd > lastLastMacd && macd < lastMacd) {
 			if (macd > 0) {
 				marketType =
-					adx >= 25 ? '趋势多且增强-R-1' : '趋势多且增强-R-2';
+					adx >= 25 ? '趋势空且增强-R-1' : '趋势多且增强-R-2';
 			}
 		}
 
