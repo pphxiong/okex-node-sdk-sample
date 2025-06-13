7683024ff1bbde596ec00feb7952ccf411708bbb
pphxiong <1017551718@qq.com>
2025-06-13 12:29:47 +0800

    n

--- server/app/swapApiDSTest.js
+++ server/app/swapApiDSTest.js
@@ -1157,10 +1157,10 @@ function carryForluma(p, rl, rw) {
 					.filter(
 						(item) =>
 							moment(item.timestamp).isAfter(
-								moment('2023-03-18 13:00:00')
+								moment('2023-03-15 08:00:00')
 							) &&
 							moment(item.timestamp).isBefore(
-								moment('2023-03-18 17:00:00')
+								moment('2023-03-15 09:00:00')
 							)
 					)
 					.map((candle) =>
