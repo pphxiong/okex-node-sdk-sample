f33225d15f2e722d9fe4064c0d541335a15f93e2
pphxiong <1017551718@qq.com>
2025-06-13 15:21:14 +0800

    n

--- server/app/swapApiDSTest.js
+++ server/app/swapApiDSTest.js
@@ -564,7 +564,7 @@ class Backtester {
 					}
 				}
 				if (macd < lastMacd && lastMacd > lastLastMacd) {
-					marketType = rsi > 70 ? '趋势空' : rsi > 60 ? '趋势多' : '';
+					marketType = rsi > 65 ? '趋势空' : rsi > 55 ? '趋势多' : '';
 				}
 			}
 		}
@@ -580,7 +580,7 @@ class Backtester {
 					}
 				}
 				if (macd > lastMacd && lastMacd < lastLastMacd) {
-					marketType = rsi < 30 ? '趋势多' : rsi < 40 ? '趋势空' : '';
+					marketType = rsi < 35 ? '趋势多' : rsi < 45 ? '趋势空' : '';
 				}
 			}
 		}
