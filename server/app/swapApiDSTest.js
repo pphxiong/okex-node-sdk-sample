44b555342241c137b81c1cdc923eb753f7710c31
pphxiong <1017551718@qq.com>
2025-06-13 10:03:49 +0800

    n

--- server/app/swapApiDSTest.js
+++ server/app/swapApiDSTest.js
@@ -550,7 +550,6 @@ class Backtester {
 				macd > lastMacd &&
 				lastMacd < 0 &&
 				rsi > 55 &&
-				open > emaSlow &&
 				close > emaSlow
 			) {
 				marketType = '趋势多且增强';
@@ -562,7 +561,6 @@ class Backtester {
 				macd < lastMacd &&
 				lastMacd > 0 &&
 				rsi < 45 &&
-				open < emaSlow &&
 				close < emaSlow
 			) {
 				marketType = '趋势空且增强';
@@ -1038,8 +1036,8 @@ class Backtester {
 
 		const profitTotal = this.balance - config.initialBalance;
 
-		// console.log('\n最近20笔交易:');
-		// console.table(this.trades);
+		console.log('\n最近20笔交易:');
+		console.table(this.trades);
 
 		const maxLoss = Math.min(...this.trades.map((t) => t.profit)).toFixed(
 			2
@@ -1096,8 +1094,8 @@ function carryForluma(p, rl, rw) {
 // 执行回测
 (async () => {
 	const backtester = new Backtester();
-	// const start = '2025-01-10';
-	const start = '2021-01-01';
+	const start = '2025-04-10';
+	// const start = '2021-01-01';
 	const end = '2025-06-01';
 	const interval = 30;
 	let profitTotal = 0;
@@ -1111,8 +1109,8 @@ function carryForluma(p, rl, rw) {
 	let i = 0;
 	let loop = 1;
 	let startTime = moment(start).add(i, 'days');
-	while (moment(end).isAfter(startTime)) {
-		// while (i === 0) {
+	// while (moment(end).isAfter(startTime)) {
+	while (i === 0) {
 		loop += 1;
 		try {
 			backtester.data = {
