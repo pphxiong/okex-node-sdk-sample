e66eef2b2ca8acbf86bb283cd74b1bb1d338ff7b
pphxiong <1017551718@qq.com>
2025-06-13 09:46:00 +0800

    n

--- server/app/swapApiDSTest.js
+++ server/app/swapApiDSTest.js
@@ -536,12 +536,12 @@ class Backtester {
 		const lastStronger = lastEmaFast > lastEmaSlow;
 		const lastWeeker = lastEmaFast < lastEmaSlow;
 
-		if (lastMacd > lastLastMacd && macd > lastMacd) {
-			marketType = '趋势多';
+		if ((lastMacd > lastLastMacd && macd > lastMacd) || close > emaSlow) {
+			marketType = '趋势多趋势空';
 		}
 
-		if (lastMacd < lastLastMacd && macd < lastMacd) {
-			marketType = '趋势空';
+		if ((lastMacd < lastLastMacd && macd < lastMacd) || close < emaSlow) {
+			marketType = '趋势多趋势空';
 		}
 
 		if (emaFast > emaSlow) {
