2a1ab674ffca36cb960f6dcc709e5ed184b94e0e
pphxiong <1017551718@qq.com>
2025-06-16 08:46:32 +0800

    n

--- server/app/swapApiDSTest.js
+++ server/app/swapApiDSTest.js
@@ -543,7 +543,7 @@ class Backtester {
 			if (adxPlusDI > adxMinusDI) {
 				marketType =
 					rsi < 30 ? '趋势多' : rsi < 40 ? '趋势空' : marketType;
-				if (rsi > 30 && rsi < 65) {
+				if (rsi > 35 && rsi < 65) {
 					marketType =
 						emaSlope < -config.emaSlope.emaSlopeThreshold * 2
 							? '趋势多且增强-R-2-1'
@@ -555,7 +555,7 @@ class Backtester {
 							? ''
 							: '';
 				}
-				if (rsi > 30 && rsi < 65 && adx > 25) {
+				if (rsi > 35 && rsi < 65 && adx > 25) {
 					marketType =
 						emaSlope > config.emaSlope.emaSlopeThreshold * 2
 							? '趋势多且增强-R-2-1'
@@ -586,7 +586,7 @@ class Backtester {
 		if (macd < lastMacd && lastMacd > lastLastMacd) {
 			marketType = rsi > 72 ? '趋势空' : rsi > 60 ? '趋势多' : '';
 			if (adxPlusDI < adxMinusDI) {
-				if (rsi > 30 && rsi < 65) {
+				if (rsi > 35 && rsi < 65) {
 					marketType =
 						emaSlope < -config.emaSlope.emaSlopeThreshold * 3
 							? '趋势空且增强-R-1-1'
@@ -600,7 +600,7 @@ class Backtester {
 							? '趋势空'
 							: '';
 				}
-				if (rsi > 30 && rsi < 65 && adx > 25) {
+				if (rsi > 35 && rsi < 65 && adx > 25) {
 					marketType =
 						emaSlope > config.emaSlope.emaSlopeThreshold * 2
 							? '趋势空且增强-R-2-1'
@@ -613,7 +613,7 @@ class Backtester {
 							: marketType;
 				}
 			} else {
-				if (rsi > 30 && rsi < 65 && adx < 20) {
+				if (rsi > 35 && rsi < 65 && adx < 20) {
 					marketType =
 						emaSlope < -config.emaSlope.emaSlopeThreshold * 3
 							? '趋势空且增强-R-3-1'
