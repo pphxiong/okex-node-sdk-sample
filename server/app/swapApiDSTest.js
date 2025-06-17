fd072499175d104bdf3d7ab81c8fc7a5cf3fa2e1
pphxiong <1017551718@qq.com>
2025-06-17 11:01:07 +0800

    n

--- server/app/swapApiDSTest.js
+++ server/app/swapApiDSTest.js
@@ -553,22 +553,20 @@ class Backtester {
 							? '趋势多'
 							: '';
 				}
-			} else {
-				// if (adxPlusDI > adxMinusDI) {
-				marketType =
-					emaSlope > config.emaSlope.emaSlopeThreshold * 2
-						? '趋势多且增强-L-2-1'
-						: emaSlope > config.emaSlope.emaSlopeThreshold
-						? '趋势多且增强-L-2-2'
-						: emaSlope > config.emaSlope.emaSlopeThreshold / 2
-						? '趋势多且增强-L-2-3'
-						: emaSlope > 0
-						? '趋势多且增强-L-2-4'
-						: '趋势空';
-				// }
 			}
-			if (rsi > 70 && emaSlope > config.emaSlope.emaSlopeThreshold * 3) {
-				marketType = adx > 30 ? '趋势多' : '趋势空';
+			if (adx > 30) {
+				if (emaFast > emaSlow) {
+					marketType =
+						emaSlope > config.emaSlope.emaSlopeThreshold * 2
+							? '趋势多且增强-L-2-1'
+							: emaSlope > config.emaSlope.emaSlopeThreshold
+							? '趋势多且增强-L-2-2'
+							: emaSlope > config.emaSlope.emaSlopeThreshold / 2
+							? '趋势多且增强-L-2-3'
+							: emaSlope > 0
+							? '趋势多且增强-L-2-4'
+							: '趋势空';
+				}
 			}
 		}
 
@@ -586,23 +584,30 @@ class Backtester {
 							? '趋势空且增强-R-2-4'
 							: marketType;
 				}
-			} else {
-				// if (adxPlusDI < adxMinusDI) {
-				marketType =
-					emaSlope < -config.emaSlope.emaSlopeThreshold * 2
-						? '趋势空且增强-R-3-1'
-						: emaSlope < -config.emaSlope.emaSlopeThreshold
-						? '趋势空且增强-R-3-2'
-						: emaSlope < -config.emaSlope.emaSlopeThreshold / 2
-						? '趋势空且增强-R-3-3'
-						: emaSlope < 0
-						? '趋势空'
-						: '趋势多';
-				// }
 			}
-			if (rsi < 30 && emaSlope < -config.emaSlope.emaSlopeThreshold * 3) {
-				marketType = adx > 30 ? '趋势空' : '趋势多';
+
+			if (adx > 30) {
+				if (emaFast < emaSlow) {
+					marketType =
+						emaSlope < -config.emaSlope.emaSlopeThreshold * 2
+							? '趋势空且增强-R-3-1'
+							: emaSlope < -config.emaSlope.emaSlopeThreshold
+							? '趋势空且增强-R-3-2'
+							: emaSlope < -config.emaSlope.emaSlopeThreshold / 2
+							? '趋势空且增强-R-3-3'
+							: emaSlope < 0
+							? '趋势空'
+							: '趋势多';
+				}
 			}
+		}
+
+		if (rsi > 70 && emaSlope > config.emaSlope.emaSlopeThreshold * 3) {
+			marketType = adx > 30 ? (rsi < 80 ? '趋势多' : '趋势空') : '趋势空';
+		}
+
+		if (rsi < 30 && emaSlope < -config.emaSlope.emaSlopeThreshold * 3) {
+			marketType = adx > 30 ? (adx > 20 ? '趋势空' : '趋势空') : '趋势多';
 		}
 
 		// if (
