9d28719022d7048330a457fc9affd708d6fa8bfa
pphxiong <1017551718@qq.com>
2025-06-17 17:46:08 +0800

    n

--- server/app/swapApiDSTest.js
+++ server/app/swapApiDSTest.js
@@ -547,13 +547,13 @@ class Backtester {
 			if (emaFast > emaSlow) {
 				if (rsi > 55 && rsi < 65) {
 					if (adxPlusDI > adxMinusDI) {
-						if (macd < lastMacd && lastMacd > lastLastMacd) {
+						if (macd > lastMacd && lastMacd < lastLastMacd) {
 							marketType =
 								rsi > 60
 									? '趋势空且增强-L-3-2-1'
 									: '趋势空且增强-L-3-2-2';
-						} 
-            if(macd > lastMacd && lastMacd < lastLastMacd) {
+						}
+						if (macd < lastMacd && lastMacd > lastLastMacd) {
 							marketType =
 								rsi > 60
 									? '趋势多且增强-L-3-2-3'
