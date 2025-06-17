deac8b08b87e47057f30b8938eb637cef349020b
pphxiong <1017551718@qq.com>
2025-06-17 13:56:16 +0800

    n

--- server/app/swapApiDSTest.js
+++ server/app/swapApiDSTest.js
@@ -541,37 +541,39 @@ class Backtester {
 
 		if (adx < 30) {
 			if (emaFast < emaSlow) {
-				// if (macd < lastMacd && lastMacd > lastLastMacd) {
-				marketType = rsi < 50 && rsi > 40 ? '趋势多且增强-R-3-1' : '';
-				// }
+				if (macd < lastMacd && lastMacd > lastLastMacd) {
+					marketType =
+						rsi < 45 && rsi > 35 ? '趋势多且增强-R-3-1' : '';
+				}
 			}
 
 			if (emaFast > emaSlow) {
-				// if (macd > lastMacd && lastMacd < lastLastMacd) {
-				marketType = rsi > 50 && rsi < 60 ? '趋势空且增强-L-3-1' : '';
-				// }
+				if (macd > lastMacd && lastMacd < lastLastMacd) {
+					marketType =
+						rsi > 55 && rsi < 65 ? '趋势空且增强-L-3-1' : '';
+				}
 			}
 		} else if (adx > 30) {
 			if (emaFast < emaSlow) {
-				// if (macd < lastMacd && lastMacd > lastLastMacd) {
-					marketType = rsi > 40 ? '趋势空且增强-L-2-1' : '趋势空';
-				// }
+				if (macd < lastMacd && lastMacd > lastLastMacd) {
+					marketType = rsi > 35 ? '趋势空且增强-L-2-1' : '趋势空';
+				}
 			}
 
 			if (emaFast > emaSlow) {
-				// if (macd > lastMacd && lastMacd < lastLastMacd) {
-				marketType = rsi < 60 ? '趋势多且增强-R-2-1' : '趋势多';
-				// }
+				if (macd > lastMacd && lastMacd < lastLastMacd) {
+					marketType = rsi < 65 ? '趋势多且增强-R-2-1' : '趋势多';
+				}
 			}
 		} else {
 			marketType = '趋势多趋势空';
 		}
 
-		if (rsi > 70) {
+		if (rsi > 75) {
 			marketType = adx >= 30 ? '趋势多' : '趋势空';
 		}
 
-		if (rsi < 26) {
+		if (rsi < 25) {
 			marketType = adx >= 30 ? '趋势空' : '趋势多';
 		}
 
