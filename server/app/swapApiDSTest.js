48ea86944cf47b37ccd096798af2cc59bfa68f30
pphxiong <1017551718@qq.com>
2025-06-18 20:33:28 +0800

    n

--- server/app/swapApiDSTest.js
+++ server/app/swapApiDSTest.js
@@ -470,7 +470,7 @@ class Backtester {
           if (d.adx && d.atr) {
             const isVolatility = d.atr / d.close > 0.02;
             d.rsi_long = d.adx > 30 ? 48 : 42;
-            d.rsi_short = d.adx > 30 ? 52 : 58;
+            d.rsi_short = d.adx > 30 ? 60 : 58;
             d.stop_multiplier = isVolatility ? 1.8 : 2.5;
             d.adx_threshold = isVolatility ? 30 : 26;
             d.adx_stoploss_distance = isVolatility ? 5 : 3;
