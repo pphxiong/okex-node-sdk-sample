0bf86c283097e5ec216758f76f36485af0a44a34
pphxiong <1017551718@qq.com>
2025-06-18 17:25:10 +0800

    n

--- server/app/swapApiDSTest.js
+++ server/app/swapApiDSTest.js
@@ -489,10 +489,10 @@ class Backtester {
 					d.emaFast = emaFast[0][i];
 					// d.adx = adx[0][i];
 					if (d.adx && d.atr) {
-						d.rsi_long = d.adx > 40 ? 35 : 45;
-						d.rsi_short = d.adx > 40 ? 65 : 60;
+						d.rsi_long = d.adx > 40 ? 35 : 42;
+						d.rsi_short = d.adx > 40 ? 65 : 58;
 						d.stop_multiplier = d.atr / d.close > 0.02 ? 1.8 : 2.5;
-            d.adx_threshold = d.atr / d.close > 0.02 ? 28 : 25;
+						d.adx_threshold = d.atr / d.close > 0.02 ? 32 : 28;
 					}
 					d.marketType = this.getMarketType(
 						d,
