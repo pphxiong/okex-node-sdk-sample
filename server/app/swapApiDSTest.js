98a324afb7a22c26050160d4190a7265e1bfcf37
pphxiong <1017551718@qq.com>
2025-06-19 16:51:03 +0800

    n

--- server/app/swapApiDSTest.js
+++ server/app/swapApiDSTest.js
@@ -583,7 +583,9 @@ class Backtester {
 					// if (lastAdxPlusDI < lastAdxMinusDI) {
 					// 	marketType = '趋势多且增强-L-1-2';
 					// }
-					if (rsi > 70) marketType = '趋势空';
+
+					const max_rsi = adx > 40 ? 75 : 70;
+					if (rsi > max_rsi) marketType = '趋势空';
 				}
 			}
 		}
@@ -601,7 +603,9 @@ class Backtester {
 					// if (lastAdxPlusDI > lastAdxMinusDI) {
 					// 	marketType = '趋势空且增强-R-1-2';
 					// }
-					if (rsi < 30) marketType = '趋势多';
+
+					const min_rsi = adx > 40 ? 30 : 25;
+					if (rsi < min_rsi) marketType = '趋势多';
 				}
 			}
 		}
@@ -1092,8 +1096,8 @@ class Backtester {
 
 		const profitTotal = this.balance - config.initialBalance;
 
-		// console.log('\n最近20笔交易:');
-		// console.table(this.trades);
+		console.log('\n最近20笔交易:');
+		console.table(this.trades);
 
 		const maxLoss = Math.min(...this.trades.map((t) => t.profit)).toFixed(
 			2
@@ -1150,8 +1154,8 @@ function carryForluma(p, rl, rw) {
 // 执行回测
 (async () => {
 	const backtester = new Backtester();
-	// const start = '2025-06-01';
-	const start = '2021-01-01';
+	const start = '2023-05-21';
+	// const start = '2021-01-01';
 	const end = '2025-07-01';
 	const interval = 30;
 	let profitTotal = 0;
@@ -1165,8 +1169,8 @@ function carryForluma(p, rl, rw) {
 	let i = 0;
 	let loop = 1;
 	let startTime = moment(start).add(i, 'days');
-	while (moment(end).isAfter(startTime)) {
-		// while (i === 0) {
+	// while (moment(end).isAfter(startTime)) {
+	while (i === 0) {
 		loop += 1;
 		try {
 			backtester.data = {
