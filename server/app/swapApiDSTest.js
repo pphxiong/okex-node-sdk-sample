fc5b22f280fbc20b0b17166833900cf736eaa991
pphxiong <1017551718@qq.com>
2025-06-20 14:10:48 +0800

    n

--- server/app/swapApiDSTest.js
+++ server/app/swapApiDSTest.js
@@ -588,7 +588,7 @@ class Backtester {
 					// if (lastAdxPlusDI < lastAdxMinusDI) {
 					// 	marketType = '趋势多且增强-L-1-2';
 					// }
-					if (rsi > 74) marketType = '趋势空';
+					if (rsi > (adx > 40 ? 80 : 74)) marketType = '趋势空';
 				} else if (adxPlusDI < adxMinusDI) {
 					if (rsi < 30) marketType = '趋势空';
 				}
@@ -608,7 +608,7 @@ class Backtester {
 					// if (lastAdxPlusDI > lastAdxMinusDI) {
 					// 	marketType = '趋势空且增强-R-1-2';
 					// }
-					if (rsi < 26) marketType = '趋势多';
+					if (rsi < (adx > 40 ? 20 : 26)) marketType = '趋势多';
 				} else if (adxPlusDI > adxMinusDI) {
 					if (rsi > 70) marketType = '趋势多';
 				}
