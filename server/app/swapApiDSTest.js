dc19ae98d89cc700cbccd8006298802f9ce8e54a
pphxiong <1017551718@qq.com>
2025-06-20 09:22:23 +0800

    n

--- server/app/swapApiDSTest.js
+++ server/app/swapApiDSTest.js
@@ -583,7 +583,8 @@ class Backtester {
 					// if (lastAdxPlusDI < lastAdxMinusDI) {
 					// 	marketType = '趋势多且增强-L-1-2';
 					// }
-					if (rsi > 70 || rsi < 30) marketType = '趋势空';
+					if (rsi > 70 || (rsi < 30 && adxMinusDI < 10))
+						marketType = '趋势空';
 				} else if (adxPlusDI < adxMinusDI) {
 					if (rsi < 30) marketType = '趋势空';
 				}
@@ -603,7 +604,8 @@ class Backtester {
 					// if (lastAdxPlusDI > lastAdxMinusDI) {
 					// 	marketType = '趋势空且增强-R-1-2';
 					// }
-					if (rsi < 30 || rsi > 70) marketType = '趋势多';
+					if (rsi < 30 || (rsi > 70 && adxPlusDI < 10))
+						marketType = '趋势多';
 				} else if (adxPlusDI > adxMinusDI) {
 					if (rsi > 70) marketType = '趋势多';
 				}
