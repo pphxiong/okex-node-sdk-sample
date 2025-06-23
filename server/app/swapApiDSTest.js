5b4aa7c98f100e99a92ee36fe920c3543365d4c5
pphxiong <1017551718@qq.com>
2025-06-23 11:22:21 +0800

    n

--- server/app/swapApiDSTest.js
+++ server/app/swapApiDSTest.js
@@ -37,7 +37,7 @@ const config = {
 	timeframes: ['15m' /* '5m'  '1m'*/], // 多周期参数
 	emaSettings: {
 		// '30m': { periods: [10, 5], slopeWindow: 5 },
-		'15m': { periods: [15, 7], slopeWindow: 5 },
+		'15m': { periods: [50, 15], slopeWindow: 5 },
 		// "15m": { periods: [25, 5], slopeWindow: 5 },
 		// '5m': { periods: [10, 5], slopeWindow: 5 },
 	},
@@ -583,9 +583,9 @@ class Backtester {
 					if (rsi < rsi_long) {
 						marketType = '趋势多且增强-L-1-1';
 					}
-					if (rsi > 65) marketType = '趋势多';
+					// if (rsi > 65) marketType = '趋势多';
 				} else if (adxPlusDI < adxMinusDI) {
-					if (rsi < 30) marketType = '趋势空';
+					// if (rsi < 30) marketType = '趋势空';
 				}
 			}
 		}
@@ -597,9 +597,9 @@ class Backtester {
 					if (rsi > rsi_short) {
 						marketType = '趋势空且增强-R-1-1';
 					}
-					if (rsi < 35) marketType = '趋势空';
+					// if (rsi < 35) marketType = '趋势空';
 				} else if (adxPlusDI > adxMinusDI) {
-					if (rsi > 70) marketType = '趋势多';
+					// if (rsi > 70) marketType = '趋势多';
 				}
 			}
 		}
