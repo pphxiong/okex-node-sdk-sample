003a57453e2e8777dc72a102d771ef2f3f918f6e
pphxiong <1017551718@qq.com>
2025-06-25 08:58:44 +0800

    n

--- server/app/swapApiDSTest.js
+++ server/app/swapApiDSTest.js
@@ -507,7 +507,7 @@ class Backtester {
 						d.rsi_short = d.adx > 30 ? 58 : 42;
 						d.stop_multiplier = isVolatility ? 3 : 2.5;
 						d.profit_multiplier = isVolatility ? 2 : 2;
-						d.adx_threshold = isVolatility ? 30 : 28;
+						d.adx_threshold = isVolatility ? 30 : 26;
 						d.adx_stoploss_distance = isVolatility ? 5 : 3;
 						d.volatility_ratio = d.atr / d.close;
 
@@ -1234,7 +1234,7 @@ function carryForluma(p, rl, rw) {
 // 执行回测
 (async () => {
 	const backtester = new Backtester();
-	const start = '2025-06-01';
+	const start = '2025-05-01';
 	// const start = '2021-01-01';
 	const end = '2025-07-01';
 	const interval = 30;
