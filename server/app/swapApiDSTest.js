7724b8f64874474309ba3864b8e58379179500d0
pphxiong <1017551718@qq.com>
2025-06-25 10:56:59 +0800

    n

--- server/app/swapApiDSTest.js
+++ server/app/swapApiDSTest.js
@@ -1260,7 +1260,7 @@ function carryForluma(p, rl, rw) {
 // 执行回测
 (async () => {
 	const backtester = new Backtester();
-	const start = '2025-03-01';
+	const start = '2025-02-01';
 	// const start = '2021-01-01';
 	const end = '2025-07-01';
 	const interval = 30;
