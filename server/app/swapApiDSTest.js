f1ac45d2cf53b2989b758986f849cab382c0f00b
pphxiong <1017551718@qq.com>
2025-06-25 17:00:58 +0800

    n

--- server/app/swapApiDSTest.js
+++ server/app/swapApiDSTest.js
@@ -827,8 +827,8 @@ class Backtester {
 				const lnp = this.getLnp(position, d);
 
 				const longCloseConditions = [
-					position.slowMarketType.indexOf('趋势多且增强') !== -1 &&
-						position.adx - adx > 5,
+					// position.slowMarketType.indexOf('趋势多且增强') !== -1 &&
+					// 	position.adx - adx > 5,
 					position.slowMarketType.indexOf('趋势多且增强') !== -1 &&
 						slowMarketType.indexOf('趋势空') !== -1,
 					position.slowMarketType === '趋势潜在增强' &&
@@ -867,8 +867,8 @@ class Backtester {
 				];
 
 				const shortCloseConditions = [
-					position.slowMarketType.indexOf('趋势空且增强') !== -1 &&
-						position.adx - adx > 5,
+					// position.slowMarketType.indexOf('趋势空且增强') !== -1 &&
+					// 	position.adx - adx > 5,
 					position.slowMarketType.indexOf('趋势空且增强') !== 1 &&
 						slowMarketType.indexOf('趋势多') !== -1,
 					position.slowMarketType === '趋势潜在减弱' &&
