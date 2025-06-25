779e7ba4b49043b0a4bf908a5db2b2d355a1b72e
pphxiong <1017551718@qq.com>
2025-06-25 13:57:26 +0800

    n

--- server/app/swapApiDSTest.js
+++ server/app/swapApiDSTest.js
@@ -37,7 +37,7 @@ const config = {
 	timeframes: ['15m' /* '5m'  '1m'*/], // 多周期参数
 	emaSettings: {
 		// '30m': { periods: [10, 5], slopeWindow: 5 },
-		'15m': { periods: [100, 10], slopeWindow: 5 },
+		'15m': { periods: [125, 25], slopeWindow: 5 },
 		// "15m": { periods: [25, 5], slopeWindow: 5 },
 		// '5m': { periods: [10, 5], slopeWindow: 5 },
 	},
@@ -87,8 +87,8 @@ const config = {
 	simulations: 5000, // 模拟次数
 	volatility: 0.04, // 日波动率（比特币历史平均约3-5%）
 	drift: 0.0002, // 每日趋势偏移量
-	adxPeriod: 14,
-	rsiPeriod: 14,
+	adxPeriod: 9,
+	rsiPeriod: 9,
 };
 
 class Backtester {
