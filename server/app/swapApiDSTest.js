fbfb69a5822455e8edf4e732856b5e2df18a3a3e
pphxiong <1017551718@qq.com>
2025-06-26 14:00:36 +0800

    n

--- server/app/swapApiDSTest.js
+++ server/app/swapApiDSTest.js
@@ -603,7 +603,7 @@ class Backtester {
 					marketType =
 						emaFast > emaSlow
 							? close > emaFast
-								? '趋势多'
+								? '趋势多且增强'
 								: '趋势空'
 							: '趋势多';
 				}
@@ -624,7 +624,7 @@ class Backtester {
 					marketType =
 						emaFast > emaSlow
 							? close > emaFast
-								? '趋势多'
+								? '趋势多且增强'
 								: '趋势空'
 							: '趋势多';
 				}
