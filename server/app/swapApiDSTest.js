941690bef32523b0b519cf4cd594879009d8cbfb
pphxiong <1017551718@qq.com>
2025-06-27 17:00:34 +0800

    n

--- server/app/swapApiDSTest.js
+++ server/app/swapApiDSTest.js
@@ -595,7 +595,7 @@ class Backtester {
 				if (close > emaSlow) {
 					if (close < emaFast) {
 						marketType =
-							adxMinusDI < 30 ? '趋势多且增强-L-1' : '趋势多';
+							adxMinusDI < 25 ? '趋势多且增强-L-1' : '趋势多';
 					}
 					if (
 						close > emaFast &&
@@ -634,7 +634,7 @@ class Backtester {
 				if (close < emaSlow) {
 					if (close > emaFast) {
 						marketType =
-							adxPlusDI < 30 ? '趋势空且增强-R-1' : '趋势空';
+							adxPlusDI < 25 ? '趋势空且增强-R-1' : '趋势空';
 					}
 					if (
 						close < emaFast &&
