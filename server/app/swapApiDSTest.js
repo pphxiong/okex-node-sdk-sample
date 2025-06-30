a2b8ded0808b00e724c085cff06e3b9634e30a4f
pphxiong <1017551718@qq.com>
2025-06-30 08:48:07 +0800

    n

--- server/app/swapApiDSTest.js
+++ server/app/swapApiDSTest.js
@@ -626,7 +626,7 @@ class Backtester {
 				}
 				if (adxPlusDI > adxMinusDI) {
 					if (emaFast > emaSlow && close > emaFast) {
-						marketType = adx < 15 ? '趋势空' : '趋势多';
+						marketType = adx < 20 ? '趋势空' : '趋势多';
 					}
 				}
 			}
@@ -669,7 +669,7 @@ class Backtester {
 				}
 				if (adxPlusDI < adxMinusDI) {
 					if (emaFast < emaSlow && close < emaFast) {
-						marketType = adx < 15 ? '趋势多' : '趋势空';
+						marketType = adx < 20 ? '趋势多' : '趋势空';
 					}
 				}
 			}
