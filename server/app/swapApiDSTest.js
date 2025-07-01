1e7f9a90087e79c59262df51b94930df11bb4819
pphxiong <1017551718@qq.com>
2025-07-01 16:39:43 +0800

    n

--- server/app/swapApiDSTest.js
+++ server/app/swapApiDSTest.js
@@ -596,9 +596,8 @@ class Backtester {
 				if (close > emaSlow) {
 					if (close < emaFast) {
 						marketType =
-							adxMinusDI < 25
-								? volatility_ratio < 0.01 ||
-								  volatility_ratio > 0.012
+							adxMinusDI < 25 && adxPlusDI - adxMinusDI < 10
+								? volatility_ratio < 0.01
 									? '趋势多且增强-L-1'
 									: '趋势空且增强-L-1'
 								: '趋势多';
@@ -607,8 +606,7 @@ class Backtester {
 							adxPlusDI - adxMinusDI < 5
 						) {
 							marketType =
-								volatility_ratio < 0.01 ||
-								volatility_ratio > 0.012
+								volatility_ratio < 0.01
 									? '趋势多且增强-L-2'
 									: '趋势空且增强-L-2';
 						}
@@ -652,9 +650,8 @@ class Backtester {
 				if (close < emaSlow) {
 					if (close > emaFast) {
 						marketType =
-							adxPlusDI < 25
-								? volatility_ratio < 0.01 ||
-								  volatility_ratio > 0.012
+							adxPlusDI < 25 && adxMinusDI - adxPlusDI < 10
+								? volatility_ratio < 0.01
 									? '趋势空且增强-R-1'
 									: '趋势多且增强-R-1'
 								: '趋势空';
@@ -663,8 +660,7 @@ class Backtester {
 							adxMinusDI - adxPlusDI < 5
 						) {
 							marketType =
-								volatility_ratio < 0.01 ||
-								volatility_ratio > 0.012
+								volatility_ratio < 0.01
 									? '趋势空且增强-R-2'
 									: '趋势多且增强-R-2';
 						}
