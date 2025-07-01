081c2db16fbf54c7d3b92d2cd55cfa34cfbcc2a9
pphxiong <1017551718@qq.com>
2025-07-01 11:14:14 +0800

    n

--- server/app/swapApiDSTest.js
+++ server/app/swapApiDSTest.js
@@ -610,6 +610,12 @@ class Backtester {
 									: '趋势空且增强-L-2';
 						}
 					}
+					if (close > emaFast) {
+						marketType =
+							volatility_ratio > 0.01
+								? '趋势多且增强-L-4'
+								: marketType;
+					}
 				}
 			}
 			if (adx < adx_threshold) {
@@ -657,6 +663,12 @@ class Backtester {
 									? '趋势空且增强-R-2'
 									: '趋势多且增强-R-2';
 						}
+					}
+					if (close < emaFast) {
+						marketType =
+							volatility_ratio > 0.01
+								? '趋势空且增强-R-4'
+								: marketType;
 					}
 				}
 			}
