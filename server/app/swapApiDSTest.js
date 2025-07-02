36c5aac45d77c24a34ef723b22196abf6863c13f
pphxiong <1017551718@qq.com>
2025-07-02 08:35:54 +0800

    n

--- server/app/swapApiDSTest.js
+++ server/app/swapApiDSTest.js
@@ -1401,26 +1401,26 @@ function carryForluma(p, rl, rw) {
 			// 步骤2: 计算指标
 			await backtester.calculateIndicators();
 
-			// console.log(
-			// 	data[config.slowframe]
-			// 		// .slice(-5)
-			// 		.filter(
-			// 			(item) =>
-			// 				moment(item.timestamp).isAfter(
-			// 					moment('2025-03-03 08:00:00')
-			// 				) &&
-			// 				moment(item.timestamp).isBefore(
-			// 					moment('2025-03-03 15:30:00')
-			// 				)
-			// 		)
-			// 		.map((candle) =>
-			// 			Object.assign(candle, {
-			// 				timestamp: moment(candle.timestamp).format(
-			// 					'YYYY-MM-DD HH:mm:ss'
-			// 				),
-			// 			})
-			// 		)
-			// );
+			console.log(
+				data[config.slowframe]
+					// .slice(-5)
+					.filter(
+						(item) =>
+							moment(item.timestamp).isAfter(
+								moment('2025-07-01 20:00:00')
+							) &&
+							moment(item.timestamp).isBefore(
+								moment('2025-07-02 08:30:00')
+							)
+					)
+					.map((candle) =>
+						Object.assign(candle, {
+							timestamp: moment(candle.timestamp).format(
+								'YYYY-MM-DD HH:mm:ss'
+							),
+						})
+					)
+			);
 
 			// console.log(data[config.slowframe].length);
 
