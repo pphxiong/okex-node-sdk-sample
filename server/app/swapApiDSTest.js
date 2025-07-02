fe7d0b7e4e8437d30d32a021d60b0730d7e9ec82
pphxiong <1017551718@qq.com>
2025-07-02 10:12:42 +0800

    n

--- server/app/swapApiDSTest.js
+++ server/app/swapApiDSTest.js
@@ -620,7 +620,7 @@ class Backtester {
 				if (adxPlusDI > adxMinusDI) {
 					if (close > emaFast) {
 						marketType = '趋势多';
-						if (adxPlusDI > 30) {
+						if (adxPlusDI > 25) {
 							marketType = '趋势空';
 						}
 					}
@@ -658,7 +658,7 @@ class Backtester {
 				if (adxPlusDI < adxMinusDI) {
 					if (close < emaFast) {
 						marketType = '趋势空';
-						if (adxMinusDI > 30) {
+						if (adxMinusDI > 25) {
 							marketType = '趋势多';
 						}
 					}
