1e98844bf98882dccbc745a094d707e932e92815
pphxiong <1017551718@qq.com>
2025-07-02 17:35:30 +0800

    n

--- server/app/swapApiDSTest.js
+++ server/app/swapApiDSTest.js
@@ -619,7 +619,7 @@ class Backtester {
 			if (adx < adx_threshold) {
 				if (adxPlusDI < adxMinusDI) {
 					if (close < emaFast) {
-						if (close < emaSlow && adx > 15 && open > emaSlow) {
+						if (close < emaSlow && adx > 15) {
 							marketType =
 								adxMinusDI - adxPlusDI < 12 && adxMinusDI < 25
 									? '趋势多且增强-L-3-1'
@@ -664,7 +664,7 @@ class Backtester {
 			if (adx < adx_threshold) {
 				if (adxPlusDI > adxMinusDI) {
 					if (close > emaFast) {
-						if (close > emaSlow && adx > 15 && open < emaSlow) {
+						if (close > emaSlow && adx > 15) {
 							marketType =
 								adxPlusDI - adxMinusDI < 12 && adxPlusDI < 25
 									? '趋势空且增强-R-3-1'
@@ -690,7 +690,8 @@ class Backtester {
 			open > emaSlow &&
 			close > open
 		) {
-			marketType = marketType.indexOf('趋势空') ? '趋势多' : marketType;
+			marketType =
+				marketType.indexOf('趋势空') != -1 ? '趋势多' : marketType;
 		}
 
 		if (
@@ -700,7 +701,8 @@ class Backtester {
 			open < emaSlow &&
 			close < open
 		) {
-			marketType = marketType.indexOf('趋势多') ? '趋势空' : marketType;
+			marketType =
+				marketType.indexOf('趋势多') != -1 ? '趋势空' : marketType;
 		}
 
 		return marketType;
