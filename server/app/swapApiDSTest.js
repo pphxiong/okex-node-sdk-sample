4431838899e743ce39b8faed2965328c1630974f
root <1017551718@qq.com>
2025-07-03 17:30:05 +0800

    n

--- server/app/swapApiDSTest.js
+++ server/app/swapApiDSTest.js
@@ -1303,8 +1303,8 @@ function carryForluma(p, rl, rw) {
 // 执行回测
 (async () => {
 	const backtester = new Backtester();
-	// const start = '2025-01-01';
-	const start = '2021-01-01';
+	const start = '2025-01-01';
+	// const start = '2021-01-01';
 	// const end = '2021-12-01';
 	const end = '2025-07-02';
 	const interval = 30;
