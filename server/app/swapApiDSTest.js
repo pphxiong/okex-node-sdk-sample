273aaf14fa35c292c7230e97933d383cf6429657
pphxiong <1017551718@qq.com>
2025-07-03 10:00:59 +0800

    n

--- server/app/swapApiDSTest.js
+++ server/app/swapApiDSTest.js
@@ -596,15 +596,13 @@ class Backtester {
 			marketType = '趋势多';
 			if (adx < adx_threshold) {
 				if (close < emaFast) {
-					marketType = '趋势多趋势空';
+					marketType = '趋势空';
 				}
 			}
 			if (adx > adx_threshold) {
 				if (close > emaFast) {
 					marketType =
-						adxPlusDI > adxMinusDI
-							? '趋势多且增强-L-3-1'
-							: '趋势空且增强-L-3-2';
+						adxPlusDI > adxMinusDI ? '趋势多且增强-L-3-1' : '';
 				}
 			}
 		}
@@ -613,15 +611,13 @@ class Backtester {
 			marketType = '趋势空';
 			if (adx < adx_threshold) {
 				if (close > emaFast) {
-					marketType = '趋势多趋势空';
+					marketType = '趋势多';
 				}
 			}
 			if (adx > adx_threshold) {
 				if (close < emaFast) {
 					marketType =
-						adxPlusDI < adxMinusDI
-							? '趋势空且增强-R-3-1'
-							: '趋势多且增强-R-3-2';
+						adxPlusDI < adxMinusDI ? '趋势空且增强-R-3-1' : '';
 				}
 			}
 		}
