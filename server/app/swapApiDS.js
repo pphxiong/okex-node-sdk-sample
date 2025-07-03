9f965f13836fc8bfa1112fd976007948de33d4a0
pphxiong <1017551718@qq.com>
2025-07-03 15:21:48 +0800

    n

--- server/app/swapApiDS.js
+++ server/app/swapApiDS.js
@@ -198,11 +198,13 @@ function getMarketType(candle, lastCandle, lastLastCandle) {
 		}
 	}
 
+	/*
 	if (marketType.indexOf('多') != -1) {
 		marketType = marketType.replace('多', '空');
 	} else if (marketType.indexOf('空') != -1) {
 		marketType = marketType.replace('空', '多');
-	}
+	} 
+  */
 
 	return marketType;
 }
