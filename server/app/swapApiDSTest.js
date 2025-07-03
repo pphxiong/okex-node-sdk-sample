247853ce17c617bb65ac63f8f9033020c3bd8b8d
pphxiong <1017551718@qq.com>
2025-07-03 13:46:36 +0800

    n

--- server/app/swapApiDSTest.js
+++ server/app/swapApiDSTest.js
@@ -593,13 +593,13 @@ class Backtester {
 		const macd_falling = macd < lastMacd;
 
 		if (emaFast > emaSlow) {
-			marketType = '趋势多';
 			if (adx < adx_threshold) {
 				if (close < emaFast) {
 					marketType = '趋势空';
 				}
 			}
 			if (adx > adx_threshold) {
+				marketType = '趋势多';
 				if (close > emaFast) {
 					marketType =
 						adxPlusDI > adxMinusDI ? '趋势多且增强-L-3-1' : '';
@@ -608,13 +608,13 @@ class Backtester {
 		}
 
 		if (emaFast < emaSlow) {
-			marketType = '趋势空';
 			if (adx < adx_threshold) {
 				if (close > emaFast) {
 					marketType = '趋势多';
 				}
 			}
 			if (adx > adx_threshold) {
+				marketType = '趋势空';
 				if (close < emaFast) {
 					marketType =
 						adxPlusDI < adxMinusDI ? '趋势空且增强-R-3-1' : '';
