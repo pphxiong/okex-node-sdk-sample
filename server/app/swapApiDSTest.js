9cf21ebac586699e1e4b28ab09449aeb82f4e038
root <1017551718@qq.com>
2025-07-04 14:39:42 +0800

    n

--- server/app/swapApiDSTest.js
+++ server/app/swapApiDSTest.js
@@ -593,13 +593,13 @@ class Backtester {
 		const macd_falling = macd < lastMacd;
 
 		if (adx < adx_threshold) {
-			if (close < emaSlow) {
+			if (close < emaFast) {
 				marketType = '趋势多';
 				if ((emaFast - emaSlow) / emaSlow > volatility_ratio) {
 					marketType = '趋势空且增强-L-1-1';
 				}
 			}
-			if (close > emaSlow) {
+			if (close > emaFast) {
 				marketType = '趋势空';
 				if ((emaSlow - emaFast) / emaSlow > volatility_ratio) {
 					marketType = '趋势多且增强-L-2-1';
