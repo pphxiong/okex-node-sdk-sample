7bcb9fb9499e9371b7d9d63cae5b4155b734a6b4
root <1017551718@qq.com>
2025-07-04 11:19:45 +0800

    n

--- server/app/swapApiDSTest.js
+++ server/app/swapApiDSTest.js
@@ -610,15 +610,14 @@ class Backtester {
 		if (emaFast > emaSlow) {
 			if (adx > adx_threshold) {
 				marketType = '趋势多';
-				if (close > emaFast) {
-					marketType =
-						adxPlusDI > adxMinusDI
-							? '趋势多且增强-R-3-1'
-							: marketType;
-				}
+				// if (close > emaFast) {
+				marketType =
+					adxPlusDI > adxMinusDI ? '趋势多且增强-R-3-1' : marketType;
+				// }
 				if (
-					close < emaFast &&
-					(emaFast - emaSlow) / emaSlow < volatility_ratio
+					// close < emaFast &&
+					(emaFast - emaSlow) / emaSlow <
+					volatility_ratio
 				) {
 					marketType = '趋势空';
 				}
@@ -628,15 +627,14 @@ class Backtester {
 		if (emaFast < emaSlow) {
 			if (adx > adx_threshold) {
 				marketType = '趋势空';
-				if (close < emaFast) {
-					marketType =
-						adxPlusDI < adxMinusDI
-							? '趋势空且增强-R-3-2'
-							: marketType;
-				}
+				// if (close < emaFast) {
+				marketType =
+					adxPlusDI < adxMinusDI ? '趋势空且增强-R-3-2' : marketType;
+				// }
 				if (
-					close > emaFast &&
-					(emaSlow - emaFast) / emaSlow < volatility_ratio
+					// close > emaFast &&
+					(emaSlow - emaFast) / emaSlow <
+					volatility_ratio
 				) {
 					marketType = '趋势多';
 				}
