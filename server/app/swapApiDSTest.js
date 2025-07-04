1030180867ae83ad30f68165dd55df2e43c18f7c
root <1017551718@qq.com>
2025-07-04 09:19:46 +0800

    n

--- server/app/swapApiDSTest.js
+++ server/app/swapApiDSTest.js
@@ -594,64 +594,40 @@ class Backtester {
 
 		if (emaFast > emaSlow) {
 			if (adx < adx_threshold) {
+				if (close > emaFast && lastClose < lastEmaFast) {
+					marketType = '趋势多且增强-L-1-1';
+				}
 				if (close < emaFast) {
 					marketType = '趋势空';
 				}
-				if (close < emaFast) {
-					if ((emaFast - emaSlow) / emaSlow < volatility_ratio) {
-						marketType = '趋势空且增强-L-1-1';
-					}
-				}
-				if (close > emaFast) {
-					marketType = '趋势多';
-				}
-				if (close > emaFast) {
-					if ((emaFast - emaSlow) / emaSlow < volatility_ratio) {
-						marketType = '趋势多且增强-L-1-1';
-					}
-				}
 			}
 			if (adx > adx_threshold) {
-				marketType = '趋势空';
+				marketType = '趋势多';
 				if (close > emaFast) {
-					// marketType = '趋势多';
-					marketType =
-						adxPlusDI > adxMinusDI &&
-						(emaFast - emaSlow) / emaSlow > volatility_ratio
-							? '趋势多且增强-L-3-1'
-							: marketType;
+					marketType = '趋势多且增强-L-2-1';
+				}
+				if (close < emaFast) {
+					marketType = '趋势空';
 				}
 			}
 		}
 
 		if (emaFast < emaSlow) {
 			if (adx < adx_threshold) {
-				if (close < emaFast) {
-					marketType = '趋势空';
-				}
-				if (close < emaFast) {
-					if ((emaFast - emaSlow) / emaSlow < volatility_ratio) {
-						marketType = '趋势空且增强-L-1-1';
-					}
+				if (close < emaFast && lastClose > lastEmaFast) {
+					marketType = '趋势空且增强-R-1-1';
 				}
 				if (close > emaFast) {
 					marketType = '趋势多';
 				}
-				if (close > emaFast) {
-					if ((emaFast - emaSlow) / emaSlow < volatility_ratio) {
-						marketType = '趋势多且增强-L-1-1';
-					}
-				}
 			}
 			if (adx > adx_threshold) {
-				marketType = '趋势多';
+				marketType = '趋势空';
 				if (close < emaFast) {
-					// marketType = '趋势多';
-					marketType =
-						adxPlusDI < adxMinusDI &&
-						(emaFast - emaSlow) / emaSlow < -volatility_ratio
-							? '趋势空且增强-R-3-1'
-							: marketType;
+					marketType = '趋势空且增强-R-2-1';
+				}
+				if (close > emaFast) {
+					marketType = '趋势多';
 				}
 			}
 		}
