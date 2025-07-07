16ec5e9273fe4791c3b401e292a6182349c0446d
root <1017551718@qq.com>
2025-07-07 17:07:19 +0800

    n

--- server/app/swapApiDSTest.js
+++ server/app/swapApiDSTest.js
@@ -649,9 +649,7 @@ class Backtester {
 
 	toogleMarketType(marketType) {
 		const { continueLoss, marketMode } = this;
-		if (continueLoss >= 2) {
-			this.marketMode = marketMode === 1 ? 2 : 1;
-		}
+		if (continueLoss >= 3) this.marketMode = marketMode === 1 ? 2 : 1;
 		if (this.marketMode == 2) {
 			if (marketType.indexOf('多') != -1) {
 				marketType = marketType.replace('多', '空');
