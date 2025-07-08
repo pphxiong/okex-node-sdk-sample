c55bfeb3588a6d6f5fdb54ede85b797dba83e6fa
root <1017551718@qq.com>
2025-07-08 15:51:07 +0800

    n

--- server/app/swapApiDSTest.js
+++ server/app/swapApiDSTest.js
@@ -598,15 +598,19 @@ class Backtester {
 
 		if (adx < adx_threshold) {
 			if (close < emaFast) {
-				// marketType = '趋势多';
+				marketType = '趋势多';
 				if ((emaFast - emaSlow) / emaSlow > volatility_ratio) {
 					marketType = '趋势空且增强-L-1-1';
+				} else {
+					marketType = '趋势空';
 				}
 			}
 			if (close > emaFast) {
-				// marketType = '趋势空';
+				marketType = '趋势空';
 				if ((emaSlow - emaFast) / emaSlow > volatility_ratio) {
 					marketType = '趋势多且增强-L-2-1';
+				} else {
+					marketType = '趋势多';
 				}
 			}
 		}
