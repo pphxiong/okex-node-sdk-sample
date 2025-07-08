25b993f23a89e0ba98db13ae10c894c9feb16e5e
root <1017551718@qq.com>
2025-07-08 10:10:32 +0800

    n

--- server/app/swapApiDSTest.js
+++ server/app/swapApiDSTest.js
@@ -1143,10 +1143,10 @@ class Backtester {
 		}
 
 		const { continueWin, continueLoss, marketMode } = this;
-		if (continueWin >= 4 || continueLoss >= 2) {
-			const random = Math.random();
-			if (random > 0.5) this.marketMode = marketMode === 1 ? 2 : 1;
-			// this.marketMode = marketMode === 1 ? 2 : 1;
+		if (continueWin >= 3 || continueLoss >= 1) {
+			// const random = Math.random();
+			// if (random > 0.5) this.marketMode = marketMode === 1 ? 2 : 1;
+			this.marketMode = marketMode === 1 ? 2 : 1;
 		}
 	}
 
