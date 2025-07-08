6247dc00a3c48d62c6c98d2d3b24350025e01523
root <1017551718@qq.com>
2025-07-08 16:11:43 +0800

    n

--- server/app/swapApiDSTest.js
+++ server/app/swapApiDSTest.js
@@ -613,26 +613,32 @@ class Backtester {
 
 		if (emaFast > emaSlow) {
 			if (adx > adx_threshold) {
-				marketType =
-					adxPlusDI > adxMinusDI ? '趋势多且增强-R-3-1' : '趋势多';
 				if (
 					(close < emaFast && adxPlusDI - adxMinusDI > 10) ||
 					(emaFast - emaSlow) / emaSlow < volatility_ratio
 				) {
 					marketType = '趋势空';
+				} else {
+					marketType =
+						adxPlusDI > adxMinusDI
+							? '趋势多且增强-R-3-1'
+							: '趋势多';
 				}
 			}
 		}
 
 		if (emaFast < emaSlow) {
 			if (adx > adx_threshold) {
-				marketType =
-					adxPlusDI < adxMinusDI ? '趋势空且增强-R-3-2' : '趋势空';
 				if (
 					(close > emaFast && adxMinusDI - adxPlusDI > 10) ||
 					(emaSlow - emaFast) / emaSlow < volatility_ratio
 				) {
 					marketType = '趋势多';
+				} else {
+					marketType =
+						adxPlusDI < adxMinusDI
+							? '趋势空且增强-R-3-2'
+							: '趋势空';
 				}
 			}
 		}
