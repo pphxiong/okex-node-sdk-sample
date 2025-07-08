2c16d5671e4bbb345cae1ae36723c27323208551
root <1017551718@qq.com>
2025-07-08 09:56:57 +0800

    n

--- server/app/swapApiDSTest.js
+++ server/app/swapApiDSTest.js
@@ -636,13 +636,13 @@ class Backtester {
 			}
 		}
 
-		// if (true) {
-		// 	if (marketType.indexOf('多') != -1) {
-		// 		marketType = marketType.replace('多', '空');
-		// 	} else if (marketType.indexOf('空') != -1) {
-		// 		marketType = marketType.replace('空', '多');
-		// 	}
-		// }
+		if (true) {
+			if (marketType.indexOf('多') != -1) {
+				marketType = marketType.replace('多', '空');
+			} else if (marketType.indexOf('空') != -1) {
+				marketType = marketType.replace('空', '多');
+			}
+		}
 
 		return marketType;
 	}
