bf041339973d250dbe6f2e7a30725612d5ab226c
root <1017551718@qq.com>
2025-07-09 14:30:45 +0800

    n

--- server/app/swapApiDSTest.js
+++ server/app/swapApiDSTest.js
@@ -625,7 +625,8 @@ class Backtester {
 			if (adx > adx_threshold) {
 				if (
 					(close < emaFast && adxPlusDI - adxMinusDI > 10) ||
-					(emaFast - emaSlow) / emaSlow < volatility_ratio
+					(emaFast - emaSlow) / emaSlow < volatility_ratio ||
+					adxPlusDI - adxMinusDI > 15
 				) {
 					marketType = '趋势空';
 				} else {
@@ -641,7 +642,8 @@ class Backtester {
 			if (adx > adx_threshold) {
 				if (
 					(close > emaFast && adxMinusDI - adxPlusDI > 10) ||
-					(emaSlow - emaFast) / emaSlow < volatility_ratio
+					(emaSlow - emaFast) / emaSlow < volatility_ratio ||
+					adxMinusDI - adxPlusDI > 15
 				) {
 					marketType = '趋势多';
 				} else {
