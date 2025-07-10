1f3509e4123e759354f65815b9e29973d12e0c13
root <1017551718@qq.com>
2025-07-10 09:12:32 +0800

    n

--- server/app/swapApiDSTest.js
+++ server/app/swapApiDSTest.js
@@ -645,7 +645,7 @@ class Backtester {
 							? close > emaSlow
 								? '趋势多且增强-R-3-1'
 								: '趋势空'
-							: adxPlusDI - adxMinusDI > 30
+							: adxPlusDI - adxMinusDI < 30
 							? '趋势多'
 							: '趋势空';
 				}
@@ -668,7 +668,7 @@ class Backtester {
 							? close < emaSlow
 								? '趋势空且增强-R-3-2'
 								: '趋势多'
-							: adxMinusDI - adxPlusDI > 30
+							: adxMinusDI - adxPlusDI < 30
 							? '趋势空'
 							: '趋势多';
 				}
