c3afb95ef0ca3acd6c8c1898dc3b1c16509ed714
root <1017551718@qq.com>
2025-07-10 09:11:06 +0800

    n

--- server/app/swapApiDSTest.js
+++ server/app/swapApiDSTest.js
@@ -645,7 +645,9 @@ class Backtester {
 							? close > emaSlow
 								? '趋势多且增强-R-3-1'
 								: '趋势空'
-							: '趋势多趋势空';
+							: adxPlusDI - adxMinusDI > 30
+							? '趋势多'
+							: '趋势空';
 				}
 			}
 		}
@@ -666,7 +668,9 @@ class Backtester {
 							? close < emaSlow
 								? '趋势空且增强-R-3-2'
 								: '趋势多'
-							: '趋势多趋势空';
+							: adxMinusDI - adxPlusDI > 30
+							? '趋势空'
+							: '趋势多';
 				}
 			}
 		}
