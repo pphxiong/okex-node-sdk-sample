34305e526994b4232aa62fc108945638f3e65e0d
pphxiong <1017551718@qq.com>
2025-07-14 10:56:49 +0800

    n

--- server/app/swapApiDSTest.js
+++ server/app/swapApiDSTest.js
@@ -608,10 +608,12 @@ class Backtester {
 						if (
 							emaFast > emaSlow &&
 							adxPlusDI > adxMinusDI &&
-							adx > adx_threshold - 2 &&
 							(close - emaFast) / close < volatility_ratio * 2
 						) {
-							marketType = '趋势多且增强-L-1-2';
+							marketType =
+								adx > adx_threshold - 2
+									? '趋势空且增强-L-1-2'
+									: '趋势多且增强-L-1-2';
 						}
 					}
 				}
@@ -624,10 +626,12 @@ class Backtester {
 						if (
 							emaFast < emaSlow &&
 							adxPlusDI < adxMinusDI &&
-							adx > adx_threshold - 2 &&
 							(emaFast - close) / emaFast < volatility_ratio * 2
 						) {
-							marketType = '趋势空且增强-L-2-2';
+							marketType =
+								adx > adx_threshold - 2
+									? '趋势多且增强-L-2-2-1'
+									: '趋势空且增强-L-2-2-2';
 						}
 					}
 				}
