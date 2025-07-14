2bdc11e9a416afd64e48820288f0feb0079ac427
pphxiong <1017551718@qq.com>
2025-07-14 11:37:51 +0800

    n

--- server/app/swapApiDSTest.js
+++ server/app/swapApiDSTest.js
@@ -1273,7 +1273,7 @@ class Backtester {
 		// 		.reduce((a, b) => a + b, 0);
 
 		const { continueWin, continueLoss, marketMode } = this;
-		if (profit < -38.2 / 3) {
+		if (profit < -38.2) {
 			this.marketMode = marketMode === 1 ? 2 : 1;
 			config.marketMode = this.marketMode;
 		}
@@ -1456,8 +1456,8 @@ function carryForluma(p, rl, rw) {
 // 执行回测
 (async () => {
 	const backtester = new Backtester();
-	const start = '2023-01-01';
-	const end = '2023-07-01';
+	const start = '2021-01-01';
+	const end = '2025-07-01';
 	// const start = '2025-01-01';
 	// const end = '2025-07-10';
 	const interval = 30;
