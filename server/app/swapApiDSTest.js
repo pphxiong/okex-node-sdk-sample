370f836c164ce608803003fe4009dab1990553be
pphxiong <1017551718@qq.com>
2025-07-14 17:00:24 +0800

    n

--- server/app/swapApiDSTest.js
+++ server/app/swapApiDSTest.js
@@ -679,13 +679,15 @@ class Backtester {
 			if (adx > adx_threshold) {
 				if (emaFast > emaSlow && close > emaFast) {
 					marketType =
-						adx < adx_threshold + 5
+						adx < adx_threshold + 10
 							? '趋势空且增强-M-L-1'
 							: '趋势多且增强-M-L-1';
 				}
 				if (emaFast < emaSlow && close < emaFast) {
 					marketType =
-						adx < adx_threshold + 5 ? '趋势多且增强-M-L-3' : '趋势空且增强-M-L-4';
+						adx < adx_threshold + 10
+							? '趋势多且增强-M-L-3'
+							: '趋势空且增强-M-L-4';
 				}
 			}
 
