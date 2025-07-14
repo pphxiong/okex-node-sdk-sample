c7de2193ad522804061c4f5f23937cc98072cfcf
pphxiong <1017551718@qq.com>
2025-07-14 08:44:13 +0800

    n

--- server/app/swapApiDSTest.js
+++ server/app/swapApiDSTest.js
@@ -633,7 +633,7 @@ class Backtester {
 			}
 		}
 
-		if (true) {
+		if (this.marketMode === 2) {
 			if (marketType.indexOf('多') != -1) {
 				marketType = marketType.replace('多', '空');
 			} else if (marketType.indexOf('空') != -1) {
