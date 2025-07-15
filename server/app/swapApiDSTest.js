d3e013e23bea89a47952c2c9a8d41ddd704dea97
pphxiong <1017551718@qq.com>
2025-07-15 17:15:08 +0800

    n

--- server/app/swapApiDSTest.js
+++ server/app/swapApiDSTest.js
@@ -681,18 +681,15 @@ class Backtester {
 		}
 
 		if (marketMode === 2) {
-			if (adx < adx_threshold) {
+			if (emaFast > emaSlow) {
 				if (close > upper) {
-					marketType =
-						emaFast > emaSlow
-							? '趋势空且增强-M-L-1'
-							: '趋势多且增强-M-L-1';
+					marketType = '趋势多且增强-M-L-1';
 				}
+			}
+
+			if (emaFast < emaSlow) {
 				if (close < lower) {
-					marketType =
-						emaFast < emaSlow
-							? '趋势多且增强-M-L-2'
-							: '趋势空且增强-M-L-2';
+					marketType = '趋势空且增强-M-L-2';
 				}
 			}
 
