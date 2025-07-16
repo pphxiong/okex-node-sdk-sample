f8ff2539874f1e80b31942fc2d5e1d95f8faa32d
pphxiong <1017551718@qq.com>
2025-07-16 11:09:54 +0800

    n

--- server/app/swapApiDSTest.js
+++ server/app/swapApiDSTest.js
@@ -601,66 +601,6 @@ class Backtester {
 		const macd_falling = macd < lastMacd;
 
 		const { marketMode } = this;
-		if (adx < adx_threshold) {
-			if (close > emaFast) {
-				marketType = adxPlusDI > adxMinusDI ? '趋势空' : '趋势多';
-				if (adx > adx_threshold - 3) {
-					if ((emaSlow - emaFast) / emaSlow > volatility_ratio) {
-						marketType = '趋势多且增强-L-1-1';
-					}
-					if (
-						emaFast > emaSlow &&
-						adxPlusDI > adxMinusDI &&
-						adx > adx_threshold - 2 &&
-						(close - emaFast) / close < volatility_ratio * 2
-					) {
-						marketType = '趋势多且增强-L-1-2';
-					}
-				}
-			}
-			if (close < emaFast) {
-				marketType = adxPlusDI > adxMinusDI ? '趋势空' : '趋势多';
-				if (adx > adx_threshold - 3) {
-					if ((emaFast - emaSlow) / emaFast > volatility_ratio) {
-						marketType = '趋势空且增强-L-2-1';
-					}
-					if (
-						emaFast < emaSlow &&
-						adxPlusDI < adxMinusDI &&
-						adx > adx_threshold - 2 &&
-						(emaFast - close) / emaFast < volatility_ratio * 2
-					) {
-						marketType = '趋势空且增强-L-2-2';
-					}
-				}
-			}
-		}
-
-		if (marketMode === 2) {
-			if (marketType.indexOf('多') != -1) {
-				marketType = marketType.replace('多', '空');
-			} else if (marketType.indexOf('空') != -1) {
-				marketType = marketType.replace('空', '多');
-			}
-
-			// if (adx > adx_threshold) {
-			// 	if (emaFast > emaSlow && close > emaFast) {
-			// 		if (close > upper)
-			// 			marketType =
-			// 				marketType.indexOf('多') != -1
-			// 					? marketType
-			// 					: '趋势多';
-			// 	}
-
-			// 	if (emaFast < emaSlow && close < emaFast) {
-			// 		if (close < lower)
-			// 			marketType =
-			// 				marketType.indexOf('空') != -1
-			// 					? marketType
-			// 					: '趋势空';
-			// 	}
-			// }
-		}
 
 		if (emaFast > emaSlow) {
 			if (adx > adx_threshold) {
@@ -700,6 +640,67 @@ class Backtester {
 								? '趋势空且增强-R-3-2'
 								: '趋势多'
 							: '趋势空';
+				}
+			}
+		}
+
+		if (marketMode === 2) {
+			if (marketType.indexOf('多') != -1) {
+				marketType = marketType.replace('多', '空');
+			} else if (marketType.indexOf('空') != -1) {
+				marketType = marketType.replace('空', '多');
+			}
+
+			// if (adx > adx_threshold) {
+			// 	if (emaFast > emaSlow && close > emaFast) {
+			// 		if (close > upper)
+			// 			marketType =
+			// 				marketType.indexOf('多') != -1
+			// 					? marketType
+			// 					: '趋势多';
+			// 	}
+
+			// 	if (emaFast < emaSlow && close < emaFast) {
+			// 		if (close < lower)
+			// 			marketType =
+			// 				marketType.indexOf('空') != -1
+			// 					? marketType
+			// 					: '趋势空';
+			// 	}
+			// }
+		}
+
+		if (adx < adx_threshold) {
+			if (close > emaFast) {
+				marketType = adxPlusDI > adxMinusDI ? '趋势空' : '趋势多';
+				if (adx > adx_threshold - 3) {
+					if ((emaSlow - emaFast) / emaSlow > volatility_ratio) {
+						marketType = '趋势多且增强-L-1-1';
+					}
+					if (
+						emaFast > emaSlow &&
+						adxPlusDI > adxMinusDI &&
+						adx > adx_threshold - 2 &&
+						(close - emaFast) / close < volatility_ratio * 2
+					) {
+						marketType = '趋势多且增强-L-1-2';
+					}
+				}
+			}
+			if (close < emaFast) {
+				marketType = adxPlusDI > adxMinusDI ? '趋势空' : '趋势多';
+				if (adx > adx_threshold - 3) {
+					if ((emaFast - emaSlow) / emaFast > volatility_ratio) {
+						marketType = '趋势空且增强-L-2-1';
+					}
+					if (
+						emaFast < emaSlow &&
+						adxPlusDI < adxMinusDI &&
+						adx > adx_threshold - 2 &&
+						(emaFast - close) / emaFast < volatility_ratio * 2
+					) {
+						marketType = '趋势空且增强-L-2-2';
+					}
 				}
 			}
 		}
@@ -1263,12 +1264,12 @@ class Backtester {
 		// 		config.marketMode = this.marketMode;
 		// 	}
 		// }
-		// const { marketMode } = this;
-		// if (marketMode === 1 && profit > 38.2) {
-		// 	this.marketMode = 2;
-		// } else if (marketMode === 2 && profit < -38.2 / 3) {
-		// 	this.marketMode = 1;
-		// }
+		const { marketMode } = this;
+		if (marketMode === 1 && profit > 38.2) {
+			this.marketMode = 2;
+		} else if (marketMode === 2 && profit < -38.2 / 3) {
+			this.marketMode = 1;
+		}
 		config.marketMode = this.marketMode;
 	}
 
