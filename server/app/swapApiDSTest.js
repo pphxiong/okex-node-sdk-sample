61dd4a5dd6084cd1b202d50426f2321a957bd0d5
pphxiong <1017551718@qq.com>
2025-07-18 09:59:11 +0800

    n

--- server/app/swapApiDSTest.js
+++ server/app/swapApiDSTest.js
@@ -625,7 +625,7 @@ class Backtester {
 								: '趋势多且增强-L-1-2-2';
 					}
 				}
-				if (adx < adx_threshold - 10) {
+				if (adx < adx_threshold - 8) {
 					if (
 						emaFast > emaSlow &&
 						(emaFast - emaSlow) / emaFast < volatility_ratio
@@ -656,7 +656,7 @@ class Backtester {
 								: '趋势空且增强-L-2-2-2';
 					}
 				}
-				if (adx < adx_threshold - 10) {
+				if (adx < adx_threshold - 8) {
 					if (
 						emaSlow > emaFast &&
 						(emaSlow - emaFast) / emaSlow < volatility_ratio
