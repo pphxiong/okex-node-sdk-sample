e4bb9dd12b7ae4a3a725e7657656aa85d110fb35
pphxiong <1017551718@qq.com>
2025-07-18 10:29:37 +0800

    n

--- server/app/swapApiDSTest.js
+++ server/app/swapApiDSTest.js
@@ -685,7 +685,7 @@ class Backtester {
 								  adxPlusDI - adxMinusDI > 2
 									? '趋势多且增强-R-3-1-1'
 									: '趋势空且增强-R-3-1-2'
-								: '趋势空且增强-R-4-1-2'
+								: '趋势空'
 							: '趋势多';
 				}
 			}
@@ -707,7 +707,7 @@ class Backtester {
 								  adxMinusDI - adxPlusDI > 2
 									? '趋势空且增强-R-3-2-1'
 									: '趋势多且增强-R-3-2-2'
-								: '趋势多且增强-R-4-1-2'
+								: '趋势多'
 							: '趋势空';
 				}
 			}
