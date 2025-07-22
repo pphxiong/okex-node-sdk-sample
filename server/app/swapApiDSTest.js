734a5db52f3159481b689b579a724d6626c73ac4
pphxiong <1017551718@qq.com>
2025-07-22 17:16:44 +0800

    n

--- server/app/swapApiDSTest.js
+++ server/app/swapApiDSTest.js
@@ -1041,7 +1041,7 @@ class Backtester {
 					// isLastIndex ||
 					// isProfitTarget ||
 					// isStopLoss ||
-					(lnp < 0 && duration >= 30) ||
+					(lnp < 0 && duration >= 15) ||
 					(position.direction === 'long'
 						? longCloseConditions.some((c) => !!c)
 						: shortCloseConditions.some((c) => !!c));
