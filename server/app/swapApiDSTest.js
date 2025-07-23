7b8c241b09354ccaff9680afbff04edabc5dc805
pphxiong <1017551718@qq.com>
2025-07-23 10:36:03 +0800

    n

--- server/app/swapApiDSTest.js
+++ server/app/swapApiDSTest.js
@@ -660,6 +660,7 @@ class Backtester {
 		}
 
 		if (adx > adx_threshold) {
+			marketType = '趋势多趋势空';
 			if (emaFast > emaSlow) {
 				if (
 					(close < emaFast && adxPlusDI - adxMinusDI > 10) ||
@@ -673,9 +674,9 @@ class Backtester {
 						adxPlusDI - adxMinusDI < 30 &&
 						(emaFast - emaSlow) / emaFast < volatility_ratio * 3
 							? close > emaSlow
-								? close < emaFast
+								? close > emaFast
 									? '趋势多且增强-R-3-1-1'
-									: '趋势空'
+									: '趋势多且增强-R-3-1-2'
 								: '趋势空'
 							: '趋势多';
 				}
@@ -694,9 +695,9 @@ class Backtester {
 						adxMinusDI - adxPlusDI < 30 &&
 						(emaSlow - emaFast) / emaSlow < volatility_ratio * 3
 							? close < emaSlow
-								? close > emaFast
+								? close < emaFast
 									? '趋势空且增强-R-3-2-1'
-									: '趋势多'
+									: '趋势空且增强-R-3-2-2'
 								: '趋势多'
 							: '趋势空';
 				}
