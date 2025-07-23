e4b531154d4efa9fc75096136eaf68b8bc371112
pphxiong <1017551718@qq.com>
2025-07-23 10:11:06 +0800

    n

--- server/app/swapApiDSTest.js
+++ server/app/swapApiDSTest.js
@@ -651,20 +651,28 @@ class Backtester {
 			}
 		}
 
+		if (marketMode === 2) {
+			if (marketType.indexOf('多') != -1) {
+				marketType = marketType.replace('多', '空');
+			} else if (marketType.indexOf('空') != -1) {
+				marketType = marketType.replace('空', '多');
+			}
+		}
+
 		if (adx > adx_threshold) {
 			if (emaFast > emaSlow) {
 				if (
-					(close < emaFast && adxPlusDI - adxMinusDI > 10) ||
+					close < emaFast /* && adxPlusDI - adxMinusDI > 10 */ ||
 					((emaFast - emaSlow) / emaFast < volatility_ratio &&
 						adx < adx_threshold + 3)
 				) {
 					marketType = '趋势空';
 				} else {
 					marketType =
-						adxPlusDI > adxMinusDI &&
-						adxPlusDI - adxMinusDI < 30 &&
-						(emaFast - emaSlow) / emaFast < volatility_ratio * 3
-							? close > emaSlow
+						adxPlusDI > adxMinusDI
+							? // adxPlusDI - adxMinusDI < 30 &&
+							  // (emaFast - emaSlow) / emaFast < volatility_ratio * 3
+							  close > emaSlow
 								? adx > adx_threshold + 2 &&
 								  adxPlusDI - adxMinusDI > 2
 									? '趋势多且增强-R-3-1-1'
@@ -676,33 +684,23 @@ class Backtester {
 
 			if (emaFast < emaSlow) {
 				if (
-					(close > emaFast && adxMinusDI - adxPlusDI > 10) ||
+					close > emaFast /* && adxMinusDI - adxPlusDI > 10 */ ||
 					((emaSlow - emaFast) / emaSlow < volatility_ratio &&
 						adx < adx_threshold + 3)
 				) {
 					marketType = '趋势多';
 				} else {
 					marketType =
-						adxPlusDI < adxMinusDI &&
-						adxMinusDI - adxPlusDI < 30 &&
-						(emaSlow - emaFast) / emaSlow < volatility_ratio * 3
-							? close < emaSlow
+						adxPlusDI < adxMinusDI
+							? // adxMinusDI - adxPlusDI < 30 &&
+							  // (emaSlow - emaFast) / emaSlow < volatility_ratio * 3
+							  close < emaSlow
 								? adx > adx_threshold + 2 &&
 								  adxMinusDI - adxPlusDI > 2
 									? '趋势空且增强-R-3-2-1'
 									: '趋势空且增强-R-3-2-2'
 								: '趋势多'
 							: '趋势空';
-				}
-			}
-		}
-
-		if (marketMode === 2) {
-			if (marketType.indexOf('且增强-R') == -1) {
-				if (marketType.indexOf('多') != -1) {
-					marketType = marketType.replace('多', '空');
-				} else if (marketType.indexOf('空') != -1) {
-					marketType = marketType.replace('空', '多');
 				}
 			}
 		}
