bf0bbdc79a12d4b7c4dcc5e0497049567f300311
pphxiong <1017551718@qq.com>
2025-07-23 10:33:17 +0800

    n

--- server/app/swapApiDSTest.js
+++ server/app/swapApiDSTest.js
@@ -673,9 +673,9 @@ class Backtester {
 						adxPlusDI - adxMinusDI < 30 &&
 						(emaFast - emaSlow) / emaFast < volatility_ratio * 3
 							? close > emaSlow
-								? close > emaFast
+								? close < emaFast
 									? '趋势多且增强-R-3-1-1'
-									: '趋势空且增强-R-3-1-2'
+									: '趋势空'
 								: '趋势空'
 							: '趋势多';
 				}
@@ -694,9 +694,9 @@ class Backtester {
 						adxMinusDI - adxPlusDI < 30 &&
 						(emaSlow - emaFast) / emaSlow < volatility_ratio * 3
 							? close < emaSlow
-								? close < emaFast
+								? close > emaFast
 									? '趋势空且增强-R-3-2-1'
-									: '趋势多且增强-R-3-2-2'
+									: '趋势多'
 								: '趋势多'
 							: '趋势空';
 				}
