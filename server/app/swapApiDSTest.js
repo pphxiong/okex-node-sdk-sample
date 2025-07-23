cc564d0f4edb7f6e50c7a05f2bddb966474c69a4
pphxiong <1017551718@qq.com>
2025-07-23 14:18:29 +0800

    n

--- server/app/swapApiDSTest.js
+++ server/app/swapApiDSTest.js
@@ -1295,9 +1295,9 @@ class Backtester {
 			// }
 		}
 		const { marketMode } = this;
-		if (marketMode === 1 && (profit > 20 || profit < -5)) {
+		if (marketMode === 1 && (profit > 20 || profit < -10)) {
 			this.marketMode = 2;
-		} else if (marketMode === 2 && profit < -5) {
+		} else if (marketMode === 2 && profit < -10) {
 			this.marketMode = 1;
 		}
 		config.marketMode = this.marketMode;
