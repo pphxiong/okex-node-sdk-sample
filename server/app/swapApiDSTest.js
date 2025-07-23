79f7a6defa3672235eea45b8f864f36e91a63d88
pphxiong <1017551718@qq.com>
2025-07-23 14:22:50 +0800

    n

--- server/app/swapApiDSTest.js
+++ server/app/swapApiDSTest.js
@@ -762,15 +762,15 @@ class Backtester {
 
 	toogleMarketType(marketType, candle) {
 		const { adx, adx_threshold } = candle;
-		if (adx < adx_threshold) {
-			if (this.marketMode == 2) {
-				if (marketType.indexOf('多') != -1) {
-					marketType = marketType.replace('多', '空');
-				} else if (marketType.indexOf('空') != -1) {
-					marketType = marketType.replace('空', '多');
-				}
+		// if (adx < adx_threshold) {
+		if (this.marketMode == 2) {
+			if (marketType.indexOf('多') != -1) {
+				marketType = marketType.replace('多', '空');
+			} else if (marketType.indexOf('空') != -1) {
+				marketType = marketType.replace('空', '多');
 			}
 		}
+		// }
 
 		return marketType;
 	}
