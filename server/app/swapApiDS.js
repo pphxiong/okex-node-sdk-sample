c2334a4f72db041c6fc9bc7c95a8a49c7adb975e
pphxiong <1017551718@qq.com>
2025-07-25 11:15:35 +0800

    n

--- server/app/swapApiDS.js
+++ server/app/swapApiDS.js
@@ -1404,6 +1404,20 @@ function send(res, ret) {
 	res.send(str);
 }
 
+app.get('/getMode', async function (req, res) {
+	const { query = {} } = req;
+	const { pw } = query;
+	if (pw && pw.trim() === '@Xiong092479') {
+		send(res, {
+			errcode: 0,
+			errmsg: 'ok',
+			data: { marketMode: config.marketMode },
+		});
+	} else {
+		send(res, { errcode: 1, errmsg: 'password error' });
+	}
+});
+
 app.get('/changeMode', async function (req, res) {
 	const { query = {} } = req;
 	const { pw } = query;
