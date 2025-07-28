0dadc6029cbdb4c6045c7b0ae72e66a131a9672f
pphxiong <1017551718@qq.com>
2025-07-28 16:58:51 +0800

    n

--- server/app/swapApiDSTest.js
+++ server/app/swapApiDSTest.js
@@ -724,19 +724,13 @@ class Backtester {
 
 	toogleMarketType(marketType, candle) {
 		const { adx, adx_threshold } = candle;
-		if (adx < adx_threshold) {
+		if (adx < adx_threshold && adx > adx_threshold - 10) {
 			if (this.marketMode == 2) {
 				if (marketType.indexOf('多') != -1) {
 					marketType = marketType.replace('多', '空');
 				} else if (marketType.indexOf('空') != -1) {
 					marketType = marketType.replace('空', '多');
 				}
-				// const index = marketType.indexOf('且增强');
-				// if (marketType.indexOf('且增强') != -1) {
-				// 	marketType = marketType.slice(0, index);
-				// } else {
-				// 	marketType = `${marketType}且增强`;
-				// }
 			}
 		}
 
