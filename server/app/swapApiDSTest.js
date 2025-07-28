0e59cadc53de2c7d83a5e9eba68c8de557aab6eb
pphxiong <1017551718@qq.com>
2025-07-28 17:18:54 +0800

    n

--- server/app/swapApiDSTest.js
+++ server/app/swapApiDSTest.js
@@ -1281,11 +1281,7 @@ class Backtester {
 		if (config.isMarketModeAuto) {
 			if (marketMode == 1 && (lnp > 0.02 || lnp < -0.01)) {
 				this.marketMode = 2;
-			} else if (
-				marketMode == 2 &&
-				(lnp > 0.02 || lnp < -0.01) &&
-				exitCandle.adx > exitCandle.adx_threshold - 10
-			) {
+			} else if (marketMode == 2 && (lnp > 0.02 || lnp < -0.01)) {
 				this.marketMode = 1;
 			}
 		}
