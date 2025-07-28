05c92ca44b35fc3acedad3ed3888a0dc355ef294
pphxiong <1017551718@qq.com>
2025-07-28 16:37:20 +0800

    n

--- server/app/swapApiDSTest.js
+++ server/app/swapApiDSTest.js
@@ -887,7 +887,7 @@ class Backtester {
 				// 		? d.close <= position.entryPrice * (1 - 0.01)
 				// 		: d.close >= position.entryPrice * (1 + 0.01);
 
-				const isProfitTarget = lnp > 0.012 && d.adx < d.adx_threshold;
+				const isProfitTarget = lnp > 0.015 && d.adx < d.adx_threshold;
 				const isStopLoss = lnp < -0.01;
 
 				if (isStopLoss) stopLossDirection = position.direction;
