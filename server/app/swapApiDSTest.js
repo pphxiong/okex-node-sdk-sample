27e817640ea2665b3972c0b5805072647c066b5e
pphxiong <1017551718@qq.com>
2025-07-29 14:51:35 +0800

    n

--- server/app/swapApiDSTest.js
+++ server/app/swapApiDSTest.js
@@ -614,8 +614,8 @@ class Backtester {
 				}
 				if (close < emaFast) {
 					marketType =
-						(emaFast - emaSlow) / emaFast > volatility_ratio * 4
-							? '趋势空且增强-L-1-3'
+						(emaFast - emaSlow) / emaFast > volatility_ratio * 2
+							? '趋势多且增强-L-1-3'
 							: '趋势空';
 				}
 			}
@@ -630,8 +630,8 @@ class Backtester {
 				}
 				if (close > emaFast) {
 					marketType =
-						(emaSlow - emaFast) / emaSlow > volatility_ratio * 4
-							? '趋势多且增强-L-2-3'
+						(emaSlow - emaFast) / emaSlow > volatility_ratio * 2
+							? '趋势空且增强-L-2-3'
 							: '趋势多';
 				}
 			}
