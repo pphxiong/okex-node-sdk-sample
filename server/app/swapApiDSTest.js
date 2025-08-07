8857c5dfb682f10f01a2aca5225d8a2d5810edc2
pphxiong <1017551718@qq.com>
2025-08-07 08:33:28 +0800

    n

--- server/app/swapApiDSTest.js
+++ server/app/swapApiDSTest.js
@@ -911,8 +911,8 @@ class Backtester {
 				// 		: d.close >= position.entryPrice * (1 + 0.01);
 
 				const isProfitTarget =
-					(lnp > 0.012 && d.adx < d.adx_threshold) || lnp > 0.02;
-				const isStopLoss = lnp < -0.01;
+					(lnp > 0.015 && d.adx < d.adx_threshold) || lnp > 0.02;
+				const isStopLoss = lnp < -0.015;
 
 				if (isStopLoss) stopLossDirection = position.direction;
 
@@ -1302,9 +1302,9 @@ class Backtester {
 		const lnp = this.getLnp(position, exitCandle);
 		const { marketMode } = this;
 		if (config.isMarketModeAuto) {
-			if (marketMode == 1 && (lnp < -0.01 || lnp > 0.02)) {
+			if (marketMode == 1 && (lnp < -0.015 || lnp > 0.02)) {
 				this.marketMode = 2;
-			} else if (marketMode == 2 && lnp < -0.01) {
+			} else if (marketMode == 2 && lnp < -0.015) {
 				this.marketMode = 1;
 			}
 		}
