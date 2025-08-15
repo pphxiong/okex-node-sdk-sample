c26da241b4e875b464806dc0da64789252a9bd8f
pphxiong <1017551718@qq.com>
2025-08-15 09:03:05 +0800

    n

--- server/app/swapApiDSTest.js
+++ server/app/swapApiDSTest.js
@@ -603,19 +603,19 @@ class Backtester {
 
 		const { marketMode } = this;
 
-		if (close > emaFast || rsi > 55) {
+		if (close > emaFast || rsi > 60) {
 			marketType = '趋势多';
 		}
 
-		if (close < emaFast || rsi < 45) {
+		if (close < emaFast || rsi < 40) {
 			marketType = '趋势空';
 		}
 
-		if (close > emaSlow && rsi > 45) {
+		if (close > emaSlow && rsi > 40) {
 			marketType = '趋势多且增强-L-1-1';
 		}
 
-		if (close < emaSlow && rsi < 55) {
+		if (close < emaSlow && rsi < 60) {
 			marketType = '趋势空且增强-L-1-1';
 		}
 
