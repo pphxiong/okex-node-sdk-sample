6f599987f94c6a8bb13da6aa7c77185926c76c49
pphxiong <1017551718@qq.com>
2025-08-19 15:04:44 +0800

    n

--- server/app/swapApiDSTest.js
+++ server/app/swapApiDSTest.js
@@ -889,11 +889,11 @@ class Backtester {
 				const isReverse =
 					// isLastIndex ||
 					// isProfitTarget ||
-					isStopLoss ||
+					// isStopLoss ||
 					// (lnp < 0 && duration >= 60) ||
-					(position.direction === 'long'
+					position.direction === 'long'
 						? longCloseConditions.some((c) => !!c)
-						: shortCloseConditions.some((c) => !!c));
+						: shortCloseConditions.some((c) => !!c);
 
 				if (isReverse) {
 					this.closePosition(
