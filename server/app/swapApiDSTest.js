76c17cfe3894ea7eec83645092a7d6ced050a76b
pphxiong <1017551718@qq.com>
2025-08-19 17:06:05 +0800

    n

--- server/app/swapApiDSTest.js
+++ server/app/swapApiDSTest.js
@@ -814,15 +814,30 @@ class Backtester {
 				const lnp = this.getLnp(position, d);
 				const duration = this.getTimeInterval(position, d);
 
-				const isProfitTarget =
-					position.direction === 'long'
-						? d.close >= position.low + position.atr * 2.5
-						: d.close <= position.low - position.atr * 2.5;
+        let isProfitTarget = false;
+		let isStopLoss = false;
 
-				const isStopLoss =
-					position.direction === 'long'
-						? d.close >= position.low - position.atr * 0.7
-						: d.close <= position.low + position.atr * 0.7;
+		if (position.slowMarketType.indexOf('DOGE_UP_BREAKOUT') !== -1) {
+			isProfitTarget = d.close >= position.close + position.atr * 2.5;
+			isStopLoss = d.close <= position.low - position.atr * 0.7;
+		}
+
+		if (position.slowMarketType.indexOf('DOGE_DOWN_BREAKOUT') !== -1) {
+			isProfitTarget = d.close <= position.close - position.atr * 3;
+			isStopLoss = d.close >= position.high + position.atr * 0.8;
+		}
+
+		if (position.slowMarketType.indexOf('DOGE_UP_PULLBACK') !== -1) {
+			isProfitTarget = d.close >= position.emaSlow * 1.15;
+			isStopLoss = d.close <= position.low - position.atr * 0.3;
+		}
+
+		if (position.slowMarketType.indexOf('DOGE_DOWN_PULLBACK') !== -1) {
+			isProfitTarget = d.close <= position.emaSlow * 0.85;
+			isStopLoss = d.close >= position.high + position.atr * 0.3;
+		}
+
+
 
 				// const isProfitTarget =
 				// 	position.marketMode === 1
