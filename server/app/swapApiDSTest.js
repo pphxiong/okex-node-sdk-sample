836354ca8480c7ce90dba3366ab851bc6bf4ac07
pphxiong <1017551718@qq.com>
2025-08-27 15:35:24 +0800

    n

--- server/app/swapApiDSTest.js
+++ server/app/swapApiDSTest.js
@@ -90,7 +90,7 @@ const config = {
 	drift: 0.0002, // 每日趋势偏移量
 	adxPeriod: 14,
 	rsiPeriod: 14,
-	marketMode: 1,
+	marketMode: 2,
 	isMarketModeAuto: false,
 };
 
@@ -819,15 +819,15 @@ class Backtester {
 
 	toogleMarketType(marketType, candle) {
 		const { adx, adx_threshold } = candle;
-		if (adx < adx_threshold) {
-			// if (this.marketMode == 2) {
-			if (marketType.indexOf('多') != -1) {
-				marketType = marketType.replace('多', '空');
-			} else if (marketType.indexOf('空') != -1) {
-				marketType = marketType.replace('空', '多');
-			}
-			// }
-		}
+		// if (adx < adx_threshold) {
+		// 	// if (this.marketMode == 2) {
+		// 	if (marketType.indexOf('多') != -1) {
+		// 		marketType = marketType.replace('多', '空');
+		// 	} else if (marketType.indexOf('空') != -1) {
+		// 		marketType = marketType.replace('空', '多');
+		// 	}
+		// 	// }
+		// }
 
 		return marketType;
 	}
@@ -997,7 +997,7 @@ class Backtester {
 					isStopLoss = d.close >= position.high + position.atr * 0.3;
 				}
 
-				isProfitTarget = lnp > 0.015 * 1.5;
+				isProfitTarget = lnp > 0.015;
 				isStopLoss = lnp < -0.015;
 
 				// const isProfitTarget =
