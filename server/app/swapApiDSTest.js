1ef2110b88b0594334b4617aa331573d6f0cdc67
pphxiong <1017551718@qq.com>
2025-08-28 09:26:16 +0800

    n

--- server/app/swapApiDSTest.js
+++ server/app/swapApiDSTest.js
@@ -90,7 +90,7 @@ const config = {
 	drift: 0.0002, // 每日趋势偏移量
 	adxPeriod: 14,
 	rsiPeriod: 14,
-	marketMode: 1,
+	marketMode: 2,
 	isMarketModeAuto: false,
 };
 
@@ -821,14 +821,27 @@ class Backtester {
 	toogleMarketType(marketType, candle) {
 		const { adx, adx_threshold } = candle;
 		// if (adx < adx_threshold) {
+		// if (this.marketMode == 2) {
+		// 	if (marketType.indexOf('多') != -1) {
+		// 		marketType = marketType.replace('多', '空');
+		// 	} else if (marketType.indexOf('空') != -1) {
+		// 		marketType = marketType.replace('空', '多');
+		// 	}
+		// }
+		// }
 		if (this.marketMode == 2) {
+			const andIndex = marketType.indexOf('且');
+			if (andIndex != -1) {
+				marketType = marketType.substring(0, andIndex);
+			} else {
+				marketType = `${marketType}且增强`;
+			}
 			if (marketType.indexOf('多') != -1) {
 				marketType = marketType.replace('多', '空');
 			} else if (marketType.indexOf('空') != -1) {
 				marketType = marketType.replace('空', '多');
 			}
 		}
-		// }
 
 		return marketType;
 	}
