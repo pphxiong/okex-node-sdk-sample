341cc7541516b4c9ec89d7799521ded70f8c5366
pphxiong <1017551718@qq.com>
2025-08-28 13:29:29 +0800

    n

--- server/app/swapApiDSTest.js
+++ server/app/swapApiDSTest.js
@@ -90,7 +90,7 @@ const config = {
 	drift: 0.0002, // 每日趋势偏移量
 	adxPeriod: 14,
 	rsiPeriod: 14,
-	marketMode: 1,
+	marketMode: 2,
 	isMarketModeAuto: false,
 };
 
@@ -1605,7 +1605,7 @@ function carryForluma(p, rl, rw) {
 	const backtester = new Backtester();
 	// const start = '2023-01-01';
 	// const end = '2023-07-01';
-	const start = '2025-01-01';
+	const start = '2025-08-01';
 	const end = '2025-08-30';
 	const interval = 30;
 	let profitTotal = 0;
