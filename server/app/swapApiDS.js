89bb102d3932984665e4ce53c1ae94b49102e8f9
pphxiong <1017551718@qq.com>
2025-09-03 11:31:15 +0800

    n

--- server/app/swapApiDS.js
+++ server/app/swapApiDS.js
@@ -1460,6 +1460,7 @@ app.get('/changeIsPaused', async function (req, res) {
 	if (pw && pw.trim() === '@Xiong092479') {
 		config.isPaused = !config.isPaused;
 		await OrderManager.writeData();
+    restart('change isPaused restart success...');
 		send(res, {
 			errcode: 0,
 			errmsg: 'ok',
