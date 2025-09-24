3024fd7031fb54ca33d681d7fdff8292d53b9075
pphxiong <1017551718@qq.com>
2025-09-24 14:21:28 +0800

    n

--- server/app/swapApiDSTest.js
+++ server/app/swapApiDSTest.js
@@ -1062,7 +1062,7 @@ class Backtester {
 				}
 
 				const basicLnp = (0.01 * 2) / 3;
-				isProfitTarget = lnp > basicLnp * 2;
+				isProfitTarget = lnp > basicLnp * 3;
 				isStopLoss = lnp < -basicLnp;
 
 				// const isProfitTarget =
