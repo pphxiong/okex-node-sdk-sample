e3fd464049034d370ca2b0ec336302620142fe0f
pphxiong <1017551718@qq.com>
2025-09-24 14:02:49 +0800

    n

--- server/app/swapApiDSTest.js
+++ server/app/swapApiDSTest.js
@@ -517,38 +517,44 @@ class Backtester {
 				const emaFastSlopes = [];
 				const emaTrendSlopes = [];
 				for (
-				let i = config.emaSettings[tf].slopeWindow;
-				i < emaSlow[0].length;
-				i++
-			) {
-				const slope =
-					(emaSlow[0][i] -
-						emaSlow[0][i - config.emaSettings[tf].slopeWindow]) /
-					emaSlow[0][i - config.emaSettings[tf].slopeWindow];
-				emaSlowSlopes.push(slope);
-			}
-			for (
-				let i = config.emaSettings[tf].slopeWindow;
-				i < emaFast[0].length;
-				i++
-			) {
-				const slope =
-					(emaFast[0][i] -
-						emaFast[0][i - config.emaSettings[tf].slopeWindow]) /
-					emaFast[0][i - config.emaSettings[tf].slopeWindow];
-				emaFastSlopes.push(slope);
-			}
-			for (
-				let i = config.emaSettings[tf].slopeWindow;
-				i < emaTrend[0].length;
-				i++
-			) {
-				const slope =
-					(emaTrend[0][i] -
-						emaTrend[0][i - config.emaSettings[tf].slopeWindow]) /
-					emaTrend[0][i - config.emaSettings[tf].slopeWindow];
-				emaTrendSlopes.push(slope);
-			}
+					let i = config.emaSettings[tf].slopeWindow;
+					i < emaSlow[0].length;
+					i++
+				) {
+					const slope =
+						(emaSlow[0][i] -
+							emaSlow[0][
+								i - config.emaSettings[tf].slopeWindow
+							]) /
+						config.emaSettings[tf].slopeWindow;
+					emaSlowSlopes.push(slope);
+				}
+				for (
+					let i = config.emaSettings[tf].slopeWindow;
+					i < emaFast[0].length;
+					i++
+				) {
+					const slope =
+						(emaFast[0][i] -
+							emaFast[0][
+								i - config.emaSettings[tf].slopeWindow
+							]) /
+						config.emaSettings[tf].slopeWindow;
+					emaFastSlopes.push(slope);
+				}
+				for (
+					let i = config.emaSettings[tf].slopeWindow;
+					i < emaTrend[0].length;
+					i++
+				) {
+					const slope =
+						(emaTrend[0][i] -
+							emaTrend[0][
+								i - config.emaSettings[tf].slopeWindow
+							]) /
+						config.emaSettings[tf].slopeWindow;
+					emaTrendSlopes.push(slope);
+				}
 
 				// 合并指标到数据
 				this.data[tf].forEach((d, i) => {
