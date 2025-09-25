437a347d74a30ca13b297039c50aca9fbce658ca
pphxiong <1017551718@qq.com>
2025-09-25 16:17:26 +0800

    n

--- server/app/swapApiDS.js
+++ server/app/swapApiDS.js
@@ -59,7 +59,7 @@ const config = {
 	},
 	orderDepth: 0.00012, // 限价单挂单深度 (0.1%)
 	tradeAmount: 2400, // 每单交易金额(USDT)
-	maxOrderAge: 1000 * 15, // 限价单最长存活时间(30秒)
+	maxOrderAge: 1000 * 15 * 2, // 限价单最长存活时间(30秒)
 	basicLnp: (0.01 * 2) / 3,
 	profitStopLossRatio: 4, // 盈亏比
 	trailingStop: 0.0025, // 浮动止盈止损(0.25%)
@@ -1058,7 +1058,7 @@ class RiskManager {
 		const isLossFirst =
 			amount > (config.tradeAmount * 8) / 10 && lnp < -basicLnp / 2;
 		const isLossSecond =
-			amount > (config.tradeAmount * 5) / 10 && lnp > -basicLnp;
+			amount > (config.tradeAmount * 5) / 10 && lnp < -basicLnp;
 
 		// const takeProfit =
 		//   lastKline5M[config.fastframe].atr * config.atrParam.takeProfit;
