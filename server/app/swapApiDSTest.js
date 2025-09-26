a9e3993d2c5775d1b3794d2fecfb178cb0617b5f
pphxiong <1017551718@qq.com>
2025-09-26 17:27:06 +0800

    n

--- server/app/swapApiDSTest.js
+++ server/app/swapApiDSTest.js
@@ -800,7 +800,7 @@ class Backtester {
 			emaFastSlope > emaSlowSlope &&
 			emaFastSlope > 0 &&
 			emaSlowSlope < 0 &&
-			emaTrendSlope < 20
+			emaTrendSlope < -20
 		)
 			marketType = '趋势空且增强';
 	}
