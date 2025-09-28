0bb6604cdd21a7827b8dd2a92d1a7bbd44804020
pphxiong <1017551718@qq.com>
2025-09-28 08:44:51 +0800

    n

--- server/app/swapApiDS.js
+++ server/app/swapApiDS.js
@@ -166,9 +166,9 @@ function getMarketType(candle, lastCandle, lastLastCandle) {
 		// marketType = '趋势多';
 		if (
 			// emaFastSlope < emaSlowSlope &&
-			emaFastSlope < -40 &&
-			emaSlowSlope > 40 &&
-			emaTrendSlope > 40
+			emaFastSlope < -10 &&
+			emaSlowSlope > 20 &&
+			emaTrendSlope > 30
 		)
 			marketType = '趋势多且增强';
 	}
@@ -177,9 +177,9 @@ function getMarketType(candle, lastCandle, lastLastCandle) {
 		// marketType = '趋势空';
 		if (
 			// emaFastSlope > emaSlowSlope &&
-			emaFastSlope > 40 &&
-			emaSlowSlope < -40 &&
-			emaTrendSlope < -40
+			emaFastSlope > 10 &&
+			emaSlowSlope < -20 &&
+			emaTrendSlope < -30
 		)
 			marketType = '趋势空且增强';
 	}
