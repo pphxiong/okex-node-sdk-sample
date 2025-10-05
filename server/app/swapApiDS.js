4fa66087d1aee33a5e9c321f52a31ee61cc67134
pphxiong <1017551718@qq.com>
2025-10-05 09:18:19 +0800

    n

--- server/app/swapApiDS.js
+++ server/app/swapApiDS.js
@@ -1407,10 +1407,12 @@ async function strategyLoop(isShowLog = false) {
     }
     const basicLnpPercent = config.basicLnp * config.leverage * 100;
     if (
-      (Math.abs(config.maxLnpPercent) > Math.abs(config.minLnpPercent) &&
+      (Math.abs(config.maxLnpPercent) > basicLnpPercent / 1.5 &&
+        Math.abs(config.maxLnpPercent) > Math.abs(config.minLnpPercent) &&
         Math.abs(config.maxLnpPercent) - Math.abs(lnpPercent) >
           basicLnpPercent / 1.5) ||
-      (Math.abs(config.maxLnpPercent) < Math.abs(config.minLnpPercent) &&
+      (Math.abs(config.minLnpPercent) > basicLnpPercent / 1.5 &&
+        Math.abs(config.maxLnpPercent) < Math.abs(config.minLnpPercent) &&
         Math.abs(config.minLnpPercent) - Math.abs(lnpPercent) >
           basicLnpPercent / 1.5)
     )
