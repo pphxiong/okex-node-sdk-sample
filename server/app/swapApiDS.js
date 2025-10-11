5eea52e4c24f6fa9a2ebd3d448aa5b385fd69836
pphxiong <1017551718@qq.com>
2025-10-11 20:46:24 +0800

    n

--- server/app/swapApiDS.js
+++ server/app/swapApiDS.js
@@ -172,8 +172,8 @@ function getMarketType(marketData) {
   const longCloseCondition = slowClose < slowEmaSlow;
   const shortCloseCondition = slowClose > slowEmaSlow;
 
-  // if (longCloseCondition) marketType = "趋势空";
-  // if (shortCloseCondition) marketType = "趋势多";
+  if (longCloseCondition) marketType = "趋势空";
+  if (shortCloseCondition) marketType = "趋势多";
   if (longCondition) marketType = "趋势多且增强";
   if (shortCondition) marketType = "趋势空且增强";
 
