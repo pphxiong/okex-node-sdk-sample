ae0a756ac7875b7f1dec4a7124c429d3a7f98a7d
pphxiong <1017551718@qq.com>
2025-10-22 09:21:17 +0800

    n

--- server/app/swapApiDS.js
+++ server/app/swapApiDS.js
@@ -1401,7 +1401,7 @@ async function initialize() {
 		case 1: // 趋势市
 			positionSize =
 				marketState.confidence > 0.6
-					? positionSize * 0.8
+					? positionSize * 0.5
 					: positionSize * 0.6;
 			console.log('🟢 趋势市 - 积极交易模式');
 			break;
