e20ec86904e16b93efa2a5fc8fc7283553f81ec9
pphxiong <1017551718@qq.com>
2025-10-23 10:48:32 +0800

    n

--- server/app/dynamic-ema-filter.js
+++ server/app/dynamic-ema-filter.js
@@ -85,8 +85,8 @@ class DynamicEMAFilterWithATR {
 		if (normalizedVolatility > 0.03) strengthScore += 0.2;
 		else if (normalizedVolatility < 0.01) strengthScore -= 0.1;
 
-		console.log('strengthScore:', strengthScore, atrData);
-    
+		console.log('strengthScore:', strengthScore);
+
 		// 转换为强度等级
 		if (strengthScore >= 0.6) return 'strong';
 		if (strengthScore >= 0.4) return 'good';
