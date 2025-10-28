c917bf39f60009690782643160db986905805c20
pphxiong <1017551718@qq.com>
2025-10-28 19:27:47 +0800

    n

--- server/app/swapApiDSTest.js
+++ server/app/swapApiDSTest.js
@@ -822,16 +822,16 @@ class Backtester {
 			lastEmaFast > lastEmaSlow &&
 			emaTrend > ema5 &&
 			ema4 > ema6 &&
-			close > ema4;
+			close > ema5;
 
 		shortCondition =
 			emaFast > emaSlow &&
 			lastEmaFast < lastEmaSlow &&
 			emaTrend < ema5 &&
-			ema4 < ema6 && close < ema4;
+			ema4 < ema6 && close < ema5;
 
-		longCloseCondition = close < ema4 || emaTrend < ema5;
-		shortCloseCondition = close > ema4 || emaTrend > ema5;
+		longCloseCondition = close < ema5 || emaTrend < ema5;
+		shortCloseCondition = close > ema5 || emaTrend > ema5;
 
 		// if (marketMode == 2) {
 		// 	longCondition = emaFast > emaSlow && emaTrend > ema5 && ema4 > ema6;
