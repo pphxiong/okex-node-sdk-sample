f6a64bd178d6885892a9a8edc12a87492d1a4b72
pphxiong <1017551718@qq.com>
2025-10-29 05:32:43 +0800

    n

--- server/app/swapApiDSTest.js
+++ server/app/swapApiDSTest.js
@@ -802,8 +802,8 @@ class Backtester {
       // ema4 < ema5 &&
       ema4 < ema6;
 
-    longCloseCondition = close < ema6;
-    shortCloseCondition = close > ema6;
+    longCloseCondition = emaFast < ema6;
+    shortCloseCondition = emaFast > ema6;
 
     // if (marketMode == 2) {
     // 	longCondition = emaFast > emaSlow && emaTrend > ema5 && ema4 > ema6;
