581b0093d34a90c8ed17e4f0dfa158a68e66f58c
pphxiong <1017551718@qq.com>
2025-10-29 13:51:15 +0800

    n

--- server/app/swapApiDSTest.js
+++ server/app/swapApiDSTest.js
@@ -860,7 +860,7 @@ class Backtester {
 		// }
 
 		if (longCloseCondition) marketType = '趋势空';
-		if (shortCloseCondition) marketType = `趋势多`;
+		if (shortCloseCondition) marketType = '趋势多';
 		if (longCondition) marketType = '趋势多且增强';
 		if (shortCondition) marketType = '趋势空且增强';
 
