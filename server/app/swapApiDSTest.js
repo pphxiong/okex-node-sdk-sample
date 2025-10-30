9c0ac84f40ee002ed42975449054eaf103fa69e6
pphxiong <1017551718@qq.com>
2025-10-30 09:07:00 +0800

    n

--- server/app/swapApiDSTest.js
+++ server/app/swapApiDSTest.js
@@ -836,11 +836,11 @@ class Backtester {
 		const trendStrength = Math.abs(ema5 - ema6) / close;
 		longCondition =
 			longCondition &&
-			((trendStrength > 0.0035 && trendStrength < 0.005) ||
+			((trendStrength > 0.0035 && trendStrength < 0.0055) ||
 				trendStrength < 0.0005);
 		shortCondition =
 			shortCondition &&
-			((trendStrength > 0.0035 && trendStrength < 0.005) ||
+			((trendStrength > 0.0035 && trendStrength < 0.0055) ||
 				trendStrength < 0.0005);
 
 		longCloseCondition = close < emaTrend;
