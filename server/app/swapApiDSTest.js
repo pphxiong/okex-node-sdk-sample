2c2c7ba590b45b802871f9618ac28a24c327f354
pphxiong <1017551718@qq.com>
2025-10-30 17:12:54 +0800

    n

--- server/app/swapApiDSTest.js
+++ server/app/swapApiDSTest.js
@@ -848,32 +848,33 @@ class Backtester {
 		longCloseCondition = close < emaTrend;
 		shortCloseCondition = close > emaTrend;
 
-		if (
-			(trendStrength > 0.001 && trendStrength < 0.003) ||
-			trendStrength > 0.005
-		) {
-			longCondition =
-				close > emaTrend &&
-				lastClose < lastEmaTrend &&
-				emaTrend < ema5 &&
-				ema5 < ema6 &&
-				close < ema6;
-
-			shortCondition =
-				close < emaTrend &&
-				lastClose > lastEmaTrend &&
-				emaTrend > ema5 &&
-				ema5 > ema6 &&
-				close > ema6;
-
-			longCloseCondition = close < emaTrend;
-			shortCloseCondition = close > emaTrend;
-		}
-
 		if (longCloseCondition) marketType = '趋势空';
 		if (shortCloseCondition) marketType = '趋势多';
 		if (longCondition) marketType = '趋势多且增强';
 		if (shortCondition) marketType = '趋势空且增强';
+
+		if (
+			(trendStrength > 0.001 && trendStrength < 0.003) ||
+			trendStrength > 0.005
+		) {
+			// longCondition =
+			// 	close > emaTrend &&
+			// 	lastClose < lastEmaTrend &&
+			// 	emaTrend < ema5 &&
+			// 	ema5 < ema6 &&
+			// 	close < ema6;
+
+			// shortCondition =
+			// 	close < emaTrend &&
+			// 	lastClose > lastEmaTrend &&
+			// 	emaTrend > ema5 &&
+			// 	ema5 > ema6 &&
+			// 	close > ema6;
+
+			// longCloseCondition = close < emaTrend;
+			// shortCloseCondition = close > emaTrend;
+			marketType = this.toogleMarketType(marketType, candle);
+		}
 
 		// if (close > emaSlow) {
 		// 	marketType = '趋势多';
