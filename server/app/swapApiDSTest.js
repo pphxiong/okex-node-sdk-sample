9403fa120a10319dd6d29008f0837679874614a4
pphxiong <1017551718@qq.com>
2025-10-31 10:32:53 +0800

    n

--- server/app/swapApiDSTest.js
+++ server/app/swapApiDSTest.js
@@ -827,14 +827,14 @@ class Backtester {
 			lastClose < lastEmaTrend &&
 			emaTrend > ema5 &&
 			// ema5 > ema6 &&
-			close > ema5;
+			close > ema6;
 
 		shortCondition =
 			close < emaTrend &&
 			lastClose > lastEmaTrend &&
 			emaTrend < ema5 &&
 			// ema5 < ema6 &&
-			close < ema5;
+			close < ema6;
 
 		longCondition =
 			longCondition &&
